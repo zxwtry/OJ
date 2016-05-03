c6946d20789de57a466d3650ff388ae941c2689a
zxwtry <zxwtry@qq.com>
2016-05-03 18:43:40 +0800

    牛客网局部最小值位置
    时间O(logN) 空间O(1)

---
+++ src/nowcoder/zuo/局部最小值位置.java
@@ -0,0 +1,33 @@
+package nowcoder.zuo;
+
+public class 局部最小值位置 {
+	public static void main(String[] args) {
+		int[] arr = new int[]{4,1,0,3};
+		System.out.println(getLessIndex(arr));
+	}
+    public static int getLessIndex(int[] arr) {
+    	if (arr == null || arr.length == 0) {
+    		return -1;
+    	}
+    	if (arr.length == 1) {
+    		return 0;
+    	}
+    	final int len = arr.length;
+    	if (arr[len - 1] < arr[len - 2])
+    		return len-1;
+    	if (arr[0] < arr[1])
+    		return 0;
+    	int begin = 0, end = len - 1;
+    	while (begin < end) {
+    		int mid = (begin + end) >> 1;
+    		if (arr[mid+1] < arr[mid]) {
+    			begin = mid;
+    		} else if (arr[mid-1] < arr[mid]) {
+    			end = mid;
+    		} else {
+    			return mid;
+    		}
+    	}
+    	return begin;
+    }
+}
