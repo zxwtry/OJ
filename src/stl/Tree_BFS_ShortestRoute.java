50f72aab1eec3be97c8e8cbb4a4093002f73faa1
zxwtry <zxwtry@qq.com>
2016-05-23 12:38:05 +0800

    Tree_BFS_ShortestRoute

--- src/stl/Tree_BFS_ShortestRoute.java
+++ src/stl/Tree_BFS_ShortestRoute.java
@@ -3,6 +3,24 @@ package stl;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
+
+/*
+
+10 10
+#s######.#
+......#..#
+.#.##.##.#
+.#........
+##.##.####
+....#....#
+.#######.#
+....#.....
+.####.###.
+....#...e#
+
+
+ */
 
 public class Tree_BFS_ShortestRoute {
 	final static char START_CHAR = 's'; 
@@ -16,7 +34,28 @@ public class Tree_BFS_ShortestRoute {
 	static int[] dx = {1, 0, -1,  0};
 	static int[] dy = {0, 1,  0, -1};
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt(), Y = sc.nextInt();
+		boolean fde = false, fds = false;
+		field = new char[X][];
+		d = new int[X][Y];
+		for (int xi = 0; xi < X; xi ++) {
+			field[xi] = sc.next().trim().toCharArray();
+			if (!fde || !fds) {
+				for (int yi = 0; yi < Y; yi ++) {
+					if (field[xi][yi] == 's') {
+						xs = xi; ys = yi;
+						fds = true;
+					}
+					if (field[xi][yi] == 'e') {
+						xe = yi; ye = yi;
+						fde = true;
+					}
+				}
+			}
+		}
+		System.out.println(bfs());
+		sc.close();
 	}
 	static int bfs() {
 		for (int di = 0; di < d.length; di ++)
