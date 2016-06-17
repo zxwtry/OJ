fa30376407d32f88d169ec923b158f896715f255
zxwtry <zxwtry@qq.com>
2016-06-17 22:07:44 +0800

    修改构造回文

--- src/nowcoder/com/腾讯2016构造回文.java
+++ src/nowcoder/com/腾讯2016构造回文.java
@@ -1,5 +1,7 @@
 package nowcoder.com;
 
+import java.util.HashMap;
+
 public class 腾讯2016构造回文 {
 	static String line = null;
 	static int[] dps = new int[3000];
@@ -33,5 +35,49 @@ public class 腾讯2016构造回文 {
 				max_count = new_count;
 		}
 		System.out.printf("%d\n", max_count);
+	}
+	/*
+	 * 	这种算法也是错误的。
+	 */
+	static void solve() {
+		System.out.println(line.length());
+		if (line == null || line.length() == 0)
+			return;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int index = line.length()-1; index >= 0; index --) {
+			char c = line.charAt(index);
+			if (! map.containsKey(c)) {
+				map.put(c, 1);
+			} else {
+				map.put(c, map.get(c) + 1);
+			}
+		}
+		int sta = 0, end = line.length() - 1, count = 0;
+		while (sta < end) {
+			if (sta > end || end < 0)
+				break;
+			char sc = line.charAt(sta), ec = line.charAt(end);
+			int sv = map.get(sc), ev = map.get(ec);
+			if (sv == 1) {
+				if (sta == end) count ++;
+				sta ++;
+				continue;
+			}
+			if (ev == 1) {
+				if (sta == end) count ++;
+				end --;
+				continue;
+			}
+			if (sc == ec) {
+				sta ++;
+				end --;
+				count += 2;
+			} else {
+				if (sv > ev) sta ++;
+				else end --;
+			}
+		}
+		if (sta == end) count ++;
+		System.out.println(line.length() - count);
 	}
 }
