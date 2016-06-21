519c9d083dc35ea2fdf52d1c267a479c18b45781
zxwtry <zxwtry@qq.com>
2016-06-21 19:41:32 +0800

    寻找和为定制的两个数_排序再往中靠

---
+++ src/book/编程之法/Code202寻找和为定制的两个数_排序再往中靠.java
@@ -0,0 +1,22 @@
+package book.编程之法;
+
+import java.util.Arrays;
+
+public class Code202寻找和为定制的两个数_排序再往中靠 {
+	public static void main(String[] args) {
+		int[] arr = null;
+		arr = new int[] {1, 9, 2, 8, 3, 7, 4, 6, 5};
+		solve(arr, 16);
+	}
+	static void solve(int[] arr, int val) {
+		Arrays.sort(arr);
+		int var1 = 0, var2 = arr.length-1;
+		int sum = arr[var1] + arr[var2];
+		while (sum != val && var1 < var2) {
+			if (sum < val) 	sum = arr[++ var1] + arr[var2];
+			else sum = arr[var1] + arr[-- var2];
+		}
+		System.out.println("var1: "+var1+"   var2: "+var2);
+		System.out.println("var1: "+arr[var1]+"   var2: "+arr[var2]);
+	}
+}
