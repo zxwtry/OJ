a22d9643a356813a6f784f3fee71bcf396060f62
zxwtry <zxwtry@qq.com>
2016-08-11 10:28:40 +0800

    添加 leetcode P004_MedianofTwoSortedArrays 短代码的方法 14.77%

--- src/leetcode/P004_MedianofTwoSortedArrays.java
+++ src/leetcode/P004_MedianofTwoSortedArrays.java
@@ -1,6 +1,5 @@
 package leetcode;
 
-
 /*
 
 There are two sorted arrays nums1 and nums2 of size m and n respectively.
@@ -21,8 +20,21 @@ The median is (2 + 3)/2 = 2.5
 public class P004_MedianofTwoSortedArrays {
 	public static void main(String[] args) {
 //		System.out.println(new Solution1().findMedianSortedArrays(new int[] {1, 1, 3, 3}, new int[] {1, 1, 3, 3}));
-		int k = 1;
-		System.out.println(new Solution2().findKth(new int[] { 0, 2, 4, 6, 8}, 0, 4, new int[] {1, 3, 5, 7, 9}, 0, 4, k));
+//		int k = -1;
+//		while (true) {
+//			k ++;
+//			int[] arr1, arr2;
+//			System.out.print("k: " + k + "  ");
+////			System.out.println(new Solution2().findKth_zxwtry(arr1 = new int[] { 0, 2, 4, 6, 8}, 0, arr1.length - 1, arr2 = new int[] {1, 3, 5, 7, 9}, 0, arr2.length - 1, k));
+////			System.out.println(new Solution2().findKth_zxwtry(arr1 = new int[] { 0, 1, 2, 3, 4}, 0, arr1.length - 1, arr2 = new int[] {5, 6, 7, 8, 9}, 0, arr2.length - 1, k));
+////			System.out.println(new Solution2().findKth_zxwtry(arr1 = new int[] { 0 }, 0, arr1.length - 1, arr2 = new int[] {5, 6, 7, 8, 9}, 0, arr2.length - 1, k));
+//			System.out.println(new Solution2().findMedianSortedArrays(arr1 = new int[]{0, 1, 2, 3, 4}, arr2 = new int[] {5, 6, 7, 8, 9}));
+//			if (k == arr1.length + arr2.length - 1) {
+//				break;
+//			}
+//		}
+//		System.out.println(new Solution2().findMedianSortedArrays(new int[]{8}, new int[] {5, 6, 7, 8, 9}));
+		System.out.println(new Solution2().findMedianSortedArrays(new int[]{}, new int[] {5, 6}));
 	}
 	/*
 	 *  9.33%
@@ -137,36 +149,56 @@ public class P004_MedianofTwoSortedArrays {
 	/*
 	 * 	第一份代码的最重要缺陷是代码实在是太长了。
 	 * 	第二份代码应该会非常短，只要短代码就好
+	 * 	6ms
+	 * 	14.77%
+	 * 	还更快。。。
 	 */
 	static class Solution2 {
-		public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-			return 0;
-		}
-		double findKth(int[] nums1, int l1, int r1, int[] nums2, int l2, int r2, int k) {
-			int d1 = r1 - l1, d2 = r2 - l2;
-			if (d1 > d2)
-				return findKth(nums2, l2, r2, nums1, l1, r1, k);
-			if (d1 == 0)
-				return nums2[l2 + k];
-			if (k == 0)
-				return Math.min(nums1[l1], nums2[l2]);
-			int p1 = Math.min(k / 2, d1), p2 = k - p1;
-			if (nums1[l1 + p1] > nums2[l2 + p2])
-				return findKth(nums1, l1 + p1, r1, nums2, l2, r2, k - p1);
-			else if (nums1[l1 + p1] < nums2[l2 + p2])
-				return findKth(nums1, l1, r1, nums2, l2 + p2, r2, k - p2);
-			else
-				return nums1[l1 + p1 - 1];
-		}
+		/*
+		 * 	这里k从0开始计数
+		 */
 		int findKth_zxwtry(int[] nums1, int l1, int r1, int[] nums2, int l2, int r2, int k) {
 			int d1 = r1 - l1, d2 = r2 - l2;
 			if (d1 > d2)
 				return findKth_zxwtry(nums2, l2, r2, nums1, l1, r1, k);
+			if (d1 < 0)
+				return nums2[l2 + k];
 			if (k == 0)
 				return Math.min(nums1[l1], nums2[l2]);
-			int p1 = Math.min((k + 1) / 2, d1) - 1, p2 = k - p1;
-			
-			return 0;
+			int p1 = Math.min((k + 1) / 2 - 1, d1), p2 = k - p1 - 1;
+			if (p1 < 0 || p2 < 0)
+				return nums2[l2 + k];
+			if (nums1[l1 + p1] < nums2[l2 + p2]) {
+				return findKth_zxwtry(nums1, l1 + p1 + 1, r1, nums2, l2, r2, k - p1 - 1);
+			} else if (nums1[l1 + p1] > nums2[l2 + p2]){
+				return findKth_zxwtry(nums1, l1, r1, nums2, l2 + p2 + 1, r2, k - p2 - 1);
+			} else {
+				return nums1[l1 + p1];
+			}
+		}
+		public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+			if (nums1 == null || nums2 == null) {
+				int[] not_null_arr = nums1;
+				if (not_null_arr == null)
+					not_null_arr = nums2;
+				if (not_null_arr == null)
+					return 0;
+				if (not_null_arr.length == 0)
+					return 0;
+				if ( (not_null_arr.length &0x1) == 1 )
+					return not_null_arr[not_null_arr.length >>> 1];
+				else
+					return ((double) not_null_arr[not_null_arr.length >>> 1] + not_null_arr[(not_null_arr.length >>> 1) - 1]) / 2;
+			}
+			if (nums1.length > nums2.length)
+				return findMedianSortedArrays(nums2, nums1);
+			if (nums1.length == 0)
+				return findMedianSortedArrays(null, nums2);
+			if ( ( (nums1.length + nums2.length) & 0x1 ) == 1) 
+				return findKth_zxwtry(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, (nums1.length + nums2.length) >>> 1 );
+			else
+				return ((double) findKth_zxwtry(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, (nums1.length + nums2.length) >>> 1 )  + 
+						findKth_zxwtry(nums1, 0, nums1.length - 1, nums2, 0, nums2.length - 1, ( (nums1.length + nums2.length) >>> 1 )  - 1)) / 2;
 		}
 	}
 }
