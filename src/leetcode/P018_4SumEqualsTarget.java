e4a31690e1e0c5c99ffe530dfffd96337cb23ab0
zxwtry <zxwtry@qq.com>
2016-08-22 20:17:37 +0800

    添加 leetcode P018 4 Sum Equals Target 87ms 11.81%

--- src/leetcode/P018_4SumEqualsTarget.java
+++ src/leetcode/P018_4SumEqualsTarget.java
@@ -68,5 +68,52 @@ public class P018_4SumEqualsTarget {
 	    	return ans;
 	    }
 	}
-
+	/*
+	 * 	87 ms
+	 * 	11.81%
+	 */
+	static class Solution2 {
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+	    public List<List<Integer>> fourSum(int[] nums, int target) {
+	        if (nums == null || nums.length < 4)
+	        	return ans;
+	        Arrays.sort(nums);
+	        for (int i = 0; i != nums.length; i ++) {
+	        	if (i != 0 && nums[i] == nums[i - 1])
+	        		continue;
+	        	threeSum(nums, i, target - nums[i]);
+	        }
+	        return ans;
+	    }
+	    void threeSum(int[] nums, int bomb, int target) {
+	    	int i = 0, j = 0, k = 0;
+	    	for (i = bomb + 1; i != nums.length; i ++) {
+	    		if (i != 0 && nums[i] == nums[i - 1] && (i != bomb + 1))	continue;
+	    		j = i + 1;  k = nums.length - 1;
+	    		while (j < k) {
+	    			int sum = nums[i] + nums[j] + nums[k];
+	    			if (sum == target) {
+	    				List<Integer> list = new LinkedList<Integer>();
+	    				list.add(nums[i]);   list.add(nums[j]);
+	    				list.add(nums[k]);   list.add(nums[bomb]);
+	    				ans.add(list);
+	    				do {
+	    					j ++;
+	    				} while (j != nums.length && nums[j] == nums[j - 1]);
+	    				do {
+	    					k --;
+	    				} while (k != -1 && nums[k] == nums[k + 1]);
+	    			} else if (sum > target) {
+	    				do {
+	    					k --;
+	    				} while (k != -1 && nums[k] == nums[k + 1]);
+	    			} else {
+	    				do {
+	    					j ++;
+	    				} while (j != nums.length && nums[j] == nums[j - 1]);
+	    			}
+	    		}
+	    	}
+	    }
+	}
 }
