58a43b856eb6828b70a456ab7e813d760cdaa649
zxwtry <zxwtry@qq.com>
2016-08-24 14:57:23 +0800

    添加 leetcode P021 Merge Two Sorted Lists 1ms 10.81%

--- src/leetcode/P021_MergeTwoSortedLists.java
+++ src/leetcode/P021_MergeTwoSortedLists.java
@@ -60,10 +60,10 @@ public class P021_MergeTwoSortedLists {
 //		node3_ghost.next = node6_ghost;
 		
 		
-		node1 = tools.ListNode辅助.A_一维生成器(new int[] {10});
-		node2 = tools.ListNode辅助.A_一维生成器(new int[] {6});
+		node1 = tools.ListNode辅助.A_一维生成器(new int[] {1, 3, 5, 7});
+		node2 = tools.ListNode辅助.A_一维生成器(new int[] {2, 4, 6, 8});
 		
-		ListNode ans = new Solution1().mergeTwoLists(node1, node2);
+		ListNode ans = new Solution3().mergeTwoLists(node1, node2);
 		while (ans != null) {
 			System.out.println(ans.val);
 			ans = ans.next;
@@ -152,6 +152,38 @@ public class P021_MergeTwoSortedLists {
 	    			c2 = c2.next;
 	    		}
 	    		pre.next = c1;
+	    	}
+	    	return l1;
+	    }
+	}
+	/*
+	 * 	代码可以更加简单
+	 * 	25行，逻辑清晰
+	 * 	1ms
+	 * 	10.81%
+	 */
+	static class Solution3 {
+	    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+	    	if (l1 == null || l2 == null)
+	    		return l1 == null ? l2 : l1;
+	    	if (l1.val > l2.val)
+	    		return mergeTwoLists(l2, l1);
+	    	ListNode pre = l1, c1 = l1.next, c2 = l2;
+	    	while (true) {
+	    		while (c1 != null && c1.val <= c2.val) {
+	    			pre = c1;
+	    			c1 = c1.next;
+	    		}
+	    		pre.next = c2;
+	    		if (c1 == null)
+	    			break;
+	    		while (c2 != null && c2.val <= c1.val) {
+	    			pre = c2;
+	    			c2 = c2.next;
+	    		}
+	    		pre.next = c1;
+	    		if (c2 == null)
+	    			break;
 	    	}
 	    	return l1;
 	    }
