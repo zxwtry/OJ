ebfea3dcb88a42f143fa1f04698f4f472d8ac919
zxwtry <zxwtry@qq.com>
2016-08-24 16:48:24 +0800

    添加 leetcode P024 Swap Nodes In Pairs 0 ms 6.30%

--- src/leetcode/P024_SwapNodesInPairs.java
+++ src/leetcode/P024_SwapNodesInPairs.java
@@ -20,14 +20,14 @@ import tools.ListNode辅助.ListNode;
 
 public class P024_SwapNodesInPairs {
 	public static void main(String[] args) {
-		ListNode input = tools.ListNode辅助.A_一维生成器(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
-		tools.ListNode辅助.B_打印链表(new Solution1().swapPairs(input));
+		ListNode input = tools.ListNode辅助.A_一维生成器(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+		tools.ListNode辅助.B_打印链表(new Solution2().swapPairs(input));
 	}
 	/*
 	 * 	0 ms
 	 * 	6.30%
 	 */
-	static  class Solution1 {
+	static class Solution1 {
 	    public ListNode swapPairs(ListNode head) {
 	    	if (head == null || head.next == null)
 	    		return head;
@@ -43,6 +43,37 @@ public class P024_SwapNodesInPairs {
 	        		pre.next = cur;
 	        		pre = cur.next;
 	        		cur = pre.next;
+	        	} else {
+		        	cur = cur.next;
+	        	}
+	        	isOdd = ! isOdd;
+	        }
+	        return ans;
+	    }
+	}
+	/*
+	 * 	1的问题就是代码太麻烦
+	 * 	一定要有更加简洁的代码
+	 * 	0 ms
+	 * 	6.30%
+	 */
+	static class Solution2 {
+	    public ListNode swapPairs(ListNode head) {
+	    	if (head == null || head.next == null)
+	    		return head;
+	        ListNode pre = head, cur = pre.next.next, next_cur = null, ans = head.next;
+	        pre.next.next = head;
+	        head.next = cur;
+	        boolean isOdd = true;
+	        while (cur != null) {
+	        	if (! isOdd) {
+	        		next_cur = cur.next;
+	        		cur.next = pre.next;
+	        		pre.next = cur;
+	        		cur = cur.next;
+	        		cur.next = next_cur;
+	        		pre = cur;
+	        		cur = next_cur;
 	        	} else {
 		        	cur = cur.next;
 	        	}
