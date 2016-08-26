c8ebbdf400d2164194b634f47c8a6c6c0d2c49d6
zxwtry <zxwtry@qq.com>
2016-08-26 21:17:20 +0800

    添加 leetcode P032 Longest Valid Parentheses  11 ms 22.39%

---
+++ src/leetcode/P032_LongestValidParentheses.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.Stack;
+
+/*
+ * 	Given a string containing just the characters '(' and ')', 
+ * 	find the length of the longest valid (well-formed) parentheses substring.
+
+	For "(()", the longest valid parentheses substring is "()", 
+	which has length = 2.
+
+	Another example is ")()())", where the longest valid parentheses substring
+	 is "()()", which has length = 4.
+ */
+
+public class P032_LongestValidParentheses {
+	public static void main(String[] args) {
+//		System.out.println(new Solution1().longestValidParentheses("((()))(((())))()()"));
+//		System.out.println(new Solution1().longestValidParentheses("((()))((((()))))"));
+//		System.out.println(new Solution1().longestValidParentheses(")()())()()("));
+//		System.out.println(new Solution1().longestValidParentheses("(()(((()()()()"));
+//		System.out.println(new Solution1().longestValidParentheses(new StringBuilder("(()(((()()()()").reverse().toString()));
+		System.out.println(new Solution1().longestValidParentheses("()((((()"));
+	}
+	/*
+	 * 	TLE
+	 * 	对全"("的长字符串，时间超出
+	 * 	又是一次艰难AC的过程
+	 * 	11 ms
+	 * 	22.39%
+	 */
+	static class Solution1 {
+	    public int longestValidParentheses(String s) {
+	    	if (s == null)
+	    		return 0;
+	    	boolean[] isMatch = new boolean[s.length()];
+	    	Stack<Integer> stack = new Stack<Integer>();
+	    	int ans = 0, i = 0, cou = 0, left = 0, right = 0;
+	    	for (i = 0; i != isMatch.length; i ++) {
+	    		if (s.charAt(i) == '(') {
+	    			left ++;
+	    		} else {
+	    			right ++;
+	    		}
+	    	}
+	    	if (right == 0 || left == 0)
+	    		return 0;
+	    	if ((left - 1) * (right - 1) == 0 && s.charAt(0) == '(')
+	    		return 2;
+	    	for (i = 0; i != isMatch.length; i ++) {
+	    		if (s.charAt(i) == '(') {
+	    			stack.add(i);
+	    		} else {
+	    			if (stack.isEmpty())
+	    				continue;
+	    			isMatch[i] = true;
+	    			isMatch[stack.peek()] = true;
+	    			stack.pop();
+	    		}
+	    	}
+	    	for (i = 0; i!= isMatch.length; i ++) {
+	    		if(isMatch[i])	++cou;
+	    		else	cou = 0;
+	    		ans = Math.max(ans, cou);
+	    	}
+	    	return ans;
+	    }
+	}
+}
