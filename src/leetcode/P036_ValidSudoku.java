0025ce8a051f2a4784bfcbf4e35712a9f44a282f
zxwtry <zxwtry@qq.com>
2016-08-27 09:21:07 +0800

    添加 leetcode P036 Valid Sudoku  8 ms 24.64%

---
+++ src/leetcode/P036_ValidSudoku.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/*
+ * 	A valid Sudoku board (partially filled) is not necessarily solvable. 
+ * 	Only the filled cells need to be validated.	
+ * 	只需要判断已经存在的数字是否合法就行，不需要管数独是否有解
+ */
+
+public class P036_ValidSudoku {
+	public static void main(String[] args) {
+		System.out.println(new Solution().isValidSudoku(new char[][] {
+			".87654321".toCharArray(),
+			"2.......1".toCharArray(),
+			"3........".toCharArray(),
+			"4........".toCharArray(),
+			"5........".toCharArray(),
+			"6........".toCharArray(),
+			"7........".toCharArray(),
+			"8........".toCharArray(),
+			"9........".toCharArray()
+		}));
+	}
+	/*
+	 * 	8 ms
+	 * 	24.64%
+	 */
+	static class Solution {
+	    public boolean isValidSudoku(char[][] board) {
+	        boolean[] isExistRow = new boolean[9];
+	        boolean[][] isExistColumn = new boolean[9][9];
+	        boolean[][] isExistNine = new boolean[9][9];
+	        for (int i = 0; i < 9; i ++) {
+	        	Arrays.fill(isExistRow, false);
+	        	for (int j = 0; j < 9; j ++) {
+	        		if (board[i][j] == '.')
+	        			continue;
+	        		if (! isValid(isExistRow, board[i][j] - '1') ||
+	        				! isValid(isExistColumn[j], board[i][j] - '1') ||
+	        				! isValid(isExistNine[i/3*3+j/3], board[i][j] - '1'))
+	        			return false;
+	        	}
+	        }
+	        return true;
+	    }
+	    private boolean isValid(boolean[] isExist, int index) {
+	    	if(isExist[index])
+	    		return false;
+	    	isExist[index] = true;
+	    	return true;
+	    }
+	}
+}
