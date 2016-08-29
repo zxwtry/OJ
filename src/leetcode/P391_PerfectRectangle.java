3dac004f5f5d8bc7b7cd7dcae53fc84c439264cc
zxwtry <zxwtry@qq.com>
2016-08-29 10:39:27 +0800

    添加 leetcode P391 Perfect Rectangle 210 ms

--- src/leetcode/P391_PerfectRectangle.java
+++ src/leetcode/P391_PerfectRectangle.java
@@ -11,33 +11,57 @@ package leetcode;
 
 public class P391_PerfectRectangle {
 	public static void main(String[] args) {
-//		System.out.println(new Solution().isRectangleCover(new int[][]{
-//			{1,1,3,3},
-//			{3,1,4,2},
-//			{3,2,4,4},
-//			{1,3,2,4},
-//			{2,3,3,4}
-//		}));
-//		System.out.println(new Solution().isRectangleCover(new int[][]{
-//			{1,1,2,3},
-//			{1,3,2,4},
-//			{3,1,4,2},
-//			{3,2,4,4}
-//		}));
-//		System.out.println(new Solution().isRectangleCover(new int[][]{
-//			{1,1,3,3},
-//			{3,1,4,2},
-//			{1,3,2,4},
-//			{3,2,4,4}
-//		}));
-//		System.out.println(new Solution().isRectangleCover(new int[][]{
-//			{1,1,3,3},
-//		  	{3,1,4,2},
-//		  	{1,3,2,4},
-//		  	{2,2,4,4}
-//		}));
-		
+		System.out.println(new Solution2().isRectangleCover(new int[][]{
+			{1,1,3,3},
+			{3,1,4,2},
+			{3,2,4,4},
+			{1,3,2,4},
+			{2,3,3,4}
+		}));
+		System.out.println(new Solution2().isRectangleCover(new int[][]{
+			{1,1,2,3},
+			{1,3,2,4},
+			{3,1,4,2},
+			{3,2,4,4}
+		}));
+		System.out.println(new Solution2().isRectangleCover(new int[][]{
+			{1,1,3,3},
+			{3,1,4,2},
+			{1,3,2,4},
+			{3,2,4,4}
+		}));
+		System.out.println(new Solution2().isRectangleCover(new int[][]{
+			{1,1,3,3},
+		  	{3,1,4,2},
+		  	{1,3,2,4},
+		  	{2,2,4,4}
+		}));
+		System.out.println(new Solution2().isRectangleCover(new int[][]{
+			{0,0,1,1},
+		  	{0,0,1,1},
+		  	{0,2,1,3},
+		}));
+		System.out.println(new Solution2().isRectangleCover(new int[][]{
+			{0,0,1,1},
+		  	{0,0,1,1},
+		  	{1,1,2,2},
+		  	{1,1,2,2},
+		}));
+		System.out.println(new Solution2().isRectangleCover(new int[][]{
+			{1,1,3,3},
+		  	{3,1,4,2},
+		  	{3,2,4,4},
+		  	{1,3,2,4},
+		  	{2,3,3,4},
+		}));
+//		int[][] input = tools.FileUtils.A_读取二维数组("C:/a.txt", 4);
+//		System.out.println(new Solution2().isRectangleCover(input));
 	}
+	/*
+	 * 	对普通用例没有计算错误
+	 * 	大数字用例，MLE
+	 * 	主要是在判断上使用bit，还是不够，应该使用更加省内存的方法
+	 */
 	static class Solution {
 		int[] sign = null;
 		int row = 0, col = 0;
@@ -66,9 +90,6 @@ public class P391_PerfectRectangle {
 	    	for (int i = 0; i != rectangles.length; i ++) {
 	    		for (int j = rectangles[i][2] - rectangles[minI][0] - 1; j >= rectangles[i][0] - rectangles[minI][0]; j --) {
 	    			for (int k = rectangles[i][3] - rectangles[minI][1] - 1; k >= rectangles[i][1] - rectangles[minI][1]; k --) {
-//	    				System.out.println(k +"..."+j);
-//	    				if (k == 0 && j == 1)
-//	    					System.out.println();
 	    				if(! setIndex(k, j))
 	    					return false;
 	    			}
@@ -76,12 +97,66 @@ public class P391_PerfectRectangle {
 	    	}
 	    	return true;
 	    }
-	    private boolean setIndex(int i, int j) {
-	    	int len = row * i + j;
+	    private boolean setIndex(int k, int j) {
+	    	int len = col * k + j;
 	    	int index = len >> 5;
 	    	int bit = len - (index << 5);
 	    	sign[index] = sign[index] ^ (1 << bit);
 	    	return ((sign[index] >>> bit) & 0x1) == 1;
+	    }
+	}
+	/*
+	 * 	不使用bit，使用一维数组计数的方式
+	 * 	210 ms
+	 * 	逻辑还是很有点复杂，不过AC了
+	 */
+	static class Solution2 {
+		int[] sign = null;
+		int row = 0, col = 0;
+	    public boolean isRectangleCover(int[][] rectangles) {
+	    	if (rectangles == null || rectangles.length < 1 || rectangles[0].length < 1)
+	    		return false;
+	    	int maxI = 0, minI = 0, minSum = rectangles[0][0] + rectangles[0][1], maxSum = rectangles[0][2] + rectangles[0][3];
+	        long sumSquare = (rectangles[0][2] - rectangles[0][0]) * (rectangles[0][3] - rectangles[0][1]);
+	    	for (int i = 1; i != rectangles.length; i ++) {
+	    		sumSquare += (rectangles[i][2] - rectangles[i][0]) * (rectangles[i][3] - rectangles[i][1]);
+	    		if (rectangles[i][0] + rectangles[i][1] < minSum) {
+	    			minSum = rectangles[i][0] + rectangles[i][1];
+	    			minI = i;
+	    		}
+	    		if (rectangles[i][2] + rectangles[i][3] > maxSum) {
+	    			maxSum = rectangles[i][2] + rectangles[i][3];
+	    			maxI = i;
+	    		}
+	    	}
+	    	col = rectangles[maxI][2] - rectangles[minI][0];
+	    	sign = new int[col];
+	    	row = rectangles[maxI][3] - rectangles[minI][1];
+	    	boolean is00Twice = false;
+	    	if (sumSquare != ((long)rectangles[maxI][2] - rectangles[minI][0]) * (rectangles[maxI][3] - rectangles[minI][1]))
+	    		return false;
+	    	for (int i = 0; i != rectangles.length; i ++) {
+	    		if (rectangles[i][0] == rectangles[minI][0] && rectangles[i][1] == rectangles[minI][0]) {
+    				if (is00Twice)
+    					return false;
+    				is00Twice = true;
+    			}
+	    		for (int j = rectangles[i][2] - rectangles[minI][0] - 1; j >= rectangles[i][0] - rectangles[minI][0]; j --)
+	    			sign[j] += rectangles[i][3] - rectangles[i][1];
+	    	}
+	    	boolean isOK = true;
+	    	for (int i = 0; i != sign.length; i ++)
+	    		isOK &= sign[i] == row;
+	    	if (!isOK)
+	    		return false;
+	    	sign = new int[row];
+	    	for (int i = 0; i != rectangles.length; i ++)
+	    		for (int j = rectangles[i][3] - rectangles[minI][1] - 1; j >= rectangles[i][1] - rectangles[minI][1]; j --)
+	    			sign[j] += rectangles[i][2] - rectangles[i][0];
+	    	isOK = true;
+	    	for (int i = 0; i != sign.length; i ++)
+	    		isOK &= sign[i] == col;
+	    	return isOK;
 	    }
 	}
 }
