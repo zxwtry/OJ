e63cd23c95d5c01a59ac4a8d4336b1c3da91121c
zxwtry <zxwtry@qq.com>
2016-08-31 15:55:54 +0800

    添加 tools Utils A_打印二维数组

--- src/tools/Utils.java
+++ src/tools/Utils.java
@@ -120,5 +120,15 @@ public class Utils {
 				System.out.printf("%d", arr[i]);
 		}
 	}
+	public static void A_打印二维数组(int[][] arr) {
+		if (arr == null || arr.length < 1)
+			return;
+		for (int i = 0; i != arr.length; i ++) {
+			for (int j = 0; j != arr[i].length; j ++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
 	// 打印区结束
 }
