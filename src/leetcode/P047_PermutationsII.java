2b5e13c4adcf8232d54dd55f3a370dd75ccdd3f2
zxwtry <zxwtry@qq.com>
2016-08-31 14:20:13 +0800

    添加 leetcode P047 Permutations II  6 ms 44.78%

--- src/leetcode/P047_PermutationsII.java
+++ src/leetcode/P047_PermutationsII.java
@@ -22,7 +22,9 @@ public class P047_PermutationsII {
 //		System.out.println(new Solution().permuteUnique(new int[] {3, 2, 1}));
 //		System.out.println(new Solution().permuteUnique(new int[] {3, 2, 1}));
 //		System.out.println(new Solution().permuteUnique(new int[] {5, 4,3, 2, 2, 2, 1, 0, -1, -2}));
-		System.out.println(new Solution().permuteUnique(new int[] {3, 3, 2, 2, 1}));
+//		System.out.println(new Solution().permuteUnique(new int[] {3, 3, 2, 2, 1}));
+//		System.out.println(new Solution2().permuteUnique(new int[] {1, 2, 3, 4}));
+		System.out.println(new Solution2().permuteUnique(new int[] {3, 3, 5}));
 	}
 	/*
 	 * 	先用HashSet试试
@@ -69,6 +71,49 @@ public class P047_PermutationsII {
 			for (int i = 0; i != nums.length; i ++)
 				l += (nums[i] << (i * (i + 1)));
 			return l;
+		}
+	}
+	/*
+	 * 	使用HashSet是不行的。
+	 * 	没有重复的规则是：
+	 * 		第i个数与第j个数交换时，要求[i,j)中没有与第j个数相等的数
+	 * 	6 ms
+	 * 	44.78%
+	 */
+	static class Solution2 {
+		List<List<Integer>> ans = new LinkedList<List<Integer>>();
+	    public List<List<Integer>> permuteUnique(int[] nums) {
+	        if (nums == null || nums.length < 1)
+	        	return ans;
+	        java.util.Arrays.sort(nums);
+	        searchAllpermutations(nums, 0);
+	        return ans;
+	    }
+		private void searchAllpermutations(int[] nums, int index) {
+			if (index == nums.length) {
+				List<Integer> answer = new LinkedList<Integer>();
+				for (int i = 0; i != nums.length; i ++)
+					answer.add(nums[i]);
+				ans.add(answer);
+			}
+			for (int i = index; i != nums.length; i ++) {
+				if (! isDuplicated(nums, i, index)) {
+					swap(nums, i, index);
+					searchAllpermutations(nums, index + 1);
+					swap(nums, i, index);
+				}
+			}
+		}
+		private boolean isDuplicated(int[] nums, int i, int index) {
+			for (int j = index; j < i; j ++)
+				if (nums[j] == nums[i])
+					return true;
+			return false;
+		}
+		private void swap(int[] nums, int i, int j) {
+			int temp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = temp;
 		}
 	}
 }
