481dda40688e54699d16a0879252ef32da0fcd7b
zxwtry <zxwtry@qq.com>
2016-08-31 10:15:03 +0800

    添加 leetcode P044 Wildcard Matching  WA 逻辑太复杂，放弃

--- src/leetcode/P044_WildcardMatching.java
+++ src/leetcode/P044_WildcardMatching.java
@@ -22,10 +22,16 @@ public class P044_WildcardMatching {
 	public static void main(String[] args) {
 //		System.out.println(new Solution().isMatch("ab", "?*"));
 //		System.out.println(new Solution().isMatch("aab", "c*a*b"));
-		System.out.println(new Solution3().isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba", "a*******b"));
-		System.out.println(new Solution3().isMatch("abcba", "ab*ba"));
-		System.out.println(new Solution3().isMatch("aa", "*"));
-		System.out.println(new Solution3().isMatch("a", "a*"));
+//		System.out.println(new Solution3().isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba", "a*******b"));
+//		System.out.println(new Solution3().isMatch("abcba", "ab*ba"));
+//		System.out.println(new Solution3().isMatch("aa", "*"));
+//		System.out.println(new Solution3().isMatch("a", "a*"));
+//		System.out.println(new Solution3().isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba", "a*******b"));
+//		System.out.println(new Solution3().isMatch("abcba", "ab*ba"));
+//		System.out.println(new Solution3().isMatch("aa", "*"));
+//		System.out.println(new Solution3().isMatch("a", "a*"));
+//		System.out.println(new Solution3().isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba", "a*******a"));
+		System.out.println(new Solution3().isMatch("aaabbba", "a***dfdfdfdfdfdf****a"));
 	}
 	/*
 	 * 	还是没有吃透10题
@@ -102,5 +108,92 @@ public class P044_WildcardMatching {
 			}
 		}
 	}
-	
+	/*
+	 *	处理*的方法太差了，肯定有更加好的方法
+	 *	其实可以先预先处理非*的匹配 
+	 */
+	static class Solution3 {
+		private boolean isTwoStar = false;
+		public boolean isMatch(String s, String p) {
+			if (s == null || p == null)
+				return false;
+			if (s.length() == 0 && (p.length() == 0 || p.equals("*")))
+				return true;
+			return isMatchBackward(s.toCharArray(), 0, s.length() - 1, p.toCharArray(), 0, p.length() - 1);
+		}
+		public boolean isMatchForward(char[] s, int si, int se, char[] p, int pi, int pe) {
+			if (si > se || pi > pe) {
+				boolean isAllStar = true;
+				for (int i = pi; i != pe + 1; i ++)
+					isAllStar &= '*' == p[i];
+				if (isAllStar)
+					return 1 == si - se ;
+				return 1 == si - se && 1 == pi - pe;
+			}
+			if (p[pi] == '?') {
+				isTwoStar = false;
+				return isMatchForward(s, si + 1, se, p, pi + 1, pe);
+			} else if (p[pi] == '*') {
+				/*
+				 * 	这里的复杂度太高了
+				 */
+				if (isTwoStar)
+					return isMatchTwoStar(s, si, se, p, pi, pe);
+				isTwoStar = true;
+				return isMatchBackward(s, si, se, p, pi, pe);
+//				boolean matchSince = false;
+//				for (int i = si; i <= se + 1; i ++)
+//					matchSince |= isMatchForward(s, i, se, p, pi + 1, pe);
+//				return matchSince;
+			} else {
+				isTwoStar = false;
+				if (s[si] == p[pi])
+					return isMatchForward(s,  si + 1, se, p, pi + 1, pe);
+				else
+					return false;
+			}
+		}
+		public boolean isMatchBackward(char[] s, int si, int se, char[] p, int pi, int pe) {
+			if (si > se || pi > pe) {
+				boolean isAllStar = true;
+				for (int i = pe; i != -1; i --)
+					isAllStar &= '*' == p[i];
+				if (isAllStar)
+					return 1 == si - se ;
+				return 1 == si - se && 1 == pi - pe;
+			}
+			if (p[pe] == '?') {
+				isTwoStar = false;
+				return isMatchBackward(s, si, se - 1, p, pi, pe - 1);
+			} else if (p[pe] == '*') {
+				if (isTwoStar)
+					return isMatchTwoStar(s, si, se, p, pi, pe);
+				isTwoStar = true;
+				return isMatchForward(s, si, se, p, pi, pe);
+//				boolean matchSince = false;
+//				for (int i = se; i >= si -1; i --)
+//					matchSince |= isMatchBackward(s, si, i, p, pi, pe - 1);
+//				return matchSince;
+			} else {
+				isTwoStar = false;
+				if (s[se] == p[pe])
+					return isMatchBackward(s, si, se - 1, p, pi, pe - 1);
+				else
+					return false;
+			}
+		}
+		private boolean isMatchTwoStar(char[] s, int si, int se, char[] p, int pi, int pe) {
+			int countNotQNotS = 0;
+			for (int i = pi; i <= pe; i ++)
+				countNotQNotS += (p[i] == '?' || p[i] == '*') ? 0 : 1;
+			if (countNotQNotS == 0)
+				return true;
+			else if (countNotQNotS > se - si + 1)
+				return false;
+			/*
+			 * 	这里的逻辑过于复杂，放弃
+			 */
+			return false;
+		}
+	}
 }
