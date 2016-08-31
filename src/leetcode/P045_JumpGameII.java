39a2823b78141e149b72bef796cac2e9ea969edb
zxwtry <zxwtry@qq.com>
2016-08-31 10:59:52 +0800

    添加 leetcode P045 Jump Game II  5 ms 8.21%

--- src/leetcode/P045_JumpGameII.java
+++ src/leetcode/P045_JumpGameII.java
@@ -25,37 +25,59 @@ package leetcode;
 
 public class P045_JumpGameII {
 	public static void main(String[] args) {
-		System.out.println(new Solution1().jump(new int[] {2,3,1,1,4}));
-		System.out.println(new Solution1().jump(new int[] {1,2,3}));
-		System.out.println(new Solution1().jump(new int[] {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5}));
+		System.out.println(new Solution1().jump(new int[] { 2, 3, 1, 1, 4 }));
+		System.out.println(new Solution1().jump(new int[] { 1, 2, 3 }));
+		System.out.println(new Solution1().jump(new int[] { 5, 6, 4, 4, 6, 9, 4, 4, 7, 4, 4, 8, 2, 6, 8, 1, 5, 9, 6, 5,
+				2, 7, 9, 7, 9, 6, 9, 4, 1, 6, 8, 8, 4, 4, 2, 0, 3, 8, 5 }));
 	}
+
 	/*
-	 * 	最低效的解法，搜索
-	 * 	TLE
-	 * 	程序没有进入死循环，还是有解的，就是效率过低
+	 * 最低效的解法，搜索 TLE 程序没有进入死循环，还是有解的，就是效率过低
 	 */
 	static class Solution1 {
 		int count = Integer.MAX_VALUE;
-	    public int jump(int[] nums) {
-	    	if (nums == null || nums.length < 2)
-	    		return 0;
-	    	zxwtry(nums, 0, 0);
-	        return count;
-	    }
+
+		public int jump(int[] nums) {
+			if (nums == null || nums.length < 2)
+				return 0;
+			zxwtry(nums, 0, 0);
+			return count;
+		}
+
 		private void zxwtry(int[] nums, int nowIndex, int nowStep) {
 			if (nowIndex + nums[nowIndex] > nums.length - 2) {
 				count = Math.min(count, nowStep + 1);
 				return;
 			}
-			for (int i = 1; i <= nums[nowIndex]; i ++) {
+			for (int i = 1; i <= nums[nowIndex]; i++) {
 				zxwtry(nums, nowIndex + i, nowStep + 1);
 			}
 		}
 	}
+	/*
+	 * 	5 ms
+	 * 	8.21%
+	 */
 	static class Solution2 {
-	    public int jump(int[] nums) {
-	    	
-	        return 0;
-	    }
+		int jump(int[] nums) {
+			if (nums == null || nums.length < 2)
+				return 0;
+			int ret = 0, curMax = 0, curRch = 0;
+			for (int i = 0; i < nums.length; i++) {
+				if (curRch < i) {
+					ret++;
+					curRch = curMax;
+				}
+				curMax = Math.max(curMax, nums[i] + i);
+			}
+			return ret;
+		}
+	}
+
+	static class Solution3 {
+		public int jump(int[] nums) {
+
+			return 0;
+		}
 	}
 }
