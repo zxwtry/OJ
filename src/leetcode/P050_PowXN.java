10b3dcffe4a51e0153ac97dde752a3eb97007fbc
zxwtry <zxwtry@qq.com>
2016-09-01 08:17:47 +0800

    添加 leetcode P050 Pow XN  2 ms 6.16%

--- src/leetcode/P050_PowXN.java
+++ src/leetcode/P050_PowXN.java
@@ -22,4 +22,41 @@ public class P050_PowXN {
 	        return Math.pow(x, n);
 	    }
 	}
+	/*
+	 * 	2 ms
+	 * 	6.16%
+	 * 	多想想，怎么写出没有bug的代码
+	 */
+	static class Solution2 {
+		public double myPow(double x, int n) {
+			double ans = 0.0;
+			boolean isNegative = n < 0;
+			long len = 0, num = isNegative ? -(long)n : n;
+			while (num != 0) {
+				len ++;
+				num = num >>> 1;
+			}
+			num = isNegative ? -(long)n : n;
+			double[] help = new double[(int)(len + 13)];
+			help[0] = 1;
+			help[1] = x;
+			long nn = 2;
+			int count = 1;
+			ans = x;
+			while (nn < num + 1) {
+				ans = ans * ans;
+				help[++ count] = ans;
+				nn = nn << 1;
+			}
+			ans = 1;
+			for (int i = 1; i != help.length; i ++) {
+				if (Math.abs(help[i]) < 0.00000001)
+					continue;
+				int times = (int)num & (1 << (i - 1));
+				if (times != 0)
+					ans *= help[i];
+			}
+			return isNegative ? 1/ans : ans;
+		}
+	}
 }
