49040cb30252f3c2e4b438841d0ce76d7b582664
zxwtry <zxwtry@qq.com>
2016-09-07 22:18:42 +0800

    添加 tools.TreeNode辅助.TreeNode

---
+++ src/tools/TreeNode辅助.java
@@ -0,0 +1,12 @@
+package tools;
+
+
+public class TreeNode辅助 {
+	public static class TreeNode {
+		public int val;
+		public TreeNode left, right;
+		public TreeNode(int val) {
+			this.val = val;
+		}
+	}
+}
