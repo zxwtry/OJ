a9f9abd338cdfc8ea84cd9250d1ca2e644584c11
zxwtry <zxwtry@qq.com>
2016-09-08 23:40:20 +0800

    添加 leetcode P069 Sqrtx  2 ms

---
+++ src/leetcode/P069_Sqrtx.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class P069_Sqrtx {
+	public static void main(String[] args) {
+		
+	}
+	/*
+	 * 	hehe
+	 * 	2 ms
+	 */
+	static class Solution {
+	    public int mySqrt(int x) {
+	        return (int)Math.sqrt(x);
+	    }
+	}
+}
