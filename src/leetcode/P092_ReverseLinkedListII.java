ac48e197cebfe677f8b08559d6afd6821ad86223
zxwtry <zxwtry@qq.com>
2016-09-14 19:50:17 +0800

    添加 leetcode P092 Reverse Linked List II  1 ms

---
+++ src/leetcode/P092_ReverseLinkedListII.java
@@ -0,0 +1,67 @@
+package leetcode;
+
+
+/*
+ * 	Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+	For example:
+	Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+	
+	return 1->4->3->2->5->NULL.
+	
+	Note:
+	Given m, n satisfy the following condition:
+	1 ≤ m ≤ n ≤ length of list.
+ */
+
+
+
+import tools.ListNode辅助.ListNode;;
+
+public class P092_ReverseLinkedListII {
+	public static void main(String[] args) {
+		ListNode head = tools.ListNode辅助.A_一维生成器(new int[] {});
+		ListNode ans = new Solution().reverseBetween(head, 1, 1);
+		tools.ListNode辅助.B_打印链表(ans);
+	}
+	/*
+	 * 	题目非常容易，一次AC，好爽
+	 * 	1 ms
+	 */
+	static class Solution {
+	    public ListNode reverseBetween(ListNode head, int m, int n) {
+	    	if (m >= n) {
+	    		return head;
+	    	}
+	    	ListNode tra = head, one_last = null, two_first = null,
+	    			two_last = null, three_first = null, save_tra_next = null;
+	    	int count = 1;
+	    	while (tra != null) {
+	    		save_tra_next = tra.next;
+	    		if (count == m - 1) {
+	    			one_last = tra;
+	    		} else if (count == m) {
+	    			two_first = tra;
+	    			two_last = tra;
+	    		} else if (count == n + 1) {
+	    			three_first = tra;
+	    		} else if (count > m && count <= n) {
+	    			tra.next = two_first;
+	    			two_first = tra;
+	    		}
+	    		tra = save_tra_next;
+	    		count ++;
+	    	}
+	    	if (one_last != null) {
+	    		one_last.next = two_first;
+	    	}
+	    	if (two_last != null) {
+	    		two_last.next = three_first;
+	    	}
+	    	if (one_last == null) {
+	    		return two_first;
+	    	}
+	        return head;
+	    }
+	}
+}
