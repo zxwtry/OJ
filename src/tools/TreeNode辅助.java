fed2455326805291da527136e22e8ab16dc442ea
zxwtry <zxwtry@qq.com>
2016-09-18 14:52:26 +0800

    添加 TreeNode辅助.A_生成满二叉树和B_按层打印

--- src/tools/TreeNode辅助.java
+++ src/tools/TreeNode辅助.java
@@ -1,7 +1,69 @@
 package tools;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class TreeNode辅助 {
+	public static void main(String[] args) {
+		TreeNode root = A_生成满二叉树(new int[] {0, 1, 2, 3, 4, 5, 6});
+		B_按层打印(root);
+	}
+	/*
+	 * 	使用Integer.MIN_VALUE作为null的标志
+	 */
+	public static TreeNode A_生成满二叉树(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return null;
+		}
+		int judge = arr.length + 1, count_1 = 0;
+		while (judge != 0) {
+			count_1 += (judge & 1) == 1 ? 1 : 0;
+			judge = judge >>> 1;
+		}
+		if (count_1 != 1) {
+			System.out.println("输入不是一个满二叉树");
+			return null;
+		}
+		TreeNode[] tree_arr = new TreeNode[arr.length];
+		for (int i = 0; i < arr.length; i ++) {
+			tree_arr[i] = arr[i] == Integer.MIN_VALUE ? null : new TreeNode(arr[i]);
+		}
+		for (int i = (arr.length - 2) / 2; i > -1; i --) {
+			if (tree_arr[i] != null) {
+				tree_arr[i].left = tree_arr[2 * i + 1];
+				tree_arr[i].right = tree_arr[2 * i + 2];
+			}
+		}
+		return tree_arr[0];
+	}
+	public static void B_按层打印(TreeNode root) {
+		if (root == null) {
+			System.out.println("输入的TreeNode为null");
+			return;
+		}
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+		Queue<Integer> q_int = new LinkedList<Integer>();
+		q.add(root);
+		q_int.add(1);
+		int pre_int = -1;
+		while (! q.isEmpty()) {
+			TreeNode root_now = q.poll();
+			int q_now = q_int.poll();
+			if (q_now != pre_int) {
+				System.out.printf("\r\n现在开始第%d层\t",q_now);
+				pre_int = q_now;
+			}
+			System.out.print(root_now.val+"\t");
+			if (root_now.left != null) {
+				q.add(root_now.left);
+				q_int.add(q_now + 1);
+			}
+			if (root_now.right != null) {
+				q.add(root_now.right);
+				q_int.add(q_now + 1);
+			}
+		}
+	}
 	public static class TreeNode {
 		public int val;
 		public TreeNode left, right;
