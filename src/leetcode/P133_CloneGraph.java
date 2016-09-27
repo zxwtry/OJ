d0597cdf292eacbe25405862c49edebe1d93ae41
zxwtry <zxwtry@qq.com>
2016-09-27 13:05:01 +0800

    添加 leetcode P133 Clone Graph  15 ms  5.90%

--- src/leetcode/P133_CloneGraph.java
+++ src/leetcode/P133_CloneGraph.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -38,12 +39,12 @@ import tools.UndirectedGraphNode辅助.UndirectedGraphNode;
 
 public class P133_CloneGraph {
 	public static void main(String[] args) {
-//		UndirectedGraphNode root_origin = tools.UndirectedGraphNode辅助.generate_from_serialized_string("0,1,2,3#1,2,3#2,2#3");
-//		tools.UndirectedGraphNode辅助.print_node(root_origin);
-//		System.out.println("================================");
-//		Solution s = new Solution();
-//		UndirectedGraphNode root_ghost = s.cloneGraph(root_origin);
-//		tools.UndirectedGraphNode辅助.print_node(root_ghost);
+		UndirectedGraphNode root_origin = tools.UndirectedGraphNode辅助.A_从序列化字符串中反序列化出无向图("0,1#1,2#2,3#3");
+		tools.UndirectedGraphNode辅助.B_以邻接表的方式打印无向图(root_origin);
+		System.out.println("================================");
+		Solution2 s = new Solution2();
+		UndirectedGraphNode root_ghost = s.cloneGraph(root_origin);
+		tools.UndirectedGraphNode辅助.B_以邻接表的方式打印无向图(root_ghost);
 	}
 	/*
 	 * 	16 ms
@@ -53,6 +54,9 @@ public class P133_CloneGraph {
 		HashMap<Integer, UndirectedGraphNode> map_origin = new HashMap<>();
 		HashMap<Integer, UndirectedGraphNode> map_ghost = new HashMap<>();
 	    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+	    	if (node == null) {
+	    		return null;
+	    	}
 	    	Queue<UndirectedGraphNode> q = new LinkedList<UndirectedGraphNode>();
 	    	q.add(node);
 	    	while (! q.isEmpty()) {
@@ -77,6 +81,51 @@ public class P133_CloneGraph {
 	    		}
 	    	}
 	        return map_ghost.get(node.label);
+	    }
+	}
+	/*
+	 * 	15 ms
+	 * 	5.90%
+	 * 	很奇怪，更加好的复制方法竟然想不出来。。。
+	 */
+	static class Solution2 {
+		HashMap<Integer, UndirectedGraphNode> map_ghost = new HashMap<>();
+		HashSet<UndirectedGraphNode> set_origin = new HashSet<>();
+	    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+	    	if (node == null) {
+	    		return null;
+	    	}
+	    	Queue<UndirectedGraphNode> q = new LinkedList<>();
+	    	q.add(node);
+	    	while (! q.isEmpty()) {
+	    		UndirectedGraphNode root_now_ghost = null;
+	    		UndirectedGraphNode root_now_origin = q.poll();
+	    		if (set_origin.contains(root_now_origin)) {
+	    			continue;
+	    		} else {
+	    			set_origin.add(root_now_origin);
+	    		}
+	    		if (map_ghost.containsKey(root_now_origin.label)) {
+	    			root_now_ghost = map_ghost.get(root_now_origin.label);
+	    		} else {
+	    			root_now_ghost = new UndirectedGraphNode(root_now_origin.label);
+	    			map_ghost.put(root_now_ghost.label, root_now_ghost);
+	    		}
+	    		List<UndirectedGraphNode> list = root_now_ghost.neighbors;
+	    		for (UndirectedGraphNode neighbor : root_now_origin.neighbors) {
+	    			UndirectedGraphNode neighbor_ghost = null;
+	    			if (map_ghost.containsKey(neighbor.label)) {
+	    				neighbor_ghost = map_ghost.get(neighbor.label);
+	    				list.add(neighbor_ghost);
+	    			} else {
+	    				neighbor_ghost = new UndirectedGraphNode(neighbor.label);
+	    				map_ghost.put(neighbor_ghost.label, neighbor_ghost);
+	    				list.add(neighbor_ghost);
+	    			}
+	    			q.add(neighbor);
+	    		}
+	    	}
+	    	return map_ghost.get(node.label);
 	    }
 	}
 }
