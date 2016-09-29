5a0cf52242ea26c0bf1a8f262532f088c542027a
zxwtry <zxwtry@qq.com>
2016-09-29 14:39:25 +0800

    添加 leetcode P406 Queue Reconstruction by Height  AC 71 ms

--- src/leetcode/P406_QueueReconstructionbyHeight.java
+++ src/leetcode/P406_QueueReconstructionbyHeight.java
@@ -1,7 +1,10 @@
 package leetcode;
 
+import java.util.AbstractCollection;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 /*
  * Suppose you have a random list of people standing in a queue. 
@@ -26,91 +29,103 @@ import java.util.Arrays;
 public class P406_QueueReconstructionbyHeight {
 	public static void main(String[] args) {
 		int[][] people = new int[][] {
-			{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}
+			{8,2},{4,2},{4,5},{2,0},{7,2},{1,4},{9,1},{3,1},{9,0},{1,0}
+//			{0,1}, {1,4}, {1,1}, {2,1}, {2,1}, {2,2},{3,1},{3,1},{3,1},{3,1},{4,1},{4,1},{4,1},{4,1}
 		};
 		Solution s = new Solution();
 		int[][] ans = s.reconstructQueue(people);
 		if (ans != null) {
 			System.out.println(ans.length);
+			tools.Utils.A_打印二维数组(ans);
 		}
 	}
+	/*
+	 * 	AC
+	 * 	71 ms
+	 */
 	static class Solution {
 	    public int[][] reconstructQueue(int[][] people) {
-	    	ArrayList<My> ms_list = new ArrayList<My>(people.length * 2);
-	    	ArrayList<Integer> large_pre = new ArrayList<>(people.length * 2);
-	    	ArrayList<Integer> available = new ArrayList<>(people.length * 2);
-	    	generateArrayListMS(people, ms_list, large_pre, available);
-	    	while (available.size() != 0) {
-	    		int i_select = available.get(0);
-	    		if (large_pre.get(i_select) == ms_list.get(i_select).m[1]) {
-	    			available.remove(0);
-	    			continue;
-	    		}
-	    		int step = ms_list.get(i_select).m[1] - large_pre.get(i_select);
-	    		int next_select = i_select + step;
-	    		int available_remove = next_select;
-	    		for (int i = i_select; i < next_select; i ++) {
-	    			if (ms_list.get(i).m[1] == large_pre.get(i)) {
-	    				available_remove --;
-	    			}
-	    		}
-	    		ms_list.add(next_select + 1, ms_list.get(i_select));
-	    		large_pre.add(next_select + 1, ms_list.get(i_select).m[1]);
-	    		large_pre.remove(i_select);
-	    		ms_list.remove(i_select);
-	    		available.remove(available_remove);
+	    	if (people == null || people.length == 0 || people[0].length == 0) {
+	    		return people;
 	    	}
-	    	My[] ans_ms = new My[ms_list.size()];
-	    	for (int i = 0; i < ms_list.size(); i ++) {
-	    		ans_ms[i] = ms_list.get(i);
-	    	}
-	    	print(ans_ms);
-	        return null;
-	    }
-	    void generateArrayListMS(int[][] people, ArrayList<My> ms_list, 
-	    		ArrayList<Integer> large_pre, ArrayList<Integer> available) {
-	    	My[] ms = new My[people.length];
+	    	Comparator<My> com_list = new Comparator<My>() {
+				@Override
+				public int compare(My o1, My o2) {
+					return o1.m[0] == o2.m[0] ? o1.m[1] - o2.m[1] : o1.m[0] - o2.m[0];
+				}
+			};
+			Comparator<My> com_set = new Comparator<My>() {
+				@Override
+				public int compare(My o1, My o2) {
+					return o1.m[0] - o2.m[0];
+				}
+			};
+	    	ArrayList<My> list = new ArrayList<>();
+	    	TreeSet<My> set = new TreeSet<>(com_set);
 	    	for (int i = 0; i < people.length; i ++) {
-	    		My new_my = new My(people[i]);
-	    		ms[i] = new_my;
-	    	}
-	    	Arrays.sort(ms);
-	    	for (int i = 0; i < ms.length; i ++) {
-	    		if (i != 0 && ms[i].m[0] == ms[i - 1].m[0]) {
-	    			large_pre.add(large_pre.get(i - 1) + 1);
+	    		int[] p_now = people[i];
+	    		My new_my = new My(p_now);
+	    		if (p_now[1] == 0) {
+		    		set.add(new_my);
 	    		} else {
-	    			large_pre.add(0);
+	    			list.add(new_my);
 	    		}
-	    		ms_list.add(ms[i]);
-	    		available.add(i);
 	    	}
+			Collections.sort(list, com_list);
+			int[][] ans = new int[people.length][2];
+			int ans_index = 0;
+			while (set.size() != 0) {
+				My from_set = set.pollFirst();
+				ans[ans_index ++] = from_set.m;
+				if (list.size() == 0) {
+					continue;
+				}
+				if (from_set.m[0] < list.get(0).m[0]) {
+					continue;
+				}
+				int index = binary(list, from_set.m[0]);
+				for (int i = 0; i <= index; i ++) {
+					My from_list =  list.get(i);
+					from_list.pre --;
+					if (from_list.pre == 0) {
+						set.add(from_list);
+						list.remove(i);
+						i --;
+						index --;
+					}
+				}
+			}
+	    	return ans;
 	    }
-	    void print(My[] arr) {
+	    int binary(ArrayList<My> list, int m0) {
+	    	int sti = 0, eni = list.size() - 1;
+	    	while (sti < eni) {
+	    		int mid = (sti + eni + 1) / 2;
+	    		int cut = list.get(mid).m[0] - m0;
+	    		if (cut > 0) {
+	    			eni = mid - 1;
+	    		} else {
+	    			sti = mid;
+	    		}
+	    	}
+	    	return sti;
+	    }
+	    void print(AbstractCollection<My> arr) {
 	    	int count = 0;
 	    	for (My my : arr) {
-	    		System.out.printf("第%d个数据： %d...%d\r\n", count ++, my.m[0], my.m[1]);
+	    		System.out.printf("第%d个数据： %s\r\n", count ++, my);
 	    	}
 	    }
-	    void swap(My[] ms, int i, int j) {
-	    	My temp = ms[i];
-	    	ms[i] = ms[j];
-	    	ms[j] = temp;
-	    }
-	    static class My implements Comparable<My> {
+	    static class My{
 	    	int[] m = null;
-	    	int val = 0;
+	    	int pre = 0;
 	    	public My(int[] m) {
 	    		this.m = m;
-	    		val += m[0] * 10000;
-	    		val += m[1];
+	    		pre = m[1];
 	    	}
 			@Override
-			public int compareTo(My o) {
-				if (this.m[0] != o.m[0]) {
-					return this.m[0] - o.m[0];
-				} else {
-					return this.m[1] - o.m[1];
-				}
+			public String toString() {
+				return String.format("%d %d %d", m[0], m[1], pre);
 			}
 	    }
 	}
