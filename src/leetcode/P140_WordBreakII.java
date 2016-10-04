5af9d1d6920930791bfe98997ee9472a203707a6
zxwtry <zxwtry@qq.com>
2016-10-04 16:07:53 +0800

    添加 leetocde P140 Word Break II

---
+++ src/leetcode/P140_WordBreakII.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/*
+ * 	Given a string s and a dictionary of words dict, 
+ * 	add spaces in s to construct a sentence 
+ * 	where each word is a valid dictionary word.
+
+	Return all such possible sentences.
+	
+	For example, given
+	s = "catsanddog",
+	dict = ["cat", "cats", "and", "sand", "dog"].
+	
+	A solution is ["cats and dog", "cat sand dog"].
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class P140_WordBreakII {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+		List<String> ans = new ArrayList<String>();
+	    public List<String> wordBreak(String s, Set<String> wordDict) {
+	    	
+	        return ans;
+	    }
+	}
+}
