775a2a147778cff1d55f0e6f69cba5badce6884a
zxwtry <zxwtry@qq.com>
2016-10-04 16:07:23 +0800

    添加 tools.Random速记生成器

--- src/tools/Random随机生成器.java
+++ src/tools/Random随机生成器.java
@@ -1,5 +1,7 @@
 package tools;
 
+import java.util.HashSet;
+
 public class Random随机生成器 {
 	public static int[] A_生成一个随机数据(int n, int min, int max) {
 		int[] ans = new int[n];
@@ -16,10 +18,26 @@ public class Random随机生成器 {
 		int range = max - min;
 		for (int i = 0; i < n; i ++) {
 			int new_add = (int)(Math.random() * range);
-			if (new_add == val) {
+			if (new_add == val - min) {
 				i --;
 				continue;
 			}
+			ans[i] = base + new_add;
+		}
+		return ans;
+	}
+	public static int[] A_生成一个不重复随机数据(int n, int min, int max) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		int[] ans = new int[n];
+		int base = min;
+		int range = max - min;
+		for (int i = 0; i < n; i ++) {
+			int new_add = (int)(Math.random() * range);
+			if (set.contains(new_add)) {
+				i --;
+				continue;
+			}
+			set.add(new_add);
 			ans[i] = base + new_add;
 		}
 		return ans;
