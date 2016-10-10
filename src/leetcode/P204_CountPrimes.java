bb2262b7d23198a4ef76edc2850a3d2a8dfaa7ac
zxwtry <zxwtry@qq.com>
2016-10-10 22:11:23 +0800

    添加 leetcode P204 Count Primes  24 ms 87.67% 最快的解法

--- src/leetcode/P204_CountPrimes.java
+++ src/leetcode/P204_CountPrimes.java
@@ -161,6 +161,32 @@ public class P204_CountPrimes {
 		}
 	}
 	/*
+	 * 	24 ms
+	 * 	87.67%
+	 */
+	static class Solution4 {
+		public int countPrimes(int n) {
+			if (n < 3) {
+				return 0;
+			}
+			boolean[] isPrime = new boolean[n];
+			for (int i = 2; i * i < n; i ++) {
+				if (isPrime[i]) {
+					continue;
+				}
+				for (int j = i * i; j < n; j += i) {
+					isPrime[j] = true;
+				}
+			}
+			int count = 0;
+			for (int i = 2; i < n; i ++) {
+				if (!isPrime[i])
+					count ++;
+			}
+			return count;
+		}
+	}
+	/*
 	 * 	61 ms
 	 * 	14.04%
 	 */
