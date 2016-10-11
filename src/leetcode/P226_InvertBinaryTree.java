b7dec7e19962a443c2ae7ba35a8bd762a1c9f061
zxwtry <zxwtry@qq.com>
2016-10-11 22:29:09 +0800

    添加 leetcode P226 Invert Binary Tree  神题，先纪念一下

---
+++ src/leetcode/P226_InvertBinaryTree.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import tools.TreeNode辅助.TreeNode;
+
+/*
+ * 	Invert a binary tree.
+	
+	     4
+	   /   \
+	  2     7
+	 / \   / \
+	1   3 6   9
+	to
+	     4
+	   /   \
+	  7     2
+	 / \   / \
+	9   6 3   1
+	Trivia:
+	This problem was inspired by this original tweet by Max Howell:
+	Google: 90% of our engineers use the software you wrote (Homebrew),
+	 but you can’t invert a binary tree on a whiteboard so fuck off.
+ */
+
+public class P226_InvertBinaryTree {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public TreeNode invertTree(TreeNode root) {
+	        return root;
+	    }
+	}
+}
