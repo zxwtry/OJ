6850837d22eebec8461f10b8c647b387eb8681fc
zxwtry <zxwtry@qq.com>
2016-10-11 22:26:55 +0800

    添加 leetcode P2017 Course Schedule

---
+++ src/leetcode/P207_CourseSchedule.java
@@ -0,0 +1,76 @@
+package leetcode;
+
+import java.util.HashSet;
+/*
+ * 	There are a total of n courses you have to take, labeled from 0 to n - 1.
+	
+	
+	Some courses may have prerequisites, for example to take course 0 you have to
+	 first take course 1, which is expressed as a pair: [0,1]
+	  课程有依赖关系，比如[0, 1]表示：假如需要上0号课程，先必须上完1号课程
+	
+	Given the total number of courses and a list of prerequisite pairs, is it 
+	possible for you to finish all courses?
+	
+	For example:
+	
+	2, [[1,0]]
+	There are a total of 2 courses to take. To take course 1 you should have 
+	finished course 0. So it is possible.
+	
+	2, [[1,0],[0,1]]
+	There are a total of 2 courses to take. To take course 1 you should have 
+	finished course 0, and to take course 0 you should also have finished course 1.
+	 So it is impossible.
+	
+	Note:
+	The input prerequisites is a graph represented by a list of edges, 
+	not adjacency matrices. Read more about how a graph is represented.
+ */
+
+public class P207_CourseSchedule {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public boolean canFinish(int n, int[][] p) {
+	    	Node[] ns = new Node[n];
+	    	for (int i = 0; i < n; i ++) {
+	    		ns[i] = new Node(i);
+	    	}
+	    	for (int[] pArr : p) {
+	    		int wantToLearn = pArr[0];
+	    		int needToLearn = pArr[1];
+	    		ns[wantToLearn].addPre(needToLearn);
+	    		ns[needToLearn].addPos(wantToLearn);
+	    	}
+	    	
+	        return false;
+	    }
+	    static class Node {
+	    	int val;
+	    	HashSet<Integer> allPres = null;
+	    	HashSet<Integer> allPoss = null;
+	    	boolean isFinished = false;
+	    	public Node(int val) {
+	    		this.val = val;
+	    		allPres = new HashSet<Integer>();
+	    		allPoss = new HashSet<Integer>();
+	    	}
+	    	public void addPre(int pre) {
+	    		allPres.add(pre);
+	    	}
+	    	public void addPos(int pos) {
+	    		allPoss.add(pos);
+	    	}
+	    	public void release(Node[] ns) {
+	    		for (int pos : allPoss) {
+	    			HashSet<Integer> set = ns[pos].allPres;
+	    			if (set.contains(val)) {
+	    				set.remove(val);
+	    			}
+	    		}
+	    	}
+	    }
+	}
+}
