e948aaec964cb67f2987fae4ba85e6f2d9336e84
zxwtry <zxwtry@qq.com>
2016-10-12 17:46:29 +0800

    添加 leetcode P383 Ransom Note  69 ms  23.14%

---
+++ src/leetcode/P383_RansomNote.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/*\
+ * 
+ * 	 Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters 
+ * 	from  all  the  magazines,  write  a  function  that  will  return  true  if  the  ransom 
+ * 	  note  can  be  constructed  from  the  magazines ;  otherwise,  it  will  return  false.   
+
+	Each  letter  in  the  magazine  string  can  only  be  used  once  in  your  ransom  note.
+	
+	Note:
+	You may assume that both strings contain only lowercase letters.
+	
+	canConstruct("a", "b") -> false
+	canConstruct("aa", "ab") -> false
+	canConstruct("aa", "aab") -> true
+ */
+
+public class P383_RansomNote {
+	public static void main(String[] args) {
+		
+	}
+	/*
+	 * 	69 ms
+	 * 	23.14%
+	 */
+	static class Solution {
+	    public boolean canConstruct(String ransomNote, String magazine) {
+	    	HashMap<Character, Integer> mapMagazine = new HashMap<>();
+	    	if (magazine.length() < ransomNote.length()) {
+	    		return false;
+	    	}
+	    	for (int i = 0; i < magazine.length(); i ++) {
+	    		char c = magazine.charAt(i);
+	    		if (mapMagazine.containsKey(c)) {
+	    			mapMagazine.put(c, mapMagazine.get(c) + 1);
+	    		} else {
+	    			mapMagazine.put(c, 1);
+	    		}
+	    	}
+	    	for (int i = 0; i < ransomNote.length(); i ++) {
+	    		char c = ransomNote.charAt(i);
+	    		if (mapMagazine.containsKey(c)) {
+	    			int val = mapMagazine.get(c);
+	    			if (val <= 0) {
+	    				return false;
+	    			} else {
+	    				mapMagazine.put(c, val - 1);
+	    			}
+	    		} else {
+	    			return false;
+	    		}
+	    	}
+	        return true;
+	    }
+	}
+}
