ccc7d4de2ad7401345f0bd9f05298dcd51c68960
zxwtry <zxwtry@qq.com>
2016-10-12 17:34:27 +0800

    添加 leetcode P371 Sum of Two Integers  0 ms  8.45%  非常有意思的一题

---
+++ src/leetcode/P371_SumofTwoIntegers.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/*
+ * 	Calculate the sum of two integers a and b, 
+ * 	but you are not allowed to use the operator + and -.
+
+	Example:
+	Given a = 1 and b = 2, return 3.
+ */
+
+public class P371_SumofTwoIntegers {
+	public static void main(String[] args) {
+		System.out.println(new Solution().getSum(100, 199));
+	}
+	/*
+	 * 	0 ms
+	 * 	8.45%
+	 */
+	static class Solution {
+	    public int getSum(int a, int b) {
+	    	int c = 0;
+	    	while (0 != a) {
+	    		c = (a & b) << 1;
+	    		b = a ^ b;
+	    		a = c;
+	    	}
+	        return b;
+	    }
+	}
+}
