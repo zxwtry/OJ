c15b217e923bc62d76c5bd04a30fc974199e293e
zxwtry <zxwtry@qq.com>
2016-10-14 21:04:42 +0800

    添加 leetcode P214 Shortest Palindrome  使用Manacher 为什么TLE呢？

--- src/leetcode/P214_ShortestPalindrome.java
+++ src/leetcode/P214_ShortestPalindrome.java
@@ -14,7 +14,7 @@ package leetcode;
 public class P214_ShortestPalindrome {
 	public static void main(String[] args) {
 		Solution s1 = new Solution();
-		Solution2 s2 = new Solution2();
+		Solution3 s2 = new Solution3();
 		int[] arr = tools.Random随机生成器.A_生成一个随机数据(1000, 0, 26);
 		char[] cArr = new char[arr.length];
 		for (int i = 0; i < arr.length; i ++) {
@@ -24,6 +24,8 @@ public class P214_ShortestPalindrome {
 		String ans1 = s1.shortestPalindrome(s);
 		String ans2 = s2.shortestPalindrome(s);
 		System.out.println(ans1.equals(ans2));
+//		Solution3 s3 = new Solution3();
+//		System.out.println(s3.shortestPalindrome("aabaa"));
 	}
 	/*
 	 * 	213 ms
@@ -118,6 +120,80 @@ public class P214_ShortestPalindrome {
 				j --;
 			}
 			return true;
+		}
+	}
+	
+	/*
+	 * 	还是没有将Manacher的思想用上。。。
+	 * 	其实没必要构造填充式
+	 * 	总是TLE
+	 */
+	static class Solution3 {
+		final char SHARP = '#';
+		char[] cs = null;
+		int originLength = 0;
+		int manacherLength = 0;
+	    public String shortestPalindrome(String s) {
+	    	if (null == s || s.length() == 0) {
+	    		return "";
+	    	}
+	    	originLength = s.length();
+	    	manacherLength = getManacherIndexFromOriginIndex(originLength);
+	    	cs = new char[manacherLength];
+	    	for (int i = 0; i < manacherLength; i ++) {
+	    		cs[i] = i % 2 == 0 ? SHARP : s.charAt(getOriginIndexFromManacherIndex(i));
+	    	}
+	    	int startIndexOfCopy = getMaxLenOfPalindrome();
+	    	if (startIndexOfCopy == originLength) {
+	    		return s;
+	    	}
+	    	char[] ans = new char[2 * originLength - startIndexOfCopy];
+	    	for (int i = 0; i < originLength; i ++) {
+	    		ans[originLength - startIndexOfCopy + i] = cs[getManacherIndexFromOriginIndex(i)];
+	    	}
+	    	for (int i = originLength - startIndexOfCopy - 1; i > -1; i --) {
+	    		ans[i] = cs[getManacherIndexFromOriginIndex(originLength - 1 - i)];
+	    	}
+	    	return new String(ans);
+	    }
+		int getOriginIndexFromManacherIndex(int manacherIndex) {
+			return (manacherIndex - 1) / 2;
+		}
+		int getManacherIndexFromOriginIndex(int originIndex) {
+			return originIndex * 2 + 1;
+		}
+		int getMaxLenOfPalindrome() {
+			int ans = 0;
+			int[] lengthOfExpand = new int[manacherLength];
+			int pr = 0, preIndex = 0;
+			for (int index = 1; index < manacherLength; index ++) {
+				if (pr > manacherLength - 2) {
+					break;
+				}
+				if (index >= pr) {
+					int l = index, r = index;
+					while (-- l > -1 && ++ r < manacherLength && cs[l] == cs[r]) {}
+					lengthOfExpand[index] = (r - l - 1) / 2;
+					preIndex = index;
+				} else {
+					int mirrorIndex = preIndex * 2 - index;
+					int circleRight = lengthOfExpand[mirrorIndex] + index;
+					if (circleRight == manacherLength - 1) {
+						int l = index, r = index;
+						while (-- l > -1 && ++ r < manacherLength && cs[l] == cs[r]) {}
+						lengthOfExpand[index] = (r - l - 1) / 2;
+						preIndex = index;
+					} else if (circleRight < manacherLength - 1) {
+						lengthOfExpand[index] = circleRight - index;
+					} else {
+						lengthOfExpand[index] = pr - index;
+					}
+				}
+				if (lengthOfExpand[index] == index) {
+					ans = Math.max(ans, index);
+				}
+			}
+			return ans;
 		}
 	}
 }
