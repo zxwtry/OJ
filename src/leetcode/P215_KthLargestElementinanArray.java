4deef939bf918f6c9f2aa4f9b836199a2bfdffde
zxwtry <zxwtry@qq.com>
2016-10-14 21:38:58 +0800

    添加 leetcode P215 Kth Largest Element in an Array  52 ms  25.82% 自己写QuickSort，比内置的慢。。。

--- src/leetcode/P215_KthLargestElementinanArray.java
+++ src/leetcode/P215_KthLargestElementinanArray.java
@@ -16,9 +16,9 @@ import java.util.Arrays;
 
 public class P215_KthLargestElementinanArray {
 	public static void main(String[] args) {
-		int[] nums = new int[] {1};
-		int k = 1;
-		Solution s = new Solution();
+		int[] nums = new int[] {3,2,1,5,6,4};
+		int k = 6;
+		Solution2 s = new Solution2();
 		System.out.println(s.findKthLargest(nums, k));
 	}
 	/*
@@ -30,5 +30,44 @@ public class P215_KthLargestElementinanArray {
 	    	Arrays.sort(nums);
 	        return nums[nums.length - k];
 	    }
+	}
+	/*
+	 * 	写一个QuickSort
+	 * 	52 ms
+	 * 	25.82%
+	 */
+	static class Solution2 {
+		int targetIndex = 0;
+		int targetValue = 0;
+		boolean isFindAnswer = false;
+		public int findKthLargest(int[] nums, int k) {
+			targetIndex = nums.length - k;
+			quickSort(nums, 0, nums.length - 1);
+			return isFindAnswer ? targetValue : nums[targetIndex];
+		}
+		
+		void quickSort(int[] nums, int startIndex, int endIndex) {
+			if (startIndex >= endIndex || isFindAnswer) {
+				return;
+			}
+			int partitionIndex = partition(nums, startIndex, endIndex);
+			quickSort(nums, startIndex, partitionIndex - 1);
+			quickSort(nums, partitionIndex + 1, endIndex);
+		}
+		private int partition(int[] nums, int startIndex, int endIndex) {
+			int selectPivotValue = nums[startIndex];
+			while (startIndex < endIndex) {
+				while (startIndex < endIndex && nums[endIndex] >= selectPivotValue) endIndex --;
+				nums[startIndex] = nums[endIndex];
+				while (startIndex < endIndex && nums[startIndex] <= selectPivotValue) startIndex ++;
+				nums[endIndex] = nums[startIndex];
+			}
+			nums[startIndex] = selectPivotValue;
+			if (startIndex == targetIndex) {
+				isFindAnswer = true;
+				targetValue = selectPivotValue;
+			}
+			return startIndex;
+		}
 	}
 }
