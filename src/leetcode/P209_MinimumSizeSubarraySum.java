76e96b7ea57fa534c48efe080a5cdcdb88febedf
zxwtry <zxwtry@qq.com>
2016-10-14 10:24:29 +0800

    添加 leetcode P209 Minimum Size Subarray Sum  两个还是TLE Solution3 Solution4

--- src/leetcode/P209_MinimumSizeSubarraySum.java
+++ src/leetcode/P209_MinimumSizeSubarraySum.java
@@ -18,14 +18,19 @@ package leetcode;
 public class P209_MinimumSizeSubarraySum {
 	public static void main(String[] args) {
 		int[] nums = new int[] {2,3,1,2,4,3};
-		int s = 7;
+		nums = tools.Random随机生成器.A_生成一个随机数据(400, 0, 1000);
+		nums = tools.FileUtils.A_读取一维int数组_空格分割("d:/file/data/1014_leetcode209.txt");
+		int s = 120331635;
 		Solution1 s1 = new Solution1();
-		Solution2 s2 = new Solution2();
+		Solution4 s4 = new Solution4();
 		System.out.println(s1.minSubArrayLen(s, nums));
-		System.out.println(s2.minSubArrayLen(s, nums));
-//		int[] nums = tools.Random随机生成器.A_生成一个随机数据(100, 0, 1000);
-//		String s = tools.Utils.LEETCODE_int_array_序列化_(nums);
-//		System.out.println(s);
+		System.out.println(s4.minSubArrayLen(s, nums));
+//		int[] nums = tools.Random随机生成器.A_生成一个随机数据(400, 0, 1000);
+//		Arrays.sort(nums);
+//		Solution4 s4 = new Solution4();
+//		int index = s4.getIndex2(600, nums);
+//		System.out.println(nums[index - 1] + "..." + nums[index] + "..." + nums[index + 1]);
+		
 	}
 	/*
 	 * 	提示上面你说有O(N)时间复杂度的方法
@@ -82,6 +87,98 @@ public class P209_MinimumSizeSubarraySum {
 				}
 			}
 			return ans == Integer.MAX_VALUE ? 0 : ans;
+		}
+	}
+	/*
+	 * 	适当纪录一些信息，看看能不能使用
+	 * 	还是TLE
+	 */
+	static class Solution3 {
+		public int minSubArrayLen(int s, int[] nums) {
+			if (nums == null) {
+				return 0;
+			}
+			int ans = Integer.MAX_VALUE;
+			int[] recordOfAns = new int[nums.length];
+			for (int i = 0; i < nums.length; i ++) {
+				int val = s;
+				int j = i;
+				for (; j < nums.length; j ++) {
+					val -= nums[j];
+					if (val <= 0) {
+						ans = Math.min(ans, j - i + 1);
+						break;
+					}
+				}
+				if (j == nums.length) {
+					recordOfAns[i] = -1;
+					return ans == Integer.MAX_VALUE ? 0 : ans;
+				} else {
+					recordOfAns[i] = j - i + 1;
+				}
+			}
+			return ans == Integer.MAX_VALUE ? 0 : ans;
+		}
+	}
+	static class Solution4 {
+		public int minSubArrayLen(int s, int[] nums) {
+			if (nums == null || nums.length == 0) {
+				return 0;
+			}
+			int ans = Integer.MAX_VALUE;
+			int len = nums.length;
+			int[] sum = new int[len];
+			int add = 0;
+			for (int i = 0; i < len; i ++) {
+				add += nums[i];
+				if (nums[i] > s) {
+					return 1;
+				}
+				sum[i] = add;
+			}
+			if (sum[len - 1] < s) {
+				return 0;
+			}
+			int sti = getIndex1(s, sum);
+			for (int i = sti; i < sum.length; i ++) {
+				int preI = getIndex2(sum[i] - s, sum);
+				ans = Math.min(ans, i - preI);
+			}
+			return ans;
+		}
+		int getIndex1(int val, int[] sum) {
+			int sti = 0, eni = sum.length - 1;
+			if (sum[eni] < val) {
+				return eni + 1;
+			}
+			while (sti < eni) {
+				int mid = (sti + eni) / 2;
+				if (sum[mid] == val) {
+					return mid;
+				} else if (sum[mid] < val) {
+					sti = mid + 1;
+				} else {
+					eni = mid;
+				}
+			}
+			return sti;
+		}
+		int getIndex2(int val, int[] sum) {
+			int sti = 0, eni = sum.length - 1;
+			if (val < sum[sti]) {
+				return sti - 1;
+			}
+			while (sti < eni) {
+				int mid = (sti + eni + 1) / 2;
+				if (sum[mid] == val) {
+					return mid;
+				} else if (sum[mid] < val) {
+					sti = mid;
+				} else {
+					eni = mid - 1;
+				}
+			}
+			return sti;
 		}
 	}
 }
