4ac88b6aeb9e3f4967d674a3a052573065afe2e1
zxwtry <zxwtry@qq.com>
2016-10-14 17:07:58 +0800

    添加 leetcode P214 Shortest Palindrome  204 ms  34.30%  为什么会这么费时间呢？

--- src/leetcode/P214_ShortestPalindrome.java
+++ src/leetcode/P214_ShortestPalindrome.java
@@ -14,8 +14,16 @@ package leetcode;
 public class P214_ShortestPalindrome {
 	public static void main(String[] args) {
 		Solution s1 = new Solution();
-		String s = "aabba";
-		System.out.println(s1.shortestPalindrome(s));
+		Solution2 s2 = new Solution2();
+		int[] arr = tools.Random随机生成器.A_生成一个随机数据(1000, 0, 26);
+		char[] cArr = new char[arr.length];
+		for (int i = 0; i < arr.length; i ++) {
+			cArr[i] = (char) ('a' + arr[i]);
+		}
+		String s = new String(cArr);
+		String ans1 = s1.shortestPalindrome(s);
+		String ans2 = s2.shortestPalindrome(s);
+		System.out.println(ans1.equals(ans2));
 	}
 	/*
 	 * 	213 ms
@@ -50,6 +58,49 @@ public class P214_ShortestPalindrome {
 				j --;
 			}
 		}
+		private int getMaxLenOfPalindrome() {
+			for (int ans = cs.length - 1; ans > -1; ans --) {
+				if (isPalindrome(0, ans)) {
+					return ans + 1;
+				}
+			}
+			return 1;
+		}
+		private boolean isPalindrome(int i, int j) {
+			while (i < j) {
+				if (cs[i] != cs[j]) {
+					return false;
+				}
+				i ++;
+				j --;
+			}
+			return true;
+		}
+	}
+	/*
+	 * 	减少次数呗
+	 * 	204 ms
+	 * 	34.30% 
+	 */
+	static class Solution2 {
+		char[] cs = null;
+		int len = 0;
+	    public String shortestPalindrome(String s) {
+	    	if (null == s || s.length() == 0) {
+	    		return "";
+	    	}
+	    	cs = s.toCharArray();
+	    	len = cs.length;
+	    	int startIndexOfCopy = getMaxLenOfPalindrome();
+	    	System.out.println(startIndexOfCopy);
+	    	System.out.println(len);
+	    	char[] ans = new char[2 * len - startIndexOfCopy];
+	    	System.arraycopy(cs, 0, ans, len - startIndexOfCopy, cs.length);
+	    	for (int i = len - startIndexOfCopy - 1; i > -1; i --) {
+	    		ans[i] = cs[len - 1 - i];
+	    	}
+	    	return new String(ans);
+	    }
 		private int getMaxLenOfPalindrome() {
 			for (int ans = cs.length - 1; ans > -1; ans --) {
 				if (isPalindrome(0, ans)) {
