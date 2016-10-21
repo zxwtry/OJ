81fcfe63f30c59e35d934b67709f3008b3f975ec
zxwtry <zxwtry@qq.com>
2016-10-21 21:06:14 +0800

    添加 leetcode P220 Contains Duplicate III

---
+++ src/leetcode/P220_ContainsDuplicateIII.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class P220_ContainsDuplicateIII {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+		int[] maxK = null;
+		int[] minK = null;
+	    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+	    	
+	        maxK = new int[nums.length];
+	        minK = new int[nums.length];
+	        
+	        return false;
+	    }
+	}
+}
