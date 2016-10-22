d5dcd3e2e041f7c9ccc02eeb9ad00a8d3ad668f2
zxwtry <zxwtry@qq.com>
2016-10-22 12:06:32 +0800

    添加  nocoder zuo Book001_栈和队列_设计一个有getMin功能的栈  Solution2

--- src/nowcoder/zuo/Book001_栈和队列_设计一个有getMin功能的栈.java
+++ src/nowcoder/zuo/Book001_栈和队列_设计一个有getMin功能的栈.java
@@ -7,7 +7,8 @@ public class Book001_栈和队列_设计一个有getMin功能的栈 {
 		test();
 	}
 	static void test() {
-		int[] arr = tools.Random随机生成器.A_生成一个随机数据(9000, 0, 10000);
+		int[] arr = tools.Random随机生成器.A_生成一个随机数据(4, 0, 10000);
+//		arr = new int[]{4, 1, 2, 1};
 		tools.Utils.printArray(arr, 100);
 		int[] min = new int[arr.length];
 		Stack<Integer> standard = new Stack<>();
@@ -22,7 +23,7 @@ public class Book001_栈和队列_设计一个有getMin功能的栈 {
 				min[i] = Math.min(min[i - 1], arr[i]);
 			}
 		}
-		Solution1 s = new Solution1();
+		Solution2 s = new Solution2();
 		for (int val : arr) {
 			s.push(val);
 		}
@@ -47,7 +48,8 @@ public class Book001_栈和队列_设计一个有getMin功能的栈 {
 				throw new RuntimeException("Stack is Empty!");
 			}
 			//为什么stk.peek() == min.peek()就不对呢？
-			if (stk.peek() == this.getMin()) {
+//			if (stk.peek() == this.getMin()) {
+			if ((int)(this.stk.peek()) == (int)(this.min.peek())) {
 				min.pop();
 			}
 			return stk.pop();
@@ -66,8 +68,39 @@ public class Book001_栈和队列_设计一个有getMin功能的栈 {
 			}
 		}
 		public int getMin() {
-			if (min.isEmpty()) {
+			if (this.min.isEmpty()) {
 				throw new RuntimeException("Stack is Empty!");
+			}
+			return this.min.peek();
+		}
+	}
+	static class Solution2 {
+		Stack<Integer> stk = new Stack<>();
+		Stack<Integer> min = new Stack<>();
+		public void push(int val) {
+			if (stk.isEmpty()) {
+				stk.push(val);
+				min.push(val);
+			} else {
+				if (val <= min.peek()) {
+					stk.push(val);
+					min.push(val);
+				} else {
+					stk.push(val);
+					min.push(min.peek());
+				}
+			}
+		}
+		public int pop() {
+			if (stk.isEmpty()) {
+				throw new RuntimeException("Stack is Empty");
+			}
+			min.pop();
+			return stk.pop();
+		}
+		public int getMin() {
+			if (min.isEmpty()) {
+				throw new RuntimeException("Stack is Empty");
 			}
 			return min.peek();
 		}
