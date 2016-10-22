626bd3ad031e8f047ecafbc8a16c102eaa4ad682
zxwtry <zxwtry@qq.com>
2016-10-22 22:15:30 +0800

    添加 nowcoder Book010_最大值减去最小值小于或等于num的子数组数量

---
+++ src/nowcoder/zuo/Book010_最大值减去最小值小于或等于num的子数组数量.java
@@ -0,0 +1,13 @@
+package nowcoder.zuo;
+
+public class Book010_最大值减去最小值小于或等于num的子数组数量 {
+	static class Solution {
+		public int getNum(int[] arr, int num) {
+			if (arr == null || arr.length == 0) {
+				return 0;
+			}
+			int res = 0;
+			return res;
+		}
+	}
+}
