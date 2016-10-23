31573c551eabf80bdbf01a0f686bbbb669ead174
zxwtry <zxwtry@qq.com>
2016-10-23 18:23:20 +0800

    添加 nowcoder zuo Book003_仅用递归函数和栈操作逆序一个栈

--- src/nowcoder/zuo/Book003_仅用递归函数和栈操作逆序一个栈.java
+++ src/nowcoder/zuo/Book003_仅用递归函数和栈操作逆序一个栈.java
@@ -5,6 +5,16 @@ import java.util.Stack;
 public class Book003_仅用递归函数和栈操作逆序一个栈 {
 	public static void main(String[] args) {
 		test();
+//		debug();
+	}
+	static void debug() {
+		Solution1 s = new Solution1();
+		int[] arr = {1, 2, 3, 4, 5};
+		Stack<Integer> stack = new Stack<>();
+		for (int val : arr) {
+			stack.push(val);
+		}
+		System.out.println(s.getAndRemoveStackBottom(stack));
 	}
 	static void test() {
 		int[] arr = tools.Random随机生成器.A_生成一个随机数据(10, 0, 20);
@@ -13,7 +23,7 @@ public class Book003_仅用递归函数和栈操作逆序一个栈 {
 			stack.push(val);
 		}
 		Solution1 s = new Solution1();
-		s.reverseStack(stack);
+		s.reverse(stack);
 		boolean isAllTrue = true;
 		for (int val : arr) {
 			int pop = stack.pop();
@@ -25,9 +35,24 @@ public class Book003_仅用递归函数和栈操作逆序一个栈 {
 	}
 	static class Solution1 {
 
-		public void reverseStack(Stack<Integer> stack) {
-			
+		//外部保证stack不是Empty
+		int getAndRemoveStackBottom(Stack<Integer> stack) {
+			int stackValue = stack.pop();
+			if (stack.isEmpty()) {
+				return stackValue;
+			} else {
+				int last = getAndRemoveStackBottom(stack);
+				stack.push(stackValue);
+				return last;
+			}
 		}
 		
+		void reverse(Stack<Integer> stack) {
+			if (! stack.isEmpty()) {
+				int i = getAndRemoveStackBottom(stack);
+				reverse(stack);
+				stack.push(i);
+			}
+		}
 	}
 }
