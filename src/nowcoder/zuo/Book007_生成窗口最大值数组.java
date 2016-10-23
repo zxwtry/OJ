8c47905f3847859fed013c98e756e32d3f169b1a
zxwtry <zxwtry@qq.com>
2016-10-23 21:24:17 +0800

    添加 nowcoder zuo Book007_生成窗口最大值数组  添加测试

--- src/nowcoder/zuo/Book007_生成窗口最大值数组.java
+++ src/nowcoder/zuo/Book007_生成窗口最大值数组.java
@@ -3,10 +3,52 @@ package nowcoder.zuo;
 import java.util.LinkedList;
 
 public class Book007_生成窗口最大值数组 {
+	public static void main(String[] args) {
+//		debugSolution();
+		test();
+	}
+	static void test() {
+		int len = 100;
+		int max = 1000;
+		int min = 0;
+		int[] arr = tools.Random随机生成器.A_生成一个随机数据(len, min, max);
+		int w = 0;
+		boolean isAllTrue = true;
+		for (; w <= arr.length; w ++) {
+			int[] standardAns = standardAnswer(arr, w);
+			Solution s = new Solution();
+			int[] solutionAns = s.getMaxWindow(arr, w);
+			boolean isTrue = true;
+			for (int i = 0; i < solutionAns.length; i ++) {
+				isTrue &= standardAns[i] == solutionAns[i];
+			}
+//			System.out.println(isTrue);
+			isAllTrue &= isTrue;
+		}
+		System.out.println(isAllTrue);
+	}
+	private static int[] standardAnswer(int[] arr, int w) {
+		int[] ans = new int[arr.length - w + 1];
+		for (int i = 0; i < ans.length; i ++) {
+			int max = Integer.MIN_VALUE;
+			for (int j = 0; j < w; j ++) {
+				max = Math.max(max, arr[i + j]);
+			}
+			ans[i] = max;
+		}
+		return ans;
+	}
+	static void debugSolution() {
+		int[] arr = new int[]{4, 3, 5, 4, 3, 3, 6, 7};
+		int w = 3;
+		Solution s = new Solution();
+		int[] ans = s.getMaxWindow(arr, w);
+		tools.Utils.printArray(ans, 100);
+	}
 	static class Solution {
 		public int[] getMaxWindow(int[] arr, int w) {
 			if (arr == null || w < 1 || arr.length < w) {
-				return null;
+				return new int[]{};
 			}
 			LinkedList<Integer> qmax = new LinkedList<>();
 			int[] res = new int[arr.length - w + 1];
