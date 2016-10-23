46cbd4337861cdf6feb2cb694e478b2d609ab656
zxwtry <zxwtry@qq.com>
2016-10-23 21:13:40 +0800

    添加 nowcoder zuo Book006_用栈来求解汉诺塔问题  SolutionNoneRecur

--- src/nowcoder/zuo/Book006_用栈来求解汉诺塔问题.java
+++ src/nowcoder/zuo/Book006_用栈来求解汉诺塔问题.java
@@ -1,5 +1,8 @@
 package nowcoder.zuo;
 
+import java.util.Stack;
+
+
 public class Book006_用栈来求解汉诺塔问题 {
 	public static void main(String[] args) {
 		String left = "左";
@@ -7,7 +10,13 @@ public class Book006_用栈来求解汉诺塔问题 {
 		String right = "右";
 		int num = 3;
 //		debugStandardSolution(num, left, mid, right);
-		debugSolution(num, left, mid, right);
+//		debugSolution(num, left, mid, right);
+		debugSolutionNoneRecur(num, left, mid, right);
+	}
+	static void debugSolutionNoneRecur(int num, String left, String mid, String right) {
+		SolutionNoneRecur s = new SolutionNoneRecur();
+		int hanoi = s.hanoi(num, left, mid, right);
+		System.out.println("hanoi : " + hanoi);
 	}
 	static void debugSolution(int num, String left, String mid, String right) {
 		Solution s = new Solution();
@@ -61,6 +70,53 @@ public class Book006_用栈来求解汉诺塔问题 {
 				}
 			}
 		}
+	}
+	/*
+	 * 	修改游戏规则， 
+	 * 		现在限制，不能从最左边的塔直接移动到最右边的塔
+	 * 			同时，不能从最右边的塔直接移动到最左边的塔
+	 * 		同时不能使用递归来做
+	 */
+	static class SolutionNoneRecur {
+		public enum Action {
+			NO, LToM, MToL, MToR, RToM
+		}
+		
+		public int hanoi(int num, String left, String mid, String right) {
+			if (num < 1) {
+				return 0;
+			}
+			Stack<Integer> lS = new Stack<>();
+			Stack<Integer> mS = new Stack<>();
+			Stack<Integer> rS = new Stack<>();
+			lS.push(Integer.MAX_VALUE);
+			mS.push(Integer.MAX_VALUE);
+			rS.push(Integer.MAX_VALUE);
+			for (int i = num; i > 0; i --) {
+				lS.push(i);
+			}
+			Action[] record = {Action.NO}; 
+			int step = 0;
+			while(rS.size() != num + 1) {
+				step += fStackToStack(record, Action.MToL, Action.LToM, lS, mS, left, mid);
+				step += fStackToStack(record, Action.LToM, Action.MToL, mS, lS, mid, left);
+				step += fStackToStack(record, Action.RToM, Action.MToR, mS, rS, mid, right);
+				step += fStackToStack(record, Action.MToR, Action.RToM, rS, mS, right, mid);
+			}
+			return step;
+		}
+
+		private int fStackToStack(Action[] record, Action preNoAct, Action nowAct, Stack<Integer> fS, Stack<Integer> tS,
+				String from, String to) {
+			if (record[0] != preNoAct && fS.peek() < tS.peek()) {
+				tS.push(fS.pop());
+				System.out.printf("Move %d from %s to %s\r\n", tS.peek(), from , to);
+				record[0] = nowAct;
+				return 1;
+			}
+			return 0;
+		}
+
 	}
 	//标准Hanoi问题
 	static class StandardSolution {
