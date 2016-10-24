0e662d949c7044134f86dac84ce333e0cd73901f
zxwtry <zxwtry@qq.com>
2016-10-24 22:07:24 +0800

    添加 tools.DoubleNode辅助.A_双向链表生成器(int[] arr)

--- src/tools/DoubleNode辅助.java
+++ src/tools/DoubleNode辅助.java
@@ -9,4 +9,18 @@ public class DoubleNode辅助 {
 			this.val = val;
 		}
 	}
+	public static DoubleNode A_双向链表生成器(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return null;
+		}
+		DoubleNode head = new DoubleNode(arr[0]);
+		DoubleNode preHead = head;
+		for (int i = 1; i < arr.length; i ++) {
+			DoubleNode nowNode = new DoubleNode(arr[i]);
+			preHead.next = nowNode;
+			nowNode.last = preHead;
+			preHead = nowNode;
+		}
+		return head;
+	}
 }
