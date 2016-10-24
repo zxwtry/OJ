a23a150de57b0ce3c1926f82a6e20473c6a743b9
zxwtry <zxwtry@qq.com>
2016-10-24 22:15:52 +0800

    添加 nowcoder zuo Book012_在单链表和双链表中删除倒数第K个节点 双向链表版本

--- src/nowcoder/zuo/Book012_在单链表和双链表中删除倒数第K个节点.java
+++ src/nowcoder/zuo/Book012_在单链表和双链表中删除倒数第K个节点.java
@@ -1,5 +1,6 @@
 package nowcoder.zuo;
 
+import tools.DoubleNode辅助.DoubleNode;
 import tools.ListNode辅助.ListNode;
 
 public class Book012_在单链表和双链表中删除倒数第K个节点 {
@@ -25,6 +26,34 @@ public class Book012_在单链表和双链表中删除倒数第K个节点 {
 					cur = cur.next;
 				}
 				cur.next = cur.next.next;
+			}
+			return head;
+		}
+	}
+	static class SolutionDoubleList {
+		public DoubleNode removeLastKthNode(DoubleNode head, int lastKth) {
+			if (head == null || lastKth < 1) {
+				return head;
+			}
+			DoubleNode cur = head;
+			while (cur != null) {
+				lastKth --;
+				cur = cur.next;
+			}
+			if (lastKth == 0) {
+				head = head.next;
+				head.last = null;
+			}
+			if (lastKth < 0) {
+				cur = head;
+				while (++ lastKth != 0) {
+					cur = cur.next;
+				}
+				DoubleNode newNext = cur.next.next;
+				cur.next = newNext;
+				if (newNext != null) {
+					newNext.last = cur;
+				}
 			}
 			return head;
 		}
