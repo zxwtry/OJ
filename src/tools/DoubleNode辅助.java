2fb2772fee92d5845a6c356d6f04ae202f4afb83
zxwtry <zxwtry@qq.com>
2016-10-24 22:04:10 +0800

    添加 tools.DoubleNode辅助.DoubleNode

---
+++ src/tools/DoubleNode辅助.java
@@ -0,0 +1,12 @@
+package tools;
+
+public class DoubleNode辅助 {
+	public static class DoubleNode {
+		public int val;
+		public DoubleNode last;
+		public DoubleNode next;
+		public DoubleNode(int val) {
+			this.val = val;
+		}
+	}
+}
