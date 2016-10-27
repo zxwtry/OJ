19f240accaf1148b6dda3b21a264c375ff7d9e60
zxwtry <zxwtry@qq.com>
2016-10-27 12:13:06 +0800

    添加 nowcoder zuo Book017_将单向链表按某值划分成左边小_中间相等_右边大的形式
    添加要求，三个小部分按照原来的顺序排列，时间O(N)  空间O(1)

--- src/nowcoder/zuo/Book017_将单向链表按某值划分成左边小_中间相等_右边大的形式.java
+++ src/nowcoder/zuo/Book017_将单向链表按某值划分成左边小_中间相等_右边大的形式.java
@@ -5,6 +5,33 @@ import tools.ListNode辅助.ListNode;
 public class Book017_将单向链表按某值划分成左边小_中间相等_右边大的形式 {
 	public static void main(String[] args) {
 //		testSolution();
+		testAdvanSolution();
+	}
+	static void testAdvanSolution() {
+		int n = (int) (Math.random() * 30);
+		int min = 0;
+		int max = n * n * n + 1;
+		int[] arr = tools.Random随机生成器.A_生成一个随机数据(n, min, max);
+		tools.Utils.printArray(arr, 100);
+		AdvanceSolution s = new AdvanceSolution();
+		int pivot = max / 2;
+		ListNode head = tools.ListNode辅助.A_一维生成器(arr);
+		ListNode ans = s.listPartition1(head, pivot);
+		ListNode ansSave = ans;
+		int value = -2;
+		boolean isTrue = true;
+		while (ans != null && isTrue) {
+			int newValue = getValue(ans.val, pivot);
+			if (value > newValue) {
+				isTrue = false;
+			} else {
+				value = newValue;
+				ans = ans.next;
+			}
+		}
+		System.out.println("pivot : " + pivot);
+		tools.ListNode辅助.B_打印链表_一行(ansSave, 100);
+		System.out.println(isTrue);
 	}
 	static void testSolution() {
 		int n = (int) (Math.random() * 10000);
@@ -89,6 +116,66 @@ public class Book017_将单向链表按某值划分成左边小_中间相等_右
 			ListNode temp = nodeArr[i];
 			nodeArr[i] = nodeArr[j];
 			nodeArr[j] = temp;
+		}
+	}
+	/*
+	 * 	在Solution的基础上，增加如下两个要求：
+	 * 		*	在左、中、右三个部分的内部也做顺序要求，
+	 * 			要求每部分里的节点从左到右的顺序与原链表中节点的先后次序一致。
+	 * 		*	时间O(N) 空间O(1)
+	 * 	假设一个链表9->0->4->5->1，pivot=3
+	 * 	现在答案只有一个：
+	 * 		0->1->9->4->5
+	 */
+	static class AdvanceSolution {
+		public ListNode listPartition1(ListNode head, int pivot) {
+			ListNode sH = null;
+			ListNode sT = null;
+			ListNode eH = null;
+			ListNode eT = null;
+			ListNode bH = null;
+			ListNode bT = null;
+			ListNode next = null;
+			while (head != null) {
+				next = head.next;
+				head.next = null;
+				if (head.val < pivot) {
+					if (sH == null) {
+						sH = head;
+						sT = head;
+					} else {
+						sT.next = head;
+						sT = head;
+					}
+				} else if (head.val == pivot) {
+					if (eH == null) {
+						eH = head;
+						eT = head;
+					} else {
+						eT.next = head;
+						eT = head;
+					}
+				} else {
+					if (bH == null) {
+						bH = head;
+						bT = head;
+					} else {
+						bT.next = head;
+						bT = head;
+					}
+				}
+				head = next;
+			}
+			//小的和相等的重新连接
+			if (sT != null) {
+				sT.next = eH;
+				eT = eT == null ? sT : eT;
+			}
+			//所有的重新连接
+			if (eT != null) {
+				eT.next = bH;
+			}
+			return sH != null ? sH : (eH != null ? eH : bH);
 		}
 	}
 }
