81b0220c571ad8ec9731c12f3723e1cc900834e1
zxwtry <zxwtry@qq.com>
2016-10-27 10:51:12 +0800

    添加 nowcoder zuo Book016_判断一个链表是否是回文结构  BookSolution

--- src/nowcoder/zuo/Book016_判断一个链表是否是回文结构.java
+++ src/nowcoder/zuo/Book016_判断一个链表是否是回文结构.java
@@ -4,9 +4,24 @@ import tools.ListNode辅助.ListNode;
 
 public class Book016_判断一个链表是否是回文结构 {
 	public static void main(String[] args) {
-		testSolution();
+//		testSolution();
+		testBookSolution();
 	}
-	private static void testSolution() {
+	static void testBookSolution() {
+		int n = (int)(Math.random() * 1000);
+		int[] arr = tools.Random随机生成器.A_生成一个不重复随机数据(n, 0, n * n * n + 1);
+		int[] arrGhost = arr.clone();
+		reverse(arrGhost);
+		int[] arrReal = new int[arr.length * 2];
+		System.arraycopy(arr, 0, arrReal, 0, arr.length);
+		System.arraycopy(arrGhost, 0, arrReal, arr.length, arr.length);
+//		arrReal[0] = 0;
+		System.out.println(new StanrdSolution().judge(arrReal));
+		ListNode head = tools.ListNode辅助.A_一维生成器(arrReal);
+		BookSolution bookSolution = new BookSolution();
+		System.out.println(bookSolution.isPalindrome(head));
+	}
+	static void testSolution() {
 		int n = (int)(Math.random() * 1000);
 		int[] arr = tools.Random随机生成器.A_生成一个不重复随机数据(n, 0, n * n * n + 1);
 		int[] arrGhost = arr.clone();
@@ -69,6 +84,53 @@ public class Book016_判断一个链表是否是回文结构 {
 				travel = travel.next;
 			}
 			return !isFalse;
+		}
+	}
+	/*
+	 * 	这里有两个优化：
+	 * 		1,	查找中间节点，是两个指针的方法。
+	 * 		2,	逆序后半段链表来进行优化
+	 */
+	static class BookSolution {
+		public boolean isPalindrome(ListNode head) {
+			if (head == null || head.next == null) {
+				return true;
+			}
+			ListNode n1 = head;
+			ListNode n2 = head;
+			while (n2.next != null && n2.next.next != null) {
+				n1 = n1.next;
+				n2 = n2.next.next;
+			}
+			n2 = n1.next;
+			n1.next = null;
+			ListNode n3 = null;
+			while (n2 != null) {
+				n3 = n2.next;
+				n2.next = n1;
+				n1 = n2;
+				n2 = n3;
+			}
+			n3 = n1;
+			n2 = head;
+			boolean res = true;
+			while (n1 != null && n2 != null && res) {
+				if (n1.val != n2.val) {
+					res = false;
+				} else {
+					n1 = n1.next;
+					n2 = n2.next;
+				}
+			}
+			n1 = n3.next;
+			n3.next = null;
+			while (n1 != null) {
+				n2 = n1.next;
+				n1.next = n3;
+				n3 = n1;
+				n1 = n2;
+			}
+			return res;
 		}
 	}
 }
