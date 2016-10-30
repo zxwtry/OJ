5171c4b527436a33855e87cba73cc98ed61089c3
zxwtry <zxwtry@qq.com>
2016-10-30 11:21:07 +0800

    添加 nowcoder zuo Book024_将搜索二叉树转换称为双向链表  RecursionSolution O(N) O(h)

--- src/nowcoder/zuo/Book024_将搜索二叉树转换称为双向链表.java
+++ src/nowcoder/zuo/Book024_将搜索二叉树转换称为双向链表.java
@@ -44,4 +44,47 @@ public class Book024_将搜索二叉树转换称为双向链表 {
 			inOrderToQueue(head.right, queue);
 		}
 	}
+	//时间复杂度O(N)，额外空间复杂度O(h)，h为二叉树的高度
+	static class RecursionSolution {
+		public TreeNode convert(TreeNode head) {
+			if (head == null) {
+				return null;
+			}
+			TreeNode last = process(head);
+			head = last.right;
+			last.right = null;
+			return head;
+		}
+
+		private TreeNode process(TreeNode head) {
+			if (head == null) {
+				return null;
+			}
+			TreeNode leftE = process(head.left);	//左边结束
+			TreeNode rightE = process(head.right);	//右边结束
+			TreeNode leftS = leftE != null ? leftE.right : null;	//左边开始
+			TreeNode rightS = rightE != null ? rightE.right : null;	//右边开始
+			if (leftE != null && rightE != null) {
+				leftE.right = head;
+				head.left = leftE;
+				head.right = rightS;
+				rightS.left = head;
+				rightE.right = leftS;
+				return rightE;
+			} else if (leftE != null) {
+				leftE.right = head;
+				head.left = rightE;
+				head.right = leftS;
+				return head;
+			} else if (rightE != null) {
+				head.right = rightS;
+				rightS.left = head;
+				rightE.right = head;
+				return rightE;
+			} else {
+				head.right = head;
+				return head;
+			}
+		}
+	}
 }
