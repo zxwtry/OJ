9b9bc23c068ae04e162022f850f6727d171244db
zxwtry <zxwtry@qq.com>
2016-10-31 16:54:12 +0800

    添加 nowcoder zuo Book030_分别用递归和非递归方式实现二叉树先序_中序_后序的遍历  非递归方法 NoneRecursionSolution

--- src/nowcoder/zuo/Book030_分别用递归和非递归方式实现二叉树先序_中序_后序的遍历.java
+++ src/nowcoder/zuo/Book030_分别用递归和非递归方式实现二叉树先序_中序_后序的遍历.java
@@ -1,5 +1,7 @@
 package nowcoder.zuo;
 
+import java.util.Stack;
+
 import tools.TreeNode辅助.TreeNode;
 
 public class Book030_分别用递归和非递归方式实现二叉树先序_中序_后序的遍历 {
@@ -30,6 +32,83 @@ public class Book030_分别用递归和非递归方式实现二叉树先序_中�
 			posOrder(head.left);
 			posOrder(head.right);
 			System.out.print(head.val + " ");
+		}
+	}
+	static class NoneRecursionSolution {
+		public void preOrder(TreeNode head) {
+			System.out.print("pre-order: ");
+			if (head != null) {
+				Stack<TreeNode> stack = new Stack<TreeNode>();
+				stack.add(head);
+				while (! stack.isEmpty()) {
+					head = stack.pop();
+					System.out.println(head.val + " ");
+					if (head.right != null) {
+						stack.push(head.right);
+					}
+					if (head.left != null) {
+						stack.push(head.left);
+					}
+				}
+			}
+		}
+		public void inOrder(TreeNode head) {
+			System.out.println("in-order: ");
+			if (head != null) {
+				Stack<TreeNode> stack = new Stack<TreeNode>();
+				while (! stack.isEmpty()) {
+					if (head != null) {
+						stack.push(head);
+						head = head.left;
+					} else {
+						head = stack.pop();
+						System.out.println(head.val + " ");
+						head = head.right;
+					}
+				}
+			}
+			System.out.println();
+		}
+		public void posOrder(TreeNode head) {
+			System.out.println("pos-order: ");
+			if (head != null) {
+				Stack<TreeNode> s1 = new Stack<TreeNode>();
+				Stack<TreeNode> s2 = new Stack<TreeNode>();
+				s1.push(head);
+				while (! s1.isEmpty()) {
+					head = s1.pop();
+					s2.push(head);
+					if (head.left != null) {
+						s1.push(head.left);
+					}
+					if (head.right != null) {
+						s1.push(head.right);
+					}
+				}
+				while (! s2.isEmpty()) {
+					System.out.print(s2.pop().val + " ");
+				}
+			}
+			System.out.println();
+		}
+		public void posOrder_OneStack(TreeNode head) {
+			System.out.println("pos-order: ");
+			if (head != null) {
+				Stack<TreeNode> stack = new Stack<>();
+				stack.push(head);
+				TreeNode curHead = null;
+				while (! stack.isEmpty()) {
+					curHead = stack.peek();
+					if (curHead.left == null && head!= curHead.left && head != curHead.right) {
+						stack.push(curHead.left);
+					} else if (curHead.right != null && head != curHead.right) {
+						stack.push(curHead.left);
+					} else {
+						System.out.print(stack.pop().val + " ");
+						head = curHead;
+					}
+				}
+			}
 		}
 	}
 }
