ae449dfc4e3454b425104b4bfc44a51b0e7f3913
zxwtry <zxwtry@qq.com>
2016-10-31 16:05:16 +0800

    添加 nowcoder zuo Book028_合并两个有序的单链表  MySolution O(N) O(1)

--- src/nowcoder/zuo/Book028_合并两个有序的单链表.java
+++ src/nowcoder/zuo/Book028_合并两个有序的单链表.java
@@ -32,4 +32,32 @@ public class Book028_合并两个有序的单链表 {
 			return head;
 		}
 	}
+	static class MySolution {
+		public ListNode merge(ListNode head1, ListNode head2) {
+			if (head1 == null || head2 == null) {
+				return head1 == null ? head2 : head1;
+			}
+			if (head1.val > head2.val) {
+				return merge(head2, head1);
+			}
+			ListNode cur1 = head1;
+			ListNode cur2 = head2;
+			ListNode pre = null;
+			ListNode next = null;
+			while (cur1 != null && cur2 != null) {
+				if (cur1.val <= cur2.val) {
+					pre = cur1;
+					cur1 = cur1.next;
+				} else {
+					next = cur2.next;
+					pre.next = cur2;
+					cur2.next = cur1;
+					pre = cur2;
+					cur2 = next;
+				}
+			}
+			pre.next = cur1 == null ? cur2 : cur1;
+			return head1;
+		}
+	}
 }
