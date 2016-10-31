920a5f4247fe0b1f66a6aba7bd7179d17135dd57
zxwtry <zxwtry@qq.com>
2016-10-31 16:13:52 +0800

    添加 nowcoder zuo Book029_按照左右半区的方式重新组合单链表 RelocateSolution O(N) O(1)

---
+++ src/nowcoder/zuo/Book029_按照左右半区的方式重新组合单链表.java
@@ -0,0 +1,40 @@
+package nowcoder.zuo;
+
+import tools.ListNode辅助.ListNode;
+
+/*
+ * 	前N/2算作左半区，其余算作右半区
+ */
+
+public class Book029_按照左右半区的方式重新组合单链表 {
+	public static void main(String[] args) {
+		
+	}
+	static class RelocateSolution {
+		public void relocate(ListNode head) {
+			if (null == head || head.next == null) {
+				return;
+			}
+			ListNode mid = head;
+			ListNode right = head.next;
+			while (right.next != null && right.next.next != null) {
+				mid = mid.next;
+				right = right.next.next;
+			}
+			right = mid.next;
+			mid.next = null;
+			mergeLR(head, right);
+		}
+		private void mergeLR(ListNode left, ListNode right) {
+			ListNode next = null;
+			while (left.next != null) {
+				next = right.next;
+				right.next = left.next;
+				left.next = right;
+				left = right.next;
+				right = next;
+			}
+			left.next = right;
+		}
+	}
+}
