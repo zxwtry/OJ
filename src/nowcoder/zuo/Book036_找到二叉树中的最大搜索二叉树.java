6ad86dbd810b8488b789e3bb5ebe3ca5101894d2
zxwtry <zxwtry@qq.com>
2016-11-02 23:20:39 +0800

    添加  nowcoder zuo Book036_找到二叉树中的最大搜索二叉树  test

--- src/nowcoder/zuo/Book036_找到二叉树中的最大搜索二叉树.java
+++ src/nowcoder/zuo/Book036_找到二叉树中的最大搜索二叉树.java
@@ -4,7 +4,20 @@ import tools.TreeNode辅助.TreeNode;
 
 public class Book036_找到二叉树中的最大搜索二叉树 {
 	public static void main(String[] args) {
-		
+		test();
+	}
+	private static void test() {
+		int N = Integer.MIN_VALUE;
+		int[] arr = new int[] {
+			1,
+			2, 3,
+			1, 2, 3, 4,
+			N, N, N, N, N, N, N, N
+		};
+		TreeNode head = tools.TreeNode辅助.A_生成满二叉树(arr);
+		Solution s = new Solution();
+		TreeNode ans = s.biggestSubBST(head);
+		tools.TreeNode辅助.B_按层打印(ans);
 	}
 	//O(N) O(h)
 	static class Solution {
@@ -30,7 +43,7 @@ public class Book036_找到二叉树中的最大搜索二叉树 {
 			int rSize = record[0];
 			int rMin = record[1];
 			int rMax = record[2];
-			record[1] = Math.min(lMax, val);
+			record[1] = Math.min(lMin, val);
 			record[2] = Math.max(rMax, val);
 			if (left == lBST && right == rBST && lMax < val && val < rMin) {
 				record[0] = lSize + rSize + 1;
