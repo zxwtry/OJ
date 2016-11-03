8225286605b47b01f0405256d4acad3782980bd2
zxwtry <zxwtry@qq.com>
2016-11-03 23:32:15 +0800

    添加 nowcoder zuo Book027_向有序的环形单链表里面插入新节点 test

--- src/nowcoder/zuo/Book027_向有序的环形单链表里面插入新节点.java
+++ src/nowcoder/zuo/Book027_向有序的环形单链表里面插入新节点.java
@@ -4,7 +4,18 @@ import tools.ListNode辅助.ListNode;
 
 class Book027_向有序的环形单链表里面插入新节点 {
 	public static void main(String[] args) {
-		
+		test();
+	}
+	static void test() {
+		int n = 10;
+		int min = 0;
+		int max = 100;
+		ListNode head = tools.ListNode辅助.A_一维生成器(tools.Random随机生成器.A_生成一个不重复随机数据(n, min, max));
+		int num = (int)(Math.random() * (max - min + 1));
+		InsertSolution is = new InsertSolution();
+		ListNode newHead = is.insert(head, num);
+		int sizeOfLine = n + 100;
+		tools.ListNode辅助.B_打印链表_一行(newHead, sizeOfLine);
 	}
 	static class InsertSolution {
 		public ListNode insert(ListNode head, int num) {
