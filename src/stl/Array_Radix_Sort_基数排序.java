025a27dc765af7fd7bce72236b78e211dd665e23
zxwtry <zxwtry@qq.com>
2016-11-04 19:32:31 +0800

    删除 stl Array_Radix_Sort_基数排序    部分冗余代码

--- src/stl/Array_Radix_Sort_基数排序.java
+++ src/stl/Array_Radix_Sort_基数排序.java
@@ -1,11 +1,6 @@
 package stl;
 
 public class Array_Radix_Sort_基数排序 {
-	public static void main(String[] args) {
-		int[] arr = tools.Random随机生成器.A_生成一个随机数据(20, 0, 10000);
-		radixSort_noneNegative(arr);
-		tools.Utils.printArray(arr, 100);
-	}
 	static final int LEN_BITS = 4;
 	static final int LEN = 1 << LEN_BITS;		//设定基数排序总共设置多少个组，为了避免过多乘除，使用2^4=16
 	//先比较低位，后比较高位
