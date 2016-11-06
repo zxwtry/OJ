34f0208f47976ca2590114713745c51520ecbd35
zxwtry <zxwtry@qq.com>
2016-11-06 20:18:12 +0800

    添加 nowcoder zuo Book038_二叉树的按层打印与ZigZag打印  testBookSolutionLevel

--- src/nowcoder/zuo/Book038_二叉树的按层打印与ZigZag打印.java
+++ src/nowcoder/zuo/Book038_二叉树的按层打印与ZigZag打印.java
@@ -7,7 +7,18 @@ import tools.TreeNode辅助.TreeNode;
 
 public class Book038_二叉树的按层打印与ZigZag打印 {
 	public static void main(String[] args) {
+		testBookSolutionLevel();
 		
+	}
+	static void testBookSolutionLevel() {
+		TreeNode head = tools.TreeNode辅助.A_生成满二叉树(new int[] {
+			1,
+			2, 3,
+			4, 5, 6, 7,
+			8, 9, 10, 11, 12, 13, 14, 15
+		});
+		BookSolutionLevel bsl = new BookSolutionLevel();
+		bsl.printByLevel(head);
 	}
 	static class BookSolutionLevel {
 		public void printByLevel(TreeNode head) {
