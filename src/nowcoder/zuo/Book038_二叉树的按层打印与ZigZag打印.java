9fde7c421d3187dbc5932da8559710ebbc709966
zxwtry <zxwtry@qq.com>
2016-11-06 21:18:11 +0800

    添加 nowcoder zuo Book038_二叉树的按层打印与ZigZag打印 BookSolutionZigZag

--- src/nowcoder/zuo/Book038_二叉树的按层打印与ZigZag打印.java
+++ src/nowcoder/zuo/Book038_二叉树的按层打印与ZigZag打印.java
@@ -1,9 +1,19 @@
 package nowcoder.zuo;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import org.w3c.dom.ls.LSException;
+
+import com.sun.istack.internal.localization.NullLocalizable;
+
 import tools.TreeNode辅助.TreeNode;
+
+/*
+ * 	 所谓ZigZag就是，
+ * 	1	3	2	4	5	6	7	15	14	13	12	11	10	9	8
+ */
 
 public class Book038_二叉树的按层打印与ZigZag打印 {
 	public static void main(String[] args) {
@@ -48,6 +58,56 @@ public class Book038_二叉树的按层打印与ZigZag打印 {
 				}
 			}
 			System.out.println();
+		}
+	}
+	static class BookSolutionZigZag {
+		public void printByZigZag(TreeNode head) {
+			if (head == null) {
+				return;
+			}
+			Deque<TreeNode> dq = new LinkedList<TreeNode>();
+			int level = 1;
+			boolean lr = true;
+			TreeNode last = head;
+			TreeNode nLast = null;
+			dq.offerFirst(head);
+			printLevelAndOrientation(level ++, lr);
+			while (! dq.isEmpty()) {
+				if (lr) {
+					head = dq.pollFirst();
+					if (head.left != null) {
+						nLast = nLast == null ? head.left : nLast;
+						dq.offerLast(head.left);
+					}
+					if (head.right != null) {
+						nLast = nLast == null ? head.right : nLast;
+						dq.offerLast(head.right);
+					}
+				} else {
+					head = dq.pollLast();
+					if (head.right == null) {
+						nLast = nLast == null ? head.right : nLast;
+						dq.offerFirst(head.right);
+					}
+					if (head.left == null) {
+						nLast = nLast == null ? head.left : nLast;
+						dq.offerFirst(head.left);
+					}
+				}
+				System.out.print(head.val + " ");
+				if (head == last && ! dq.isEmpty()) {
+					lr = !lr;
+					last = nLast;
+					nLast = null;
+					System.out.println();
+					printLevelAndOrientation(level ++, lr);
+				}
+			}
+			System.out.println();
+		}
+		void printLevelAndOrientation(int level, boolean lr) {
+			System.out.print("Level " + level + " from ");
+			System.out.print(lr ? "left to right: " : "right to left: ");
 		}
 	}
 }
