eca0e7d3fb07975353fdd1f2b84392c4d63a6fcd
zxwtry <zxwtry@qq.com>
2016-11-09 21:48:41 +0800

    添加 nowcoder com 百度17_度度熊找子串 solve2  时间O(N)  空间O(1)

--- src/nowcoder/com/百度17_度度熊找子串.java
+++ src/nowcoder/com/百度17_度度熊找子串.java
@@ -15,6 +15,35 @@ import java.util.Scanner;
 public class 百度17_度度熊找子串 {
 	public static void main(String[] args) {
 //		solve1();
+		solve2();
+	}
+	//时间O(N) 空间O(1)
+	static void solve2() {
+		Scanner scan = new Scanner(System.in);
+		String line = scan.nextLine();
+		if (line == null || line.length() == 0) {
+			System.out.println(0);
+		} else {
+			int pre = 1;
+			int[] map = new int[26];
+			for (int i = 1; i <= line.length(); i ++) {
+				if (i != line.length() && line.charAt(i) == line.charAt(i - 1)) {
+					pre = pre + 1;
+				} else {
+					int mapIndex = line.charAt(i - 1) - 'a';
+					map[mapIndex] = Math.max(map[mapIndex], pre);
+					if (i != line.length()) {
+						pre = 1;
+					}
+				}
+			}
+			long ans = 0;
+			for (int val : map) {
+				ans += val;
+			}
+			System.out.println(ans);
+		}
+		scan.close();
 	}
 	//时间O(N) 空间O(N)
 	static void solve1() {
