217cd86f84bda29a704ab5466a4576d073b35d5a
zxwtry <zxwtry@qq.com>
2016-11-10 22:28:00 +0800

    添加 leetcode P231_PowerOfTwo

---
+++ src/leetcode/P231_PowerOfTwo.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/*
+ * 	Given an integer, write a function to determine if it is a power of two.
+
+	Credits:
+	Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+ */
+
+public class P231_PowerOfTwo {
+
+}
