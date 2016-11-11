dac7527a76a63cc60af76aa766455878be24c05f
zxwtry <zxwtry@qq.com>
2016-11-11 18:19:36 +0800

    添加 tools.Complex辅助

---
+++ src/tools/Complex辅助.java
@@ -0,0 +1,39 @@
+package tools;
+
+public class Complex辅助 {
+	public static class Complex {
+		public double real;
+		public double imag;
+		public Complex() {
+			this.real = 0;
+			this.imag = 0;
+		}
+		public Complex(double real, double imag) {
+			this.real = real;
+			this.imag = imag;
+		}
+		@Override
+		public String toString() {
+			if (this.imag < 0) {
+				return this.real +""+this.imag+"i";
+			} else {
+				return this.real +"+"+this.imag+"i";
+			}
+		}
+	}
+	public static Complex add(Complex c1, Complex c2) {
+		return new Complex(c1.real + c2.real, c1.imag + c2.imag);
+	}
+	public static Complex minus(Complex c1, Complex c2) {
+		return new Complex(c1.real - c2.real, c1.imag - c2.imag);
+	}
+	public static Complex multiply(Complex c1, Complex c2) {
+		double real = c1.real * c2.real - c1.imag * c2.imag;
+		double imag = c1.real * c2.imag + c1.imag * c2.real;
+		return new Complex(real, imag);
+	}
+	public static Complex divide(Complex c1, Complex c2) {
+		return multiply(new Complex(1/(c2.real * c2.real + c2.imag * c2.imag), 0), 
+				multiply(c1, new Complex(c2.real, - c2.imag)));
+	}
+}
