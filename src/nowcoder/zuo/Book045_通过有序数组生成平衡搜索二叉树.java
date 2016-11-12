084a15c904f24d86bd01fecf4bd662db5845acfb
zxwtry <zxwtry@qq.com>
2016-11-12 21:57:06 +0800

    添加 nowcoder zuo Book045_通过有序数组生成平衡搜索二叉树

---
+++ src/nowcoder/zuo/Book045_通过有序数组生成平衡搜索二叉树.java
@@ -0,0 +1,15 @@
+package nowcoder.zuo;
+
+/*
+ * 	给定一个有序数组sortArr，已知其中没有重复值，用这个有序数组
+ * 	生成一棵平衡搜索二叉树，并且该搜索二叉树中序遍历的结果与sortArr一致
+ */
+
+public class Book045_通过有序数组生成平衡搜索二叉树 {
+	public static void main(String[] args) {
+		
+	}
+	static class BookSolution {
+		
+	}
+}
