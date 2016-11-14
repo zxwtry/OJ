71c482d01d43693db4c78fb99942aaa099428f46
zxwtry <zxwtry@qq.com>
2016-11-14 20:46:02 +0800

    添加 nowcoder zuo Book046_在二叉树中找到一个节点的后继节点

---
+++ src/nowcoder/zuo/Book046_在二叉树中找到一个节点的后继节点.java
@@ -0,0 +1,23 @@
+package nowcoder.zuo;
+
+/*
+ * 	
+ */
+
+public class Book046_在二叉树中找到一个节点的后继节点 {
+	public static void main(String[] args) {
+		
+	}
+	static class BookSolution {
+		
+	}
+	static class PosNode {
+		public int val;
+		public PosNode left;
+		public PosNode right;
+		public PosNode parent;
+		public PosNode(int val) {
+			this.val = val;
+		}
+	}
+}
