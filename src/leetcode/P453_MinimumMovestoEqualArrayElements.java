cfc7bf55340fe2c5dc6b6e982df8be003ee3252f
zxwtry <zxwtry@qq.com>
2016-11-14 20:49:43 +0800

    添加 leetcode P453_MinimumMovestoEqualArrayElements

---
+++ src/leetcode/P453_MinimumMovestoEqualArrayElements.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/*
+ * 	Given a non-empty integer array of size n, find the minimum number of 
+ * 	moves required to make all array elements equal, 
+ * 	where a move is incrementing n - 1 elements by 1.
+
+	Example:
+	
+	Input:
+	[1,2,3]
+	
+	Output:
+	3
+	
+	Explanation:
+	Only three moves are needed (remember each move increments two elements):
+	
+	[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+ */
+
+public class P453_MinimumMovestoEqualArrayElements {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int minMoves(int[] nums) {
+	        return 0;
+	    }
+	}
+}
