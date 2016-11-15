66b7cdf8c54ba2544c831acf578d5973ec97fac3
zxwtry <zxwtry@qq.com>
2016-11-15 12:21:00 +0800

    添加 leetcode P456_132Pattern

--- src/leetcode/P456_132Pattern.java
+++ src/leetcode/P456_132Pattern.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /*
  * 	Given a sequence of n integers a1, a2, ..., an, a 132 pattern is
  * 	 a subsequence ai, aj, ak such that i < j < k and ai < ak < aj. 
@@ -30,7 +32,12 @@ package leetcode;
 
 public class P456_132Pattern {
 	public static void main(String[] args) {
-		debugSolution();
+		int n = 10;
+		int min = 0;
+		int max = 20;
+		int[] arr = tools.Random随机生成器.A_生成一个随机数据(n, min, max);
+		tools.Utils.printArray(arr, 100);
+//		debugSolution();
 	}
 	static void debugSolution() {
 		int[] nums = new int[] {1, 1, 2};
@@ -50,5 +57,47 @@ public class P456_132Pattern {
 	        }
 	        return isFound;
 	    }
+		public int[] getSmallArray(int[] arr) {
+			if (arr == null || arr.length == 0) {
+				return new int[0];
+			}
+			int[] small = new int[arr.length];
+			Arrays.fill(small, -1);
+			long min = Long.MAX_VALUE;
+			for (int index = 0; index < arr.length; index ++) {
+				if (min > arr[index]) {
+					min = arr[index];
+					small[index] = index;
+				} else {
+					arr[index] = arr[index - 1];
+				}
+			}
+			return small;
+		}
+	}
+	
+	/*
+	 * 	先将问题进行简化为：
+	 * 	一个数组：arr	，不是有序的。
+	 * 	返回一个数组：small，表示第一个比当前val小的下标
+	 */
+	static class SmallSolution {
+		public int[] getSmallArray(int[] arr) {
+			if (arr == null || arr.length == 0) {
+				return new int[0];
+			}
+			int[] small = new int[arr.length];
+			Arrays.fill(small, -1);
+			long min = Long.MAX_VALUE;
+			for (int index = 0; index < arr.length; index ++) {
+				if (min > arr[index]) {
+					min = arr[index];
+					small[index] = index;
+				} else {
+					arr[index] = arr[index - 1];
+				}
+			}
+			return small;
+		}
 	}
 }
