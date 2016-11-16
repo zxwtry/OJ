93b0dc80c6270b99fde1cf45f6d218e75f08e870
zxwtry <zxwtry@qq.com>
2016-11-16 20:55:16 +0800

    添加 leetcode P452_MinimumNumberofArrowstoBurstBalloons debugSolutionSort

--- src/leetcode/P452_MinimumNumberofArrowstoBurstBalloons.java
+++ src/leetcode/P452_MinimumNumberofArrowstoBurstBalloons.java
@@ -1,12 +1,12 @@
 package leetcode;
 
 /*
- * 	There are a number of spherical balloons spread in two-dimensional space. 
- * 	For each balloon, provided input is the start and 
- * 	end coordinates of the horizontal diameter. 
- * 	Since it's horizontal, y-coordinates don't matter and 
- * 	hence the x-coordinates of start and end of the diameter suffice. 
- * 	Start is always smaller than end. There will be at most 104 balloons.
+	There are a number of spherical balloons spread in two-dimensional space. 
+	For each balloon, provided input is the start and 
+	end coordinates of the horizontal diameter. 
+	Since it's horizontal, y-coordinates don't matter and 
+	hence the x-coordinates of start and end of the diameter suffice. 
+	Start is always smaller than end. There will be at most 104 balloons.
 
 	An arrow can be shot up exactly vertically from different points along the x-axis. 
 	A balloon with xstart and xend bursts by an arrow shot at x if xstart ≤ x ≤ xend. 
@@ -31,11 +31,50 @@ package leetcode;
 
 public class P452_MinimumNumberofArrowstoBurstBalloons {
 	public static void main(String[] args) {
-		
+		debugSolutionSort();
+	}
+	static void debugSolutionSort() {
+		int[][] points = new int[][] {
+			{1, 2},
+			{7, 10},
+			{2, 20},
+			{3, 6},
+			{8, 9},
+			{0, 9},
+			{0, 9},
+		};
+		Solution s = new Solution();
+		s.sort(points, 0, points.length - 1);
+		for (int[] arr : points) {
+			System.out.println(arr[0] + "   " + arr[1]);
+		}
 	}
 	static class Solution {
 	    public int findMinArrowShots(int[][] points) {
+	    	sort(points, 0, points.length - 1);
+	    	int[] count = new int[points.length];
+	    	for (int i = 0; i < points.length; i ++) {
+	    		
+	    	}
 	        return 0;
 	    }
+		void sort(int[][] points, int sti, int eni) {
+			if (sti < eni) {
+				int p = part(points, sti, eni);
+				sort(points, sti, p - 1);
+				sort(points, p + 1, eni);
+			}
+		}
+		private int part(int[][] points, int sti, int eni) {
+			int[] pivot = points[sti];
+			while (sti < eni) {
+				while (sti < eni && points[eni][0] >= pivot[0])		eni --;
+				points[sti] = points[eni];
+				while (sti < eni && points[sti][0] <= pivot[0]) 	sti ++;
+				points[eni] = points[sti];
+			}
+			points[sti] = pivot;
+			return sti;
+		}
 	}
 }
