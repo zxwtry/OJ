74d534224086c71ca33d0f19f4228ae36708e266
zxwtry <zxwtry@qq.com>
2016-11-16 13:20:41 +0800

    添加 leetcode P453_MinimumMovestoEqualArrayElements Solution AC 13 ms 52.55%

--- src/leetcode/P453_MinimumMovestoEqualArrayElements.java
+++ src/leetcode/P453_MinimumMovestoEqualArrayElements.java
@@ -21,11 +21,31 @@ package leetcode;
 
 public class P453_MinimumMovestoEqualArrayElements {
 	public static void main(String[] args) {
-		
+		debugSolution();
 	}
+	static void debugSolution() {
+		int[] nums = new int[] {1, 2, 3};
+		Solution s = new Solution();
+		System.out.println(s.minMoves(nums));
+	}
+	/*
+	 * 	AC
+	 * 	13 ms
+	 * 	52.55%
+	 */
 	static class Solution {
 	    public int minMoves(int[] nums) {
-	        return 0;
+	    	if (nums == null || nums.length < 2) {
+	    		return 0;
+	    	}
+	    	int min = Integer.MAX_VALUE;
+	    	long sum = 0;
+	    	for (int val : nums) {
+	    		min = Math.min(min, val);
+	    		sum += val;
+	    	}
+	    	sum -= nums.length * min;
+	        return (int) sum;
 	    }
 	}
 }
