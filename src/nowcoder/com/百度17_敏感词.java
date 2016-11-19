13ee40d277fe70814d486a82382a256f0c5a40f8
zxwtry <zxwtry@qq.com>
2016-11-19 22:01:12 +0800

    添加 nowcoder com 百度17_敏感词  到  Trie图 AC自动机基本结构

--- src/nowcoder/com/百度17_敏感词.java
+++ src/nowcoder/com/百度17_敏感词.java
@@ -1,5 +1,10 @@
 package nowcoder.com;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 /*
  * 	题目描述
 									
@@ -44,6 +49,98 @@ package nowcoder.com;
 
 public class 百度17_敏感词 {
 	public static void main(String[] args) {
-		
+		solve1();
+	}
+	
+	/**
+	 * @method		solve1 
+	 * @parameter	void
+	 * @return 		void
+	 * @details 	第一个Solution
+	 */
+	private static void solve1() {
+		Scanner sc = new Scanner(System.in);
+		TrieNode root = new TrieNode();
+		for (int times = Integer.parseInt(sc.nextLine().trim()) - 1; times > -1; times --) {
+			solve1insert(sc.nextLine().trim(), root);
+		}
+		root.suffix = root;
+		Queue<TrieNode> trieNodeQueue = new LinkedList<TrieNode>();
+		for (int i = 0; i < 26; i ++) {
+			if (null == root.nexts[i]) {
+				root.nexts[i] = root;
+			} else {
+				root.nexts[i].suffix = root;
+				trieNodeQueue.add(root.nexts[i]);
+			}
+		}
+		TrieNode rootNow = null;
+		while (! trieNodeQueue.isEmpty()) {
+			rootNow = trieNodeQueue.poll();
+			for (int i = 0; i < 26; i ++) {
+				if (null == root.nexts[i]) {
+					rootNow.nexts[i] = rootNow.suffix.nexts[i];
+				} else {
+					rootNow.nexts[i].suffix = rootNow.suffix.nexts[i];
+					trieNodeQueue.add(rootNow.nexts[i]);
+				}
+			}
+		}
+		String str = sc.nextLine().trim();
+		rootNow = root;
+		StringBuilder st = new StringBuilder(str);
+		str = str.toLowerCase();
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		for (int i= 0; i < str.length(); i ++) {
+			rootNow = rootNow.nexts[str.charAt(i) - 'a'];
+			if (rootNow.isEndOfAWord) {
+				list.add(i);
+				System.out.println(i);
+			}
+		}
+		Iterator<Integer> it = list.iterator();
+		while (it.hasNext()) {
+			
+		}
+		sc.close();
+	}
+
+	/**
+	 * @method		solve1insert 
+	 * @parameter	s		---	匹配串
+	 * @parameter	root	---	Trie图的root
+	 * @return 		void
+	 * @details 	对应第一个Solution
+	 */
+	private static void solve1insert(String s, TrieNode root) {
+		TrieNode rootNow = root;
+		for (int i = 0; i < s.length(); i ++) {
+			int nextIndex = s.charAt(i) - 'a';
+			if (null != rootNow.nexts[nextIndex]) {
+				rootNow = rootNow.nexts[nextIndex];
+			} else {
+				TrieNode addTrieNode = new TrieNode();
+				rootNow.nexts[nextIndex] = addTrieNode;
+				rootNow = addTrieNode;
+			}
+		}
+		rootNow.isEndOfAWord = true;
+	}
+
+	/**
+	 * @author		zxwtry
+	 * @email		zxwtry@qq.com
+	 * @project		OJ
+	 * @package		nowcoder.com
+	 * @file		百度17_敏感词.java
+	 * @date		2016年11月19日 下午7:01:01
+	 * @details		TrieNode前缀数的修改版
+	 * @details		限定匹配串全部是小写字母
+	 * @details		对应第一个Solution
+	 */
+	static class TrieNode {
+		boolean isEndOfAWord = false;
+		TrieNode[] nexts = new TrieNode[26];
+		TrieNode suffix = null;
 	}
 }
