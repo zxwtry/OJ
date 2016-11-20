2dd50518ced7b048e4d4195d8c08a39ae7bf8505
zxwtry <zxwtry@qq.com>
2016-11-20 18:49:09 +0800

    添加 leetcode P463_IslandPerimeter Solution2 AC 229ms

--- src/leetcode/P463_IslandPerimeter.java
+++ src/leetcode/P463_IslandPerimeter.java
@@ -36,9 +36,21 @@ import java.util.ArrayList;
  */
 public class P463_IslandPerimeter {
 	public static void main(String[] args) {
-		
+		debugSolution2();
 	}
 	
+	/**
+	 * @method      debugSolution2
+	 * @parameter   
+	 * @return      void
+	 * @details   	  
+	 */
+	private static void debugSolution2() {
+		Solution2 s = new Solution2();
+		int[][] arr = tools.FileUtils.C_读取int二维数组_LEETCODE("D:/file/data/P463_IslandPerimeter.txt", 1);
+		System.out.println(s.islandPerimeter(arr));
+	}
+
 	/**
 	 * @method      debugSolution
 	 * @parameter
@@ -111,5 +123,70 @@ public class P463_IslandPerimeter {
 			}
 	    	return ans;
 	    }
+	}
+	
+	/**
+	 * @auther      zxwtry
+	 * @email       zxwtry@qq.com
+	 * @project     OJ
+	 * @package     leetcode
+	 * @file        P463_IslandPerimeter.java
+	 * @type        Solution2
+	 * @date        2016年11月20日 下午5:47:22
+	 * @details     采用搜索记数法
+	 * @details     5833 test cases passed.
+	 * @details     229 ms
+	 * @details     Status: Accepted
+	 */
+	static class Solution2 {
+		int[] surx = new int[] {0, -1, 1,  0};
+		int[] sury = new int[] {-1, 0, 0,  1};
+		/**
+		 * @method      islandPerimeter
+		 * @parameter   int[][] grid
+		 * @return      int
+		 * @details     Solution2的入口
+		 */
+		public int islandPerimeter(int[][] grid) {
+			boolean[][] isTraced = new boolean[grid.length][grid[0].length];
+			int i = 0, j = 0;
+			boolean find = false;
+			for (i = 0; ! find && i < grid.length; i ++) {
+				for (j = 0; ! find && j < grid[0].length; j ++) {
+					if (grid[i][j] == 1) {
+						find = true;
+						System.out.println(i + "..." + j);
+					}
+				}
+			}
+			int[] ans = new int[]{0};
+			search(grid, isTraced, i - 1, j - 1, ans);
+			return ans[0];
+		}
+
+		/**
+		 * @method      search
+		 * @parameter   grid : 输入图
+		 * @parameter   isTraced : 访问纪录
+		 * @parameter   i : 横坐标
+		 * @parameter   j : 纵坐标
+		 * @parameter   ans : 用堆对象保存结果
+		 * @return      void
+		 * @details     搜索的程序
+		 */
+		private void search(int[][] grid, boolean[][] isTraced, int i, int j, int[] ans) {
+			if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || isTraced[i][j]) {
+				return;
+			}
+			isTraced[i][j] = true;
+			for (int index = 0; index < surx.length; index ++) {
+				int newI = i + surx[index], newJ = j + sury[index];
+				if (newI < 0 || newI >= grid.length || newJ < 0 || newJ >= grid[0].length || grid[newI][newJ] == 0) {
+					ans[0] = ans[0] + 1;
+				} else {
+					search(grid, isTraced, newI, newJ, ans);
+				}
+			}
+		}
 	}
 }
