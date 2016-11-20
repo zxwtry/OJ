fc51d9b078053a6ac9e2e321ad674aa5216b4003
zxwtry <zxwtry@qq.com>
2016-11-20 15:31:01 +0800

    添加 leetcode P463_IslandPerimeter

---
+++ src/leetcode/P463_IslandPerimeter.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+/*
+ * 	You are given a map in form of a two-dimensional integer grid 
+ * 	where 1 represents land and 0 represents water. Grid cells are 
+ * 	connected horizontally/vertically (not diagonally). 
+ * 	The grid is completely surrounded by water, and 
+ * 	there is exactly one island (i.e., one or more connected land cells). 
+ * 	The island doesn't have "lakes" (water inside that isn't connected to 
+ * 	the water around the island). One cell is a square with side length 1. 
+ * 	The grid is rectangular, width and height don't exceed 100. 
+ * 	Determine the perimeter of the island.
+
+    Example:
+    
+    [[0,1,0,0],
+     [1,1,1,0],
+     [0,1,0,0],
+     [1,1,0,0]]
+    
+    Answer: 16
+    Explanation: The perimeter is the 16 yellow stripes in the image below:
+ */
+
+/**
+ * @auther      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P463_IslandPerimeter.java
+ * @type        P463_IslandPerimeter
+ * @date        2016年11月20日 下午3:13:12
+ * @details     
+ */
+public class P463_IslandPerimeter {
+	public static void main(String[] args) {
+		debugSolution();
+	}
+	
+	/**
+	 * @method      debugSolution
+	 * @parameter
+	 * @return      void
+	 * @details
+	 */
+	private static void debugSolution() {
+		Solution s = new Solution();
+	}
+
+	/**
+	 * @auther      zxwtry
+	 * @email       zxwtry@qq.com
+	 * @project     OJ
+	 * @package     leetcode
+	 * @file        P463_IslandPerimeter.java
+	 * @type        Solution
+	 * @date        2016年11月20日 下午3:15:39
+	 * @details     
+	 */
+	static class Solution {
+		
+	    /**
+	     * @method      islandPerimeter
+	     * @parameter   int[][] grid		---	输入
+	     * @return      int
+	     * @details     Solution的入口
+	     */
+	    public int islandPerimeter(int[][] grid) {
+	        
+	    }
+	}
+}
