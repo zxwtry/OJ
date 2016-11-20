947dba16af461fe7b1fe2765b0dacff639d78ac8
zxwtry <zxwtry@qq.com>
2016-11-20 17:46:51 +0800

    添加 leetcode P463_IslandPerimeter  Solution  WA

--- src/leetcode/P463_IslandPerimeter.java
+++ src/leetcode/P463_IslandPerimeter.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.ArrayList;
+
 /*
  * 	You are given a map in form of a two-dimensional integer grid 
  * 	where 1 represents land and 0 represents water. Grid cells are 
@@ -34,7 +36,7 @@ package leetcode;
  */
 public class P463_IslandPerimeter {
 	public static void main(String[] args) {
-		debugSolution();
+		
 	}
 	
 	/**
@@ -43,8 +45,10 @@ public class P463_IslandPerimeter {
 	 * @return      void
 	 * @details
 	 */
-	private static void debugSolution() {
+	static void debugSolution() {
 		Solution s = new Solution();
+		int[][] arr = tools.FileUtils.C_读取int二维数组_LEETCODE("D:/file/data/P463_IslandPerimeter.txt", 2);
+		System.out.println(s.islandPerimeter(arr));
 	}
 
 	/**
@@ -55,7 +59,7 @@ public class P463_IslandPerimeter {
 	 * @file        P463_IslandPerimeter.java
 	 * @type        Solution
 	 * @date        2016年11月20日 下午3:15:39
-	 * @details     
+	 * @details     会一直WA，对于2这种数据是没法高效解决的。
 	 */
 	static class Solution {
 		
@@ -66,7 +70,46 @@ public class P463_IslandPerimeter {
 	     * @details     Solution的入口
 	     */
 	    public int islandPerimeter(int[][] grid) {
-	        
+	    	ArrayList<Integer> list = new ArrayList<Integer>();
+	    	int minJ = Integer.MAX_VALUE, maxJ = Integer.MIN_VALUE;
+	    	int ans = 0;
+	    	for (int i = 0; i < grid.length; i ++) {
+	    		int listI = 0;
+	    		for (int j = 0; j < grid[i].length; j ++) {
+	    			if (grid[i][j] == 1) {
+	    				listI += 1;
+	    				minJ = Math.min(minJ, j);
+	    				maxJ = Math.max(maxJ, j);
+	    			}
+	    		}
+	    		if (listI != 0) {
+	    			list.add(listI);
+	    		}
+	    	}
+	    	tools.Utils.B_打印List_Integer(list);
+	    	ans += (maxJ - minJ + 1 + list.size()) * 2;
+			for (int index = 1; index < list.size() - 1; index ++) {
+				int sti = index - 1, eni = index + 1;
+				int val = list.get(index);
+				while (sti > -1) {
+					if (list.get(sti) > val) {
+						break;
+					} else {
+						sti --;
+					}
+				}
+				while (eni < list.size()) {
+					if (list.get(eni) > val) {
+						break;
+					} else {
+						eni ++;
+					}
+				}
+				if (sti != -1 && eni != list.size()) {
+					ans += Math.min(list.get(sti), list.get(eni));
+				}
+			}
+	    	return ans;
 	    }
 	}
 }
