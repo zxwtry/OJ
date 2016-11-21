bbc8d29015fb4c039deca8e8d22cf4b7ed5eadbb
zxwtry <zxwtry@qq.com>
2016-11-21 18:24:37 +0800

    添加 nowcoder com 百度17_特征距离  solve1 TLE

--- src/nowcoder/com/百度17_特征距离.java
+++ src/nowcoder/com/百度17_特征距离.java
@@ -1,6 +1,7 @@
 package nowcoder.com;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /*
@@ -58,21 +59,182 @@ import java.util.Scanner;
 
 public class 百度17_特征距离 {
 	public static void main(String[] args) {
-		
+		try {
+			solve1();
+//			solve2();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
-	 * @method      solve1
+	 * @method      solve2
 	 * @parameter   
 	 * @return      void
 	 * @details     
 	 */
-	static void solve1() throws Exception {
-		Scanner sc = new Scanner(new File("D:/file/data/百度17_特征距离.txt"));
-		int T = sc.nextInt();
-		for (int t = 1; t <= T; t ++) {
+	static void solve2() throws Exception {
+		Scanner scan = new Scanner(new File("D:/file/data/百度17_特征距离.txt"));
+		int T, n, m, s, t, u, v, w, result;
+		int[][] graph;
+		while(scan.hasNext()){
+			T = scan.nextInt();
+			for(int i = 0; i < T; i++){
+				n = scan.nextInt();
+				m = scan.nextInt();
+				s = scan.nextInt();
+				t = scan.nextInt();
+				graph = new int[n][n];
+				solve2initGaph(graph);
+				for(int j = 0; j < m; j++){
+					u = scan.nextInt();
+					v = scan.nextInt();
+					w = scan.nextInt();
+					graph[u-1][v-1] = w;
+					graph[v-1][u-1] = w;
+				}
+				result = solve2getSpecialDis(graph, s-1, t-1);
+				if(result == -1){
+					System.out.println("Case #" + (i+1) + ": No answer");
+				}else{
+					System.out.println("Case #" + (i+1) + ": " + result);
+				}
+			}
+		}
+		scan.close();
+	}
+	
+	static int solve2getSpecialDis(int[][] graph, int start, int end){
+		//存储已经找到最短路径的点
+		boolean[] min_vertex = new boolean[graph.length];
+		min_vertex[start] = true;
+		//初始化存储距离的向量
+		int[] temp_arr = new int[graph.length];
+		//存储路径
+		int[] posi = new int[graph.length];
+		for(int i = 0; i < temp_arr.length; i++){
+			temp_arr[i] = graph[start][i];
+			if(temp_arr[i] != -1){
+				posi[i] = start;
+			}else{
+				posi[i] = -1;
+			}
+		}
+		
+		boolean isFound = false;
+		while(!isFound){
+			//找最近点
+			int temp_index = -1, temp_num = -1;
+			for(int i = 0; i < temp_arr.length; i++){
+				if(min_vertex[i]) continue;
+				if(temp_arr[i] != -1){
+					if(temp_num == -1){
+						temp_num = temp_arr[i];
+						temp_index = i;
+					}else if(temp_arr[i] < temp_num){
+						temp_num = temp_arr[i];
+						temp_index = i;
+					}
+				}
+			}
+			if(temp_index == end){
+				isFound = true;
+				continue;
+			}else if(temp_index == -1){
+				return -1;
+			}
+			min_vertex[temp_index] = true;
+			
+			
+			
+			//更新一次存储距离的数组
+			for(int i = 0; i < temp_arr.length; i++){
+				if(min_vertex[i]) continue;
+				if(graph[temp_index][i] != -1){
+					if(temp_arr[i] == -1 || temp_arr[temp_index] + graph[temp_index][i] < temp_arr[i]){
+						temp_arr[i] = temp_arr[temp_index] + graph[temp_index][i];
+						posi[i] = temp_index;
+					}
+				}
+			}
 			
 		}
+		
+		int max_dis = 0, pre = posi[end], cur = end;
+		while(pre != start){
+			if(graph[pre][cur] > max_dis){
+				max_dis = graph[pre][cur];
+			}
+			cur = pre;
+			pre = posi[cur];
+		}
+		if(graph[pre][cur] > max_dis) max_dis = graph[pre][cur];
+		return max_dis;
+	}
+	
+	static void solve2initGaph(int[][] graph){
+		for(int i = 0; i < graph.length; i++){
+			for(int j = 0; j < graph.length; j++){
+				graph[i][j] = -1;
+			}
+		}
+	}
+
+	/**
+	 * @method      solve1
+	 * @parameter   
+	 * @return      void
+	 * @details     TLE了
+	 */
+	static void solve1() throws Exception {
+		Scanner sc = new Scanner(new File("D:/file/data/百度17_特征距离.txt"));
+		int TIME = sc.nextInt();
+		for (int times = 1; times <= TIME; times ++) {
+			int n = sc.nextInt(), m = sc.nextInt(), s = sc.nextInt(), t = sc.nextInt();
+			HashMap<String, Integer> map = new HashMap<String, Integer>();
+			for (int mi = 0; mi < m; mi ++) {
+				int u = sc.nextInt(), v = sc.nextInt(), w = sc.nextInt();
+				map.put(Math.min(u, v)+"#"+Math.max(u, v), w);
+			}
+			int[] val = new int[] {Integer.MAX_VALUE, 0};
+			boolean[] isTravled = new boolean[n+1];
+			solve1search(n, s, t, 0, 0, val, map, isTravled);
+			System.out.print("Case #"+times+": ");
+			if (val[0] == Integer.MAX_VALUE && val[1] == 0) {
+				System.out.println("No answer");
+			} else {
+				System.out.println(val[1]);
+			}
+		}
 		sc.close();
+	}
+
+	/**
+	 * @method      solve1search
+	 * @parameter   
+	 * @return      void
+	 * @details     
+	 */
+	private static void solve1search(int n, int now, int taget, int count, int max, int[] val, HashMap<String, Integer> map, boolean[] isTravled) {
+		if (count > val[0])	return;
+		for (int next = 1; next <= n; next ++) {
+			String key = Math.min(now, next)+"#"+Math.max(now, next);
+			if (map.containsKey(key) && ! isTravled[next]) {
+				int w = map.get(key);
+				int thismax = Math.max(max, w);
+				if (next == taget) {
+					if (count + w < val[0]) {
+						val[0] = count + w;
+						val[1] = thismax;
+					} else if (count + w == val[0] && thismax > val[1]) {
+						val[1] = thismax;
+					}
+				} else {
+					isTravled[next] = true;
+					solve1search(n, next, taget, count + w, thismax, val, map, isTravled);
+					isTravled[next] = false;
+				}
+			}
+		}
 	}
 }
