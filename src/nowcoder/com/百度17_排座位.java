ae0a02a12fad97aff4cdb41d815c6dd9461909d8
zxwtry <zxwtry@qq.com>
2016-11-21 13:58:58 +0800

    添加 nowcoder com 百度17_排座位  solve1  AC

--- src/nowcoder/com/百度17_排座位.java
+++ src/nowcoder/com/百度17_排座位.java
@@ -1,12 +1,14 @@
 package nowcoder.com;
 
+import java.io.File;
+import java.util.Scanner;
+
 /*
  * 	餐厅里有一张圆形的桌子，桌子上有n个座位，座位按照顺序从1到n编号，而且n号座位和1号座位相邻，
  * 	每个座位都不一样。m个人一起去这家餐厅吃饭。为了更好地就餐，他们不想坐得太过拥挤，因此，不允许有任意两个人相邻。
 	你知道一共有多少种不同的就坐方法吗？最后答案可能非常大，输出答案除以1000000007之后的余数。
 	两种坐法不一样当且仅当存在一个人，在两种坐法中他坐在了不同的位置上。
 	
-									
 	输入
 	第一行一个数字T（T＜=10）表示测试数据的组数。对于每组测试数据，输入两个数n和m（1＜=n,m＜=100000）。
 	样例输入
@@ -39,7 +41,11 @@ package nowcoder.com;
  */
 public class 百度17_排座位 {
 	public static void main(String[] args) {
-		solve1();
+		try {
+			solve1();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -48,8 +54,23 @@ public class 百度17_排座位 {
 	 * @return 		void
 	 * @details 	
 	 */
-	private static void solve1() {
-		
+	private static void solve1() throws Exception{
+		final long cut = 1000000007;
+		Scanner sc = new Scanner(new File("D:/file/data/百度17_排座位.txt"));
+		for (int times = sc.nextInt() - 1; times > -1; times --) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			if (m * 2 > n) {
+				System.out.println(0);
+			} else {
+				long ans = n;
+				for (int mi = 1; mi < m; mi ++) {
+					ans = (n - m - mi) * ans % cut;
+				}
+				System.out.println(ans % cut);
+			}
+		}
+		sc.close();
 	}
 	
 }
