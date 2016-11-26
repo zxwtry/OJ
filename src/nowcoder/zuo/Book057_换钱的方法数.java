64e6f7f5250bc5b579284da497d8a5bdbd28a09e
zxwtry <zxwtry@qq.com>
2016-11-26 20:40:28 +0800

    添加 nowcoder zuo Book057_换钱的方法数 Solution5动态规划，测试通过

--- src/nowcoder/zuo/Book057_换钱的方法数.java
+++ src/nowcoder/zuo/Book057_换钱的方法数.java
@@ -17,9 +17,24 @@ import java.util.Arrays;
  */
 public class Book057_换钱的方法数 {
 	public static void main(String[] args) {
-		debugSolution4动态规划();
+		debugSolution5动态规划();
 	}
 	
+	static void debugSolution5动态规划() {
+		for (int times = 0; times < 100; times ++) {
+			int n = times;
+			int min = 1;
+			int max = n * n + 12;
+			int[] arr = tools.Random随机生成器.A_生成一个不重复随机数据(n, min, max);
+			int aim = (int) (Math.random() * max);
+			Solution3 s3 = new Solution3();
+			Solution5动态规划 s5 = new Solution5动态规划();
+			int a3 = s3.numOfWays(arr, aim);
+			int a5 = s5.numOfWays(arr, aim);
+			System.out.printf("%d...%d\r\n", a3, a5);
+		}
+	}
+
 	static void debugSolution4动态规划() {
 		for (int times = 0; times < 100; times ++) {
 			int n = times;
@@ -100,6 +115,7 @@ public class Book057_换钱的方法数 {
 	 * @type        Solution2
 	 * @date        2016年11月26日 上午11:47:05
 	 * @details     是对Solution1的纪录优化
+	 * @details     时间复杂度O(N*aim*aim)
 	 */
 	static class Solution2 {
 		public int numOfWays(int[] arr, int aim) {
@@ -137,6 +153,7 @@ public class Book057_换钱的方法数 {
 	 * @type        Solution3
 	 * @date        2016年11月26日 下午8:23:21
 	 * @details     对Solution2的逻辑简化
+	 * @details     时间复杂度O(N*aim*aim)
 	 */
 	static class Solution3 {
 		public int numOfWays(int[] arr, int aim) {
@@ -165,6 +182,16 @@ public class Book057_换钱的方法数 {
 		}
 	}
 	
+	/**
+	 * @auther      zxwtry
+	 * @email       zxwtry@qq.com
+	 * @project     OJ
+	 * @package     nowcoder.zuo
+	 * @file        Book057_换钱的方法数.java
+	 * @type        Solution4动态规划
+	 * @date        2016年11月26日 下午8:33:03
+	 * @details     时间复杂度O(N*aim*aim)
+	 */
 	static class Solution4动态规划 {
 		public int numOfWays(int[] arr, int aim) {
 			if (arr == null || arr.length < 1 || aim < 0)	return 0;
@@ -180,6 +207,31 @@ public class Book057_换钱的方法数 {
 					dp[i][j] = num;
 				}
 			}
+			return dp[arr.length - 1][aim];
+		}
+	}
+	
+	/**
+	 * @auther      zxwtry
+	 * @email       zxwtry@qq.com
+	 * @project     OJ
+	 * @package     nowcoder.zuo
+	 * @file        Book057_换钱的方法数.java
+	 * @type        Solution5动态规划
+	 * @date        2016年11月26日 下午8:34:20
+	 * @details     时间复杂度O(N*aim)
+	 */
+	static class Solution5动态规划 {
+		public int numOfWays(int[] arr, int aim) {
+			if (arr == null || arr.length < 1 || aim < 0)	return 0;
+			int[][] dp = new int[arr.length][aim + 1];
+			for (int i = 0; i < arr.length; i ++)	dp[i][0] = 1;
+			for (int j = 0; j * arr[0] <= aim; j ++)	dp[0][j * arr[0]] = 1;
+			for (int i = 1; i < arr.length; i ++)
+				for (int j = 1; j <= aim; j ++) {
+					dp[i][j] = dp[i - 1][j];
+					dp[i][j] += j - arr[i] >= 0 ? dp[i][j - arr[i]] : 0;
+				}
 			return dp[arr.length - 1][aim];
 		}
 	}
