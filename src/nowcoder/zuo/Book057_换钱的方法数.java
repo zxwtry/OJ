91325c6fc4a522778a893846c384f722a20ebbd0
zxwtry <zxwtry@qq.com>
2016-11-26 20:50:32 +0800

    添加 nowcoder zuo Book057_换钱的方法数 Solution6动态规划，测试通过

--- src/nowcoder/zuo/Book057_换钱的方法数.java
+++ src/nowcoder/zuo/Book057_换钱的方法数.java
@@ -17,9 +17,24 @@ import java.util.Arrays;
  */
 public class Book057_换钱的方法数 {
 	public static void main(String[] args) {
-		debugSolution5动态规划();
+		debugSolution6动态规划();
 	}
 	
+	static void debugSolution6动态规划() {
+		for (int times = 0; times < 100; times ++) {
+			int n = times;
+			int min = 1;
+			int max = n * n + 12;
+			int[] arr = tools.Random随机生成器.A_生成一个不重复随机数据(n, min, max);
+			int aim = (int) (Math.random() * max);
+			Solution3 s3 = new Solution3();
+			Solution6动态规划 s6 = new Solution6动态规划();
+			int a3 = s3.numOfWays(arr, aim);
+			int a6 = s6.numOfWays(arr, aim);
+			System.out.printf("%d...%d\r\n", a3, a6);
+		}
+	}
+
 	static void debugSolution5动态规划() {
 		for (int times = 0; times < 100; times ++) {
 			int n = times;
@@ -197,7 +212,7 @@ public class Book057_换钱的方法数 {
 			if (arr == null || arr.length < 1 || aim < 0)	return 0;
 			int[][] dp = new int[arr.length][aim + 1];
 			for (int i = 0; i < arr.length; i ++)	dp[i][0] = 1;
-			for (int j = 0; j * arr[0] <= aim; j ++)	dp[0][j * arr[0]] = 1;
+			for (int j = 1; j * arr[0] <= aim; j ++)	dp[0][j * arr[0]] = 1;
 			int num = 0;
 			for (int i = 1; i < arr.length; i ++) {
 				for (int j = 1; j <= aim; j ++) {
@@ -226,13 +241,37 @@ public class Book057_换钱的方法数 {
 			if (arr == null || arr.length < 1 || aim < 0)	return 0;
 			int[][] dp = new int[arr.length][aim + 1];
 			for (int i = 0; i < arr.length; i ++)	dp[i][0] = 1;
-			for (int j = 0; j * arr[0] <= aim; j ++)	dp[0][j * arr[0]] = 1;
+			for (int j = 1; j * arr[0] <= aim; j ++)	dp[0][j * arr[0]] = 1;
 			for (int i = 1; i < arr.length; i ++)
 				for (int j = 1; j <= aim; j ++) {
 					dp[i][j] = dp[i - 1][j];
 					dp[i][j] += j - arr[i] >= 0 ? dp[i][j - arr[i]] : 0;
 				}
 			return dp[arr.length - 1][aim];
+		}
+	}
+	
+	/**
+	 * @auther      zxwtry
+	 * @email       zxwtry@qq.com
+	 * @project     OJ
+	 * @package     nowcoder.zuo
+	 * @file        Book057_换钱的方法数.java
+	 * @type        Solution6动态规划
+	 * @date        2016年11月26日 下午8:41:27
+	 * @details     对Solution5动态规划的空间优化
+	 * @details     目前的最优解，时间O(N*aim) 空间O(aim)
+	 */
+	static class Solution6动态规划 {
+		public int numOfWays(int[] arr, int aim) {
+			if (arr == null || arr.length < 1 || aim < 0)	return 0;
+			int[] dp = new int[aim + 1];
+			for (int j = 0; j * arr[0] <= aim; j ++)	dp[j * arr[0]] = 1;
+			for (int i = 1; i < arr.length; i ++)
+				for (int j = 1; j <= aim; j ++) {
+					dp[j] += j - arr[i] >= 0 ? dp[j - arr[i]] : 0;
+				}
+			return dp[aim];
 		}
 	}
 	
