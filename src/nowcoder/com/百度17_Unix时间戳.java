76b1fec41531d07819d4f21905e5176be5edeaa7
zxwtry <zxwtry@qq.com>
2016-11-27 22:00:55 +0800

    添加 nowcoder com 百度17_Unix时间戳

---
+++ src/nowcoder/com/百度17_Unix时间戳.java
@@ -0,0 +1,50 @@
+package nowcoder.com;
+
+/**
+ * Unix 时间戳（百度2017秋招真题）
+	 题目描述
+										
+	Unix 时间戳(Unix timestamp)，或称Unix时间(Unix time)、POSIX 时间(POSIX time)，
+	是一种时间表示方式，定义为从格林威治时间1970年01月01日00时00分00秒起至现在的总秒数。
+	Unix时间戳不仅被使用在 Unix 系统、类 Unix 系统中，也在许多其他操作系统中被广泛采用。
+	
+	Moment.js 是一个 Javascript 中用来处理时间的库，给定 Unix 时间戳，
+	他能够返回当前的年月日。你可以实现这个功能吗？
+	
+	提示：4年一闰100年不闰400年又闰
+	
+	
+									
+	输入
+	输入不超过50行，每行包含一个整数 x 表示一个 Unix 时间戳。（0＜=x＜=10^10）
+	样例输入
+	103424
+	1368864000
+	
+	输出
+	对于每一行，返回 yyyy mm dd 格式，表示对应的年月日。
+	样例输出
+	1970 01 02
+	2013 05 18
+	
+	时间限制
+	C/C++语言：1000MS其它语言：3000MS	
+	内存限制
+	C/C++语言：65536KB其它语言：589824KB
+ */
+
+/**
+ * @auther      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        百度17_Unix时间戳.java
+ * @type        百度17_Unix时间戳
+ * @date        2016年11月27日 下午10:00:05
+ * @details     
+ */
+public class 百度17_Unix时间戳 {
+	public static void main(String[] args) {
+		
+	}
+}
