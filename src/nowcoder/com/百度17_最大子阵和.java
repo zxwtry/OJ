2a998db2bef7801a29caa2927b6963a43835348d
zxwtry <zxwtry@qq.com>
2016-11-29 09:22:10 +0800

    添加 nowcoder zuo 百度17_最大子阵和  测试速度  solve2比solve1快很多倍

--- src/nowcoder/com/百度17_最大子阵和.java
+++ src/nowcoder/com/百度17_最大子阵和.java
@@ -1,5 +1,6 @@
 package nowcoder.com;
 
+import java.io.File;
 import java.util.Scanner;
 
 /**
@@ -44,7 +45,20 @@ import java.util.Scanner;
 public class 百度17_最大子阵和 {
 	public static void main(String[] args) {
 		try {
-			solve1();
+			System.gc();
+			Thread.sleep(100000);
+			long t1 = System.currentTimeMillis();
+			for (int times = 0; times < 10000; times ++)
+				solve1();
+			long t2 = System.currentTimeMillis();
+			System.out.println(t2 - t1);
+			System.gc();
+			Thread.sleep(100000);
+			t1 = System.currentTimeMillis();
+			for (int times = 0; times < 10000; times ++)
+				solve2();
+			t2 = System.currentTimeMillis();
+			System.out.println(t2 - t1);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -55,9 +69,10 @@ public class 百度17_最大子阵和 {
 	 * @parameter   
 	 * @return      void
 	 * @details     AC
+	 * @details     时间复杂度：O(N^2 * M^2)
 	 */
 	static void solve1() throws Exception {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(new File("D:/file/data/百度17_最大子阵和.txt"));
 		int times = sc.nextInt();
 		for (int timesIndex = 1; timesIndex <= times; timesIndex ++) {
 			int n = sc.nextInt(), m = sc.nextInt();
@@ -83,8 +98,62 @@ public class 百度17_最大子阵和 {
 					}
 				}
 			}
-			System.out.println(ans);
+//			System.out.println(ans);
 		}
 		sc.close();
 	}
+	
+    /**
+     * @method      solve2
+     * @parameter   
+     * @return      void
+     * @details     效率是solve1的很多倍就是了
+     * @details     时间复杂度：O(N^2 * M)
+     */
+    public static void solve2() throws Exception{
+        Scanner sc = new Scanner(new File("D:/file/data/百度17_最大子阵和.txt"));
+        while(sc.hasNext()){
+            int c = sc.nextInt();
+            for(int x = 0; x<c; x++){
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+                int[][] mat = new int[n][m];
+                for(int i= 0;i<n; i++){
+                    for(int j = 0; j<m; j++){
+                        mat[i][j]=sc.nextInt();
+                    }
+                }
+                solve2_subMatrix(mat);
+//                System.out.println(solve2_subMatrix(mat));
+            }
+        }
+        sc.close();
+    }
+	public static int solve2_subMatrix(int[][] mat) {
+        int maxSum = Integer.MIN_VALUE;
+        int n = mat.length;
+        int m = mat[0].length;
+        int sum[] = new int[m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++)
+                sum[j]=0;
+            for(int t=0; t<n-i; t++){
+                for(int j=0; j<m; j++)
+                    sum[j]+=mat[i+t][j];
+                maxSum = Math.max(maxSum, solve2_getMaxSum(sum));
+            }
+        }
+        return maxSum;
+    }
+    public static int solve2_getMaxSum(int a[]){
+        int maxSum = Integer.MIN_VALUE;
+        int curSum = 0;
+        int n = a.length;
+        for(int i=0; i<n; i++){
+            curSum += a[i];
+            maxSum = Math.max(maxSum, curSum);
+            if(curSum<0) curSum=0;
+        }
+        return maxSum;
+    }
 }
