f74c8731afda152f34ef893ed6fc97a8af2a9084
zxwtry <zxwtry@qq.com>
2016-11-30 22:33:00 +0800

    添加  nowcoder com 头条17_异或

---
+++ src/nowcoder/com/头条17_异或.java
@@ -0,0 +1,38 @@
+package nowcoder.com;
+
+/**
+ * 	给定整数m以及n个数字A1, A2, …, An，将数列A中所有元素两两异或，共能得到n(n-1)/2个结果。
+ * 	请求出这些结果中大于m的有多少个。
+	
+									
+	输入
+	第一行包含两个整数n, m。
+	第二行给出n个整数A1, A2, …, An。
+	样例输入
+	3 10
+	6 5 10
+	输出
+	输出仅包括一行，即所求的答案。
+	样例输出
+	2
+	时间限制
+	C/C++语言：1000MS其它语言：3000MS	
+	内存限制
+	C/C++语言：65536KB其它语言：589824KB
+ */
+
+/**
+ * @auther      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        头条17_异或.java
+ * @type        头条17_异或
+ * @date        2016年11月30日 下午10:32:24
+ * @details     
+ */
+public class 头条17_异或 {
+	public static void main(String[] args) {
+		
+	}
+}
