6a21e7256f7961aaccf571176b02c42a938a0fb1
zxwtry <zxwtry@qq.com>
2016-11-30 22:32:03 +0800

    添加  nowcoder com 头条17_StringShifting

---
+++ src/nowcoder/com/头条17_StringShifting.java
@@ -0,0 +1,42 @@
+package nowcoder.com;
+
+/**
+ * 	我们规定对一个字符串的shift操作如下：
+	shift(“ABCD”, 0) = “ABCD”
+	shift(“ABCD”, 1) = “BCDA”
+	shift(“ABCD”, 2) = “CDAB”
+	换言之, 我们把最左侧的N个字符剪切下来, 按序附加到了右侧。
+	给定一个长度为n的字符串，我们规定最多可以进行n次向左的循环shift操作。
+	如果shift(string, x) = string (0＜= x ＜n), 我们称其为一次匹配(match)。
+	求在shift过程中出现匹配的次数。
+	
+									
+	输入
+	输入仅包括一个给定的字符串，只包含大小写字符。
+	样例输入
+	byebyebye
+	输出
+	输出仅包括一行，即所求的匹配次数。
+	样例输出
+	3
+	时间限制
+	C/C++语言：1000MS其它语言：3000MS	
+	内存限制
+	C/C++语言：65536KB其它语言：589824KB
+ */
+
+/**
+ * @auther      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        头条17_StringShifting.java
+ * @type        头条17_StringShifting
+ * @date        2016年11月30日 下午10:31:22
+ * @details     
+ */
+public class 头条17_StringShifting {
+	public static void main(String[] args) {
+		
+	}
+}
