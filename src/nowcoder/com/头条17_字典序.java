77c9457f3b73b2a983914e37e82ed2bfc3205b1b
zxwtry <zxwtry@qq.com>
2016-12-01 19:54:22 +0800

    添加 nowcoder com 头条17_字典序

---
+++ src/nowcoder/com/头条17_字典序.java
@@ -0,0 +1,36 @@
+package nowcoder.com;
+
+/**
+ * 	给定整数n和m，将1到n的这n个整数按字典序排列之后，求其中的第m个数字。
+	对于n = 11，m = 4，按字典序排列依次为1, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9，因此第4个数字为2。
+	
+									
+	输入
+	输入仅包含两个整数n和m。
+	样例输入
+	11 4
+	输出
+	输出仅包括一行，即所求排列中的第m个数字。
+	样例输出
+	2
+	时间限制
+	C/C++语言：1000MS其它语言：3000MS	
+	内存限制
+	C/C++语言：65536KB其它语言：589824KB
+ */
+
+/**
+ * @auther      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        头条17_字典序.java
+ * @type        头条17_字典序
+ * @date        2016年11月30日 下午10:33:16
+ * @details     
+ */
+public class 头条17_字典序 {
+	public static void main(String[] args) {
+		
+	}
+}
