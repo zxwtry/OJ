ecf98c06e1e13c4089f02d2d0885dc613bcdee66
zxwtry <zxwtry@qq.com>
2016-12-02 21:26:07 +0800

    添加 nowcoder com 百度17_时钟 solve1

--- src/nowcoder/com/百度17_时钟.java
+++ src/nowcoder/com/百度17_时钟.java
@@ -1,5 +1,8 @@
 package nowcoder.com;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * 	小A、小B、小C三人正组队参加现场赛。小A刚过了一道大模拟，伸出手想看看几点了，
  * 	却发现自己没有带表，队友也没有带，因为大家平时是用手机看的。小A发现现场有一个电子显示屏上面有时间。
@@ -41,6 +44,78 @@ package nowcoder.com;
  */
 public class 百度17_时钟 {
 	public static void main(String[] args) {
-		
+		solve1();
+	}
+	/**
+	 * @method      solve1
+	 * @parameter   
+	 * @return      void
+	 * @details     AC
+	 * @details     就是花的时间有点多。。。
+	 */
+	static void solve1() {
+		Scanner sc = new Scanner(System.in);
+		char[][] c = new char[7][];
+		int times = sc.nextInt();
+		sc.nextLine();
+		boolean[][] iv = new boolean[7][100];
+		int[] s = new int[] {0};
+		for (int timeIndex = 1; timeIndex <= times; timeIndex ++) {
+			for (int i = 0; i < 7; i ++) Arrays.fill(iv[i], false);
+			for (int i = 0; i < 7; i ++) c[i] = sc.nextLine().toCharArray();
+			int i = 0;
+			int[] arr = new int[4];
+			int arrIndex = 0; 
+			while ( i < 100 ) {
+				s[0] = 0;
+				if (c[0][i] == star && ! iv[0][i]) {
+					search(c, iv, 0, i, s);
+					arr[arrIndex ++] = convert(c, s[0], 0, i);
+					if (arrIndex == 4) break;
+					while (++ i < 100 && c[0][i] == star) {};
+				} else i ++;
+			}
+			System.out.printf("Case #%d: %d:%d\r\n", timeIndex, arr[0]*10+arr[1], arr[2]*10+arr[3]);
+		}
+		sc.close();
+	}
+	
+	private static void search(char[][] c, boolean[][] iv, int i, int j, int[] s) {
+		s[0] ++;
+		iv[i][j] = true;
+		for (int k = 0; k < xs.length; k ++) {
+			int ni = i + xs[k], nj = j + ys[k];
+			if (ni > -1 && ni < c.length && nj > -1 && nj < c[ni].length && c[ni][nj] == star && ! iv[ni][nj])
+				search(c, iv, ni, nj, s);
+		}
+	}
+
+	final static char star = '*', blank = ' ';
+	final static int[] n = new int[] {22,  7, 22, 22, 15,
+									  22, 24, 12, 26, 24};
+	final static int[] xs = new int[] {1, -1, 0, 0};
+	final static int[] ys = new int[] {0, 0, -1, 1};
+
+	static int convert(char[][] c, int num, int i, int j) {
+		switch (num) {
+		case 7:
+			return 1;
+		case 15:
+			return 4;
+		case 12:
+			return 7;
+		case 26:
+			return 8;
+		case 22:
+			if (c[i + 3][j + 1] == blank) return 0;
+			if (c[i + 1][j] == star) return 5;
+			if (c[i + 4][j] == star) return 2;
+			return 3;
+		case 24:
+			if (c[i + 4][j] == star) return 6;
+			return 9;
+		default:
+			return 0;
+		}
 	}
 }
