d3f17c6a6d4f40c563061e719aec712bee891faa
zxwtry <zxwtry@qq.com>
2016-12-08 22:37:59 +0800

    添加 leetcode P363_MaxSumOfRectangleNoLargerThanK

---
+++ src/leetcode/P363_MaxSumOfRectangleNoLargerThanK.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/**
+ * 	Given a non-empty 2D matrix matrix and an integer k,
+ *  find the max sum of a rectangle in the matrix 
+ *  such that its sum is no larger than k.
+	
+	Example:
+	Given matrix = [
+	  [1,  0, 1],
+	  [0, -2, 3]
+	]
+	k = 2
+	The answer is 2. Because the sum of rectangle [[0, 1], [-2, 3]] is 2 
+	and 2 is the max number no larger than k (k = 2).
+	
+	Note:
+	The rectangle inside the matrix must have an area > 0.
+	What if the number of rows is much larger than the number of columns?
+	Credits:
+	Special thanks to @fujiaozhu for adding this problem and creating all test cases.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P363_MaxSumOfRectangleNoLargerThanK.java
+ * @type        P363_MaxSumOfRectangleNoLargerThanK
+ * @date        2016年12月8日 下午10:37:04
+ * @details     
+ */
+public class P363_MaxSumOfRectangleNoLargerThanK {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int maxSumSubmatrix(int[][] matrix, int k) {
+	        
+	    }
+	}
+}
