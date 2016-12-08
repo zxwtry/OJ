d124e9506e8aacd42044675edbc8e3013ece072f
zxwtry <zxwtry@qq.com>
2016-12-08 22:33:19 +0800

    添加 leetcode P367_ValidPerfectSquare

---
+++ src/leetcode/P367_ValidPerfectSquare.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+/**
+ * Given a positive integer num, write a function which returns True
+ * 	 if num is a perfect square else False.
+
+	Note: Do not use any built-in library function such as sqrt.
+	
+	Example 1:
+	
+	Input: 16
+	Returns: True
+	Example 2:
+	
+	Input: 14
+	Returns: False
+	Credits:
+	Special thanks to @elmirap for adding this problem and creating all test cases.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P367_ValidPerfectSquare.java
+ * @type        P367_ValidPerfectSquare
+ * @date        2016年12月8日 下午10:32:36
+ * @details     
+ */
+public class P367_ValidPerfectSquare {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public boolean isPerfectSquare(int num) {
+	        
+	    }
+	}
+}
