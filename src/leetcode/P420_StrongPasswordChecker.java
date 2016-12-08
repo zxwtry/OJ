8f689c686ed10c8d402f376793ab0ae861c11c5d
zxwtry <zxwtry@qq.com>
2016-12-08 22:26:23 +0800

    添加 leetcode P420_StrongPasswordChecker

---
+++ src/leetcode/P420_StrongPasswordChecker.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * A password is considered strong if below conditions are all met:
+
+	It has at least 6 characters and at most 20 characters.
+	It must contain at least one lowercase letter, at least one uppercase letter,
+	 and at least one digit.
+	It must NOT contain three repeating characters in a row ("...aaa..." is weak,
+	 but "...aa...a..." is strong, assuming other conditions are met).
+	Write a function strongPasswordChecker(s), that takes a string s as input, 
+	and return the MINIMUM change required to make s a strong password. 
+	If s is already strong, return 0.
+	
+	Insertion, deletion or replace of any one character are all considered as one change.
+	
+	Subscribe to see which companies asked this question
+ */
+
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P420_StrongPasswordChecker.java
+ * @type        P420_StrongPasswordChecker
+ * @date        2016年12月8日 下午10:25:31
+ * @details     
+ */
+public class P420_StrongPasswordChecker {
+	public static void main(String[] args) {
+		
+	}
+	
+	static class Solution {
+	    public int strongPasswordChecker(String s) {
+	        
+	    }
+	}
+}
