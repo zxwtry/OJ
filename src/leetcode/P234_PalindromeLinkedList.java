7b9363fad02a74c51f728f7916f40c844bc80cd0
zxwtry <zxwtry@qq.com>
2016-12-09 22:13:57 +0800

    添加 leetcode P234_PalindromeLinkedList

---
+++ src/leetcode/P234_PalindromeLinkedList.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * 	Given a singly linked list, determine if it is a palindrome.
+
+	Follow up:
+	Could you do it in O(n) time and O(1) space?
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P234_PalindromeLinkedList.java
+ * @type        P234_PalindromeLinkedList
+ * @date        2016年12月9日 下午10:13:29
+ * @details     
+ */
+public class P234_PalindromeLinkedList {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public boolean isPalindrome(ListNode head) {
+	        
+	    }
+	}
+}
