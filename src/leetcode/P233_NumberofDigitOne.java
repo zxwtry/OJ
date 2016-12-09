9f93ebee19a715cadac69d7daceee16ba0fc37c4
zxwtry <zxwtry@qq.com>
2016-12-09 22:12:32 +0800

    添加 leetcode P233_NumberofDigitOne

---
+++ src/leetcode/P233_NumberofDigitOne.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * Given an integer n, count the total number of digit 1 appearing 
+ * in all non-negative integers less than or equal to n.
+ * 
+
+	For example:
+	Given n = 13,
+	Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+	
+	Hint:
+	
+	Beware of overflow.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P233_NumberofDigitOne.java
+ * @type        P233_NumberofDigitOne
+ * @date        2016年12月9日 下午10:11:50
+ * @details     
+ */
+public class P233_NumberofDigitOne {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int countDigitOne(int n) {
+	        
+	    }
+	}
+}
