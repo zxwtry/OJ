5d0a9b4bded4e977d5c7965139764e99622ba7e5
zxwtry <zxwtry@qq.com>
2016-12-09 22:10:58 +0800

    添加 leetcode P232_ImplementQueueusingStacks

---
+++ src/leetcode/P232_ImplementQueueusingStacks.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+/**
+ * Implement the following operations of a queue using stacks.
+
+	push(x) -- Push element x to the back of queue.
+	pop() -- Removes the element from in front of queue.
+	peek() -- Get the front element.
+	empty() -- Return whether the queue is empty.
+	Notes:
+	You must use only standard operations of a stack -- 
+	which means only push to top, peek/pop from top, size, 
+	and is empty operations are valid.
+	Depending on your language, stack may not be supported natively. 
+	You may simulate a stack by using a list or deque (double-ended queue),
+	 as long as you use only standard operations of a stack.
+	You may assume that all operations are valid (for example, 
+	no pop or peek operations will be called on an empty queue).
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P232_ImplementQueueusingStacks.java
+ * @type        P232_ImplementQueueusingStacks
+ * @date        2016年12月9日 下午10:10:17
+ * @details     
+ */
+public class P232_ImplementQueueusingStacks {
+	public static void main(String[] args) {
+		
+	}
+	static class MyQueue {
+	    // Push element x to the back of queue.
+	    public void push(int x) {
+	        
+	    }
+
+	    // Removes the element from in front of queue.
+	    public void pop() {
+	        
+	    }
+
+	    // Get the front element.
+	    public int peek() {
+	        
+	    }
+
+	    // Return whether the queue is empty.
+	    public boolean empty() {
+	        
+	    }
+	}
+}
