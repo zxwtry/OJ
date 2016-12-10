fef2ad8bee5a7cb3ce0412f8b0b479dcb28238b8
zxwtry <zxwtry@qq.com>
2016-12-10 22:26:20 +0800

    添加 leetcode P238_ProductofArrayExceptSelf

---
+++ src/leetcode/P238_ProductofArrayExceptSelf.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * 	Given an array of n integers where n > 1, nums, return an array output such that
+ *  output[i] is equal to the product of all the elements of nums except nums[i].
+	
+	Solve it without division and in O(n).
+	
+	For example, given [1,2,3,4], return [24,12,8,6].
+	
+	Follow up:
+	Could you solve it with constant space complexity? (Note: The output array
+	 does not count as extra space for the purpose of space complexity analysis.)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P238_ProductofArrayExceptSelf.java
+ * @type        P238_ProductofArrayExceptSelf
+ * @date        2016年12月10日 下午10:25:35
+ * @details     
+ */
+public class P238_ProductofArrayExceptSelf {
+	static class Solution {
+	    public int[] productExceptSelf(int[] nums) {
+	        
+	    }
+	}
+}
