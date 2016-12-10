206051b545f01f84528f200dc50696f119fdf7bf
zxwtry <zxwtry@qq.com>
2016-12-10 11:51:04 +0800

    添加 nowcoder zuo Book060_最长公共子串

--- src/nowcoder/zuo/Book060_最长公共子串.java
+++ src/nowcoder/zuo/Book060_最长公共子串.java
@@ -54,4 +54,41 @@ public class Book060_最长公共子串 {
 		}
 	}
 	
+	/**
+	 * @author      zxwtry
+	 * @email       zxwtry@qq.com
+	 * @project     OJ
+	 * @package     nowcoder.zuo
+	 * @file        Book060_最长公共子串.java
+	 * @type        Solution2
+	 * @date        2016年12月10日 上午10:33:25
+	 * @details     根据dp[i1][i2]只和dp[i1-1][i2-1]相关
+	 * @details     可以将空间优化到O(1)
+	 */
+	static class Solution2 {
+		public String longestCommonSubstring(String s1, String s2) {
+			int s = 0, t = 0, n1 = s1.length(), n2 = s2.length();
+			int i1 = 0, i2 = 0, nm = 0;
+			for (int k = 0; k <= n1+n2; k ++) {
+				i1 = k > n1 ? 0 : n1 - k;
+				i2 = k > n1 ? k - n1 : 0;
+				nm = 0;
+				while (true) {
+					i1 ++; i2 ++;
+					if (i1 > n1 || i2 > n2)  break;
+					if (s1.charAt(i1-1) == s2.charAt(i2-1)) {
+						nm++;
+						if (nm > t-s) {
+							s = i1 - nm;
+							t = i1;
+						}
+					} else {
+						nm = 0;
+					}
+				}
+			}
+			return s1.substring(s, t);
+		}
+	}
+	
 }
