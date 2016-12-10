8589b4204343eabc73d75d13bd857ce6b2f7f460
zxwtry <zxwtry@qq.com>
2016-12-10 22:24:40 +0800

    添加 leetcode P237_DeleteNodeinaLinkedList

---
+++ src/leetcode/P237_DeleteNodeinaLinkedList.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * 	Write a function to delete a node (except the tail) in a singly linked list, 
+ * 	given only access to that node.
+
+	Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node
+	 with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P237_DeleteNodeinaLinkedList.java
+ * @type        P237_DeleteNodeinaLinkedList
+ * @date        2016年12月10日 下午10:23:39
+ * @details     
+ */
+public class P237_DeleteNodeinaLinkedList {
+	public static void main(String[] args) {
+		
+	}
+	
+	static class Solution {
+	    public void deleteNode(ListNode node) {
+	        
+	    }
+	}
+	
+}
