f190174e1d89244e22652032474f84116fd3c03a
zxwtry <zxwtry@qq.com>
2016-12-12 22:01:29 +0800

    添加 leetcode P241_DifferentWaysToAddParentheses

---
+++ src/leetcode/P241_DifferentWaysToAddParentheses.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+/**
+ * 	Given a string of numbers and operators, return all possible 
+ * 	results from computing all the different possible ways to group
+ *  numbers and operators. The valid operators are +, - and *.
+
+	
+	Example 1
+	Input: "2-1-1".
+	
+	((2-1)-1) = 0
+	(2-(1-1)) = 2
+	Output: [0, 2]
+	
+	
+	Example 2
+	Input: "2*3-4*5"
+	
+	(2*(3-(4*5))) = -34
+	((2*3)-(4*5)) = -14
+	((2*(3-4))*5) = -10
+	(2*((3-4)*5)) = -10
+	(((2*3)-4)*5) = 10
+	Output: [-34, -14, -10, -10, 10]
+	
+	Credits:
+	Special thanks to @mithmatt for adding this problem and creating all test cases.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P241_DifferentWaysToAddParentheses.java
+ * @type        P241_DifferentWaysToAddParentheses
+ * @date        2016年12月12日 下午10:00:28
+ * @details     
+ */
+public class P241_DifferentWaysToAddParentheses {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public List<Integer> diffWaysToCompute(String input) {
+	        
+	    }
+	}
+}
