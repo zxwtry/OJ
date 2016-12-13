2a7ccf2f227390c4a029a133cfd9157aa038343b
zxwtry <zxwtry@qq.com>
2016-12-13 22:19:33 +0800

    添加 leetcode P258_AddDigits

---
+++ src/leetcode/P258_AddDigits.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/**
+ * 	Given a non-negative integer num, repeatedly add
+ *  all its digits until the result has only one digit.
+
+	For example:
+	
+	Given num = 38, the process is like: 3 + 8 = 11, 
+	1 + 1 = 2. Since 2 has only one digit, return it.
+	
+	Follow up:
+	Could you do it without any loop/recursion in O(1) 
+	runtime?
+	
+	Hint:
+	
+	A naive implementation of the above process is trivial.
+	 Could you come up with other methods?
+	What are all the possible results?
+	How do they occur, periodically or randomly?
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P258_AddDigits.java
+ * @type        P258_AddDigits
+ * @date        2016年12月13日 下午10:18:15
+ * @details     
+ */
+public class P258_AddDigits {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int addDigits(int num) {
+	        
+	    }
+	}
+}
