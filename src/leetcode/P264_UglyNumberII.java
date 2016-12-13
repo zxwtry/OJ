36be9f19200cb3e9097470a4bac156016632a76a
zxwtry <zxwtry@qq.com>
2016-12-13 22:24:36 +0800

    添加 leetcode P264_UglyNumberII

---
+++ src/leetcode/P264_UglyNumberII.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * 	Write a program to find the n-th ugly number.
+
+	Ugly numbers are positive numbers whose prime 
+	factors only include 2, 3, 5. For example, 
+	1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence 
+	of the first 10 ugly numbers.
+	
+	Note that 1 is typically treated as an ugly number.
+	
+	Hint:
+	
+	The naive approach is to call isUgly for every 
+	number until you reach the nth one. Most numbers 
+	are not ugly. Try to focus your effort on generating 
+	only the ugly ones.
+	An ugly number must be multiplied by either 2, 3, 
+	or 5 from a smaller ugly number.
+	The key is how to maintain the order of the ugly 
+	numbers. Try a similar approach of merging from 
+	three sorted lists: L1, L2, and L3.
+	Assume you have Uk, the kth ugly number. Then Uk+1 
+	must be Min(L1 * 2, L2 * 3, L3 * 5).
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P264_UglyNumberII.java
+ * @type        P264_UglyNumberII
+ * @date        2016年12月13日 下午10:23:30
+ * @details     
+ */
+public class P264_UglyNumberII {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int nthUglyNumber(int n) {
+	        
+	    }
+	}
+}
