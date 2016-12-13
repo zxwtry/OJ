1bdbca867ed400cf407d404d0d2d4bb93f92be31
zxwtry <zxwtry@qq.com>
2016-12-13 22:28:01 +0800

    添加 leetcode P273_IntegerToEnglishWords

---
+++ src/leetcode/P273_IntegerToEnglishWords.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+/**
+ * 	Convert a non-negative integer to its english 
+ * 	words representation. Given input is guaranteed 
+ * 	to be less than 231 - 1.
+
+	For example,
+	123 -> "One Hundred Twenty Three"
+	12345 -> "Twelve Thousand Three Hundred Forty Five"
+	1234567 -> "One Million Two Hundred Thirty Four 
+	Thousand Five Hundred Sixty Seven"
+	Hint:
+	
+	Did you see a pattern in dividing the number 
+	into chunk of words? For example, 123 and 123000.
+	Group the number by thousands (3 digits). 
+	You can write a helper function that takes 
+	a number less than 1000 and convert just that 
+	chunk to words.
+	There are many edge cases. What are some good 
+	test cases? Does your code work with input 
+	such as 0? Or 1000010? (middle chunk is zero 
+	and should not be printed out)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P273_IntegerToEnglishWords.java
+ * @type        P273_IntegerToEnglishWords
+ * @date        2016年12月13日 下午10:26:55
+ * @details     
+ */
+public class P273_IntegerToEnglishWords {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public String numberToWords(int num) {
+	        
+	    }
+	}
+}
