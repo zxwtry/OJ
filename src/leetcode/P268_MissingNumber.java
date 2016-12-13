0777cb11acf97135c63ba1b6595a14fbd6e0d9c8
zxwtry <zxwtry@qq.com>
2016-12-13 22:26:16 +0800

    添加 leetcode P268_MissingNumber

---
+++ src/leetcode/P268_MissingNumber.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * 	Given an array containing n distinct numbers 
+ * 	taken from 0, 1, 2, ..., n, find the one that 
+ * 	is missing from the array.
+
+	For example,
+	Given nums = [0, 1, 3] return 2.
+	
+	Note:
+	Your algorithm should run in linear runtime 
+	complexity. Could you implement it using only 
+	constant extra space complexity?
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P268_MissingNumber.java
+ * @type        P268_MissingNumber
+ * @date        2016年12月13日 下午10:25:19
+ * @details     
+ */
+public class P268_MissingNumber {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int missingNumber(int[] nums) {
+	        
+	    }
+	}
+}
