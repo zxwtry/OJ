5343da99299301d6a9c229a2293b31e1285f0624
zxwtry <zxwtry@qq.com>
2016-12-13 22:22:50 +0800

    添加 leetcode P263_UglyNumber

---
+++ src/leetcode/P263_UglyNumber.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * 	Write a program to check whether a given number 
+ * 	is an ugly number.
+	
+	Ugly numbers are positive numbers whose prime 
+	factors only include 2, 3, 5. For example, 6, 8 
+	are ugly while 14 is not ugly since it includes 
+	another prime factor 7.
+	
+	Note that 1 is typically treated as an ugly number.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P263_UglyNumber.java
+ * @type        P263_UglyNumber
+ * @date        2016年12月13日 下午10:22:02
+ * @details     
+ */
+public class P263_UglyNumber {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public boolean isUgly(int num) {
+	        
+	    }
+	}
+}
