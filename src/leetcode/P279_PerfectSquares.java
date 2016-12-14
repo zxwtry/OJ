d32a386e265323c435cc49a3f9c8aa4a52b8bce3
zxwtry <zxwtry@qq.com>
2016-12-14 22:23:54 +0800

    添加 leetcode P279_PerfectSquares

---
+++ src/leetcode/P279_PerfectSquares.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * 	Given a positive integer n, find the least number of perfect 
+ * 	square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+	
+	For example, given n = 12, return 3 because 12 = 4 + 4 + 4; 
+	given n = 13, return 2 because 13 = 4 + 9.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P279_PerfectSquares.java
+ * @type        P279_PerfectSquares
+ * @date        2016年12月14日 下午10:23:11
+ * @details     
+ */
+public class P279_PerfectSquares {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int numSquares(int n) {
+	        
+	    }
+	}
+}
