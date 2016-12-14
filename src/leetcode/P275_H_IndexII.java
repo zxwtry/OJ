ada5002f3e9f7ed816eab68088c136acb49219c9
zxwtry <zxwtry@qq.com>
2016-12-14 22:21:05 +0800

    添加 leetcode P275_H_IndexII

---
+++ src/leetcode/P275_H_IndexII.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 	Follow up for H-Index: What if the citations array is sorted in 
+ * 	ascending order? Could you optimize your algorithm?
+
+	Hint:
+	
+	Expected runtime complexity is in O(log n) and the input is sorted.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P275_H_IndexII.java
+ * @type        P275_H_IndexII
+ * @date        2016年12月14日 下午10:20:20
+ * @details     
+ */
+public class P275_H_IndexII {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public int hIndex(int[] citations) {
+	        
+	    }
+	}
+}
