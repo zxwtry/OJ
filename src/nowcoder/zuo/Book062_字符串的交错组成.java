714eff8d545548f882238de4a3c3285a252575e8
zxwtry <zxwtry@qq.com>
2016-12-14 22:16:39 +0800

    添加 nowcoder zuo Book062_字符串的交错组成 Solution2 debugRight测试通过

--- src/nowcoder/zuo/Book062_字符串的交错组成.java
+++ src/nowcoder/zuo/Book062_字符串的交错组成.java
@@ -1,7 +1,5 @@
 package nowcoder.zuo;
 
-import com.sun.xml.internal.fastinfoset.algorithm.BooleanEncodingAlgorithm;
-
 /**
  * 	给定三个字符串s1  s2  和  aim，如果aim包含且仅包含来自s1和s2的所有字符，
  * 	而且在aim中属于s1的字符之间保持原来在s1中的顺序，属于s2的字符之间保持原来在s2中的顺序，
@@ -22,7 +20,8 @@ public class Book062_字符串的交错组成 {
 	public static void main(String[] args) {
 		String[] s = new String[] {null, null, null};
 		debugRight(s);
-		System.out.println(new Solution1().isCross1(s[0], s[1], s[2]));
+		System.out.println(new Solution1().isCross(s[0], s[1], s[2]));
+		System.out.println(new Solution2().isCross(s[0], s[1], s[2]));
 	}
 	private static void debugRight(String[] s) {
 		String s1 = tools.StringUtils.A_生成随机数组A_Z(300);
@@ -30,10 +29,13 @@ public class Book062_字符串的交错组成 {
 		char[] ans = new char[s1.length() + s2.length()];
 		int a1 = 0, a2 = 0;
 		for (int ai = 0; ai < ans.length; ai ++) {
-			if (a1 < s1.length() && Math.random() > 0.5) {
-				ans[ai] = s1.charAt(a1 ++);
+			if (a1 < s1.length()) {
+				if (Math.random() > 0.5)
+					ans[ai] = s1.charAt(a1 ++);
+				else
+					ai --;
 			} else {
-				ans[ai] = s1.charAt(a2 ++);
+				ans[ai] = s2.charAt(a2 ++);
 			}
 		}
 		s[0] = s1;
@@ -42,7 +44,7 @@ public class Book062_字符串的交错组成 {
 	}
 	
 	static class Solution1 {
-		public boolean isCross1(String s1, String s2, String aim) {
+		public boolean isCross(String s1, String s2, String aim) {
 			if (s1 == null || s2 == null || aim == null) return false;
 			char[] c1 = s1.toCharArray();
 			char[] c2 = s2.toCharArray();
@@ -64,6 +66,35 @@ public class Book062_字符串的交错组成 {
 							(c2[j - 1] == cm[i + j - 1] && dp[i][j - 1]))
 						dp[i][j] = true;
 			return dp[c1.length][c2.length];
+		}
+	}
+	
+	static class Solution2 {
+		public boolean isCross(String s1, String s2, String aim) {
+			if (s1 == null || s2 == null || aim == null) return false;
+			char[] c1 = s1.toCharArray();
+			char[] c2 = s2.toCharArray();
+			char[] cm = aim.toCharArray();
+			if (cm.length != c1.length + c2.length) return false;
+			char[] longs = c1.length >= c2.length ? c1 : c2;
+			char[] shorts = c1.length < c2.length ? c1 : c2;
+			boolean[] dp = new boolean[shorts.length + 1];
+			dp[0] = true;
+			for (int i = 1; i <= shorts.length; i ++) {
+				if (shorts[i - 1] != cm[i - 1]) break;
+				dp[i] = true;
+			}
+			for (int i = 1; i <= longs.length; i ++) {
+				dp[0] = dp[0] && longs[i - 1] == cm[i - 1];
+				for (int j = 1; j <= shorts.length; j ++) {
+					if ((longs[i - 1] == cm[i + j - 1] && dp[j]) ||
+							shorts[j - 1] == cm[i + j - 1] && dp[j - 1])
+						dp[j] = true;
+					else 
+						dp[j] = false;
+				}
+			}
+			return dp[shorts.length];
 		}
 	}
 	
