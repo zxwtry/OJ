aa0b54e40e9bbe7e0acda769e7c7f161a5eb496f
zxwtry <zxwtry@qq.com>
2016-12-14 22:25:55 +0800

    添加 leetcode P283_MoveZeroes

---
+++ src/leetcode/P283_MoveZeroes.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * 	Given an array nums, write a function to move all 0's to the 
+ * 	end of it while maintaining the relative order of the non-zero elements.
+
+	For example, given nums = [0, 1, 0, 3, 12], after calling your 
+	function, nums should be [1, 3, 12, 0, 0].
+	
+	Note:
+	You must do this in-place without making a copy of the array.
+	Minimize the total number of operations.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P283_MoveZeroes.java
+ * @type        P283_MoveZeroes
+ * @date        2016年12月14日 下午10:25:13
+ * @details     
+ */
+public class P283_MoveZeroes {
+	public static void main(String[] args) {
+		
+	}
+	static class Solution {
+	    public void moveZeroes(int[] nums) {
+	        
+	    }
+	}
+}
