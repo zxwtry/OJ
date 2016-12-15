f6336a331c78c7b0722e99c84b935a1633918294
zxwtry <zxwtry@qq.com>
2016-12-15 21:44:54 +0800

    添加 nowcoder zuo Book064_数字字符串转换为字母组合的种数

---
+++ src/nowcoder/zuo/Book064_数字字符串转换为字母组合的种数.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个字符串s，s全部由数字字符组成，
+ * 	如果s中某一个或相邻两个字符组成的子串值在1~26之间，
+ * 	则这个子串可以转换为一个字母，
+ * 	'1' --> 'A'
+ * 	'2' --> 'B'
+ * 	     |
+ * 	     |
+ * 	'26'--> 'Z'
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book064_数字字符串转换为字母组合的种数.java
+ * @type        Book064_数字字符串转换为字母组合的种数
+ * @date        2016年12月15日 下午9:42:09
+ * @details     
+ */
+public class Book064_数字字符串转换为字母组合的种数 {
+
+}
