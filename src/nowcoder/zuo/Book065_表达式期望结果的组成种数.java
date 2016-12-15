26d0e4b1b587f59dbd4b8bd418b5391a5098120b
zxwtry <zxwtry@qq.com>
2016-12-15 22:26:31 +0800

    添加 nowcoder zuo Book065_表达式期望结果的组成种数 Solution1

--- src/nowcoder/zuo/Book065_表达式期望结果的组成种数.java
+++ src/nowcoder/zuo/Book065_表达式期望结果的组成种数.java
@@ -21,5 +21,66 @@ package nowcoder.zuo;
  * @details     
  */
 public class Book065_表达式期望结果的组成种数 {
-
+	public static void main(String[] args) {
+	}
+	static class Solution1 {
+		public boolean isValid(String e) {
+			if ((e.length() & 0x1) == 0) return false;
+			for (int i = 0; i < e.length(); i += 2)
+				if (e.charAt(i) != '1' && e.charAt(i) != '0') return false;
+			for (int i = 1; i < e.length(); i += 2)
+				if (e.charAt(i) != '&' && e.charAt(i) != '|' && e.charAt(i) != '^') return false;
+			return true;
+		}
+		public int num(String e, boolean d) {
+			if (e == null || e.length() == 0) return 0;
+			if (! isValid(e)) return 0;
+			return p(e, d, 0, e.length()-1);
+		}
+		private int p(String e, boolean d, int l, int r) {
+			if (l == r) {
+				if (e.charAt(l) == '1')
+					return d ? 1 : 0;
+				else
+					return d ? 0 : 1;
+			}
+			int res = 0;
+			if (d) {
+				for (int i = l+1; i < r; i += 2) {
+					switch (e.charAt(i)) {
+					case '&':
+						res += p(e, true, l, i-1) * p(e, true, i+1, r);
+						break;
+					case '|':
+						res += p(e, true, l, i-1) * p(e, false, i+1, r);
+						res += p(e, false, l, i-1) * p(e, true, i+1, r);
+						res += p(e, true, l, i-1) * p(e, true, i+1, r);
+						break;
+					case '^':
+						res += p(e, true, l, i-1) * p(e, false, i+1, r);
+						res += p(e, false, l, i-1) * p(e, true, i+1, r);
+						break;
+					}
+				}
+			} else {
+				for (int i = l+1; i < r; i += 2) {
+					switch (e.charAt(i)) {
+					case '&':
+						res += p(e, false, l, i-1) * p(e, true, i+1, r);
+						res += p(e, true, l, i-1) * p(e, false, i+1, r);
+						res += p(e, false, l, i-1) * p(e, false, i+1, r);
+						break;
+					case '|':
+						res += p(e, false, l, i-1) * p(e, false, i+1, r);
+						break;
+					case '^':
+						res += p(e, true, l, i-1) * p(e, true, i+1, r);
+						res += p(e, false, l, i-1) * p(e, false, i+1, r);
+						break;
+					}
+				}
+			}
+			return res;
+		}
+	}
 }
