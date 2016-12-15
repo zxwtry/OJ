8f07c090f997106fc3a6e299daaa919974dd37a2
zxwtry <zxwtry@qq.com>
2016-12-15 21:36:25 +0800

    添加 nowcoder zuo Book063_龙与地下城_最小初始血量

--- src/nowcoder/zuo/Book063_龙与地下城_最小初始血量.java
+++ src/nowcoder/zuo/Book063_龙与地下城_最小初始血量.java
@@ -23,7 +23,15 @@ package nowcoder.zuo;
  */
 public class Book063_龙与地下城_最小初始血量 {
 	public static void main(String[] args) {
-		
+		int[][] m = new int[][] {
+			{-2,-3,3},
+			{-5,-10,1},
+			{0,30,-5},
+		};
+		Solution2 s2 = new Solution2();
+		System.out.println(s2.minHP(m));
+		Solution1 s1 = new Solution1();
+		System.out.println(s1.minHP(m));
 	}
 	static class Solution1 {
 		public int minHP(int[][] m) {
@@ -37,11 +45,45 @@ public class Book063_龙与地下城_最小初始血量 {
 				dp[i][col] = Math.max(dp[i+1][col] - m[i][col], 1);
 				for (int j = col - 1; j >= 0; j --) {
 					r = Math.max(dp[i][j+1] - m[i][j], 1);
-					r = Math.max(dp[i+1][j] - m[i][j], 1);
-					dp[i][j] = Math.max(r, d);
+					d = Math.max(dp[i+1][j] - m[i][j], 1);
+					dp[i][j] = Math.min(r, d);
 				}
 			}
 			return dp[0][0];
+		}
+	}
+	
+	static class Solution2 {
+		public int minHP(int[][] m) {
+			if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) return 1;
+			int more = Math.max(m.length, m[0].length);
+			int less = Math.min(m.length, m[0].length);
+			boolean rowmore = more == m.length;
+			int[] dp = new int[less];
+			int tmp = m[m.length - 1][m[0].length - 1];
+			dp[less - 1] = tmp > 0 ? 1 : -tmp+1;
+			int row = 0;
+			int col = 0;
+			for (int j = less - 2; j >= 0; j --) {
+				row = rowmore ? more - 1 : j;
+				col = rowmore ? j : more - 1;
+				dp[j] = Math.max(dp[j + 1] - m[row][col], 1);
+			}
+			int c1 = 0;
+			int c2 = 0;
+			for (int i = more - 2; i >= 0; i --) {
+				row = rowmore ? i : less - 1;
+				col = rowmore ? less - 1 : i;
+				dp[less - 1] = Math.max(dp[less - 1] - m[row][col], 1);
+				for (int j = less - 2; j >= 0; j --) {
+					row = rowmore ? i : j;
+					col = rowmore ? j : i;
+					c1 = Math.max(dp[j] - m[row][col], 1);
+					c2 = Math.max(dp[j+1] - m[row][col], 1);
+					dp[j] = Math.min(c1, c2);
+				}
+			}
+			return dp[0];
 		}
 	}
 }
