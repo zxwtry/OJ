a085e60d4742c8ec110b00168b12b68151d3ad2c
zxwtry <zxwtry@qq.com>
2016-12-15 22:10:13 +0800

    添加 nowcoder zuo Book065_表达式期望结果的组成种数

---
+++ src/nowcoder/zuo/Book065_表达式期望结果的组成种数.java
@@ -0,0 +1,25 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个只由0(false), 1(true), &(逻辑与), |(逻辑或)和^(异或)
+ * 	五种字符组成的字符串e，再给定一个布尔值d。返回e能够有多少种组合方式达到d的结果。
+ * 	举例：
+ * 		e="1^0|0|1" d=false
+ * 		只由1^((0|0)|1)和1^(0|(0|1))的组合可以得到false，返回 2
+ * 		e="1" d=false
+ * 		没有组合可以得到false，返回0
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book065_表达式期望结果的组成种数.java
+ * @type        Book065_表达式期望结果的组成种数
+ * @date        2016年12月15日 下午10:05:10
+ * @details     
+ */
+public class Book065_表达式期望结果的组成种数 {
+
+}
