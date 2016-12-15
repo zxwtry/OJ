a393dfd6ee8bc505c4729e0309f56c139d05816c
zxwtry <zxwtry@qq.com>
2016-12-15 21:12:24 +0800

    添加 nowcoder zuo Book063_龙与地下城_最小初始血量

---
+++ src/nowcoder/zuo/Book063_龙与地下城_最小初始血量.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个二维数组map，含义是一张地图，例如：
+ * 	-2	-3	3
+ * 	-5	-10	1
+ * 	0	30	-5
+ * 	游戏规则：
+ * 		1，从左上角出发，每次只能向右或向下走，最后到达右下角。
+ * 		2，map[i][j] 表示血量增减。
+ * 		3，当血量小于等于0，这条路径失败。
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book063_龙与地下城_最小初始血量.java
+ * @type        Book063_龙与地下城_最小初始血量
+ * @date        2016年12月15日 下午9:07:20
+ * @details     
+ */
+public class Book063_龙与地下城_最小初始血量 {
+	
+}
