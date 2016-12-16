97814fe30620b94a2f53bb9ecee4cf4d25a8847c
zxwtry <zxwtry@qq.com>
2016-12-16 21:50:15 +0800

    添加 nowcoder zuo Book069_N皇后问题

---
+++ src/nowcoder/zuo/Book069_N皇后问题.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	N皇后问题是指在N*N的棋盘上要摆N个皇后
+ * 	要求任何两个皇后不同行、不同列、不在同一条斜线上。
+ * 	给定一个整数n，返回n皇后的摆法有多少种。
+ * 	举例：
+ * 		n=1，返回1
+ * 		n=2，返回0
+ * 		n=3，返回0
+ * 		n=8，返回92
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book069_N皇后问题.java
+ * @type        Book069_N皇后问题
+ * @date        2016年12月16日 下午9:36:33
+ * @details     
+ */
+public class Book069_N皇后问题 {
+	
+}
