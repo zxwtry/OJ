68679af89361a3ce01f389f307f47d623e6af500
zxwtry <zxwtry@qq.com>
2016-12-16 20:57:47 +0800

    添加 nowcoder zuo Book067_跳跃游戏

---
+++ src/nowcoder/zuo/Book067_跳跃游戏.java
@@ -0,0 +1,25 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定数组arr，arr[i]==k代表可以从位置i向右条1~k个距离。
+ * 	比如arr[2]==3，代表从位置2跳到位置3、位置4、位置5。
+ * 	如果从位置0出发，返回最少跳几次能够跳到arr最后的位置上。
+ * 	举例：
+ * 			 0 1 2 3 4 5
+ * 		arr=[3,2,3,1,1,4]
+ * 		0~2~5完成，返回2
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book067_跳跃游戏.java
+ * @type        Book067_跳跃游戏
+ * @date        2016年12月16日 下午8:43:00
+ * @details     
+ */
+public class Book067_跳跃游戏 {
+	
+}
