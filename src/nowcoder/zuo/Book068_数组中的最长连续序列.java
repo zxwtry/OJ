98a3df976712e7afabecbb80b0c789cf1fecb84d
zxwtry <zxwtry@qq.com>
2016-12-16 21:20:35 +0800

    添加 nowcoder zuo Book068_数组中的最长连续序列

---
+++ src/nowcoder/zuo/Book068_数组中的最长连续序列.java
@@ -0,0 +1,24 @@
+package nowcoder.zuo;
+
+import java.util.HashMap;
+
+/**
+ * 	给定无序数组，返回其中最长的连续序列的长度。
+ * 	举例：
+ * 		arr=[100,4,200,1,3,2]
+ * 		最长连续序列为[1,2,3,4]，所以返回4
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book068_数组中的最长连续序列.java
+ * @type        Book068_数组中的最长连续序列
+ * @date        2016年12月16日 下午9:03:12
+ * @details     
+ */
+public class Book068_数组中的最长连续序列 {
+	
+}
