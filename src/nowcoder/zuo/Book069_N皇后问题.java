b0a19e0809dde51e87f5e9b04e9a125f7e10b058
zxwtry <zxwtry@qq.com>
2016-12-16 21:50:35 +0800

    添加 nowcoder zuo Book069_N皇后问题

--- src/nowcoder/zuo/Book069_N皇后问题.java
+++ src/nowcoder/zuo/Book069_N皇后问题.java
@@ -22,5 +22,28 @@ package nowcoder.zuo;
  * @details     
  */
 public class Book069_N皇后问题 {
-	
+	static class Solution1 {
+		public int num(int n) {
+			if (n < 1) return 0;
+			int[] record = new int[n];
+			return process(0, record, n);
+		}
+		private int process(int i, int[] record, int n) {
+			if (i == n) return 1;
+			int res = 0;
+			for (int j = 0; j < n; j ++) {
+				if (isValid(record, i, j)) {
+					record[i] = j;
+					res += process(i + 1, record, n);
+				}
+			}
+			return res;
+		}
+		private boolean isValid(int[] record, int i, int j) {
+			for (int k = 0; k < i; k ++)
+				if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k))
+					return false;
+			return true;
+		}
+	}
 }
