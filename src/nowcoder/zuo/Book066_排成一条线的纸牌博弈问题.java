26919c2d79ea6586ac16f57184ca276f92e04b0e
zxwtry <zxwtry@qq.com>
2016-12-16 19:30:19 +0800

    添加 nowcoder zuo Book066_排成一条线的纸牌博弈问题

---
+++ src/nowcoder/zuo/Book066_排成一条线的纸牌博弈问题.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个整型数组arr，代表数值不同的纸牌排成一条线。
+ * 	玩家A和玩家B一次拿走每张纸牌，规定玩家A先拿。
+ * 	每个玩家每次只能拿走最左或最右的纸牌。
+ * 	玩家A和玩家B都绝顶聪明，返回最后获胜者的分数。
+ * 
+ * 	举例：
+ * 		arr=[1,2,100,4]   返回101
+ * 		arr=[1,100,4]     返回100
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book066_排成一条线的纸牌博弈问题.java
+ * @type        Book066_排成一条线的纸牌博弈问题
+ * @date        2016年12月16日 下午7:24:31
+ * @details     
+ */
+public class Book066_排成一条线的纸牌博弈问题 {
+
+}
