39bad41973bd22cb4c0eadcf26b36c2253ab3329
zxwtry <zxwtry@qq.com>
2016-12-16 21:34:41 +0800

    添加 nowcoder zuo Book068_数组中的最长连续序列

--- src/nowcoder/zuo/Book068_数组中的最长连续序列.java
+++ src/nowcoder/zuo/Book068_数组中的最长连续序列.java
@@ -20,5 +20,30 @@ import java.util.HashMap;
  * @details     
  */
 public class Book068_数组中的最长连续序列 {
-	
+	static class Solution {
+		public int longestConsecutive(int[] arr) {
+			if (arr == null || arr.length == 0) return 0;
+			int max = 1;
+			HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+			for (int i = 0; i < arr.length; i ++) {
+				if (! map.containsKey(arr[i])) {
+					map.put(arr[i], 1);
+					if (map.containsKey(arr[i] - 1))
+						max = Math.max(max, merge(map, arr[i] - 1, arr[i]));
+					if (map.containsKey(arr[i] + 1))
+						max = Math.max(max, merge(map, arr[i], arr[i] + 1));
+				}
+			}
+			return max;
+		}
+
+		private int merge(HashMap<Integer, Integer> map, int less, int more) {
+			int l = less - map.get(less) + 1;
+			int r = more + map.get(more) - 1;
+			int len = r - l + 1;
+			map.put(l, len);
+			map.put(r, len);
+			return len;
+		}
+	}
 }
