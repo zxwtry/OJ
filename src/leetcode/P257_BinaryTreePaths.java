342c2e54becea58aeb3c600a976fdf870cda4e22
zxwtry <zxwtry@qq.com>
2016-12-17 22:16:11 +0800

    添加 leetcode P257_BinaryTreePaths

---
+++ src/leetcode/P257_BinaryTreePaths.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * Given a binary tree, return all root-to-leaf paths.
+
+	For example, given the following binary tree:
+	
+	   1
+	 /   \
+	2     3
+	 \
+	  5
+	All root-to-leaf paths are:
+	
+	["1->2->5", "1->3"]
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P257_BinaryTreePaths.java
+ * @type        P257_BinaryTreePaths
+ * @date        2016年12月12日 下午10:05:01
+ * @details     
+ */
+public class P257_BinaryTreePaths {
+	public static void main(String[] args) {
+		
+	}
+	static class  Solution {
+	    public List<String> binaryTreePaths(TreeNode root) {
+	        
+	    }
+	}
+	
+}
