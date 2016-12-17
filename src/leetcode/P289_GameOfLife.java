c7fe728f583ad97ca61cf80f76d228f137d3d22d
zxwtry <zxwtry@qq.com>
2016-12-17 22:28:57 +0800

    添加 leetcode P289_GameOfLife

---
+++ src/leetcode/P289_GameOfLife.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/**
+ * 	According to the Wikipedia's article: "The Game of Life, also known 
+ * 	simply as Life, is a cellular automaton devised by the British mathematician
+ *  John Horton Conway in 1970."
+
+	Given a board with m by n cells, each cell has an initial state live 
+	(1) or dead (0). Each cell interacts with its eight neighbors (horizontal, 
+	vertical, diagonal) using the following four rules (taken from the above 
+	Wikipedia article):
+	
+	Any live cell with fewer than two live neighbors dies, as if caused by
+	under-population.
+	Any live cell with two or three live neighbors lives on to the next generation.
+	Any live cell with more than three live neighbors dies, as if by over-population..
+	Any dead cell with exactly three live neighbors becomes a live cell, as if 
+	by reproduction.
+	Write a function to compute the next state (after one update) of the board
+	given its current state.
+	
+	Follow up: 
+	Could you solve it in-place? Remember that the board needs to be updated 
+	at the same time: You cannot update some cells first and then use their 
+	updated values to update other cells.
+	In this question, we represent the board using a 2D array. In principle, 
+	the board is infinite, which would cause problems when the active area 
+	encroaches the border of the array. How would you address these problems?
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P289_GameOfLife.java
+ * @type        P289_GameOfLife
+ * @date        2016年12月17日 下午10:27:41
+ * @details     
+ */
+public class P289_GameOfLife {
+
+}
