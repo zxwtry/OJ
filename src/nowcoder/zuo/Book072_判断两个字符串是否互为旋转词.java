26c5853d91c17ed299e6f51809a6014c7e18d063
zxwtry <zxwtry@qq.com>
2016-12-17 21:56:18 +0800

    添加 nowcoder zuo Book072_判断两个字符串是否互为旋转词

---
+++ src/nowcoder/zuo/Book072_判断两个字符串是否互为旋转词.java
@@ -0,0 +1,29 @@
+package nowcoder.zuo;
+
+/**
+ * 	如果一个字符串s，把字符串s前面任意的部分挪到后面
+ * 	形成的字符串叫座s的旋转词。
+ * 	例如：
+ * 		s="12345"
+ * 		s的旋转词有："12345"、"23451"、"34512"、"45123"、"51234"
+ * 	给定两个字符串a和b，判断a和b是否互为旋转词。
+ * 
+ * 	举例：
+ * 		a="cdab"，b="abcd"，返回true
+ * 		a="1ab2"，b="ab12"，返回false
+ * 		a="2ab1"，b="ab12"，返回true
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book072_判断两个字符串是否互为旋转词.java
+ * @type        Book072_判断两个字符串是否互为旋转词
+ * @date        2016年12月17日 下午9:52:14
+ * @details     
+ */
+public class Book072_判断两个字符串是否互为旋转词 {
+
+}
