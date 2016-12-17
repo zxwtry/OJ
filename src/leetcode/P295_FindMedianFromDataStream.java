2d8fff8c109fb3a8429db88b717259486ff42e5c
zxwtry <zxwtry@qq.com>
2016-12-17 22:33:06 +0800

    添加 leetcode

---
+++ src/leetcode/P295_FindMedianFromDataStream.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+/**
+ * 	Median is the middle value in an ordered integer list. 
+ * 	If the size of the list is even, there is no middle value. 
+ * 	So the median is the mean of the two middle value.
+
+	Examples: 
+	[2,3,4] , the median is 3
+	
+	[2,3], the median is (2 + 3) / 2 = 2.5
+	
+	Design a data structure that supports the following two operations:
+	
+	void addNum(int num) - Add a integer number from the data stream to 
+	the data structure.
+	double findMedian() - Return the median of all elements so far.
+	For example:
+	
+	add(1)
+	add(2)
+	findMedian() -> 1.5
+	add(3) 
+	findMedian() -> 2
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P295_FindMedianFromDataStream.java
+ * @type        P295_FindMedianFromDataStream
+ * @date        2016年12月17日 下午10:31:58
+ * @details     
+ */
+public class P295_FindMedianFromDataStream {
+	static class MedianFinder {
+
+	    // Adds a number into the data structure.
+	    public void addNum(int num) {
+	        
+	    }
+
+	    // Returns the median of current data stream
+	    public double findMedian() {
+	        
+	    }
+	};
+
+	// Your MedianFinder object will be instantiated and called as such:
+	// MedianFinder mf = new MedianFinder();
+	// mf.addNum(1);
+	// mf.findMedian();
+}
