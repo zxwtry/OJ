1593eb732d33d9b24ce964f0c3aba69cfcdf9c8e
zxwtry <zxwtry@qq.com>
2016-12-17 22:30:01 +0800

    添加 leetcode P290_WordPattern

---
+++ src/leetcode/P290_WordPattern.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 	Given a pattern and a string str, find if str follows the same pattern.
+
+	Here follow means a full match, such that there is a bijection between 
+	a letter in pattern and a non-empty word in str.
+	
+	Examples:
+	pattern = "abba", str = "dog cat cat dog" should return true.
+	pattern = "abba", str = "dog cat cat fish" should return false.
+	pattern = "aaaa", str = "dog cat cat dog" should return false.
+	pattern = "abba", str = "dog dog dog dog" should return false.
+	Notes:
+	You may assume pattern contains only lowercase letters, and str 
+	contains lowercase letters separated by a single space
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P290_WordPattern.java
+ * @type        P290_WordPattern
+ * @date        2016年12月17日 下午10:29:25
+ * @details     
+ */
+public class P290_WordPattern {
+
+}
