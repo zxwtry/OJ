5b5a176de9b573c38f98aa147eeb32b292e59250
zxwtry <zxwtry@qq.com>
2016-12-17 21:43:41 +0800

    添加 nowcoder zuo Book071_去掉字符串中连续出现K个0的子串

---
+++ src/nowcoder/zuo/Book071_去掉字符串中连续出现K个0的子串.java
@@ -0,0 +1,25 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个字符串s和一个整数k，
+ * 	如果s中正好有连续的k个'0'字符出现时，
+ * 	把k个连续'0'字符去除，返回处理后的字符串
+ * 
+ * 	举例：
+ * 		s="A00B"，k=2，返回"AB"
+ * 		s="A0000B000"，k=3，返回"A0000B"
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book071_去掉字符串中连续出现K个0的子串.java
+ * @type        Book071_去掉字符串中连续出现K个0的子串
+ * @date        2016年12月17日 下午4:28:21
+ * @details     
+ */
+public class Book071_去掉字符串中连续出现K个0的子串 {
+	
+}
