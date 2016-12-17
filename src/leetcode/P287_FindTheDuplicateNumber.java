d962c14188a17c51fe740e489dd907850df55aab
zxwtry <zxwtry@qq.com>
2016-12-17 22:27:07 +0800

    添加 leetcode P287_FindTheDuplicateNumber

---
+++ src/leetcode/P287_FindTheDuplicateNumber.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * 	Given an array nums containing n + 1 integers where each integer is 
+ * 	between 1 and n (inclusive), prove that at least one duplicate number
+ *  must exist. Assume that there is only one duplicate number, find the 
+ *  duplicate one.
+
+	Note:
+	You must not modify the array (assume the array is read only).
+	You must use only constant, O(1) extra space.
+	Your runtime complexity should be less than O(n2).
+	There is only one duplicate number in the array, but it could 
+	be repeated more than once.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P287_FindTheDuplicateNumber.java
+ * @type        P287_FindTheDuplicateNumber
+ * @date        2016年12月17日 下午10:26:14
+ * @details     
+ */
+public class P287_FindTheDuplicateNumber {
+
+}
