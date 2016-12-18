76e41151588f37b2d1ccea37650bca0906a3fdf1
zxwtry <zxwtry@qq.com>
2016-12-18 20:55:42 +0800

    添加 nowcoder zuo Book075_字符串的统计字符串

---
+++ src/nowcoder/zuo/Book075_字符串的统计字符串.java
@@ -0,0 +1,28 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个字符串s，返回s的统计字符串，
+ * 	例如："aaabbadddffc"的统计字符串为：
+ * 	"a_3_b_2_a_1_d_3_f_2_c_1"
+ * 
+ * 	补充题目：
+ * 		给定一个字符串的统计字符串cs，再给定一个index，返回
+ * 		cs所代表的原始字符串上的第index个字符。
+ * 		举例：
+ * 			"a_1_b_100"所代表的原始字符串中第0个字符是'a'
+ * 			"a_1_b_100"所代表的原始字符串中第50个字符是'b'
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book075_字符串的统计字符串.java
+ * @type        Book075_字符串的统计字符串
+ * @date        2016年12月18日 下午8:52:06
+ * @details     
+ */
+public class Book075_字符串的统计字符串 {
+
+}
