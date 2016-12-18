11ae76bd827c52f036f0642f27eb56fb727919c4
zxwtry <zxwtry@qq.com>
2016-12-18 20:24:13 +0800

    添加 nowcoder zuo Book074_替换字符串中连续出现的指定字符串

---
+++ src/nowcoder/zuo/Book074_替换字符串中连续出现的指定字符串.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定三个字符串s, f, t，已知f字符串中无重复字符，
+ * 	把s中所有f的子串全部替换成t字符串，
+ * 	对于连续出现f的部分，要求只替换成一个t字符串，返回最终的结果字符串。
+ * 
+ * 	举例：
+ * 		s="123abc" f="abc" t="4567" 返回"1234567"
+ * 		s="123" f="abc" t="4567" 返回"123"
+ * 		s="123abcabc" f="abc" t="4567" 返回"1234567"
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book074_替换字符串中连续出现的指定字符串.java
+ * @type        Book074_替换字符串中连续出现的指定字符串
+ * @date        2016年12月18日 下午7:41:38
+ * @details     
+ */
+public class Book074_替换字符串中连续出现的指定字符串 {
+	
+}
