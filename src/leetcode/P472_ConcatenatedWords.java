d0cea71c6bf63b9539e5604f7dd5f8b4921edc7f
zxwtry <zxwtry@qq.com>
2016-12-18 16:27:04 +0800

    添加 leetcode P472_ConcatenatedWords

---
+++ src/leetcode/P472_ConcatenatedWords.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * 	Given a list of words, please write a program that returns all concatenated words
+ * 	in the given list of words.
+
+	A concatenated word is defined as a string that is comprised entirely of at least
+	 two shorter words in the given array.
+	
+	Example:
+	Input: ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat",
+	"ratcatdogcat"]
+	
+	Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+	
+	Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+	 "dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+	"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+	Note:
+	The number of elements of the given array will not exceed 10,000
+	The length sum of elements in the given array will not exceed 600,000.
+	The returned elements order does not matter.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P472_ConcatenatedWords.java
+ * @type        P472_ConcatenatedWords
+ * @date        2016年12月18日 上午11:40:43
+ * @details     
+ */
+public class P472_ConcatenatedWords {}
