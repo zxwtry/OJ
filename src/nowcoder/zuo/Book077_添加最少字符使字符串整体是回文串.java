d708e1582e4a70154b08e9021f4e634e1968cea1
zxwtry <zxwtry@qq.com>
2016-12-20 22:04:25 +0800

    添加 nowcoder zuo Book077_添加最少字符使字符串整体是回文串

---
+++ src/nowcoder/zuo/Book077_添加最少字符使字符串整体是回文串.java
@@ -0,0 +1,30 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个字符串s，如果可以在s的任何为止添加字符，
+ * 	要使s整体回文，且添加最少字符。
+ * 	举例：
+ * 		s="ABA"，返回"ABA"
+ * 		s="AB"，返回"ABA"、"BAB"都行
+ * 
+ * 	进阶：
+ * 		给定一个字符串s，给定s的最长回文子序列字符串p
+ * 		返回添加字符最少的情况下，让s整体回文。
+ * 		举例：
+ * 			s="A1B21C"，p="121"
+ * 			返回"AC1B2B1CA"或"CA1B2B1AC"都行
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book077_添加最少字符使字符串整体是回文串.java
+ * @type        Book077_添加最少字符使字符串整体是回文串
+ * @date        2016年12月20日 上午10:33:20
+ * @details     
+ */
+public class Book077_添加最少字符使字符串整体是回文串 { 
+	
+}
