bb1248220e0563cfd581eff67eb3f5f72a163755
zxwtry <zxwtry@qq.com>
2016-12-21 17:55:55 +0800

    添加 nowcoder zuo Book083_最小包含子串的长度

---
+++ src/nowcoder/zuo/Book083_最小包含子串的长度.java
@@ -0,0 +1,22 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定字符串s1和s2，求s1的子串中&&包含s2所有的字符 中最小子串的长度
+ * 	举例：
+ * 		s1="abcde"，s2="ac"，返回3 ("abc")
+ * 		s1="12345"，s2="344"，返回0 (没有两个4)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book083_最小包含子串的长度.java
+ * @type        Book083_最小包含子串的长度
+ * @date        2016年12月21日 下午5:53:13
+ * @details     
+ */
+public class Book083_最小包含子串的长度 {
+
+}
