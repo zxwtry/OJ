dfe96311c8521a593f90e9e16f9fd3e12442f857
zxwtry <zxwtry@qq.com>
2016-12-21 09:55:04 +0800

    添加 nowcoder zuo Book077_括号字符串的有效性和最长有效长度

---
+++ src/nowcoder/zuo/Book077_括号字符串的有效性和最长有效长度.java
@@ -0,0 +1,31 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个字符串s，判断是不是整体有效的括号字符串
+ * 	举例：
+ * 		s="()"，返回true
+ * 		s="(()())"，返回true
+ * 		s="(())"，返回true
+ * 		s="())"，返回false
+ * 		s="(()"，返回false
+ * 		s="()a()"，返回false
+ * 	补充问题：
+ * 		给定一个括号字符串s，返回最长的有效括号子串
+ * 		s="(()())"，返回6
+ * 		s="())"，返回2
+ * 		s="()(()()("，返回4
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book077_括号字符串的有效性和最长有效长度.java
+ * @type        Book077_括号字符串的有效性和最长有效长度
+ * @date        2016年12月21日 上午9:50:51
+ * @details     
+ */
+public class Book077_括号字符串的有效性和最长有效长度 {
+
+}
