5257c42681fc1fd01d501eec4a8fd797415ec88c
zxwtry <zxwtry@qq.com>
2016-12-22 09:35:13 +0800

    添加 nowcoder zuo Book083_最小包含子串的长度 Solution

--- src/nowcoder/zuo/Book083_最小包含子串的长度.java
+++ src/nowcoder/zuo/Book083_最小包含子串的长度.java
@@ -18,5 +18,24 @@ package nowcoder.zuo;
  * @details     
  */
 public class Book083_最小包含子串的长度 {
-
+	static class Solution {
+		public int minLength(String s1, String s2) {
+			if (s1 == null || s2 == null || s1.length() < s2.length()) return 0;
+			int[] map = new int[256];
+			for (int i = 0; i < s2.length(); i ++) map[s2.charAt(i)] ++;
+			int l = 0, r = 0, m = s2.length(), a = Integer.MAX_VALUE;
+			while (r != s1.length()) {
+				map[s1.charAt(r)] --;
+				if (map[s1.charAt(r)] >= 0) m --;
+				if (m == 0) {
+					while (map[s1.charAt(l)] < 0) map[s1.charAt(l++)] ++;
+					a = Math.min(a, r-l+1);
+					m ++;
+					map[s1.charAt(l++)] ++;
+				}
+				r ++;
+			}
+			return a == Integer.MAX_VALUE ? 0 : a;
+		}
+	}
 }
