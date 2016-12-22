1695825731bda102f3592a72975decf27e846435
zxwtry <zxwtry@qq.com>
2016-12-22 16:30:18 +0800

    添加 nowcoder zuo Book085_字符串匹配问题

---
+++ src/nowcoder/zuo/Book085_字符串匹配问题.java
@@ -0,0 +1,31 @@
+package nowcoder.zuo;
+
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
+/**
+ * 	字符串匹配问题
+ * 	给定字符串s，其中不包含'.'和'*'
+ * 	给定字符串p，其中可以包含'.'和'*'
+ * 	'*'字符不能是p的首字符，并且任意两个'*'字符都不相邻
+ * 	p中的'.'代表任何一个字符
+ * 	p中的'*'代表'*'的前一个字符可以有0个或多个
+ * 	举例：
+ * 		s="abc", p="abc", 返回true
+ * 		s="abc", p="a.c", 返回true
+ * 		s="abc", p=".*", 返回true
+ * 		s="", p="..*", 返回false
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book085_字符串匹配问题.java
+ * @type        Book085_字符串匹配问题
+ * @date        2016年12月22日 下午3:54:09
+ * @details     
+ */
+public class Book085_字符串匹配问题 {
+	
+}
