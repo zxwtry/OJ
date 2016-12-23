0dd132c7b8c4a100b3be5424e60ce779df19aee3
zxwtry <zxwtry@qq.com>
2016-12-23 21:50:37 +0800

    添加 format leetcode P230 P233

--- src/leetcode/P230_KthSmallestElementInABST.java
+++ src/leetcode/P230_KthSmallestElementInABST.java
@@ -1,13 +1,16 @@
 package leetcode;
 
 /*
- * 	Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+ * 	Given a binary search tree, write a function kthSmallest to find the kth 
+ * smallest element in it.
 
 	Note: 
 	You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
 	
 	Follow up:
-	What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+	What if the BST is modified (insert/delete operations) often and you 
+	need to find the kth smallest frequently? How would you optimize 
+	the kthSmallest routine?
 	
 	Hint:
 	
--- src/leetcode/P233_NumberofDigitOne.java
+++ src/leetcode/P233_NumberofDigitOne.java
@@ -1,13 +1,14 @@
 package leetcode;
 
 /**
- * Given an integer n, count the total number of digit 1 appearing 
- * in all non-negative integers less than or equal to n.
+ * 	Given an integer n, count the total number of digit 1 appearing 
+ * 	in all non-negative integers less than or equal to n.
  * 
 
 	For example:
 	Given n = 13,
-	Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+	Return 6, because digit 1 occurred in the following numbers:
+	 1, 10, 11, 12, 13.
 	
 	Hint:
 	
@@ -26,11 +27,33 @@ package leetcode;
  */
 public class P233_NumberofDigitOne {
 	public static void main(String[] args) {
-		
+		System.out.println(new Solution().countDigitOne(13));
 	}
 	static class Solution {
 	    public int countDigitOne(int n) {
-	        
+	    	if (n < 10) return 1;
+	        String s = String.valueOf(n);
+	        int max = 1;
+	        while (max <= n) {
+	        	max = max * 10;
+	        }
+	        max = max / 100;
+	        System.out.println(max);
+	        int ans = 0;
+	        if (s.charAt(0) > '1') {
+		        ans += (s.length() - 1) * max;
+	        	ans += ans * (s.charAt(0) - '1');
+	        	ans += countDigitOne(n - (s.charAt(0) - '0') * max * 10);
+	        	ans += max * 10;
+	        	return ans;
+	        } else {
+	        	ans += (s.length() - 1) * max;
+	        	ans += ans * (s.charAt(0) - '1');
+	        	ans += n - (s.charAt(0) - '0') * max * 10 + 1;
+	        	return ans;
+	        }
+	        return
 	    }
+	    
 	}
 }
