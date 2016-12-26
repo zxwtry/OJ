51a2c7219daab96e3c30e789a8a737a3df798205
zxwtry <zxwtry@qq.com>
2016-12-26 21:41:43 +0800

    添加 nowcoder zuo Book094_找到数组中出现次数大于N_K的数

--- src/nowcoder/zuo/Book094_找到数组中出现次数大于N_K的数.java
+++ src/nowcoder/zuo/Book094_找到数组中出现次数大于N_K的数.java
@@ -19,8 +19,32 @@ package nowcoder.zuo;
  * @file        Book094_找到数组中出现次数大于N_K的数.java
  * @type        Book094_找到数组中出现次数大于N_K的数
  * @date        2016年12月26日 下午9:31:03
- * @details     
+ * @details     Solution1:原问题
  */
 public class Book094_找到数组中出现次数大于N_K的数 {
-
+	static class Soltuion1 {
+		public void printHalfMajor(int[] arr) {
+			int cand = 0;
+			int times = 0;
+			for (int i = 0; i != arr.length; i ++) {
+				if (times == 0) {
+					cand = arr[i];
+					times = 1;
+				} else if (arr[i] == cand) {
+					times ++;
+				} else {
+					times --;
+				}
+			}
+			times = 0;
+			for (int i = 0; i != arr.length; i ++) {
+				if (arr[i] == cand)
+					times ++;
+			}
+			if (times > arr.length / 2)
+				System.out.println(cand);
+			else
+				System.out.println("no such number.");
+		}
+	}
 }
