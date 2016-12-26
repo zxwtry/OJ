6f86fd2ddd31c99213079ae30f1460c11881799d
zxwtry <zxwtry@qq.com>
2016-12-26 21:30:35 +0800

    添加 nowcoder zuo Book093_需要排序的最短子数组长度

--- src/nowcoder/zuo/Book093_需要排序的最短子数组长度.java
+++ src/nowcoder/zuo/Book093_需要排序的最短子数组长度.java
@@ -18,6 +18,20 @@ package nowcoder.zuo;
  */
 public class Book093_需要排序的最短子数组长度 {
 	static class Solution {
-		
+		public int getMinLength(int[] arr) {
+			if (arr == null || arr.length < 2) return 0;
+			int min = arr[arr.length - 1];
+			int noMinIndex = -1;
+			for (int i = arr.length - 2; i != -1; i --)
+				if (arr[i] > min) noMinIndex = i;
+				else min = Math.min(min, arr[i]);
+			if (noMinIndex == -1) return 0;
+			int max = arr[0];
+			int noMaxIndex = -1;
+			for (int i = 1; i != arr.length; i ++)
+				if (arr[i] < max) noMaxIndex = i;
+				else max = Math.max(max, arr[i]);
+			return noMaxIndex - noMinIndex + 1;
+		}
 	}
 }
