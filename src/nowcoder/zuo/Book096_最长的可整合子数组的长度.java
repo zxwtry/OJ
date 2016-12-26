dc6378e73d48ef0556e66f78015c0405c5549505
zxwtry <zxwtry@qq.com>
2016-12-26 22:05:50 +0800

    添加 nowcoder zuo Book096_最长的可整合子数组的长度

---
+++ src/nowcoder/zuo/Book096_最长的可整合子数组的长度.java
@@ -0,0 +1,23 @@
+package nowcoder.zuo;
+
+import java.util.Arrays;
+
+/**
+ * 	先给出可整合数组的定义。如果一个数组在排序之后，每相邻两个数之差的绝对值
+ * 	都为1，则该数组为可整合数组。
+ * 	例如：	[5,3,4,6,2]，排序之后为[2,3,4,5,6]，符合。
+ * 	给定一个整型数组，请返回其中最大可整合子数组的长度。
+ * 	例如：	[5,5,3,2,6,4,3]，的最大可整合子数组为[5,3,2,6,4]，所以返回5
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book096_最长的可整合子数组的长度.java
+ * @type        Book096_最长的可整合子数组的长度
+ * @date        2016年12月26日 下午9:59:02
+ * @details     
+ */
+public class Book096_最长的可整合子数组的长度 {}
