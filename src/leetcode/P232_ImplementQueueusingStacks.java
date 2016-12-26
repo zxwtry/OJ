a4cd6e2a9e82d83b24add5380f6b1270990d7259
zxwtry <zxwtry@qq.com>
2016-12-26 10:17:47 +0800

    添加 leetcode P232_ImplementQueueusingStacks

--- src/leetcode/P232_ImplementQueueusingStacks.java
+++ src/leetcode/P232_ImplementQueueusingStacks.java
@@ -1,7 +1,9 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
- * Implement the following operations of a queue using stacks.
+ * 	Implement the following operations of a queue using stacks.
 
 	push(x) -- Push element x to the back of queue.
 	pop() -- Removes the element from in front of queue.
@@ -26,31 +28,46 @@ package leetcode;
  * @file        P232_ImplementQueueusingStacks.java
  * @type        P232_ImplementQueueusingStacks
  * @date        2016年12月9日 下午10:10:17
- * @details     
+ * @details     MyQueue AC 99ms 63.30%
  */
 public class P232_ImplementQueueusingStacks {
-	public static void main(String[] args) {
-		
-	}
 	static class MyQueue {
+		Stack<Integer> s1 = new Stack<Integer>();
+		Stack<Integer> s2 = new Stack<Integer>();
 	    // Push element x to the back of queue.
 	    public void push(int x) {
-	        
+	        s1.push(x);
 	    }
-
 	    // Removes the element from in front of queue.
 	    public void pop() {
-	        
+	    	if (! s2.isEmpty()) {
+	    		s2.pop();
+	    		return;
+	    	}
+	        while (! s1.isEmpty()) {
+	        	s2.push(s1.pop());
+	        }
+	        s2.pop();
 	    }
-
 	    // Get the front element.
 	    public int peek() {
-	        
+	    	if (! s2.isEmpty()) {
+	    		return s2.peek();
+	    	}
+	    	while (! s1.isEmpty()) {
+	        	s2.push(s1.pop());
+	        }
+	    	return s2.peek();
 	    }
-
 	    // Return whether the queue is empty.
 	    public boolean empty() {
-	        
+	    	if (! s2.isEmpty()) {
+	    		return false;
+	    	}
+	    	while (! s1.isEmpty()) {
+	        	s2.push(s1.pop());
+	        }
+	    	return s2.isEmpty();
 	    }
 	}
 }
