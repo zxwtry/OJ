e83eb5141fb24b290e2a0b6142ec30916a72800e
zxwtry <zxwtry@qq.com>
2016-12-26 22:06:09 +0800

    添加 nowcoder zuo Book096_最长的可整合子数组的长度 Solution1

--- src/nowcoder/zuo/Book096_最长的可整合子数组的长度.java
+++ src/nowcoder/zuo/Book096_最长的可整合子数组的长度.java
@@ -20,4 +20,28 @@ import java.util.Arrays;
  * @date        2016年12月26日 下午9:59:02
  * @details     
  */
-public class Book096_最长的可整合子数组的长度 {}
+public class Book096_最长的可整合子数组的长度 {
+	static class Solution1 {
+		public int getLIT1(int[] arr) {
+			if (arr == null || arr.length == 0) return 0;
+			int len = 0;
+			for (int i = 0; i < arr.length; i ++) {
+				for (int j = i; j < arr.length; j ++)
+				{
+					if (isIntegrated(arr, i, j))
+						len = Math.max(len, j-i+1);
+					
+					}
+				}
+			return len;
+		}
+		private boolean isIntegrated(int[] arr, int left, int right) {
+			int[] newArr = Arrays.copyOfRange(arr, left, right + 1);
+			Arrays.sort(newArr);
+			for (int i = 0; i < newArr.length; i ++)
+				if (newArr[i - 1] != newArr[i] - 1)
+					return false;
+			return true;
+		}
+	}
+}
