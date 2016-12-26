36a1f3483236ac9e2217b4ffa2b5faf880449553
zxwtry <zxwtry@qq.com>
2016-12-26 21:33:36 +0800

    添加 nowcoder zuo Book094_找到数组中出现次数大于N_K的数

---
+++ src/nowcoder/zuo/Book094_找到数组中出现次数大于N_K的数.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个整型数组arr，打印其中出现次数大于一半的数，
+ * 	如果没有这样的数，打印提示信息
+ * 	要求：时间O(N)，空间O(1)	
+ * 
+ * 	进阶：
+ * 		给定一个整型数组arr，和整数K，打印其中出现次数大于N/K的数，
+ * 		如果没有这样的数，打印提示信息
+ * 		要求：时间O(N*K)，空间O(K)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book094_找到数组中出现次数大于N_K的数.java
+ * @type        Book094_找到数组中出现次数大于N_K的数
+ * @date        2016年12月26日 下午9:31:03
+ * @details     
+ */
+public class Book094_找到数组中出现次数大于N_K的数 {
+
+}
