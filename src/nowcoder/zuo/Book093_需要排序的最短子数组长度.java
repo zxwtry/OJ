c64523b858bdbc98d7c6049f017fb9ca9120e050
zxwtry <zxwtry@qq.com>
2016-12-26 21:26:19 +0800

    添加 nowcoder zuo Book093_需要排序的最短子数组长度

---
+++ src/nowcoder/zuo/Book093_需要排序的最短子数组长度.java
@@ -0,0 +1,23 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定一个无序数组arr，求出需要排序的最短子数组长度
+ * 	举例：
+ * 		arr=[1,5,3,4,2,6,7]，返回4，因为只有[5,3,4,2]需要排序
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book093_需要排序的最短子数组长度.java
+ * @type        Book093_需要排序的最短子数组长度
+ * @date        2016年12月26日 下午9:24:22
+ * @details     
+ */
+public class Book093_需要排序的最短子数组长度 {
+	static class Solution {
+		
+	}
+}
