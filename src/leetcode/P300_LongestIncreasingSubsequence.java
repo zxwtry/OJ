91c469104bf1a55ac7f29facf56c1c2e86d83088
zxwtry <zxwtry@qq.com>
2016-12-28 22:20:07 +0800

    添加 leetcode P300_LongestIncreasingSubsequence

---
+++ src/leetcode/P300_LongestIncreasingSubsequence.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * 	Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+	For example,
+	Given [10, 9, 2, 5, 3, 7, 101, 18],
+	The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. 
+	Note that there may be more than one LIS combination, it is only necessary for 
+	you to return the length.
+	
+	Your algorithm should run in O(n2) complexity.
+	
+	Follow up: Could you improve it to O(n log n) time complexity?
+	
+	Credits:
+	Special thanks to @pbrother for adding this problem and creating all test cases.
+ * 	
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P300_LongestIncreasingSubsequence.java
+ * @type        P300_LongestIncreasingSubsequence
+ * @date        2016年12月28日 下午10:18:57
+ * @details     
+ */
+public class P300_LongestIncreasingSubsequence {
+	static class Solution {
+	    public int lengthOfLIS(int[] nums) {
+	        
+	    }
+	}
+}
