3d911cce9447330f627cac55fd167bc1b61b1067
zxwtry <zxwtry@qq.com>
2016-12-28 22:27:12 +0800

    添加 leetcode P303_RangeSumQueryImmutable

---
+++ src/leetcode/P303_RangeSumQueryImmutable.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+/**
+ * 	Given an integer array nums, find the sum of the elements between 
+ * 	indices i and j (i ≤ j), inclusive.
+
+	Example:
+	Given nums = [-2, 0, 3, -5, 2, -1]
+	
+	sumRange(0, 2) -> 1
+	sumRange(2, 5) -> -1
+	sumRange(0, 5) -> -3
+	Note:
+	You may assume that the array does not change.
+	There are many calls to sumRange function.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P303_RangeSumQueryImmutable.java
+ * @type        P303_RangeSumQueryImmutable
+ * @date        2016年12月28日 下午10:25:59
+ * @details     
+ */
+public class P303_RangeSumQueryImmutable {
+	static class NumArray {
+
+	    public NumArray(int[] nums) {
+	        
+	    }
+
+	    public int sumRange(int i, int j) {
+	        
+	    }
+	}
+}
