05e2bb2634529cee7671adeafe84d9a00628479f
zxwtry <zxwtry@qq.com>
2016-12-29 20:47:43 +0800

    添加 leetcode P140_WordBreakII Solution2 AC 4.50%

--- src/leetcode/P140_WordBreakII.java
+++ src/leetcode/P140_WordBreakII.java
@@ -27,6 +27,7 @@ import java.util.Set;
  * @type        P140_WordBreakII
  * @date        2016年12月29日 下午8:39:47
  * @details     Solution1: AC 31 ms 3.30%
+ * @details     Solution2: AC 29 ms 4.50%
  */
 public class P140_WordBreakII {
 	static class Solution1 {
@@ -64,6 +65,49 @@ public class P140_WordBreakII {
 					st.insert(0, s);
 					a.add(st.toString());
 					st.delete(0, s.length());
+				}
+			}
+		}
+	}
+	static class Solution2 {
+		List<String> a = new LinkedList<String>();
+		char[] cs;
+		int ci;
+		LinkedList<String>[] dp;
+		@SuppressWarnings("unchecked")
+		public List<String> wordBreak(String s, Set<String> wd) {
+			if (s == null || s.length() == 0 || wd == null || wd.size() == 0) return a;
+			dp = new LinkedList[s.length()];
+			cs = new char[s.length() * 2];
+			ci = cs.length - 1;
+			for (int i = 0; i < dp.length; i ++) {
+				for (String w : wd) {
+					int j = i+1-w.length();
+					if (j < 0) continue;
+					if (s.substring(j, i+1).equals(w) && 
+							(j == 0 || dp[j-1] != null)) {
+						if (dp[i] == null) dp[i] = new LinkedList<String>();
+						dp[i].add(w);
+					}
+				}
+			}
+			dfs(dp.length - 1);
+			return a;
+		}
+		private void dfs(int i) {
+			if (i < 0) return;
+			if (dp[i] != null)
+			for (String s : dp[i]) {
+				int j = i - s.length();
+				for (int k = s.length() - 1; k > -1; k --)
+					cs[ci --] = s.charAt(k);
+				if (j != -1) {
+					cs[ci --] = ' ';
+					dfs(j);
+					ci += s.length()+1;
+				} else {
+					a.add(new String(cs, ci + 1, cs.length - ci - 1));
+					ci += s.length();
 				}
 			}
 		}
