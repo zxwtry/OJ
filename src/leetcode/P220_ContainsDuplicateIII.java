1f05cad2dd800a5bbeb713b6d7c2ae694faed767
zxwtry <zxwtry@qq.com>
2016-12-29 21:28:56 +0800

    添加 leetcode P220_ContainsDuplicateIII Solution1 AC 37.03%

--- src/leetcode/P220_ContainsDuplicateIII.java
+++ src/leetcode/P220_ContainsDuplicateIII.java
@@ -1,17 +1,36 @@
 package leetcode;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * 	Given an array of integers, find out whether there are 
+ * 	two distinct indices i and j in the array such that the difference 
+ * 	between nums[i] and nums[j] is at most t and the difference between 
+ * 	i and j is at most k.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P220_ContainsDuplicateIII.java
+ * @type        P220_ContainsDuplicateIII
+ * @date        2016年12月29日 下午8:57:50
+ * @details     Solution1 : 54 ms 37.03% 这个解法太BUG了
+ */
 public class P220_ContainsDuplicateIII {
-	public static void main(String[] args) {
-		syso
-	}
-	static class Solution {
-		int[] maxK = null;
-		int[] minK = null;
-	    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-	    	
-	        maxK = new int[nums.length];
-	        minK = new int[nums.length];
-	        
+	static class Solution1 {
+	    public boolean containsNearbyAlmostDuplicate(int[] arr, int k, int t) {
+	        if(k < 1 || t < 0 || arr == null || arr.length < 2) return false;
+	        SortedSet<Long> set = new TreeSet<Long>();
+	        for(int j = 0; j < arr.length; j ++) {
+	            SortedSet<Long> subSet =  set.subSet((long)arr[j]-t, (long)arr[j]+t+1);
+	            if(!subSet.isEmpty()) return true;
+	            if(j >= k) set.remove((long)arr[j-k]);
+	            set.add((long)arr[j]);
+	        }
 	        return false;
 	    }
 	}
