9a64049f949bb2ba94cc63e7ba6b81e8cad32f82
zxwtry <zxwtry@qq.com>
2016-12-29 22:27:03 +0800

    添加 leetcode P315_CountOfSmallerNumbersAfterSelf

---
+++ src/leetcode/P315_CountOfSmallerNumbersAfterSelf.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ *  You are given an integer array nums and you have to return 
+ *  a new counts array. The counts array has the property where counts[i] 
+ *  is the number of smaller elements to the right of nums[i].
+	
+	Example:
+	
+	Given nums = [5, 2, 6, 1]
+	
+	To the right of 5 there are 2 smaller elements (2 and 1).
+	To the right of 2 there is only 1 smaller element (1).
+	To the right of 6 there is 1 smaller element (1).
+	To the right of 1 there is 0 smaller element.
+	Return the array [2, 1, 1, 0].
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P315_CountOfSmallerNumbersAfterSelf.java
+ * @type        P315_CountOfSmallerNumbersAfterSelf
+ * @date        2016年12月29日 下午10:25:51
+ * @details     
+ */
+public class P315_CountOfSmallerNumbersAfterSelf {
+	static class Solution {
+	    public List<Integer> countSmaller(int[] nums) {
+	        
+	    }
+	}
+}
