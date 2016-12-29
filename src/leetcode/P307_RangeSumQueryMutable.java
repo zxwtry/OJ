b9310fe5b1c75bdaaa6e5e9ce63ba37dd4545cb1
zxwtry <zxwtry@qq.com>
2016-12-29 20:52:57 +0800

    添加 leetcode P307_RangeSumQueryMutable

---
+++ src/leetcode/P307_RangeSumQueryMutable.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+/**
+ * 	Given an integer array nums, find the sum of the elements between indices 
+ * 	i and j (i ≤ j), inclusive.
+
+	The update(i, val) function modifies nums by updating the element at 
+	index i to val.
+	Example:
+	Given nums = [1, 3, 5]
+	
+	sumRange(0, 2) -> 9
+	update(1, 2)
+	sumRange(0, 2) -> 8
+	Note:
+	The array is only modifiable by the update function.
+	You may assume the number of calls to update and sumRange function 
+	is distributed evenly.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P307_RangeSumQueryMutable.java
+ * @type        P307_RangeSumQueryMutable
+ * @date        2016年12月29日 下午8:52:11
+ * @details     
+ */
+public class P307_RangeSumQueryMutable {
+	static class NumArray {
+
+	    public NumArray(int[] nums) {
+	        
+	    }
+
+	    void update(int i, int val) {
+	        
+	    }
+
+	    public int sumRange(int i, int j) {
+	        
+	    }
+	}
+
+
+	// Your NumArray object will be instantiated and called as such:
+	// NumArray numArray = new NumArray(nums);
+	// numArray.sumRange(0, 1);
+	// numArray.update(1, 10);
+	// numArray.sumRange(1, 2);
+}
