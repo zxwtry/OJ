66a44b05c05787a943a1fb52e79785023be91c64
zxwtry <zxwtry@qq.com>
2016-12-30 10:03:11 +0800

    添加 leetcode P230_KthSmallestElementInABST Solution1 AC 57.78%

--- src/leetcode/P230_KthSmallestElementInABST.java
+++ src/leetcode/P230_KthSmallestElementInABST.java
@@ -1,8 +1,10 @@
 package leetcode;
 
-/*
+import tools.TreeNode辅助.TreeNode;
+
+/**
  * 	Given a binary search tree, write a function kthSmallest to find the kth 
- * smallest element in it.
+ *  smallest element in it.
 
 	Note: 
 	You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
@@ -17,12 +19,33 @@ package leetcode;
 	Try to utilize the property of a BST.
 	What if you could modify the BST node's structure?
 	The optimal runtime complexity is O(height of BST).
-	Credits:
-	Special thanks to @ts for adding this problem and creating all test cases.
  */
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P230_KthSmallestElementInABST.java
+ * @type        P230_KthSmallestElementInABST
+ * @date        2016年12月30日 上午10:02:18
+ * @details     Solution1: AC 1ms 51.78%
+ */
 public class P230_KthSmallestElementInABST {
-	public static void main(String[] args) {
-		syso
+	static class Solution1 {
+		int c = 0;
+		int save = 0;
+	    public int kthSmallest(TreeNode root, int k) {
+	    	c = k;
+	    	in(root);
+	        return save;
+	    }
+		private void in(TreeNode root) {
+			if (root == null) return;
+			in(root.left);
+			c --;
+			if (c == 0) save = root.val;
+			in(root.right);
+		}
 	}
 }
