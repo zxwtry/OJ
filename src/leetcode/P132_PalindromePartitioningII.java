1fb139d660915b06c8f3003415a9d5eb3d098747
zxwtry <zxwtry@qq.com>
2016-12-30 18:02:40 +0800

    添加  leetcode P132_PalindromePartitioningII Solution2 AC 37.04%

--- src/leetcode/P132_PalindromePartitioningII.java
+++ src/leetcode/P132_PalindromePartitioningII.java
@@ -20,6 +20,7 @@ package leetcode;
  * @type        P132_PalindromePartitioningII
  * @date        2016年12月13日 下午7:49:29
  * @details     Solution1: AC 52ms 14.09%
+ * @details     Solution2: AC 39ms 37.04%	manacher还可以优化
  */
 public class P132_PalindromePartitioningII {
 	static class Solution1 {
@@ -68,6 +69,54 @@ public class P132_PalindromePartitioningII {
 					R[i] = R[syi] + i < mti ? R[syi] : mti - i;
 				}
 			}
+		}
+	}
+	static class Solution2 {
+		int[] R = null;
+		public int minCut(String s) {
+			if (s == null || s.length() < 2) return 0;
+			init(s);
+			int[] ndp = new int[s.length()];
+			for (int i = 0; i < s.length(); i ++) {
+				ndp[i] = Integer.MAX_VALUE;
+				for (int j = 0; j <= i; j ++) {
+					if (! np(i, j)) continue;
+					if (j == 0) {
+						ndp[i] = 0;
+						break;
+					}
+					if (ndp[j-1] != Integer.MAX_VALUE)
+						ndp[i] = Math.min(ndp[j-1] + 1, ndp[i]);
+				}
+			}
+			return ndp[ndp.length - 1];
+		}
+		private boolean np(int i, int j) {
+			return R[(2*i+2*j+2)/2] > (i-j);
+		}
+		private void init(String s) {
+			R = new int[s.length()*2 + 1];
+			int mti = 0;	//maxTouchedIndex
+			int lci = 0;	//lastCircleIndex
+			int syi = 0;	//symmetric index about lci
+			for (int i = 0; i < R.length; i ++) {
+				syi = 2 * lci - i;
+				if (i >= mti || R[syi] + i == mti) {
+					int l = i, r = i;
+					while (l > 0 && r < R.length - 1 && getM(s, l) == getM(s, r)) {
+						l --;
+						r ++;
+					}
+					mti = r;
+					lci = i;
+					R[i] = (r - l) / 2;
+				} else {
+					R[i] = R[syi] + i < mti ? R[syi] : mti - i;
+				}
+			}
+		}
+		private char getM(String s, int i) {
+			return i % 2 == 1 ? s.charAt((i-1)/2) : 0;
 		}
 	}
 }
