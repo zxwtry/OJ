3ea2c1e5b90198f13961fedde27c8a751490ee8e
zxwtry <zxwtry@qq.com>
2016-12-30 22:08:27 +0800

    添加 leetcode P235_LowestCommonAncestorOfABinarySearchTree Solution2

--- src/leetcode/P235_LowestCommonAncestorOfABinarySearchTree.java
+++ src/leetcode/P235_LowestCommonAncestorOfABinarySearchTree.java
@@ -1,8 +1,10 @@
 package leetcode;
 
+import tools.TreeNode辅助.TreeNode;
+
 /**
- * Given a binary search tree (BST), find the lowest common ancestor (LCA) 
- * of two given nodes in the BST.
+ *  Given a binary search tree (BST), find the lowest common ancestor (LCA) 
+ *  of two given nodes in the BST.
 
 	According to the definition of LCA on Wikipedia: “The lowest common ancestor
 	 is defined between two nodes v and w as the lowest node in T that has both v
@@ -28,15 +30,32 @@ package leetcode;
  * @file        P235_LowestCommonAncestorOfABinarySearchTree.java
  * @type        P235_LowestCommonAncestorOfABinarySearchTree
  * @date        2016年12月9日 下午10:15:09
- * @details     
+ * @details     Solution1: AC 8ms 54.82%
+ * @details     Solution2: AC 8ms 54.82%
  */
 public class P235_LowestCommonAncestorOfABinarySearchTree {
-	public static void main(String[] args) {
-		
-	}
-	static class Solution {
+	static class Solution1 {
 	    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-	        
+	        if (calc(root, p, q) <= 0) return root;
+	        else
+	        	return root.val > p.val ? lowestCommonAncestor(root.left, p, q)
+	        			: lowestCommonAncestor(root.right, p, q);
+	    }
+	    private int calc(TreeNode p1, TreeNode p2, TreeNode p3) {
+	    	return (p1.val - p2.val) * (p1.val - p3.val);
+	    }
+	}
+	static class Solution2 {
+	    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+	    	while (true) {
+	    		if (calc(root, p, q) <= 0) return root;
+	    		root = root.val > p.val ? root.left : root.right;
+	    		if (root == null) break;
+	    	}
+	    	return null;
+	    }
+	    private int calc(TreeNode p1, TreeNode p2, TreeNode p3) {
+	    	return (p1.val - p2.val) * (p1.val - p3.val);
 	    }
 	}
 }
