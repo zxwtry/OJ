e1a8727556d9b2fcb7d38068b874e4dafc42dc2e
zxwtry <zxwtry@qq.com>
2016-12-30 21:55:25 +0800

    添加 leetcode P234_PalindromeLinkedList Solution AC 89.64%

--- src/leetcode/P234_PalindromeLinkedList.java
+++ src/leetcode/P234_PalindromeLinkedList.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import tools.ListNode辅助.ListNode;
+
 /**
  * 	Given a singly linked list, determine if it is a palindrome.
 
@@ -15,15 +17,36 @@ package leetcode;
  * @file        P234_PalindromeLinkedList.java
  * @type        P234_PalindromeLinkedList
  * @date        2016年12月9日 下午10:13:29
- * @details     
+ * @details     Solution: AC 1ms 89.64%
  */
 public class P234_PalindromeLinkedList {
-	public static void main(String[] args) {
-		
-	}
 	static class Solution {
 	    public boolean isPalindrome(ListNode head) {
-	        
+	    	if (head == null) return true;
+	        int len = 0;
+	        ListNode t = head;
+	        while (t != null) {
+	        	len ++;
+	        	t = t.next;
+	        }
+	        t = head;
+	        for (int i = len / 2; i > 0; i --) t = t.next;
+	        ListNode n = t.next, ts, ns;
+	        while (n != null) {
+	        	ts = n;
+	        	ns = n.next;
+	        	n.next = t;
+	        	t = ts;
+	        	n = ns;
+	        }
+	        n = t;
+	        t = head;
+	        for (int i = len/2; i > 0; i --) {
+	        	if (t.val != n.val) return false;
+	        	t = t.next;
+	        	n = n.next;
+	        }
+	        return true;
 	    }
 	}
 }
