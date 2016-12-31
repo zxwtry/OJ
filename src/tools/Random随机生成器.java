dbaa636f3f77e8f83519c3b72b3dd295ddee9c9c
zxwtry <zxwtry@qq.com>
2016-12-31 21:29:24 +0800

    添加 tools Random随机生成器 修改不能得到min的bug

--- src/tools/Random随机生成器.java
+++ src/tools/Random随机生成器.java
@@ -6,40 +6,41 @@ public class Random随机生成器 {
 	//按照自己的习惯，应该将是[min, max]
 	public static int[] A_生成一个随机数据(int n, int min, int max) {
 		int[] ans = new int[n];
-		int base = min;
 		long range = (long)max - min + 1;
 		for (int i = 0; i < n; i ++) {
-			ans[i] = base + (int)(Math.random() * range);
+			double d = Math.random() * range + min;
+			if (d < 0) d -= 1;
+			ans[i] = (int)(d);
 		}
 		return ans;
 	}
 	public static int[] A_生成一个随机数据_除了某个数(int n, int min, int max, int val) {
 		int[] ans = new int[n];
-		int base = min;
 		long range = (long)max - min + 1;
 		for (int i = 0; i < n; i ++) {
-			int new_add = (int)(Math.random() * range);
-			if (new_add == val - min) {
+			double d = Math.random() * range + min;
+			if (d < 0) d -= 1;
+			ans[i] = (int)(d);
+			if (ans[i] == val) {
 				i --;
 				continue;
 			}
-			ans[i] = base + new_add;
 		}
 		return ans;
 	}
 	public static int[] A_生成一个不重复随机数据(int n, int min, int max) {
 		HashSet<Integer> set = new HashSet<Integer>();
 		int[] ans = new int[n];
-		int base = min;
 		long range = (long)max - min + 1;
 		for (int i = 0; i < n; i ++) {
-			int new_add = (int)(Math.random() * range);
-			if (set.contains(new_add)) {
+			double d = Math.random() * range + min;
+			if (d < 0) d -= 1;
+			ans[i] = (int)(d);
+			if (set.contains(ans[i])) {
 				i --;
 				continue;
 			}
-			set.add(new_add);
-			ans[i] = base + new_add;
+			set.add(ans[i]);
 		}
 		return ans;
 	}
