0f73f56ff25fb63162b5df8c40120e54def947bd
zxwtry <zxwtry@qq.com>
2016-12-31 20:25:33 +0800

    添加 nowcoder zuo Book097_不重复打印排序数组中相加和为给定值的所有二元组和三元组

---
+++ src/nowcoder/zuo/Book097_不重复打印排序数组中相加和为给定值的所有二元组和三元组.java
@@ -0,0 +1,22 @@
+package nowcoder.zuo;
+
+/**
+ * 	给定排序数组arr和整数k。
+ * 	不重复打印arr中所有相加和为k的不降序二元组
+ * 	例如，arr=[-8,-4,-3,0,1,2,4,5,8,9]，k=10，打印结果为：
+ * 	1,9
+ * 	2,8
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book097_不重复打印排序数组中相加和为给定值的所有二元组和三元组.java
+ * @type        Book097_不重复打印排序数组中相加和为给定值的所有二元组和三元组
+ * @date        2016年12月31日 下午5:55:15
+ * @details     Solution1: 打印所有二元和三元，包含重复
+ */
+public class Book097_不重复打印排序数组中相加和为给定值的所有二元组和三元组 {
+}
