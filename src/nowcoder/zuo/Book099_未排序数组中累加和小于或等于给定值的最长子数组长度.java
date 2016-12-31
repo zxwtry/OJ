82302bf666816e5f5f59d7eeb67ea9eb25d77460
zxwtry <zxwtry@qq.com>
2016-12-31 21:43:45 +0800

    添加 nowcoder zuo Book099_未排序数组中累加和小于或等于给定值的最长子数组长度 Solution1

--- src/nowcoder/zuo/Book099_未排序数组中累加和小于或等于给定值的最长子数组长度.java
+++ src/nowcoder/zuo/Book099_未排序数组中累加和小于或等于给定值的最长子数组长度.java
@@ -18,8 +18,23 @@ package nowcoder.zuo;
  * @file        Book099_未排序数组中累加和小于或等于给定值的最长子数组长度.java
  * @type        Book099_未排序数组中累加和小于或等于给定值的最长子数组长度
  * @date        2016年12月31日 下午9:36:00
- * @details     
+ * @details     Solution1: 时间O(N^2)，空间O(1)
  */
 public class Book099_未排序数组中累加和小于或等于给定值的最长子数组长度 {
-	
+	static class Solution1 {
+		public int getMaxLen(int[] a, int k) {
+			if (a == null || a.length < 1) return 0;
+			int maxLen = 0;
+			int v = 0;
+			for (int i = 0; i < a.length; i ++) {
+				for (int j = i; j < a.length; j ++) {
+					v = i == j ? a[i] : v + a[j];
+					if (v <= k) {
+						maxLen = Math.max(maxLen, j - i + 1);
+					}
+				}
+			}
+			return maxLen;
+		}
+	}
 }
