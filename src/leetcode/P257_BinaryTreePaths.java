b5b630bf60ab65acd12dfe2f6ceead0820fd39bd
zxwtry <zxwtry@qq.com>
2016-12-31 09:13:14 +0800

    添加 leetcode P257_BinaryTreePaths Solution AC 25.79%

--- src/leetcode/P257_BinaryTreePaths.java
+++ src/leetcode/P257_BinaryTreePaths.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import tools.TreeNode辅助.TreeNode;
+
 /**
  * Given a binary tree, return all root-to-leaf paths.
 
@@ -23,16 +28,35 @@ package leetcode;
  * @file        P257_BinaryTreePaths.java
  * @type        P257_BinaryTreePaths
  * @date        2016年12月12日 下午10:05:01
- * @details     
+ * @details     Solution: AC 3ms  25.79%
  */
 public class P257_BinaryTreePaths {
-	public static void main(String[] args) {
-		
-	}
 	static class  Solution {
+		List<String> ans = new LinkedList<String>();
 	    public List<String> binaryTreePaths(TreeNode root) {
-	        
+	    	if (root == null) return ans;
+	        StringBuilder st = new StringBuilder();
+	        s(root, st, root);
+	        return ans;
 	    }
+		private void s(TreeNode n, StringBuilder st, TreeNode root) {
+			if (n == null) {
+				ans.add(st.toString());
+				return;
+			}
+			st.append((n == root ? "" : "->")+n.val);
+			if (n.left == null && n.right == null) {
+				s(null, st, root);
+			} else {
+				if (n.left != null) {
+					s(n.left, st, root);
+					st.delete(st.lastIndexOf("->") , st.length());
+				}
+				if (n.right != null) {
+					s(n.right, st, root);
+					st.delete(st.lastIndexOf("->") , st.length());
+				}
+			}
+		}
 	}
-	
 }
