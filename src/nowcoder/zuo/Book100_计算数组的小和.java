a193797192fe95e60a823335888d3b71c09ce45d
zxwtry <zxwtry@qq.com>
2017-01-01 17:48:31 +0800

    添加 nowcoder zuo Book100_计算数组的小和 Solution2

--- src/nowcoder/zuo/Book100_计算数组的小和.java
+++ src/nowcoder/zuo/Book100_计算数组的小和.java
@@ -1,5 +1,9 @@
 package nowcoder.zuo;
 
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  * 	[题目]
  * 	数组小和定义如下：
@@ -19,17 +23,41 @@ package nowcoder.zuo;
  * @type        Book100_计算数组的小和
  * @date        2016年12月31日 下午10:24:15
  * @details     Solution1: 时间O(N^2)，空间O(1)
+ * @details     Solution2: 时间O(N)，空间O(N)
+ * @details     Solution3: 时间O(N*logN)，空间O(N)
+ * @details     N=10000耗时Solution1:Solution2大概是：6:1
  */
 public class Book100_计算数组的小和 {
 	static class Solution1 {
 		public int getArrMinSum(int[] arr) {
 			if (arr == null || arr.length < 1) return 0;
 			int arrMinSum = 0;
+			int min = Integer.MAX_VALUE;
 			for (int i = 0; i < arr.length; i ++) {
+				if (arr[i] > min)
 				for (int j = 0; j < i; j ++) {
 					if (arr[j] < arr[i])
 						arrMinSum += arr[j];
 				}
+				min = Math.min(min, arr[i]);
+			}
+			return arrMinSum;
+		}
+	}
+	static class Solution2 {
+		public int getArrMinSum(int[] arr) {
+			TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+			int min = Integer.MAX_VALUE;
+			int arrMinSum = 0;
+			for (int v : arr) {
+				if (min < v) {
+					SortedMap<Integer, Integer> sub = map.subMap(min, v);
+					for (Entry<Integer, Integer> nv : sub.entrySet())
+						arrMinSum += nv.getValue() * nv.getKey();
+				}
+				min = Math.min(v, min);
+				Integer val = map.get(v);
+				map.put(v, val == null ? 1 : val + 1);
 			}
 			return arrMinSum;
 		}
