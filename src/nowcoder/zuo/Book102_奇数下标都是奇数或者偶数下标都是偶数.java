01cdea1745a47727eedfaa06f544cd5d5a872819
zxwtry <zxwtry@qq.com>
2017-01-01 20:07:44 +0800

    添加 nowcoder zuo Book102_奇数下标都是奇数或者偶数下标都是偶数 Solution

--- src/nowcoder/zuo/Book102_奇数下标都是奇数或者偶数下标都是偶数.java
+++ src/nowcoder/zuo/Book102_奇数下标都是奇数或者偶数下标都是偶数.java
@@ -19,8 +19,27 @@ package nowcoder.zuo;
  * @file        Book102_奇数下标都是奇数或者偶数下标都是偶数.java
  * @type        Book102_奇数下标都是奇数或者偶数下标都是偶数
  * @date        2017年1月1日 下午7:50:42
- * @details     
+ * @details     Solution: 时间O(N)，空间O(1)
  */
 public class Book102_奇数下标都是奇数或者偶数下标都是偶数 {
-
+	static class Solution {
+		public void modify(int[] arr) {
+			if (arr == null || arr.length < 2) return;
+			int even = 0, odd = 1, end = arr.length - 1;
+			while (even <= end && odd <= end) {
+				if ((arr[end] & 1) == 0) {
+					swap(arr, end, even);
+					even += 2;
+				} else {
+					swap(arr, end, odd);
+					odd += 2;
+				}
+			}
+		}
+		private void swap(int[] a, int i, int j) {
+			int t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+		}
+	}
 }
