39417f81e84b97e9e88c19b48198df95c742e974
zxwtry <zxwtry@qq.com>
2017-01-01 22:23:20 +0800

    添加 nowcoder zuo Book104_子矩阵的最大累加和问题 Solution2

--- src/nowcoder/zuo/Book104_子矩阵的最大累加和问题.java
+++ src/nowcoder/zuo/Book104_子矩阵的最大累加和问题.java
@@ -59,5 +59,25 @@ public class Book104_子矩阵的最大累加和问题 {
 			return max;
 		}
 	}
-
+	static class Solution2 {
+		public int getMaxSubMatSum(int[][] m) {
+			if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) return 0;
+			int max = Integer.MIN_VALUE;
+			int cur = 0;
+			int[] s = null;
+			for (int i = 0; i < m.length; i ++) {
+				s = new int[m[0].length];
+				for (int j = i; j < m.length; j ++) {
+					cur = 0;
+					for (int k = 0; k < s.length; k ++) {
+						s[k] += m[j][k];
+						cur += s[k];
+						max = Math.max(max, cur);
+						cur = cur < 0 ? 0 : cur;
+					}
+				}
+			}
+			return max;
+		}
+	}
 }
