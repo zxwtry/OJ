e772a86680bf3bd51df95f4e6e83aa554d07ce90
zxwtry <zxwtry@qq.com>
2017-01-01 19:52:50 +0800

    添加 nowcoder zuo Book102_奇数下标都是奇数或者偶数下标都是偶数

---
+++ src/nowcoder/zuo/Book102_奇数下标都是奇数或者偶数下标都是偶数.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个长度不小于2的数组arr，
+ * 	实现一个函数调整arr，
+ * 	要么让所有的偶数下标都是偶数，
+ * 	要么让所有的奇数下标都是奇数
+ * 	
+ * 	[要求]
+ * 	如果arr的长度为N，函数要求时间O(N)，空间O(1)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book102_奇数下标都是奇数或者偶数下标都是偶数.java
+ * @type        Book102_奇数下标都是奇数或者偶数下标都是偶数
+ * @date        2017年1月1日 下午7:50:42
+ * @details     
+ */
+public class Book102_奇数下标都是奇数或者偶数下标都是偶数 {
+
+}
