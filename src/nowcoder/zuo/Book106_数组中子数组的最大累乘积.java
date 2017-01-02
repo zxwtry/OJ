39c37570953750077e92cec72a4a20d8173c8330
zxwtry <zxwtry@qq.com>
2017-01-02 10:37:07 +0800

    添加 nowcoder zuo Book106_数组中子数组的最大累乘积

---
+++ src/nowcoder/zuo/Book106_数组中子数组的最大累乘积.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个double类型的数组arr，
+ * 	其中的元素可正、可负、可0，
+ * 	返回子数组累乘的最大乘积。
+ * 
+ * 	[举例]
+ * 	arr=[-2.5, 4, 0, 3, 0.5, 8, -1]
+ * 	子数组[3, 0.5, 8]可以得到最大乘积12，返回12
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book106_数组中子数组的最大累乘积.java
+ * @type        Book106_数组中子数组的最大累乘积
+ * @date        2017年1月2日 上午10:34:32
+ * @details     
+ */
+public class Book106_数组中子数组的最大累乘积 {
+
+}
