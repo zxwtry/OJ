ec1c99ed8cb0b16f49180db6008140736736b824
zxwtry <zxwtry@qq.com>
2017-01-02 11:42:28 +0800

    添加 nowcoder zuo Book107_找出N个数组整体最大的TopK

---
+++ src/nowcoder/zuo/Book107_找出N个数组整体最大的TopK.java
@@ -0,0 +1,32 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	有N个长度不一的数组，所有的数组都是有序的。
+ * 	从大到小打印这N个数组整体最大的前K个数
+ * 	
+ * 	[举例]
+ * 	219, 405, 538, 845, 971
+ * 	148, 558
+ * 	52, 99, 348, 691
+ * 	再输入整数K=5，则打印
+ * 	Top5: 971, 845, 691, 558, 538
+ * 
+ * 	[要求]
+ * 	1,	如果所有数组的元素个数小于K，则从大到小打印所有的数
+ * 	2,	要求时间O(K*logN)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book107_找出N个数组整体最大的TopK.java
+ * @type        Book107_找出N个数组整体最大的TopK
+ * @date        2017年1月2日 上午11:38:19
+ * @details     
+ */
+public class Book107_找出N个数组整体最大的TopK {
+	
+}
