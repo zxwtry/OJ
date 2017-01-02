2eb8105dcf0c89cc0ba2b50e45a1e99b08c910d9
zxwtry <zxwtry@qq.com>
2017-01-02 09:41:27 +0800

    添加 nowcoder zuo Book105_在数组中找到一个局部最小的位置

---
+++ src/nowcoder/zuo/Book105_在数组中找到一个局部最小的位置.java
@@ -0,0 +1,26 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	定义局部最小的概念。
+ * 	arr长度为1时，arr[0]是局部最小。
+ * 	arr长度为N(N>1)时，	arr[0]<arr[1]，则arr[0]是局部最小。
+ * 						arr[N-1]<arr[N-2]，则arr[N-1]是局部最小。
+ * 						0<i<N-1，如果arr[i]<arr[i+1] && arr[i]<arr[i-1]，则arr[i]是局部最小。
+ * 	给定无序数组arr，已知arr中任意两个相邻的数都不相邻，
+ * 	返回arr中任意一个局部最小的位置。
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book105_在数组中找到一个局部最小的位置.java
+ * @type        Book105_在数组中找到一个局部最小的位置
+ * @date        2017年1月2日 上午9:32:38
+ * @details     
+ */
+public class Book105_在数组中找到一个局部最小的位置 {
+	
+}
