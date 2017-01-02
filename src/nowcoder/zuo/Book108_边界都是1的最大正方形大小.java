607b4467dda1fb9ce17969ba3d373c1c40d36658
zxwtry <zxwtry@qq.com>
2017-01-02 22:13:28 +0800

    添加 nowcoder zuo Book108_边界都是1的最大正方形大小 Solution2

--- src/nowcoder/zuo/Book108_边界都是1的最大正方形大小.java
+++ src/nowcoder/zuo/Book108_边界都是1的最大正方形大小.java
@@ -26,6 +26,7 @@ package nowcoder.zuo;
  * @type        Book108_边界都是1的最大正方形大小
  * @date        2017年1月2日 下午9:21:05
  * @details     Solution1: 时间O(N^4)，空间O(1)
+ * @details     Solution2: 时间O(N^4)，空间O(1)
  */
 public class Book108_边界都是1的最大正方形大小 {
 	static class Solution1 {
@@ -45,6 +46,32 @@ public class Book108_边界都是1的最大正方形大小 {
 							int addj = k >= len ? k - len + 1 : 0;
 							if (m[addi + i1][addj + j1] != 1 || m[addj + i1][addi + j1] != 1)
 								isBreak = true;
+						}
+						if (! isBreak) max = len;
+					}
+				}
+			}
+			return max;
+		}
+	}
+	static class Solution2 {
+		public int maxSquareLayer(int[][] m) {
+			if (m == null || m.length == 0 || m[0].length == 0) return 0; 
+			int n = m.length;
+			int max = 0;
+			boolean isBreak = false;
+			for (int i1 = 0; i1 < n; i1 ++) {
+				for (int j1 = 0; j1 < n; j1 ++) {
+					if (m[i1][j1] == 0) continue;
+					for (int len = n - Math.max(i1, j1); len > max; len --) {
+						isBreak = false;
+						int i2 = i1 + len - 1;
+						int j2 = j1 + len - 1;
+						for (int i = i1; i <= i2 && ! isBreak; i ++) {
+							if (m[i][j1] == 0 || m[i][j2] == 0) isBreak = true;
+						}
+						for (int j = j1; j <= j2 && ! isBreak; j ++) {
+							if (m[i1][j] == 0 || m[i2][j] == 0) isBreak = true; 
 						}
 						if (! isBreak) max = len;
 					}
