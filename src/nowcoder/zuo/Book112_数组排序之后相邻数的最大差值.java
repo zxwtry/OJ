d481a936d11b0bad711b337697c9aca2b5fd66cf
zxwtry <zxwtry@qq.com>
2017-01-03 22:12:46 +0800

    添加 nowcoder zuo Book112_数组排序之后相邻数的最大差值 Solution2

--- src/nowcoder/zuo/Book112_数组排序之后相邻数的最大差值.java
+++ src/nowcoder/zuo/Book112_数组排序之后相邻数的最大差值.java
@@ -2,8 +2,6 @@ package nowcoder.zuo;
 
 import java.util.Arrays;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 /**
  * 	[题目]
  * 	给定一个整型数组arr，返回排序后的相邻两数的最大差值。
@@ -40,5 +38,45 @@ public class Book112_数组排序之后相邻数的最大差值 {
 			return maxGap;
 		}
 	}
-	
+	static class Solution2 {
+		public int getMaxGap(int[] arr) {
+			int len = arr.length;
+			int min = Integer.MAX_VALUE;
+			int max = Integer.MIN_VALUE;
+			for (int i = 0; i < len; i ++) {
+				min = Math.min(min, arr[i]);
+				max = Math.max(max, arr[i]);
+			}
+			if (min == max) return 0;
+			boolean[] hasNum = new boolean[len + 1];
+			int[] maxs = new int[len + 1];
+			int[] mins = new int[len + 1];
+			int bid = 0;
+			for (int i = 0; i < len; i ++) {
+				bid = bucket(arr[i], len, min, max);
+				mins[bid] = hasNum[bid] ? Math.min(mins[bid], arr[i]) : arr[i];
+				maxs[bid] = hasNum[bid] ? Math.max(maxs[bid], arr[i]) : arr[i];
+				hasNum[bid] = true;
+			}
+			int maxGap = 0;
+			int lastMax = 0;
+			int i = 0;
+			while (i <= len) {
+				if (hasNum[i--]) {
+					lastMax = maxs[i - 1];
+					break;
+				}
+			}
+			for (; i <= len; i ++) {
+				if (hasNum[i]) {
+					maxGap = Math.max(maxGap, mins[i] - lastMax); 
+					lastMax = maxs[i];
+				}
+			}
+			return maxGap;
+		}
+		private int bucket(long num, long len, long min, long max) {
+			return (int)((num - min) * len / (max - min));
+		}
+	}
 }
