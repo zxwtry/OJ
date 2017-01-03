e896ba24372155b50c55af6d99d0658a234a0048
zxwtry <zxwtry@qq.com>
2017-01-03 21:53:09 +0800

    添加 nowcoder zuo Book111_数组中未出现的最小正整数 Solution

--- src/nowcoder/zuo/Book111_数组中未出现的最小正整数.java
+++ src/nowcoder/zuo/Book111_数组中未出现的最小正整数.java
@@ -20,5 +20,25 @@ package nowcoder.zuo;
  * @details     
  */
 public class Book111_数组中未出现的最小正整数 {
-	
+	static class Solution {
+		public int missNum(int[] arr) {
+			int left = 0, right = arr.length;
+			while (left < right) {
+				if (arr[left] == left + 1) {
+					left ++;
+				} else if (arr[left] <= left || arr[left] > right || 
+						arr[arr[left] - 1] == arr[left]) {
+					arr[left] = arr[-- right];
+				} else {
+					swap(arr, left, arr[left] - 1);
+				}
+			}
+			return left + 1;
+		}
+		private void swap(int[] arr, int i, int j) {
+			int t = arr[i];
+			arr[i] = arr[j];
+			arr[j] = t;
+		}
+	}
 }
