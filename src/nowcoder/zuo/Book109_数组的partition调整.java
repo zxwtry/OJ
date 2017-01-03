c11ef875993e63f1b51fe6d026e6099cd530b495
zxwtry <zxwtry@qq.com>
2017-01-03 21:14:38 +0800

    添加 nowcoder zuo Book109_数组的partition调整

---
+++ src/nowcoder/zuo/Book109_数组的partition调整.java
@@ -0,0 +1,25 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个有序数组arr，调整arr使得这个数组的左半部分没有重复元素且升序，
+ * 	而不用保证右部分是否有序。
+ * 	
+ * 	[举例]
+ *	arr=[1,2,2,2,3,3,4,5,6,6,7,7,8,8,8,9]
+ *	调整之后arr=[1,2,3,4,5,6,7,8,9,...]
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book109_数组的partition调整.java
+ * @type        Book109_数组的partition调整
+ * @date        2017年1月3日 下午9:09:03
+ * @details     
+ */
+public class Book109_数组的partition调整 {
+
+}
