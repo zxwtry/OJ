5503114fcc34018e1c9ee9f728b69d18bfb5fbfe
zxwtry <zxwtry@qq.com>
2017-01-03 22:01:15 +0800

    添加 nowcoder zuo Book112_数组排序之后相邻数的最大差值

---
+++ src/nowcoder/zuo/Book112_数组排序之后相邻数的最大差值.java
@@ -0,0 +1,28 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个整型数组arr，返回排序后的相邻两数的最大差值。
+ * 
+ * 	[举例]
+ * 	arr=[9,3,1,10]，如果排序，结果为[1,3,9,10]，
+ * 	9和3的差为最大差值，故返回6
+ * 	arr=[5,5,5,5]，返回0
+ * 
+ * 	[要求]
+ * 	时间O(N)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book112_数组排序之后相邻数的最大差值.java
+ * @type        Book112_数组排序之后相邻数的最大差值
+ * @date        2017年1月3日 下午9:58:42
+ * @details     
+ */
+public class Book112_数组排序之后相邻数的最大差值 {
+
+}
