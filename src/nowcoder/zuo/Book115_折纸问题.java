cde903c57e2031c993f01c3c9af12a4fdf81c526
zxwtry <zxwtry@qq.com>
2017-01-04 22:08:43 +0800

    添加 nowcoder zuo Book115_折纸问题

---
+++ src/nowcoder/zuo/Book115_折纸问题.java
@@ -0,0 +1,27 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	将一段纸条竖着放在桌子上，然后从纸条的下边向上方对折一次，
+ * 	对折N次展开，此时共有2^N-1条折痕，从上到下打印所有折痕的方向。
+ * 	
+ * 	[举例]
+ * 	N=1打印：	down
+ * 	N=2打印：	down
+ * 	        down
+ * 			up
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book115_折纸问题.java
+ * @type        Book115_折纸问题
+ * @date        2017年1月4日 下午10:05:24
+ * @details     
+ */
+public class Book115_折纸问题 {
+
+}
