11e1d1c1f7bc5e9041a2149aad2b5469a0d3eac1
zxwtry <zxwtry@qq.com>
2017-01-04 22:19:05 +0800

    添加 leetcode P318_MaximumProductOfWordLengths

---
+++ src/leetcode/P318_MaximumProductOfWordLengths.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+/**
+ * 	Given a string array words, find the maximum value of 
+ * 	length(word[i]) * length(word[j]) where the two words 
+ * 	do not share common letters. You may assume that each 
+ * 	word will contain only lower case letters. If no such 
+ * 	two words exist, return 0.
+
+	Example 1:
+	Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+	Return 16
+	The two words can be "abcw", "xtfn".
+	
+	Example 2:
+	Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
+	Return 4
+	The two words can be "ab", "cd".
+	
+	Example 3:
+	Given ["a", "aa", "aaa", "aaaa"]
+	Return 0
+	No such pair of words.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P318_MaximumProductOfWordLengths.java
+ * @type        P318_MaximumProductOfWordLengths
+ * @date        2017年1月4日 下午10:18:10
+ * @details     
+ */
+public class P318_MaximumProductOfWordLengths {
+	static class Solution {
+	    public int maxProduct(String[] words) {
+	        
+	    }
+	}
+}
