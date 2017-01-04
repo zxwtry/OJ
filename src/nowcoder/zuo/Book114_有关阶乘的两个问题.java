5306ccc4d9434a96c5925ea42b2bbf7265e5c6be
zxwtry <zxwtry@qq.com>
2017-01-04 21:46:20 +0800

    添加 nowcoder zuo Book114_有关阶乘的两个问题

---
+++ src/nowcoder/zuo/Book114_有关阶乘的两个问题.java
@@ -0,0 +1,32 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个非负整数N，返回N!结果的末尾为0的数量
+ * 
+ * 	[举例]
+ * 	3 ---> 返回0
+ * 	5 ---> 返回1
+ * 
+ * 	[进阶题目]
+ * 	给定一个非负整数N，返回N!的二进制表示中，
+ * 	最低位的1在哪个位置上。
+ * 	
+ * 	[举例]
+ * 	1 ---> 返回0
+ * 	2 ---> 返回1
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book114_有关阶乘的两个问题.java
+ * @type        Book114_有关阶乘的两个问题
+ * @date        2017年1月4日 下午9:42:41
+ * @details     
+ */
+public class Book114_有关阶乘的两个问题 {
+
+}
