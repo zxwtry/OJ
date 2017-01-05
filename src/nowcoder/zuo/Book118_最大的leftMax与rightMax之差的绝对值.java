930eeb0b99354e50a34c814b057d07f39e8df66e
zxwtry <zxwtry@qq.com>
2017-01-05 21:42:21 +0800

    添加 nowcoder zuo Book118_最大的leftMax与rightMax之差的绝对值

---
+++ src/nowcoder/zuo/Book118_最大的leftMax与rightMax之差的绝对值.java
@@ -0,0 +1,25 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个长度为N(N>1)的整型数组arr，可以划分成左右两个部分，
+ * 	左部分为arr[0...k]，右部分为arr[k+1...N-1]，k的范围[0,N-2]
+ * 	求这么多划分方案中，左部分中的最大值减去右部分最大值的绝对值中，最大是多少。
+ * 	
+ * 	[举例]
+ * 	arr=[2,7,1,1]，返回6
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book118_最大的leftMax与rightMax之差的绝对值.java
+ * @type        Book118_最大的leftMax与rightMax之差的绝对值
+ * @date        2017年1月5日 下午9:38:19
+ * @details     
+ */
+public class Book118_最大的leftMax与rightMax之差的绝对值 {
+
+}
