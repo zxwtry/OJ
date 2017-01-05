3c454f1f941eceaddb95495aea46e90e89c93c67
zxwtry <zxwtry@qq.com>
2017-01-05 22:26:06 +0800

    添加 nowcoder zuo Book120_设计一个RandomPool结构 RandomPool

--- src/nowcoder/zuo/Book120_设计一个RandomPool结构.java
+++ src/nowcoder/zuo/Book120_设计一个RandomPool结构.java
@@ -1,5 +1,7 @@
 package nowcoder.zuo;
 
+import java.util.HashMap;
+
 /**
  * 	[题目]
  * 	设计一种结构，在该结构中有如下三个功能
@@ -19,5 +21,38 @@ package nowcoder.zuo;
  * @details     
  */
 public class Book120_设计一个RandomPool结构 {
-
+	static class RandomPool<T> {
+		private HashMap<T, Integer> keyIndexMap;
+		private HashMap<Integer, T> indexKeyMap;
+		private int size;
+		public RandomPool() {
+			this.keyIndexMap = new HashMap<T, Integer>();
+			this.indexKeyMap = new HashMap<Integer, T>();
+			this.size = 0;
+		}
+		public void insert(T key) {
+			if (! this.keyIndexMap.containsKey(key)) {
+				this.keyIndexMap.put(key, this.size);
+				this.indexKeyMap.put(this.size ++, key);
+			}
+		}
+		public void delete(T key) {
+			if (this.keyIndexMap.containsKey(key)) {
+				int deleteIndex = this.keyIndexMap.get(key);
+				int lastIndex = -- this.size;
+				T lastKey = this.indexKeyMap.get(lastIndex);
+				this.keyIndexMap.put(lastKey, deleteIndex);
+				this.indexKeyMap.put(deleteIndex, lastKey);
+				this.keyIndexMap.remove(key);
+				this.indexKeyMap.remove(lastIndex);
+			}
+		}
+		public T getRandom() {
+			if (this.size == 0) {
+				return null;
+			}
+			int randomIndex = (int) (Math.random() * this.size);
+			return this.indexKeyMap.get(randomIndex);
+		}
+	}
 }
