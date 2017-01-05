d5f118b25b89b4ed2d946d75bcd0104d88ac825f
zxwtry <zxwtry@qq.com>
2017-01-05 22:16:28 +0800

    添加 nowcoder zuo Book120_设计一个RandomPool结构

---
+++ src/nowcoder/zuo/Book120_设计一个RandomPool结构.java
@@ -0,0 +1,23 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	设计一种结构，在该结构中有如下三个功能
+ * 	1,	insert(key):		将某个key加入到该结构，做到不重复加入
+ * 	2,	delete(key):		移除某个key
+ * 	3,	getRandom():		等概率随机返回结构中的任何一个key
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book120_设计一个RandomPool结构.java
+ * @type        Book120_设计一个RandomPool结构
+ * @date        2017年1月5日 下午10:13:55
+ * @details     
+ */
+public class Book120_设计一个RandomPool结构 {
+
+}
