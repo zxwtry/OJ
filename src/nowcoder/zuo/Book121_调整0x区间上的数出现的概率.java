9c477a84b30fd23ec332b389cd7f4f36b91f4a3e
zxwtry <zxwtry@qq.com>
2017-01-06 16:31:48 +0800

    添加 nowcoder zuo Book121_调整0x区间上的数出现的概率

---
+++ src/nowcoder/zuo/Book121_调整0x区间上的数出现的概率.java
@@ -0,0 +1,22 @@
+package nowcoder.zuo;
+
+/**
+ * 	Math.random()等概率随机返回一个在[0,1)范围上的数。
+ * 	[0, x)区间上的数出现的概率为x(0<x<=1)，
+ * 	给定一个大于0的整数k，并且可以使用Math.random()函数，
+ * 	在[0, x)区间上的数出现的概率为x^k(0<x<=1)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book121_调整0x区间上的数出现的概率.java
+ * @type        Book121_调整0x区间上的数出现的概率
+ * @date        2017年1月6日 下午4:27:37
+ * @details     
+ */
+public class Book121_调整0x区间上的数出现的概率 {
+
+}
