c857e1ec7b3442d4eab390d023ea0a6997073251
zxwtry <zxwtry@qq.com>
2017-01-06 16:38:33 +0800

    添加 nowcoder zuo Book122_路径数组变为统计数组

---
+++ src/nowcoder/zuo/Book122_路径数组变为统计数组.java
@@ -0,0 +1,27 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个路径数组path, 表示一张图。
+ * 	paths[i]==j代表城市i连向城市j，
+ * 	如果paths[i]==i, 则表示i城市是首都，
+ * 	一张图里只会有一个首都且图中除首都指向自己之外，
+ * 	不会有环。例如，paths={9,1,4,9,0,4,8,9,0,1}
+ * 
+ * 	[要求]
+ * 	时间O(N)，空间O(1)
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book122_路径数组变为统计数组.java
+ * @type        Book122_路径数组变为统计数组
+ * @date        2017年1月6日 下午4:34:21
+ * @details     
+ */
+public class Book122_路径数组变为统计数组 {
+
+}
