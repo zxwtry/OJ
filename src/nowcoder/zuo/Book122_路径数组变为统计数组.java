5e59b23bdc1fdb169666eb353fe6ae72707aad97
zxwtry <zxwtry@qq.com>
2017-01-07 20:23:54 +0800

    添加 nowcoder zuo Book122_路径数组变为统计数组 Solution

--- src/nowcoder/zuo/Book122_路径数组变为统计数组.java
+++ src/nowcoder/zuo/Book122_路径数组变为统计数组.java
@@ -1,6 +1,6 @@
 package nowcoder.zuo;
 
-/**
+/**  
  * 	[题目]
  * 	给定一个路径数组path, 表示一张图。
  * 	paths[i]==j代表城市i连向城市j，
@@ -23,5 +23,60 @@ package nowcoder.zuo;
  * @details     
  */
 public class Book122_路径数组变为统计数组 {
-
+	static class Solution {
+		public void pathsToNums(int[] paths) {
+			if (paths == null || paths.length == 0) return;
+			pathsToDistans(paths);
+			distansToNums(paths);
+		}
+		public void pathsToDistans(int[] paths) {
+			int cap = 0;
+			for (int i = 0; i < paths.length; i ++) {
+				if (paths[i] == i) {
+					cap = i;
+				} else if (paths[i] > -1) {
+					int curI = paths[i];
+					paths[i] = -1;
+					int preI = i;
+					while (paths[curI] != curI) {
+						if (paths[curI] > -1) {
+							int nextI = paths[curI];
+							paths[curI] = preI;
+							preI = curI;
+							curI = nextI;
+						} else break;
+	 				}
+					int value = paths[curI] == curI ? 0 : paths[curI];
+					while (paths[preI] != -1) {
+						int lastPreI = paths[preI];
+						paths[preI] = -- value;
+						curI = preI;
+						preI = lastPreI;
+					}
+					paths[preI] = --value;
+				}
+			}
+			paths[cap] = 0;
+		}
+		public void distansToNums(int[] disArr) {
+			for (int i = 0; i < disArr.length; i ++) {
+				int index = disArr[i];
+				if (index < 0) {
+					disArr[i] = 0;
+					while (true) {
+						index = -index;
+						if (disArr[index] > -1) {
+							disArr[index] ++;
+							break;
+						} else {
+							int nextIndex = disArr[index];
+							disArr[index] = 1;
+							index = nextIndex;
+						}
+					}
+				}
+			}
+			disArr[0] = 1;
+		}
+	}
 }
