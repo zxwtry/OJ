e9037aea232ab13174feb7a0645f4ca0d4c858e8
zxwtry <zxwtry@qq.com>
2017-01-07 21:39:18 +0800

    添加 nowcoder zuo Book122_正数数组的最小不可组成和 Solution2

--- src/nowcoder/zuo/Book122_正数数组的最小不可组成和.java
+++ src/nowcoder/zuo/Book122_正数数组的最小不可组成和.java
@@ -56,4 +56,24 @@ public class Book122_正数数组的最小不可组成和 {
 			}
 		}
 	}
+	static class Solution2 {
+		public int getMinUnformedSum(int[] arr) {
+			if (arr == null || arr.length == 0) return 1;
+			int sum = 0;
+			int min = Integer.MAX_VALUE;
+			for (int i = 0; i < arr.length; i ++) {
+				sum += arr[i];
+				min = Math.min(min, arr[i]);
+			}
+			boolean[] dp = new boolean[sum + 1];
+			dp[0] = true;
+			for (int i = 0; i < arr.length; i ++)
+				for (int j = sum; j >= arr[i]; j --)
+					dp[j] = dp[j - arr[i]] ? true : dp[j];
+			for (int i = min; i < dp.length; i ++)
+				if (! dp[i])
+					return i;
+			return sum + 1;
+		}
+	}
 }
