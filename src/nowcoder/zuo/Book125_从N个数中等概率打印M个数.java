67b4f48de3299dcb58a6b36bd889f3ff4fd960b3
zxwtry <zxwtry@qq.com>
2017-01-08 22:10:43 +0800

    添加 nowcoder zuo Book124_1到n中1出现的次数

---
+++ src/nowcoder/zuo/Book125_从N个数中等概率打印M个数.java
@@ -0,0 +1,25 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个长度为N且没有重复的数组arr和一个整数n，实现函数等概率随机打印arr中的M个数
+ * 	
+ * 	[要求]
+ * 	1,	相同的数不要重复打印
+ * 	2,	时间O(M)，空间O(1)
+ * 	3,	可以改变arr数组
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book125_从N个数中等概率打印M个数.java
+ * @type        Book125_从N个数中等概率打印M个数
+ * @date        2017年1月8日 下午10:08:19
+ * @details     
+ */
+public class Book125_从N个数中等概率打印M个数 {
+
+}
