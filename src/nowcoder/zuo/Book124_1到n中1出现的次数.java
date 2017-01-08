4e95eebf0167e3efc578dd9d3ccff0bf5f9a891b
zxwtry <zxwtry@qq.com>
2017-01-08 21:21:42 +0800

    添加 nowcoder zuo Book124_1到n中1出现的次数

---
+++ src/nowcoder/zuo/Book124_1到n中1出现的次数.java
@@ -0,0 +1,24 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	给定一个整数n，返回从1到n的数字中1出现的次数
+ * 	
+ * 	[举例]
+ * 	n=5，1~n为1,2,3,4,5。那么1出现1次
+ * 	n=11，1出现4次
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book124_1到n中1出现的次数.java
+ * @type        Book124_1到n中1出现的次数
+ * @date        2017年1月8日 下午9:19:50
+ * @details     
+ */
+public class Book124_1到n中1出现的次数 {
+
+}
