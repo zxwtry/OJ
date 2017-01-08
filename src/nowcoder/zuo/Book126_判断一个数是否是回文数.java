2301e29fbcadbd35735de9d1d07dfb442365d509
zxwtry <zxwtry@qq.com>
2017-01-08 22:16:25 +0800

    添加 nowcoder zuo Book126_判断一个数是否是回文数

---
+++ src/nowcoder/zuo/Book126_判断一个数是否是回文数.java
@@ -0,0 +1,21 @@
+package nowcoder.zuo;
+
+/**
+ * 	[题目]
+ * 	定一个回文数：
+ * 	如果一个数的绝对值是回文数，那么这个数是回文数。
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.zuo
+ * @file        Book126_判断一个数是否是回文数.java
+ * @type        Book126_判断一个数是否是回文数
+ * @date        2017年1月8日 下午10:14:59
+ * @details     
+ */
+public class Book126_判断一个数是否是回文数 {
+
+}
