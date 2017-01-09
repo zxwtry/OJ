c9ee0068b9976c8177246b6e23873e2eeaee434a
zxwtry <zxwtry@qq.com>
2017-01-09 21:09:39 +0800

    添加 leetcode P322_CoinChange

---
+++ src/leetcode/P322_CoinChange.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * 	You are given coins of different denominations and a total amount of money amount. 
+ * 	Write a function to compute the fewest number of coins that you need to make up that amount.
+ *  If that amount of money cannot be made up by any combination of the coins, return -1.
+	
+	Example 1:
+	coins = [1, 2, 5], amount = 11
+	return 3 (11 = 5 + 5 + 1)
+	
+	Example 2:
+	coins = [2], amount = 3
+	return -1.
+	
+	Note:
+	You may assume that you have an infinite number of each kind of coin
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P322_CoinChange.java
+ * @type        P322_CoinChange
+ * @date        2017年1月9日 下午9:09:00
+ * @details     
+ */
+public class P322_CoinChange {
+	static class Solution {
+	    public int coinChange(int[] coins, int amount) {
+	        
+	    }
+	}
+}
