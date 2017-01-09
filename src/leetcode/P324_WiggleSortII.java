a3768b4790db0fd516bafc9035afdbc08bb79f75
zxwtry <zxwtry@qq.com>
2017-01-09 21:10:40 +0800

    添加 leetcode P324_WiggleSortII

---
+++ src/leetcode/P324_WiggleSortII.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * 	Given an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+ * 	
+
+	Example:
+	(1) Given nums = [1, 5, 1, 1, 6, 4], one possible answer is [1, 4, 1, 5, 1, 6]. 
+	(2) Given nums = [1, 3, 2, 2, 3, 1], one possible answer is [2, 3, 1, 3, 1, 2].
+	
+	Note:
+	You may assume all input has valid answer.
+	
+	Follow Up:
+	Can you do it in O(n) time and/or in-place with O(1) extra space?
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P324_WiggleSortII.java
+ * @type        P324_WiggleSortII
+ * @date        2017年1月9日 下午9:09:56
+ * @details     
+ */
+public class P324_WiggleSortII {
+	static class Solution {
+	    public void wiggleSort(int[] nums) {
+	        return
+	    }
+	}
+}
