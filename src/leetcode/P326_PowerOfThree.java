bfee8de786360f14841121a00d487f992e3bcae4
zxwtry <zxwtry@qq.com>
2017-01-10 21:58:58 +0800

    添加 leetcode P326_PowerOfThree

---
+++ src/leetcode/P326_PowerOfThree.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * 	Given an integer, write a function to determine if it is a power of three.
+	
+	Follow up:
+	Could you do it without using any loop / recursion?
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P326_PowerOfThree.java
+ * @type        P326_PowerOfThree
+ * @date        2017年1月10日 下午9:58:20
+ * @details     
+ */
+public class P326_PowerOfThree {
+	static  class Solution {
+	    public boolean isPowerOfThree(int n) {
+	        
+	    }
+	}
+}
