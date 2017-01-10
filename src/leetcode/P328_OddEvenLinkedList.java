b6a3eb218310fa737509fa90411ef2cc8a2121ef
zxwtry <zxwtry@qq.com>
2017-01-10 22:01:28 +0800

    添加 leetcode P328_OddEvenLinkedList

---
+++ src/leetcode/P328_OddEvenLinkedList.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * 	Given a singly linked list, group all odd nodes together followed by the even nodes.
+ *  Please note here we are talking about the node number and not the value in the nodes.
+
+	You should try to do it in place. The program should run in O(1) space complexity
+	 and O(nodes) time complexity.
+	
+	Example:
+	Given 1->2->3->4->5->NULL,
+	return 1->3->5->2->4->NULL.
+	
+	Note:
+	The relative order inside both the even and odd groups should remain as it was 
+	in the input. 
+	The first node is considered odd, the second node even and so on ...
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P328_OddEvenLinkedList.java
+ * @type        P328_OddEvenLinkedList
+ * @date        2017年1月10日 下午10:00:45
+ * @details     
+ */
+public class P328_OddEvenLinkedList {
+	static class Solution {
+	    public ListNode oddEvenList(ListNode head) {
+	        
+	    }
+	}
+}
