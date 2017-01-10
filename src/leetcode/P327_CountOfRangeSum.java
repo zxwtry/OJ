b39af53dc0f0eca83ae8e2c0ef9eaa1b0d9db143
zxwtry <zxwtry@qq.com>
2017-01-10 22:00:16 +0800

    添加 leetcode P327_CountOfRangeSum

---
+++ src/leetcode/P327_CountOfRangeSum.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * 	Given an integer array nums, return the number of range sums that lie in 
+ * 	[lower, upper] inclusive.
+	Range sum S(i, j) is defined as the sum of the elements in nums between
+	indices i and j (i ≤ j), inclusive.
+	
+	Note:
+	A naive algorithm of O(n2) is trivial. You MUST do better than that.
+	
+	Example:
+	Given nums = [-2, 5, -1], lower = -2, upper = 2,
+	Return 3.
+	The three ranges are : [0, 0], [2, 2], [0, 2] and their respective sums 
+	are: -2, -1, 2.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P327_CountOfRangeSum.java
+ * @type        P327_CountOfRangeSum
+ * @date        2017年1月10日 下午9:59:31
+ * @details     
+ */
+public class P327_CountOfRangeSum {
+	static class Solution {
+	    public int countRangeSum(int[] nums, int lower, int upper) {
+	        
+	    }
+	}
+}
