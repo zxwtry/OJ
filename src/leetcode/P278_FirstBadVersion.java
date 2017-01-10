124061e3c2727378dda444070f97417928916d50
zxwtry <zxwtry@qq.com>
2017-01-10 20:40:01 +0800

    添加 leetcode P278_FirstBadVersion Solution

--- src/leetcode/P278_FirstBadVersion.java
+++ src/leetcode/P278_FirstBadVersion.java
@@ -22,15 +22,28 @@ package leetcode;
  * @file        P278_FirstBadVersion.java
  * @type        P278_FirstBadVersion
  * @date        2016年12月14日 下午10:21:45
- * @details     
+ * @details     Solution: AC  18ms 35.75% 
  */
 public class P278_FirstBadVersion {
-	public static void main(String[] args) {
-		
-	}
 	static class Solution extends VersionControl {
 	    public int firstBadVersion(int n) {
-	        
+	        int si = 1, ei = n, mi = 0;
+	        if (isBadVersion(si))	return 1;
+	        while (si < ei) {
+	        	mi = (int)(((long)si + ei) / 2);
+	        	boolean isBad = isBadVersion(mi);
+	        	if (isBad) {
+	        		ei = mi;
+	        	} else {
+	        		si = mi + 1;
+	        	}
+	        }
+	        return ei;
 	    }
+	}
+	static class VersionControl {
+		public boolean isBadVersion(int v) {
+			return v >= 3;
+		}
 	}
 }
