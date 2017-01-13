1b029fc05a9dff1c07270c5e5b82871fd910520a
zxwtry <zxwtry@qq.com>
2017-01-13 22:22:51 +0800

    添加 leetcode P334_IncreasingTripletSubsequence

---
+++ src/leetcode/P334_IncreasingTripletSubsequence.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * 	Given an unsorted array return whether an increasing subsequence of length 3 
+ * 	exists or not in the array.
+
+	Formally the function should:
+	Return true if there exists i, j, k 
+	such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+	Your algorithm should run in O(n) time complexity and O(1) space complexity.
+	
+	Examples:
+	Given [1, 2, 3, 4, 5],
+	return true.
+	
+	Given [5, 4, 3, 2, 1],
+	return false.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P334_IncreasingTripletSubsequence.java
+ * @type        P334_IncreasingTripletSubsequence
+ * @date        2017年1月13日 下午10:21:48
+ * @details     
+ */
+public class P334_IncreasingTripletSubsequence {
+	static class Solution {
+	    public boolean increasingTriplet(int[] nums) {
+	        
+	    }
+	}
+}
