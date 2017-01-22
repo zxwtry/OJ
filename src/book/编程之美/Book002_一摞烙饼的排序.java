7f45762a360b8f8dddd706952ccf694f751da946
zxwtry <zxwtry@qq.com>
2017-01-22 23:21:46 +0800

    添加 book.编程之美.Book002_一摞烙饼的排序

--- src/book/编程之美/Book002_一摞烙饼的排序.java
+++ src/book/编程之美/Book002_一摞烙饼的排序.java
@@ -19,5 +19,31 @@ package book.编程之美;
  * @details     
  */
 public class Book002_一摞烙饼的排序 {
-
+	static class Solution1 {
+		public void sort(int[] a) {
+			if (a == null || a.length < 2) return;
+			int mi = -1;
+			for (int ei = a.length - 1; ei > -1; ei --) {
+				mi = -1;
+				for (int i = 0; i < ei; i ++)
+					if (-1 == mi || a[mi] < a[i])
+						mi = i;
+				if (mi == -1 || a[mi] <= a[ei]) continue;
+				revser(a, 0, mi);
+				revser(a, 0, ei);
+			}
+		}
+		public void revser(int[] a, int i, int j) {
+			while (i < j) {
+				swap(a, i, j);
+				i ++;
+				j --;
+			}
+		}
+		public void swap(int[] a, int i, int j) {
+			int t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+		}
+	}
 }
