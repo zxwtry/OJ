4bfa015469d00f9cc1e32ecbde82fffc6c8de8ec
zxwtry <zxwtry@qq.com>
2017-01-22 23:00:29 +0800

    添加 book.编程之美.Book002_一摞烙饼的排序

---
+++ src/book/编程之美/Book002_一摞烙饼的排序.java
@@ -0,0 +1,23 @@
+package book.编程之美;
+
+/**
+ * 	[题目]
+ * 	每次只能将最上面几张饼，颠倒个个
+ * 	最后，小的在上面，大的在下面。
+ * 	
+ * 	[要求]
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     book.编程之美
+ * @file        Book002_一摞烙饼的排序.java
+ * @type        Book002_一摞烙饼的排序
+ * @date        2017年1月22日 下午10:51:49
+ * @details     
+ */
+public class Book002_一摞烙饼的排序 {
+
+}
