d7a41a893378010d4bdd7f941d74a9518f121826
zxwtry <zxwtry@qq.com>
2017-01-22 22:23:19 +0800

    添加 book.编程之美.Book001_中国象棋将帅问题

---
+++ src/book/编程之美/Book001_中国象棋将帅问题.java
@@ -0,0 +1,28 @@
+package book.编程之美;
+
+/**
+ * 	[题目]
+ * 	A表示'将'，B表示'帅'
+ * 	A,B限制在3*3的格子里运动。
+ * 	在书中，d,e,f是AB的三条竖线
+ * 	在书中，1,2,3是B的运动的行
+ * 	在书中，8,9,10是A的运动行
+ * 	输出A,B所有合法位置。
+ * 
+ * 	[要求]
+ * 	在代码中，只能使用一个变量。
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     book.编程之美
+ * @file        Book001_中国象棋将帅问题.java
+ * @type        Book001_中国象棋将帅问题
+ * @date        2017年1月22日 下午10:15:31
+ * @details		
+ */
+public class Book001_中国象棋将帅问题 {
+
+}
