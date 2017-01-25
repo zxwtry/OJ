4098dee367a99f4389c818be09ee6ae508d488b4
zxwtry <zxwtry@qq.com>
2017-01-25 21:38:58 +0800

    添加  leetcode P334_IncreasingTripletSubsequence

--- src/leetcode/P334_IncreasingTripletSubsequence.java
+++ src/leetcode/P334_IncreasingTripletSubsequence.java
@@ -25,12 +25,29 @@ package leetcode;
  * @file        P334_IncreasingTripletSubsequence.java
  * @type        P334_IncreasingTripletSubsequence
  * @date        2017年1月13日 下午10:21:48
- * @details     
+ * @details     Solution1: AC 时间O(N)，空间O(N)
  */
 public class P334_IncreasingTripletSubsequence {
-	static class Solution {
+	static class Solution1 {
 	    public boolean increasingTriplet(int[] nums) {
-	        
+	    	if (nums == null || nums.length < 3)
+	    		return false;
+	    	int len = nums.length;
+	    	int[] big = new int[len];
+	    	big[len - 1] = nums[len - 1];
+	    	for (int i = len - 2; i >= 0; i --) {
+	    		big[i] = Math.max(nums[i], big[i + 1]);
+	    	}
+	    	int[] small = new int[len];
+	    	small[0] = nums[0];
+	    	for (int i = 1; i < len; i ++) {
+	    		small[i] = Math.min(nums[i], small[i - 1]);
+	    	}
+	    	for (int i = 0; i < len; i ++) {
+	    		if (small[i] < nums[i] && big[i] > nums[i])
+	    			return true;
+	    	}
+	    	return false;
 	    }
 	}
 }
