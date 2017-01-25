bde047068dc68977cd033312e3df62908f617a74
zxwtry <zxwtry@qq.com>
2017-01-25 20:21:53 +0800

    添加 leetcode P367_ValidPerfectSquare Solution1

--- src/leetcode/P367_ValidPerfectSquare.java
+++ src/leetcode/P367_ValidPerfectSquare.java
@@ -1,8 +1,8 @@
 package leetcode;
 
 /**
- * Given a positive integer num, write a function which returns True
- * 	 if num is a perfect square else False.
+ *  Given a positive integer num, write a function which returns True
+ * 	if num is a perfect square else False.
 
 	Note: Do not use any built-in library function such as sqrt.
 	
@@ -26,15 +26,14 @@ package leetcode;
  * @file        P367_ValidPerfectSquare.java
  * @type        P367_ValidPerfectSquare
  * @date        2016年12月8日 下午10:32:36
- * @details     
+ * @details     Solution1: AC 
+ * @details     Solution2: AC 速度大概是Solution1的1/3
  */
 public class P367_ValidPerfectSquare {
-	public static void main(String[] args) {
-		
-	}
-	static class Solution {
+	static class Solution1 {
 	    public boolean isPerfectSquare(int num) {
-	        
+	        int sqrt = (int)Math.sqrt(num);
+	        return sqrt * sqrt == num;
 	    }
 	}
 }
