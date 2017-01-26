0be734a7fbcd1094655ab7aa42bf4153f61b8831
zxwtry <zxwtry@qq.com>
2017-01-26 23:36:17 +0800

    添加 leetcode P312_BurstBalloons

--- src/leetcode/P312_BurstBalloons.java
+++ src/leetcode/P312_BurstBalloons.java
@@ -7,21 +7,21 @@ package leetcode;
  * 	nums[i] * nums[right] coins. Here left and right are adjacent indices of i. 
  * 	After the burst, the left and right then becomes adjacent.
 
-	Find the maximum coins you can collect by bursting the balloons wisely.
-	
-	Note: 
-	(1) You may imagine nums[-1] = nums[n] = 1. They are not real therefore 
-	you can not burst them.
-	(2) 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
-	
-	Example:
-	
-	Given [3, 1, 5, 8]
-	
-	Return 167
-	
-	    nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
-	   coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
+ *	Find the maximum coins you can collect by bursting the balloons wisely.
+ *	
+ *	Note: 
+ *	(1) You may imagine nums[-1] = nums[n] = 1. They are not real therefore 
+ *	you can not burst them.
+ *	(2) 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
+ *	
+ *	Example:
+ *	
+ *	Given [3, 1, 5, 8]
+ *	
+ *	Return 167
+ *	
+ *	    nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
+ *	   coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
  */
 
 /**
