9a2a418591a46d7a1809eb27fcb71ec141350e29
zxwtry <zxwtry@qq.com>
2017-01-26 21:36:20 +0800

    添加 leetcode P326_PowerOfThree

--- src/leetcode/P326_PowerOfThree.java
+++ src/leetcode/P326_PowerOfThree.java
@@ -15,17 +15,19 @@ package leetcode;
  * @file        P326_PowerOfThree.java
  * @type        P326_PowerOfThree
  * @date        2017年1月10日 下午9:58:20
- * @details     Solution1: AC 使用pow
+ * @details     Solution1: AC 使用了loop
  */
 public class P326_PowerOfThree {
 	static class Solution1 {
 	    public boolean isPowerOfThree(int n) {
-	    	boolean isNegative = n < 0;
-	    	n = isNegative ? -n : n;
-	    	double pow = Math.pow(n, (double)1/3);
-	        int a = (int)pow;
-	        if (pow - a > 0.5) a ++;
-	        return a * a * a == n;
+	    	if (n < 1) return false; 
+	    	while (n != 1) {
+	    		if (n % 3 != 0)
+	    			return false;
+	    		n = n / 3;
+	    	}
+	    	return true;
 	    }
 	}
+	
 }
