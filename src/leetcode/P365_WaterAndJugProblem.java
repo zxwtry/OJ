e34c4df9eacdcb8ee3f5849282bea0ab8f80eb1d
zxwtry <zxwtry@qq.com>
2017-01-27 22:39:43 +0800

    添加 leetcode P365_WaterAndJugProblem

--- src/leetcode/P365_WaterAndJugProblem.java
+++ src/leetcode/P365_WaterAndJugProblem.java
@@ -1,11 +1,10 @@
 package leetcode;
 
-
 /**
  * 	You are given two jugs with capacities x and y litres. 
- * There is an infinite amount of water supply available. 
- * You need to determine whether it is possible to measure 
- * exactly z litres using these two jugs.
+ *  There is an infinite amount of water supply available. 
+ *  You need to determine whether it is possible to measure 
+ *  exactly z litres using these two jugs.
 
 	If z liters of water is measurable, you must have z 
 	liters of water contained within one or both buckets by the end.
@@ -24,8 +23,6 @@ package leetcode;
 	
 	Input: x = 2, y = 6, z = 5
 	Output: False
-	Credits:
-	Special thanks to @vinod23 for adding this problem and creating all test cases.
  */
 
 /**
