cd4e1dbf0b8d5ffc52a7d73b326dae3495409b8d
zxwtry <zxwtry@qq.com>
2017-01-27 22:58:53 +0800

    添加 leetcode P292_NimGame

--- src/leetcode/P292_NimGame.java
+++ src/leetcode/P292_NimGame.java
@@ -7,18 +7,18 @@ package leetcode;
  * 	the last stone will be the winner. You will take the first turn
  *  to remove the stones.
 
-	Both of you are very clever and have optimal strategies for the
-	 game. Write a function to determine whether you can win the game
-	  given the number of stones in the heap.
-	
-	For example, if there are 4 stones in the heap, then you will
-	never win the game: no matter 1, 2, or 3 stones you remove, the 
-	last stone will always be removed by your friend.
-	
-	Hint:
-	
-	If there are 5 stones in the heap, could you figure out a way to 
-	remove the stones such that you will always be the winner?
+ *	Both of you are very clever and have optimal strategies for the
+ *	game. Write a function to determine whether you can win the game
+ *	given the number of stones in the heap.
+ *	
+ *	For example, if there are 4 stones in the heap, then you will
+ *	never win the game: no matter 1, 2, or 3 stones you remove, the 
+ *	last stone will always be removed by your friend.
+ *	
+ *	Hint:
+ *	
+ *	If there are 5 stones in the heap, could you figure out a way to 
+ *	remove the stones such that you will always be the winner?
  */
 
 /**
