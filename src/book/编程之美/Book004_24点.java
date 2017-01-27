2e619c41e82b3ca74916aa86c1b6918193314384
zxwtry <zxwtry@qq.com>
2017-01-27 23:01:02 +0800

    添加 book.编程之美.Book004_24点

---
+++ src/book/编程之美/Book004_24点.java
@@ -0,0 +1,15 @@
+package book.编程之美;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     book.编程之美
+ * @file        Book004_24点.java
+ * @type        Book004_24点
+ * @date        2017年1月25日 下午6:32:15
+ * @details     
+ */
+public class Book004_24点 {
+	
+}
