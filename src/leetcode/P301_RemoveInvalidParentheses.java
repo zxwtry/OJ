855115cb1e9eb9361f5bf86298232130f67b7a64
zxwtry <zxwtry@qq.com>
2017-01-28 21:26:36 +0800

    添加  leetcode P301_RemoveInvalidParentheses

--- src/leetcode/P301_RemoveInvalidParentheses.java
+++ src/leetcode/P301_RemoveInvalidParentheses.java
@@ -3,15 +3,15 @@ package leetcode;
 /**
  * 	Remove the minimum number of invalid parentheses in order to make the 
  * 	input string valid. Return all possible results.
-
-	Note: The input string may contain letters other than the parentheses ( and ).
-	
-	Examples:
-	"()())()" -> ["()()()", "(())()"]
-	"(a)())()" -> ["(a)()()", "(a())()"]
-	")(" -> [""]
-	Credits:
-	Special thanks to @hpplayer for adding this problem and creating all test cases.
+ *	
+ *	Note: The input string may contain letters other than the parentheses ( and ).
+ *	
+ *	Examples:
+ *	"()())()" -> ["()()()", "(())()"]
+ *	"(a)())()" -> ["(a)()()", "(a())()"]
+ *	")(" -> [""]
+ *	Credits:
+ *	Special thanks to @hpplayer for adding this problem and creating all test cases.
  */
 
 /**
