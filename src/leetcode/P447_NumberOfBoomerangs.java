0042b26125b359c45d984e5087b9e0a5d542536a
zxwtry <zxwtry@qq.com>
2017-01-30 23:38:47 +0800

    添加 leetcode P447_NumberOfBoomerangs

--- src/leetcode/P447_NumberOfBoomerangs.java
+++ src/leetcode/P447_NumberOfBoomerangs.java
@@ -3,31 +3,28 @@ package leetcode;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-/*
+/**
  * 	Given n points in the plane that are all pairwise distinct,
  * 	a "boomerang" is a tuple of points (i, j, k) such that
  * 	the distance between i and j equals the distance 
  * 	between i and k (the order of the tuple matters).
-
-	Find the number of boomerangs. You may assume that 
-	n will be at most 500 and coordinates of points
-	 are all in the range [-10000, 10000] (inclusive).
-	
-	Example:
-	Input:
-	[[0,0],[1,0],[2,0]]
-	
-	Output:
-	2
-	
-	Explanation:
-	The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+ *	
+ *	Find the number of boomerangs. You may assume that 
+ *	n will be at most 500 and coordinates of points
+ *	 are all in the range [-10000, 10000] (inclusive).
+ *	
+ *	Example:
+ *	Input:
+ *	[[0,0],[1,0],[2,0]]
+ *	
+ *	Output:
+ *	2
+ *	
+ *	Explanation:
+ *	The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
  */
 
 public class P447_NumberOfBoomerangs {
-	public static void main(String[] args) {
-		debugMapSolution();
-	}
 	static void debugMapSolution() {
 //		int[][] points = tools.Utils.LEETCODE_int_二位数组_反序列化_("[[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8]]");
 //		int[][] points = tools.Utils.LEETCODE_int_二位数组_反序列化_("[[1,1],[2,2],[3,3],[4,4]]");
