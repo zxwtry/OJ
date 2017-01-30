7170432852115f85c2398deec81dcb3ad061b2a5
zxwtry <zxwtry@qq.com>
2017-01-30 23:41:44 +0800

    添加 leetcode P437_PathSumIII

--- src/leetcode/P437_PathSumIII.java
+++ src/leetcode/P437_PathSumIII.java
@@ -4,53 +4,37 @@ import java.util.LinkedList;
 
 import tools.TreeNode辅助.TreeNode;
 
-/*
+/**
  * 	You are given a binary tree in which each node contains an integer value.
-
-	Find the number of paths that sum to a given value.
-	
-	The path does not need to start or end at the root or a leaf, 
-	but it must go downwards (traveling only from parent nodes to child nodes).
-	
-	The tree has no more than 1,000 nodes and the values are in the range
-	 -1,000,000 to 1,000,000.
-	
-	Example:
-	
-	root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
-	
-	      10
-	     /  \
-	    5   -3
-	   / \    \
-	  3   2   11
-	 / \   \
-	3  -2   1
-	
-	Return 3. The paths that sum to 8 are:
-	
-	1.  5 -> 3
-	2.  5 -> 2 -> 1
-	3. -3 -> 11
+ *	
+ *	Find the number of paths that sum to a given value.
+ *	
+ *	The path does not need to start or end at the root or a leaf, 
+ *	but it must go downwards (traveling only from parent nodes to child nodes).
+ *	
+ *	The tree has no more than 1,000 nodes and the values are in the range
+ *	 -1,000,000 to 1,000,000.
+ *	
+ *	Example:
+ *	
+ *	root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ *	
+ *	      10
+ *	     /  \
+ *	    5   -3
+ *	   / \    \
+ *	  3   2   11
+ *	 / \   \
+ *	3  -2   1
+ *	
+ *	Return 3. The paths that sum to 8 are:
+ *	
+ *	1.  5 -> 3
+ *	2.  5 -> 2 -> 1
+ *	3. -3 -> 11
  */
 
 public class P437_PathSumIII {
-	public static void main(String[] args) {
-		final int N = Integer.MIN_VALUE;
-		TreeNode root = tools.TreeNode辅助.A_生成满二叉树(new int[] {
-//			10,
-//			5,-3,
-//			3,2,N,11,
-//			3,-2,N,1,N,N,N,N
-			1,
-			-2,-3,
-			1,3,-2,N,
-			-1,N,N,N,N,N,N,N
-		});
-		Solution s = new Solution();
-		int sum = -1;
-		System.out.println(s.pathSum(root, sum));
-	}
 	/*
 	 * 	AC
 	 * 	32 ms
