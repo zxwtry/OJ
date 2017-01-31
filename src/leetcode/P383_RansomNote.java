df83f135dad29d7bf8f2da6fd13b749be080d646
zxwtry <zxwtry@qq.com>
2017-02-01 07:39:18 +0800

    添加 leetcode P383_RansomNote

--- src/leetcode/P383_RansomNote.java
+++ src/leetcode/P383_RansomNote.java
@@ -2,26 +2,23 @@ package leetcode;
 
 import java.util.HashMap;
 
-/*\
+/**
  * 
  * 	 Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters 
  * 	from  all  the  magazines,  write  a  function  that  will  return  true  if  the  ransom 
- * 	  note  can  be  constructed  from  the  magazines ;  otherwise,  it  will  return  false.   
-
-	Each  letter  in  the  magazine  string  can  only  be  used  once  in  your  ransom  note.
-	
-	Note:
-	You may assume that both strings contain only lowercase letters.
-	
-	canConstruct("a", "b") -> false
-	canConstruct("aa", "ab") -> false
-	canConstruct("aa", "aab") -> true
+ * 	 note  can  be  constructed  from  the  magazines ;  otherwise,  it  will  return  false.   
+ * 	 
+ * 	 Each  letter  in  the  magazine  string  can  only  be  used  once  in  your  ransom  note.
+ * 	 
+ * 	 Note:
+ * 	 You may assume that both strings contain only lowercase letters.
+ * 	 
+ * 	 canConstruct("a", "b") -> false
+ * 	 canConstruct("aa", "ab") -> false
+ * 	 canConstruct("aa", "aab") -> true
  */
 
 public class P383_RansomNote {
-	public static void main(String[] args) {
-		
-	}
 	/*
 	 * 	69 ms
 	 * 	23.14%
