f71f63254614d72415af89700f19865654619997
zxwtry <zxwtry@qq.com>
2017-01-31 23:26:46 +0800

    添加 leetcode P405_ConvertaNumbertoHexadecimal

--- src/leetcode/P405_ConvertaNumbertoHexadecimal.java
+++ src/leetcode/P405_ConvertaNumbertoHexadecimal.java
@@ -2,43 +2,43 @@ package leetcode;
 
 import java.util.ArrayList;
 
-/*
+/**
  * 	Given an integer, write an algorithm to convert it to hexadecimal. 
  * 	For negative integer, two’s complement method is used.
-
-	IMPORTANT:
-	You must not use any method provided by the library which
-	 converts/formats the number to hex directly. 
-	 Such solution will result in disqualification of all your submissions 
-	 to this problem. Users may report such solutions after the contest ends 
-	 and we reserve the right of final decision and interpretation 
-	 in the case of reported solutions.
-	
-	Note:
-	
-	All letters in hexadecimal (a-f) must be in lowercase.
-	The hexadecimal string must not contain extra leading 0s. 
-	If the number is zero, it is represented by a single zero character '0'; 
-	otherwise, the first character in the hexadecimal string will 
-	not be the zero character.
-	The given number is guaranteed to fit within the range of
-	 a 32-bit signed integer.
-	You must not use any method provided by the library which 
-	converts/formats the number to hex directly.
-	Example 1:
-	
-	Input:
-	26
-	
-	Output:
-	"1a"
-	Example 2:
-	
-	Input:
-	-1
-	
-	Output:
-	"ffffffff"
+ *	
+ *	IMPORTANT:
+ *	You must not use any method provided by the library which
+ *	 converts/formats the number to hex directly. 
+ *	 Such solution will result in disqualification of all your submissions 
+ *	 to this problem. Users may report such solutions after the contest ends 
+ *	 and we reserve the right of final decision and interpretation 
+ *	 in the case of reported solutions.
+ *	
+ *	Note:
+ *	
+ *	All letters in hexadecimal (a-f) must be in lowercase.
+ *	The hexadecimal string must not contain extra leading 0s. 
+ *	If the number is zero, it is represented by a single zero character '0'; 
+ *	otherwise, the first character in the hexadecimal string will 
+ *	not be the zero character.
+ *	The given number is guaranteed to fit within the range of
+ *	 a 32-bit signed integer.
+ *	You must not use any method provided by the library which 
+ *	converts/formats the number to hex directly.
+ *	Example 1:
+ *	
+ *	Input:
+ *	26
+ *	
+ *	Output:
+ *	"1a"
+ *	Example 2:
+ *	
+ *	Input:
+ *	-1
+ *	
+ *	Output:
+ *	"ffffffff"
  * 
  */
 
