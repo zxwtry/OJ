7220e15d3df83c7fb583168c09c2008c7d193b32
zxwtry <zxwtry@qq.com>
2017-02-01 07:41:15 +0800

    添加 leetcode P368_LargestDivisibleSubset

--- src/leetcode/P368_LargestDivisibleSubset.java
+++ src/leetcode/P368_LargestDivisibleSubset.java
@@ -4,21 +4,21 @@ package leetcode;
  * Given a set of distinct positive integers, find the largest 
  * 	subset such that every pair (Si, Sj) of elements in this subset
  *  satisfies: Si % Sj = 0 or Sj % Si = 0.
-
-	If there are multiple solutions, return any subset is fine.
-	
-	Example 1:
-	
-	nums: [1,2,3]
-	
-	Result: [1,2] (of course, [1,3] will also be ok)
-	Example 2:
-	
-	nums: [1,2,4,8]
-	
-	Result: [1,2,4,8]
-	Credits:
-	Special thanks to @Stomach_ache for adding this problem and creating all test cases.
+ *	
+ *	If there are multiple solutions, return any subset is fine.
+ *	
+ *	Example 1:
+ *	
+ *	nums: [1,2,3]
+ *	
+ *	Result: [1,2] (of course, [1,3] will also be ok)
+ *	Example 2:
+ *	
+ *	nums: [1,2,4,8]
+ *	
+ *	Result: [1,2,4,8]
+ *	Credits:
+ *	Special thanks to @Stomach_ache for adding this problem and creating all test cases.
  */
 
 /**
@@ -32,10 +32,6 @@ package leetcode;
  * @details     
  */
 public class P368_LargestDivisibleSubset {
-	public static void main(String[] args) {
-		
-	}
-	
 	static class Solution {
 	    public List<Integer> largestDivisibleSubset(int[] nums) {
 	        
