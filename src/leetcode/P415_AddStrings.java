abf2850583da7ffb6e8a14891fe3513eed2d6c98
zxwtry <zxwtry@qq.com>
2017-01-31 23:20:08 +0800

    添加 leetcode P415_AddStrings

--- src/leetcode/P415_AddStrings.java
+++ src/leetcode/P415_AddStrings.java
@@ -1,17 +1,17 @@
 package leetcode;
 
 
-/*
+/**
  * 	Given two non-negative numbers num1 and num2 represented as string, 
  * 	return the sum of num1 and num2.
-
-	Note:
-	
-	The length of both num1 and num2 is < 5100.
-	Both num1 and num2 contains only digits 0-9.
-	Both num1 and num2 does not contain any leading zero.
-	You must not use any built-in BigInteger library or convert 
-	the inputs to integer directly.
+ * 	
+ * 	Note:
+ * 	
+ * 	The length of both num1 and num2 is < 5100.
+ * 	Both num1 and num2 contains only digits 0-9.
+ * 	Both num1 and num2 does not contain any leading zero.
+ * 	You must not use any built-in BigInteger library or convert 
+ * 	the inputs to integer directly.
  */
 
 public class P415_AddStrings {
