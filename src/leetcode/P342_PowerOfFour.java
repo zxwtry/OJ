be2f8c36d3b38ed53f7ef2020f81814170b31bb3
zxwtry <zxwtry@qq.com>
2017-02-02 21:17:00 +0800

    添加 leetcode P342_PowerOfFour

---
+++ src/leetcode/P342_PowerOfFour.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * 	Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+ * 	
+ * 	Example:
+ * 	Given num = 16, return true. Given num = 5, return false.
+ * 	
+ * 	Follow up: Could you solve it without loops/recursion?
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P342_PowerOfFour.java
+ * @type        P342_PowerOfFour
+ * @date        2017年2月2日 下午9:16:18
+ * @details     
+ */
+public class P342_PowerOfFour {
+	static class Solution {
+	    public boolean isPowerOfFour(int num) {
+	        
+	    }
+	}
+}
