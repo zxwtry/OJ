378b4987b92863e6dd3d8262412d858e059382aa
zxwtry <zxwtry@qq.com>
2017-02-02 23:16:07 +0800

    添加 leetcode P345_ReverseVowelsOfAString

---
+++ src/leetcode/P345_ReverseVowelsOfAString.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * 	Write a function that takes a string as input and reverse only the vowels of a string.
+ * 	
+ * 	Example 1:
+ * 	Given s = "hello", return "holle".
+ * 	
+ * 	Example 2:
+ * 	Given s = "leetcode", return "leotcede".
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P345_ReverseVowelsOfAString.java
+ * @type        P345_ReverseVowelsOfAString
+ * @date        2017年2月2日 下午11:15:26
+ * @details     
+ */
+public class P345_ReverseVowelsOfAString {
+
+}
