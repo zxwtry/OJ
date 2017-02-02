07a990cdebc980367fe9cd2443e202bbaa48fe11
zxwtry <zxwtry@qq.com>
2017-02-02 21:18:04 +0800

    添加 leetcode P343_IntegerBreak

---
+++ src/leetcode/P343_IntegerBreak.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * 	Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
+ *	
+ *	For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
+ *	
+ *	Note: You may assume that n is not less than 2 and not larger than 58.
+ *	
+ *	Hint:
+ *	
+ *	There is a simple O(n) solution to this problem.
+ *	You may check the breaking results of n ranging from 7 to 10 to discover the regularities.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P343_IntegerBreak.java
+ * @type        P343_IntegerBreak
+ * @date        2017年2月2日 下午9:17:23
+ * @details     
+ */
+public class P343_IntegerBreak {
+	static class Solution {
+	    public int integerBreak(int n) {
+	        
+	    }
+	}
+}
