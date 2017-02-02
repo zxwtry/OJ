6771ee0208f53bd95340948cdb4cc41ab76b9256
zxwtry <zxwtry@qq.com>
2017-02-02 23:26:27 +0800

    添加 leetcode P345_ReverseVowelsOfAString Solution AC 5ms 84.40%

--- src/leetcode/P345_ReverseVowelsOfAString.java
+++ src/leetcode/P345_ReverseVowelsOfAString.java
@@ -18,8 +18,34 @@ package leetcode;
  * @file        P345_ReverseVowelsOfAString.java
  * @type        P345_ReverseVowelsOfAString
  * @date        2017年2月2日 下午11:15:26
- * @details     
+ * @details     Solution: AC 5ms 84.40%
  */
 public class P345_ReverseVowelsOfAString {
-
+	static class Solution {
+	    public String reverseVowels(String s) {
+	    	if (s == null) return null;
+	        char[] cs = s.toCharArray();
+	        int i = 0, j = cs.length - 1;
+	        char ct = '\0';
+	        while (i < j) {
+	        	while (i < j && i < cs.length && ! isVowel(cs[i]))  i++;
+	        	while (i < j &&j > -1 && ! isVowel(cs[j])) j --;
+	        	if (i < j) {
+		        	ct = cs[i];
+		        	cs[i] = cs[j];
+		        	cs[j] = ct;
+		        	i ++;
+		        	j --;
+	        	}
+	        }
+	        return new String(cs);
+	    }
+	    private boolean isVowel(char c) {
+	    	return 	c == 'a' || c == 'A' ||
+	    			c == 'e' || c == 'E' ||
+	    			c == 'i' || c == 'I' ||
+	    			c == 'o' || c == 'O' ||
+	    			c == 'u' || c == 'U';
+	    }
+	}
 }
