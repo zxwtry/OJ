7f48b31398b5343bc3060719c29bc766196c9cf2
zxwtry <zxwtry@qq.com>
2017-02-02 21:11:50 +0800

    添加 leetcode P336_PalindromePairs

---
+++ src/leetcode/P336_PalindromePairs.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * 	Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+ * 	
+ * 	Example 1:
+ * 	Given words = ["bat", "tab", "cat"]
+ * 	Return [[0, 1], [1, 0]]
+ * 	The palindromes are ["battab", "tabbat"]
+ * 	Example 2:
+ * 	Given words = ["abcd", "dcba", "lls", "s", "sssll"]
+ * 	Return [[0, 1], [1, 0], [3, 2], [2, 4]]
+ * 	The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P336_PalindromePairs.java
+ * @type        P336_PalindromePairs
+ * @date        2017年2月2日 下午9:11:08
+ * @details     
+ */
+public class P336_PalindromePairs {
+	static class Solution {
+	    public List<List<Integer>> palindromePairs(String[] words) {
+	        
+	    }
+	}
+}
