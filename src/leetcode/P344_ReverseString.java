b4361a880bdec6f28e34aa9691a6605ac692adc1
zxwtry <zxwtry@qq.com>
2017-02-02 23:14:23 +0800

    添加 leetcode P344_ReverseString AC 2ms 82.98%

--- src/leetcode/P344_ReverseString.java
+++ src/leetcode/P344_ReverseString.java
@@ -15,12 +15,23 @@ package leetcode;
  * @file        P344_ReverseString.java
  * @type        P344_ReverseString
  * @date        2017年2月2日 下午9:18:26
- * @details     
+ * @details     Solution: AC 2ms 82.98%
  */
 public class P344_ReverseString {
 	static class Solution {
 	    public String reverseString(String s) {
-	        
+	        if (s == null) return null;
+	        char[] c = s.toCharArray();
+	        int i = 0, j = c.length - 1;
+	        char ct = '\0';
+	        while (i < j) {
+	        	ct = c[i];
+	        	c[i] = c[j];
+	        	c[j] = ct;
+	        	i ++;
+	        	j --;
+	        }
+	        return new String(c);
 	    }
 	}
 }
