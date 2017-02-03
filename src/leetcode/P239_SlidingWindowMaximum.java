ae591af896ae9e5185acd13b4d4c6c1225e867e9
zxwtry <zxwtry@qq.com>
2017-02-03 23:31:13 +0800

    添加 leetcode P239_SlidingWindowMaximum Solution

--- src/leetcode/P239_SlidingWindowMaximum.java
+++ src/leetcode/P239_SlidingWindowMaximum.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 /**
  * 	Given an array nums, there is a sliding window of size k which is moving from
  *  the very left of the array to the very right. You can only see the k numbers 
@@ -43,11 +45,46 @@ package leetcode;
  */
 public class P239_SlidingWindowMaximum {
 	public static void main(String[] args) {
-		
+		int[] nums = new int[] {1,3,-1,-3,5,3,6,7};
+		int k = 3;
+		Solution solution = new Solution();
+		tools.Utils.printArray(solution.maxSlidingWindow(nums, k), nums.length);
 	}
 	static class Solution {
 	    public int[] maxSlidingWindow(int[] nums, int k) {
-	        
+	    	if (nums == null || nums.length < 1 || k < 1)
+	    		return nums;
+	        LinkedList<Integer> l1 = new LinkedList<Integer>();
+	        LinkedList<Integer> l2 = new LinkedList<Integer>();
+	        l1.add(nums[0]);
+	        l2.add(0);
+	        int[] ans = new int[nums.length - k + 1];
+	        int ansIndex = 0;
+	        int i = 1;
+	        for (; i < k; i ++) {
+	        	while (! l1.isEmpty() && nums[i] > l1.peekLast()) {
+	        		l1.pollLast();
+	        		l2.pollLast();
+	        	}
+	        	l1.add(nums[i]);
+	        	l2.add(i);
+	        }
+	        ans[ansIndex ++] = l1.peekLast();
+ 	        for (; i < nums.length; i ++) {
+ 	        	while (i < k && ! l1.isEmpty() && nums[i] > l1.peekLast()) {
+	        		l1.pollLast();
+	        		l2.pollLast();
+	        		if (i - l2.peek() == k - 1) {
+	        			break;
+	        		}
+	        	}
+ 	        	if (l1.peekLast() >= nums[i]) {
+	 	        	l1.add(nums[i]);
+	 	        	l2.add(i);
+ 	        	}
+ 	        	ans[ansIndex ++] = l1.peekLast();
+	        }
+ 	        return ans;
 	    }
 	}
 }
