932388927f1b7bbf37b7dc4c51e510ce45430364
zxwtry <zxwtry@qq.com>
2017-02-03 23:41:40 +0800

    添加 leetcode P347_TopKFrequentElements

---
+++ src/leetcode/P347_TopKFrequentElements.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * 	Given a non-empty array of integers, return the k most frequent elements.
+ *	
+ *	For example,
+ *	Given [1,1,1,2,2,3] and k = 2, return [1,2].
+ *	
+ *	Note: 
+ *	You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ *	Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ *	
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P347_TopKFrequentElements.java
+ * @type        P347_TopKFrequentElements
+ * @date        2017年2月3日 下午11:39:29
+ * @details     
+ */
+public class P347_TopKFrequentElements {
+	main
+}
