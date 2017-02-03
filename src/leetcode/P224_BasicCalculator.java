5a7be423c3ae3c217ac3ac5cfc6ca3c87264407d
zxwtry <zxwtry@qq.com>
2017-02-03 19:40:41 +0800

    添加 leetcode P224_BasicCalculator Solution1 AC 16ms 75.51%

--- src/leetcode/P224_BasicCalculator.java
+++ src/leetcode/P224_BasicCalculator.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * 	Implement a basic calculator to evaluate a simple expression string.
  *	
@@ -21,8 +23,47 @@ package leetcode;
  * @file        P224_BasicCalculator.java
  * @type        P224_BasicCalculator
  * @date        2017年2月3日 下午6:51:00
- * @details     
+ * @details     Solution1: AC 16ms 75.51%
  */
 public class P224_BasicCalculator {
-
+	static class Solution1 {
+		final int addOperator = 1, subtractOperator = -1;
+	    public int calculate(String s) {
+	    	long ans = 0;
+	    	int signOfOperator = addOperator;
+	    	char c = '\0';
+	    	Stack<Long> stkAns = new Stack<Long>(); 
+	    	Stack<Integer> stkSgn = new Stack<Integer>(); 
+	        for (int i = 0; i < s.length(); i ++) {
+	        	c = s.charAt(i);
+	        	switch(c) {
+	        	case '+':
+	        		signOfOperator = addOperator;
+	        		break;
+	        	case '-':
+	        		signOfOperator = subtractOperator;
+	        		break;
+	        	case '(':
+	        		stkAns.add(ans);
+	        		ans = 0;
+	        		stkSgn.add(signOfOperator);
+	        		signOfOperator = addOperator;
+	        		break;
+	        	case ')':
+	        		ans = stkAns.pop() + (stkSgn.pop() == addOperator ? ans : - ans);
+	        		signOfOperator = addOperator;
+	        		break;
+	        	case ' ': 
+	        		break;
+	        	default:
+	        		int v = c - '0';
+	        		while (i+1 < s.length() && Character.isDigit(s.charAt(i+1)))
+	        			v = v * 10 + s.charAt(++ i) - '0';
+	        		ans  = ans + (signOfOperator == addOperator ? v : -v);
+	        		break;
+	        	}
+	        }
+	        return (int) ans;
+	    }
+	}
 }
