98f6536029d3aa41253cdf878f55d036ae9f6dd1
zxwtry <zxwtry@qq.com>
2017-02-03 18:53:06 +0800

    添加 leetcode P224_BasicCalculator

---
+++ src/leetcode/P224_BasicCalculator.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * 	Implement a basic calculator to evaluate a simple expression string.
+ *	
+ *	The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+ *	
+ *	You may assume that the given expression is always valid.
+ *	
+ *	Some examples:
+ *	"1 + 1" = 2
+ *	" 2-1 + 2 " = 3
+ *	"(1+(4+5+2)-3)+(6+8)" = 23
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P224_BasicCalculator.java
+ * @type        P224_BasicCalculator
+ * @date        2017年2月3日 下午6:51:00
+ * @details     
+ */
+public class P224_BasicCalculator {
+
+}
