df3b7c876ac604f754cbe9d786d0170b585145c3
zxwtry <zxwtry@qq.com>
2017-02-04 19:29:27 +0800

    添加 leetcode P352_DataStreamAsDisjointIntervals

---
+++ src/leetcode/P352_DataStreamAsDisjointIntervals.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P352_DataStreamAsDisjointIntervals.java
+ * @type        P352_DataStreamAsDisjointIntervals
+ * @date        2017年2月4日 下午7:29:16
+ * @details     
+ */
+public class P352_DataStreamAsDisjointIntervals {
+
+}
