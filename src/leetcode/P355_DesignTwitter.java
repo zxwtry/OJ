a9bb112c0b5faa8d4932a550588f3a48ea86e281
zxwtry <zxwtry@qq.com>
2017-02-04 19:41:07 +0800

    添加 leetcode P355_DesignTwitter

---
+++ src/leetcode/P355_DesignTwitter.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P355_DesignTwitter.java
+ * @type        P355_DesignTwitter
+ * @date        2017年2月4日 下午7:40:55
+ * @details     
+ */
+class P355_DesignTwitter {
+
+}
