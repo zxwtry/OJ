0aaadb6b0316f54fa686d607e0a3c8308d09b3de
zxwtry <zxwtry@qq.com>
2017-02-05 14:18:20 +0800

    添加 leetcode P483_SmallestGoodBase

---
+++ src/leetcode/P483_SmallestGoodBase.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P483_SmallestGoodBase.java
+ * @type        P483_SmallestGoodBase
+ * @date        2017年2月5日 下午2:18:07
+ * @details     
+ */
+public class P483_SmallestGoodBase {
+
+}
