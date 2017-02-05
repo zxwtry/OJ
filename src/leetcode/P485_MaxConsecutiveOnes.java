3d3606a4b482bca472a5858797c8151cc1209aed
zxwtry <zxwtry@qq.com>
2017-02-05 14:20:37 +0800

    添加 leetcode P485_MaxConsecutiveOnes

---
+++ src/leetcode/P485_MaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P485_MaxConsecutiveOnes.java
+ * @type        P485_MaxConsecutiveOnes
+ * @date        2017年2月5日 下午2:20:24
+ * @details     
+ */
+public class P485_MaxConsecutiveOnes {
+
+}
