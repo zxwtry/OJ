65b5d2d70ccc4d1f5c507233cd0aa6f2667c5a7a
zxwtry <zxwtry@qq.com>
2017-02-05 14:22:32 +0800

    添加 leetcode P486_PredictTheWinner

---
+++ src/leetcode/P486_PredictTheWinner.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P486_PredictTheWinner.java
+ * @type        P486_PredictTheWinner
+ * @date        2017年2月5日 下午2:22:21
+ * @details     
+ */
+public class P486_PredictTheWinner {
+
+}
