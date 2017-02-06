e25f80db7fed7cf40becb487befb1166223f71f0
zxwtry <zxwtry@qq.com>
2017-02-06 23:28:18 +0800

    添加 leetcode P488_ZumaGame

---
+++ src/leetcode/P488_ZumaGame.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P488_ZumaGame.java
+ * @type        P488_ZumaGame
+ * @date        2017年2月6日 下午11:28:03
+ * @details     
+ */
+public class P488_ZumaGame {
+
+}
