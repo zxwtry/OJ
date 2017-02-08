6920a9cdcbfa26dc196bd7a998181da7beb03f20
zxwtry <zxwtry@qq.com>
2017-02-08 23:22:37 +0800

    添加 leetcode P496_NextGreaterElementI

---
+++ src/leetcode/P496_NextGreaterElementI.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P496_NextGreaterElementI.java
+ * @type        P496_NextGreaterElementI
+ * @date        2017年2月8日 下午11:22:26
+ * @details     
+ */
+public class P496_NextGreaterElementI {
+
+}
