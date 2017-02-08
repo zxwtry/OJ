565f4701956215c26091c8731cea0c7efdf27fab
zxwtry <zxwtry@qq.com>
2017-02-08 23:21:21 +0800

    添加 leetcode P498_DiagonalTraverse

---
+++ src/leetcode/P498_DiagonalTraverse.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P498_DiagonalTraverse.java
+ * @type        P498_DiagonalTraverse
+ * @date        2017年2月8日 下午11:21:03
+ * @details     
+ */
+public class P498_DiagonalTraverse {
+
+}
