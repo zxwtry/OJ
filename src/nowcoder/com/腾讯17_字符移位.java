7f30b31be0b931525405cc8ae470f02f28c4bce9
zxwtry <zxwtry@qq.com>
2017-02-09 23:41:31 +0800

    添加 nowcoder.com 腾讯17_字符移位

--- src/nowcoder/com/腾讯17_字符移位.java
+++ src/nowcoder/com/腾讯17_字符移位.java
@@ -1,5 +1,14 @@
 package nowcoder.com;
 
+import java.util.LinkedList;
+
+/**
+ * 	给一个char[] arr，
+ * 	将小写字母移动到左边，大写字母移动到右边。
+ * 	保留小写字母中相对顺序，大写字母一样。
+ * 	例如：aAbBcCdD  -->  abcdABCD
+ */
+
 /**
  * @author      zxwtry
  * @email       zxwtry@qq.com
@@ -8,8 +17,5 @@ package nowcoder.com;
  * @file        腾讯17_字符移位.java
  * @type        腾讯17_字符移位
  * @date        2017年2月9日 上午11:54:12
- * @details     
  */
-public class 腾讯17_字符移位 {
-
-}
+public class 腾讯17_字符移位 {}
