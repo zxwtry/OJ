cfe5f83a4c70d44562b56600a5629d872d1a3b97
zxwtry <zxwtry@qq.com>
2017-02-09 23:47:09 +0800

    添加 test TestDemo

---
+++ src/test/TestDemo.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * 	这是用于测试的样式
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     test
+ * @file        TestDemo.java
+ * @type        TestDemo
+ * @date        2017年2月9日 下午9:24:48
+ * @details     
+ */
+public class TestDemo {}
