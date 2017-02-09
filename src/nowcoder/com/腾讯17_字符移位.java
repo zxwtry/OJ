70a8ed1621c16602d3f068137431851a9d4337da
zxwtry <zxwtry@qq.com>
2017-02-09 23:44:35 +0800

    添加 nowcoder.com 腾讯17_字符移位 StandardSolution

--- src/nowcoder/com/腾讯17_字符移位.java
+++ src/nowcoder/com/腾讯17_字符移位.java
@@ -17,5 +17,27 @@ import java.util.LinkedList;
  * @file        腾讯17_字符移位.java
  * @type        腾讯17_字符移位
  * @date        2017年2月9日 上午11:54:12
+ * @details     StandardSolution: 归并排序
  */
-public class 腾讯17_字符移位 {}
+public class 腾讯17_字符移位 {
+	static class StandardSolution {
+		public void reArrange(char[] arr) {
+			LinkedList<Character> lowerList = new LinkedList<Character>();
+			LinkedList<Character> upperList = new LinkedList<Character>();
+			for (char c : arr) {
+				if (c >= 'a' && c <= 'z') {
+					lowerList.add(c);
+				} else {
+					upperList.add(c);
+				}
+			}
+			int arrIndex = 0;
+			for (char c : lowerList) {
+				arr[arrIndex ++] = c;
+			}
+			for (char c : upperList) {
+				arr[arrIndex ++] = c;
+			}
+		}
+	}
+}
