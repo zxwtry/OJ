5387dae997185ed0d35d1eb968191a1317656ce6
zxwtry <zxwtry@qq.com>
2017-02-10 23:53:38 +0800

    添加 leetcode P268_MissingNumber Solution2

--- src/leetcode/P268_MissingNumber.java
+++ src/leetcode/P268_MissingNumber.java
@@ -23,8 +23,14 @@ package leetcode;
  * @type        P268_MissingNumber
  * @date        2016年12月13日 下午10:25:19
  * @details     Solution1: AC 2ms 23.20%
+ * @details     Solution1: AC 2ms 23.20%
  */
 public class P268_MissingNumber {
+	public static void main(String[] args) {
+		int[] nums = new int[] {0, 1, 3,4,2,6};
+		Solution1 solution1 = new Solution1();
+		System.out.println(solution1.missingNumber(nums));
+	}
 	static class Solution1 {
 	    public int missingNumber(int[] nums) {
 	        boolean[] arr = new boolean[nums.length + 1];
@@ -34,6 +40,19 @@ public class P268_MissingNumber {
 	        for (int arrIndex = 0; arrIndex < arr.length; arrIndex ++) {
 	        	if (! arr[arrIndex]) {
 	        		return arrIndex;
+	        	}
+	        }
+	        return 0;
+	    }
+	}
+	static class Solution2 {
+		public int missingNumber(int[] nums) {
+	        int leftIndex = 0, rightIndex = nums.length;
+	        while (leftIndex < rightIndex) {
+	        	if (nums[leftIndex] == leftIndex) {
+	        		leftIndex ++;
+	        	} else if (nums[rightIndex] == rightIndex) {
+	        		rightIndex --;
 	        	}
 	        }
 	        return 0;
