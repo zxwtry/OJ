7c8d992caf31530a6060a4c6249cd0e3f63f8221
zxwtry <zxwtry@qq.com>
2017-02-11 17:22:25 +0800

    添加 leetcode P273_IntegerToEnglishWords Solution1 AC 5ms 35.37%

--- src/leetcode/P273_IntegerToEnglishWords.java
+++ src/leetcode/P273_IntegerToEnglishWords.java
@@ -32,35 +32,56 @@ package leetcode;
  * @file        P273_IntegerToEnglishWords.java
  * @type        P273_IntegerToEnglishWords
  * @date        2016年12月13日 下午10:26:55
- * @details     
+ * @details     Solution1: AC 5ms 35.37%
  */
 public class P273_IntegerToEnglishWords {
-	public static void main(String[] args) {
-		Solution1 solution1 = new Solution1();
-		for (int i = 0; i < 21; i ++) {
-			System.out.println(solution1.numberToWords(i));
-		}
-		System.out.println(Integer.MAX_VALUE);
-	}
 	static class Solution1 {
 		String[] words = new String[] {
-			"zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+			"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
 			"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty",
 		};
 		String[] tens = new String[] {
-			"","Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty"
+			"","Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
 		};
 		String[] threeTens = new String[] {
 			"", "Thousand", "Million", "Billion"
 		};
+		String Hundred = "Hundred";
+		String Zero = "Zero";
 	    public String numberToWords(int num) {
-	        if (num <= 20) return words[num];
+	        if (num <= 20) return num == 0 ? Zero : words[num];
 	        char[] numChar = ("" + num).toCharArray();
-	        
-	        return "NULL";
+	        String ans = "";
+	        int numCharIndex = numChar.length - 1;
+	        int threeTensIndex = 0;
+	        while (numCharIndex > -1) {
+	        	if (numCharIndex - 2 > -1) {
+	        		ans = combineTwoString(combineTwoString(noMoreThanThreeChar(numChar, numCharIndex - 2, numCharIndex), threeTens[threeTensIndex]), ans);
+	        		numCharIndex = numCharIndex - 3;
+	        	} else {
+	        		ans = combineTwoString(combineTwoString(noMoreThanThreeChar(numChar, 0, numCharIndex), threeTens[threeTensIndex]), ans);
+	        		numCharIndex = -1;
+	        	}
+	        	threeTensIndex ++;
+	        }
+	        return ans;
 	    }
-	    public String lessThanThreeChar(char[] cs, int sti, int eni) {
-	    	
+	    public String noMoreThanThreeChar(char[] cs, int sti, int eni) {
+	    	String ans = (eni - sti == 2 ?  combineTwoString(cs[sti] == '0' ? "" : words[cs[sti] - '0'], Hundred) : "");
+	    	sti = (eni - sti == 2 ? sti + 1 : sti);
+	    	int val = (sti == eni ? cs[sti] - '0' : (cs[sti] - '0') * 10 + (cs[eni] - '0'));
+	    	if (val < words.length) {
+	    		ans = combineTwoString(ans, words[val]);
+	    	} else {
+	    		ans = combineTwoString(ans, combineTwoString(tens[val / 10], words[val % 10]));
+	    	}
+	    	return ans;
 	    }
+	    public String combineTwoString(String a, String b) {
+	    	if (a.equals("") && (b.equals("Thousand") || b.equals("Million") || b.equals("Billion") || b.equals("Hundred"))) {
+	    		return a;
+	    	}
+	    	return a.equals("") ? b : ( b.equals("") ? a : a + " " + b);
+ 	    }
 	}
 }
