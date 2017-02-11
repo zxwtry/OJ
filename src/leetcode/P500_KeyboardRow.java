40e6ff3f402b1ff4472d6a326663258eb00c1033
zxwtry <zxwtry@qq.com>
2017-02-11 23:59:43 +0800

    添加 leetcode P500_KeyboardRow

--- src/leetcode/P500_KeyboardRow.java
+++ src/leetcode/P500_KeyboardRow.java
@@ -1,6 +1,21 @@
 package leetcode;
 
 /**
+ * 	Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+
+
+American keyboard
+
+
+Example 1:
+Input: ["Hello", "Alaska", "Dad", "Peace"]
+Output: ["Alaska", "Dad"]
+Note:
+You may use one character in the keyboard more than once.
+You may assume the input string will only contain letters of alphabet.
+ */
+
+/**
  * @author      zxwtry
  * @email       zxwtry@qq.com
  * @project     OJ
