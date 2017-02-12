73d0d24cbc6598f83feea41921d5310c7344d171
zxwtry <zxwtry@qq.com>
2017-02-12 20:32:06 +0800

    添加 leetcode P513_FindLeftMostElement

--- src/leetcode/P513_FindLeftMostElement.java
+++ src/leetcode/P513_FindLeftMostElement.java
@@ -4,6 +4,33 @@ import java.util.LinkedList;
 
 import tools.TreeNode辅助.TreeNode;
 
+/**
+ *	Given a binary tree, find the left most element in the last row of the tree.
+ *	
+ *	Example 1:
+ *	Input:
+ *	
+ *	    2
+ *	   / \
+ *	  1   3
+ *	
+ *	Output:
+ *	1
+ *	Example 2: 
+ *	Input:
+ *	
+ *	        1
+ *	       / \
+ *	      2   3
+ *	     /   / \
+ *	    4   5   6
+ *	       /
+ *	      7
+ *	
+ *	Output:
+ *	7
+ *	Note: You may assume the tree is not NULL.
+ */
 
 /**
  * @author      zxwtry
