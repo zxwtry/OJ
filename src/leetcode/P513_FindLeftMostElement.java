420983a7df823aa9b137d68aea535097dc813a76
zxwtry <zxwtry@qq.com>
2017-02-12 20:32:45 +0800

    添加  leetcode P513_FindLeftMostElement Solution1

--- src/leetcode/P513_FindLeftMostElement.java
+++ src/leetcode/P513_FindLeftMostElement.java
@@ -40,6 +40,34 @@ import tools.TreeNode辅助.TreeNode;
  * @file        P513_FindLeftMostElement.java
  * @type        P513_FindLeftMostElement
  * @date        2017年2月12日 下午8:30:36
- * @details     
+ * @details     Solution1: AC
  */
-public class P513_FindLeftMostElement {}
+public class P513_FindLeftMostElement {
+	static class Solution1 {
+	    public int findLeftMostNode(TreeNode root) {
+	        LinkedList<TreeNode> layer = new LinkedList<TreeNode>();
+	        TreeNode rootNow = null;
+	        layer.add(root);
+	        int layCount = 1;
+	        int layCountPre = 0;
+	        int ans = 0;
+	        while(layer.size() != 0) {
+	        	rootNow = layer.pollFirst();
+	        	if (rootNow == null) {
+	        		layCount ++;
+	        	} else {
+	        		if (layCount == layCountPre + 1) {
+	        			ans = rootNow.val;
+	        			layCountPre = layCount;
+	        			layer.add(null);
+	        		}
+		        	if (rootNow.left != null)
+		        		layer.addLast(rootNow.left);
+		        	if (rootNow.right != null)
+		        		layer.addLast(rootNow.right);
+	        	}
+	        }
+	        return ans;
+	    }
+	}
+}
