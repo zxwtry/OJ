36526cc6236522479cc17e90ee71b73fe5acf08f
zxwtry <zxwtry@qq.com>
2017-02-12 20:28:50 +0800

    添加 leetcode P504_Base7

--- src/leetcode/P504_Base7.java
+++ src/leetcode/P504_Base7.java
@@ -8,23 +8,17 @@ package leetcode;
  *	Example 2:
  *	Input: -7
  *	Output: "-10"
- *	Note: The input will be in range of [-1e7, 1e7].
+	Note: The input will be in range of [-1e7, 1e7].
  */
-public class P504_Base7 {
-	static class Solution {
-	    public String convertTo7(int num) {
-	    	if (num == 0) return "0";
-	        char[] cs = new char[40];
-	        int csIndex = cs.length - 1;
-	        boolean isNegative = num < 0;
-	        num = Math.abs(num);
-	        int count = 0;
-	        while (num != 0) {
-	        	cs[csIndex --] = (char)(num % 7 + '0');
-	        	num = num / 7;
-	        	count ++;
-	        }
-	        return isNegative ? "-" + new String(cs, csIndex + 1, count) : new String(cs, csIndex + 1, count);
-	    }
-	}
-}
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P504_Base7.java
+ * @type        P504_Base7
+ * @date        2017年2月12日 下午8:28:20
+ * @details     
+ */
+public class P504_Base7 {}
