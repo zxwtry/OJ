ecf79713e207312cc12dfa0349722b587bdefae1
zxwtry <zxwtry@qq.com>
2017-02-13 17:21:00 +0800

    添加 leetcode P279_PerfectSquares Solution1: AC 75ms 42.59%

--- src/leetcode/P279_PerfectSquares.java
+++ src/leetcode/P279_PerfectSquares.java
@@ -3,9 +3,9 @@ package leetcode;
 /**
  * 	Given a positive integer n, find the least number of perfect 
  * 	square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
-	
-	For example, given n = 12, return 3 because 12 = 4 + 4 + 4; 
-	given n = 13, return 2 because 13 = 4 + 9.
+ * 	
+ * 	For example, given n = 12, return 3 because 12 = 4 + 4 + 4; 
+ * 	given n = 13, return 2 because 13 = 4 + 9.
  */
 
 /**
@@ -16,15 +16,29 @@ package leetcode;
  * @file        P279_PerfectSquares.java
  * @type        P279_PerfectSquares
  * @date        2016年12月14日 下午10:23:11
- * @details     
+ * @details     Solution1: AC 75ms 42.59%
  */
 public class P279_PerfectSquares {
-	public static void main(String[] args) {
-		
-	}
-	static class Solution {
+	static class Solution1 {
 	    public int numSquares(int n) {
-	        
+	    	int[] sqrtArray = new int[n + 1];
+	    	sqrtArray[0] = 0;
+	    	int sqrtIndex = 0;
+	    	int newIndex = 0;
+	    	for (int sqrtArrayIndex = 0; sqrtArrayIndex <= n; sqrtArrayIndex ++) {
+	    		sqrtIndex = 1;
+	    		while (true) {
+	    			newIndex = sqrtArrayIndex + sqrtIndex * sqrtIndex;
+	    			if (newIndex > n) break;
+	    			if (sqrtArray[newIndex] == 0) {
+	    				sqrtArray[newIndex] = sqrtArray[sqrtArrayIndex] + 1;
+	    			} else {
+	    				sqrtArray[newIndex] = Math.min(sqrtArray[newIndex],  sqrtArray[sqrtArrayIndex] + 1);
+	    			}
+ 	    			sqrtIndex ++;
+	    		}
+	    	}
+	    	return sqrtArray[n];
 	    }
 	}
 }
