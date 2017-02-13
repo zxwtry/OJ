f173f824e84d04600d2f0151e0374fe931371d1a
zxwtry <zxwtry@qq.com>
2017-02-13 17:36:51 +0800

    添加 leetcode P279_PerfectSquares Solution3: AC  61ms 74.41%

--- src/leetcode/P279_PerfectSquares.java
+++ src/leetcode/P279_PerfectSquares.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
+
+import com.sun.org.apache.xerces.internal.impl.xs.SchemaSymbols;
+
 /**
  * 	Given a positive integer n, find the least number of perfect 
  * 	square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
@@ -16,7 +20,9 @@ package leetcode;
  * @file        P279_PerfectSquares.java
  * @type        P279_PerfectSquares
  * @date        2016年12月14日 下午10:23:11
- * @details     Solution1: AC 75ms 42.59%
+ * @details     Solution1: AC  75ms 42.59%
+ * @details     Solution2: AC 100ms 33.81%
+ * @details     Solution3: AC  61ms 74.41%
  */
 public class P279_PerfectSquares {
 	static class Solution1 {
@@ -40,5 +46,35 @@ public class P279_PerfectSquares {
 	    	}
 	    	return sqrtArray[n];
 	    }
+	}
+	static class Solution2 {
+		public int numSquares(int n) {
+			ArrayList<Integer> dpList =  new ArrayList<Integer>(n);
+			dpList.add(0);
+			int numSquareNow = 0;
+			while (dpList.size() <= n) {
+				numSquareNow = Integer.MAX_VALUE;
+				for (int index = 1; index * index <= dpList.size(); index ++) {
+					numSquareNow = Math.min(numSquareNow, dpList.get(dpList.size() - index * index) + 1);
+				}
+				dpList.add(numSquareNow);
+			}
+			return dpList.get(n);
+		}
+	}
+	static class Solution3 {
+		public int numSquares(int n) {
+			int[] dp = new int[n + 1];
+			int dpIndex = 1;
+			int numSquareNow = 0;
+			while (dpIndex <= n) {
+				numSquareNow = Integer.MAX_VALUE;
+				for (int index = 1; index * index <= dpIndex; index ++) {
+					numSquareNow = Math.min(numSquareNow, dp[dpIndex - index * index] + 1);
+				}
+				dp[dpIndex ++] = numSquareNow;
+			}
+			return dp[n];
+		}
 	}
 }
