45a03f5bed4aab37cfbce47d6499740ed381af87
zxwtry <zxwtry@qq.com>
2017-02-13 09:34:59 +0800

    添加 leetcode P493_ReversePairs Solution1: AC 880 ms

--- src/leetcode/P493_ReversePairs.java
+++ src/leetcode/P493_ReversePairs.java
@@ -27,4 +27,100 @@ import java.util.ArrayList;
  * @date        2017年2月12日 下午4:03:34
  * @details     Solution1: AC 880 ms
  */
-public class P493_ReversePairs {}
+public class P493_ReversePairs {
+	static class Solution1 {
+	    public int reversePairs(int[] nums) {
+	    	int count = 0;
+	    	ArrayList<Integer> list = new ArrayList<Integer>();
+	    	int leftIndex = 0, rightIndex = 0, middleIndex = 0, minus = 0;
+	    	for (int index = nums.length - 1; index > -1; index --) {
+	    		leftIndex = 0;
+	    		rightIndex = list.size();
+	    		while (leftIndex < rightIndex) {
+	    			middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+	    			minus = cmp2(nums, index, list, middleIndex);
+	    			if (minus > 0) leftIndex = middleIndex + 1;
+	    			else rightIndex = middleIndex;
+	    		}
+	    		//System.out.printf("index=%d\t\tleftIndex=%d\t\trightIndex=%d\r\n", index, leftIndex, rightIndex);
+	    		count += leftIndex;
+	    		leftIndex = 0;
+	    		rightIndex = list.size();
+	    		while (leftIndex < rightIndex) {
+	    			middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+	    			minus = cmp1(nums, index, list, middleIndex);
+	    			if (minus > 0) leftIndex = middleIndex + 1;
+	    			else rightIndex = middleIndex;
+	    		}
+	    		list.add(rightIndex, nums[index]);
+	    		//System.out.println(list);
+	    	}
+	    	return count;
+	    }
+	    private int cmp1(int[] nums, int numsIndex, ArrayList<Integer> list, int listIndex) {
+	    	long cut = (long)nums[numsIndex] - (long)list.get(listIndex);
+	    	if (cut == 0) {
+	    		return 0;
+	    	} else {
+	    		return cut > 0 ? 1 : -1;
+	    	}
+	    }
+	    private int cmp2(int[] nums, int numsIndex, ArrayList<Integer> list, int listIndex) {
+	    	long cut = (long)nums[numsIndex] - 2 * (long)list.get(listIndex);
+	    	if (cut == 0) {
+	    		return 0;
+	    	} else {
+	    		return cut > 0 ? 1 : -1;
+	    	}
+	    }
+	}
+	
+	static class Solution2 {
+		public int reversePairs(int[] nums) {
+	    	int count = 0;
+	    	ArrayList<Integer> list = new ArrayList<Integer>();
+	    	int leftIndex = 0, rightIndex = 0, middleIndex = 0, minus = 0;
+	    	for (int index = nums.length - 1; index > -1; index --) {
+	    		//第一个大于nums[index]
+	    		list.add(binaryFirstLarger(nums, index, list), nums[index]);
+	    		System.out.println(list);
+	    	}
+	    	return count;
+		}
+		private int binaryFirstLarger(int[] nums, int numsIndex, ArrayList<Integer> list) {
+			
+		}
+		private int cmp1(int[] nums, int numsIndex, ArrayList<Integer> list, int listIndex) {
+	    	long cut = (long)nums[numsIndex] - (long)list.get(listIndex);
+	    	if (cut == 0) {
+	    		return 0;
+	    	} else {
+	    		return cut > 0 ? 1 : -1;
+	    	}
+	    }
+	    private int cmp2(int[] nums, int numsIndex, ArrayList<Integer> list, int listIndex) {
+	    	long cut = (long)nums[numsIndex] - 2 * (long)list.get(listIndex);
+	    	if (cut == 0) {
+	    		return 0;
+	    	} else {
+	    		return cut > 0 ? 1 : -1;
+	    	}
+	    }
+	}
+	
+	static class StandardSolution {
+		public int reversePairs(int[] nums) {
+	       if (nums == null || nums.length < 2) return 0; 
+	       int count = 0;
+	       for (int i = 0; i < nums.length; i ++) {
+	    	   for (int j = i + 1; j < nums.length; j ++) {
+	    		   count += isReversePairs(nums, i, j) ? 1 : 0;
+	    	   }
+	       }
+	       return count;
+	    }
+	    private boolean isReversePairs(int[] nums, int i, int j) {
+	    	return i < j && nums[i] > 2 * nums[j];
+	    }
+	}
+}
