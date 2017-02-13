387895bc356308d0a3120e5cf6496aa797832886
zxwtry <zxwtry@qq.com>
2017-02-13 16:38:15 +0800

    添加 leetcode P275_H_IndexII StandardSolution

--- src/leetcode/P275_H_IndexII.java
+++ src/leetcode/P275_H_IndexII.java
@@ -1,9 +1,5 @@
 package leetcode;
 
-import javax.xml.stream.events.StartDocument;
-
-import com.sun.xml.internal.bind.v2.model.core.LeafInfo;
-
 /**
  * 	Follow up for H-Index: What if the citations array is sorted in 
  * 	ascending order? Could you optimize your algorithm?
@@ -21,36 +17,24 @@ import com.sun.xml.internal.bind.v2.model.core.LeafInfo;
  * @file        P275_H_IndexII.java
  * @type        P275_H_IndexII
  * @date        2016年12月14日 下午10:20:20
- * @details     
+ * @details     StandardSolution: TLE
+ * @details     Solution1:  AC  24ms  17.79%
+ * @details     Solution2:  AC  13ms   41.32%
+ * @details     Solution3:  AC  11ms  76.40%
  */
 public class P275_H_IndexII {
-	public static void main(String[] args) {
-		int[] citations = new int[] {10, 8, 5, 4, 3};
-		Solution solution = new Solution();
-		System.out.println(solution.hIndex(citations));
-	}
-	static class Solution {
-	    public int hIndex(int[] citations) {
-	    	if (citations == null || citations.length < 1)
+	static class StandardSolution {
+		public int hIndex(int[] citations) {
+			if (citations == null || citations.length < 1)
 	    		return 0;
-	        int leftIndex = 0, rightIndex = citations.length - 1;
-	        int midIndex = 0;
-	        while (leftIndex <= rightIndex) {
-	        	midIndex = (leftIndex + rightIndex) / 2;
-	        	int midValue = getValue(citations, midIndex);
-	        	int midPlusValue = getValue(citations, midIndex + 1);
-	        	if (midValue == midPlusValue) {
-	        		return midValue;
-	        	} else if (midValue < midPlusValue) {
-	        		leftIndex = midIndex;
-	        	} else {
-	        		rightIndex = midIndex;
-	        	}
-	        }
-	        return 0;
-	    }
-	    private int getValue(int[] citations, int index) {
-	    	return Math.min(index + 1, citations[index]);
+			int maxHIndex = Integer.MIN_VALUE;
+			for (int index = 0; index < citations.length; index ++) {
+				maxHIndex = Math.max(maxHIndex, getValue(citations, index));
+			}
+			return maxHIndex;
+		}
+		private int getValue(int[] citations, int index) {
+	    	return Math.min(citations.length - index, citations[index]);
 	    }
 	}
 }
