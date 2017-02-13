40d810eb330484330772fe6b927954a11abe74b6
zxwtry <zxwtry@qq.com>
2017-02-13 09:31:57 +0800

    添加 leetcode P515_FindLargestElementInEachRow

---
+++ src/leetcode/P515_FindLargestElementInEachRow.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.LinkedList;
+
+import tools.TreeNode辅助.TreeNode;
+
+/**
+ * 	You need to find the largest value in each row of a binary tree.
+ *  Example:
+ *  Input: 
+ *           1
+ *          / \
+ *         3   2
+ *        / \   \  
+ *       5   3   9 
+ *  Output: [1, 3, 9]
+ */
+
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P515_FindLargestElementInEachRow.java
+ * @type        P515_FindLargestElementInEachRow
+ * @date        2017年2月13日 上午9:16:20
+ * @details     
+ */
+public class P515_FindLargestElementInEachRow {}
