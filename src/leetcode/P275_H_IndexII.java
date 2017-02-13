9e8507538e4413d6f7eba4bce50637240f29cda9
zxwtry <zxwtry@qq.com>
2017-02-13 16:39:22 +0800

    添加 P275_H_IndexII Solution1:  AC  24ms  17.79%

--- src/leetcode/P275_H_IndexII.java
+++ src/leetcode/P275_H_IndexII.java
@@ -37,4 +37,71 @@ public class P275_H_IndexII {
 	    	return Math.min(citations.length - index, citations[index]);
 	    }
 	}
+	static class Solution1 {
+		public int hIndex(int[] citations) {
+	    	if (citations == null || citations.length < 1)
+	    		return 0;
+	        return getMaxValue(citations, 0, citations.length - 1);
+	    }
+	    private int getMaxValue(int[] citations, int leftIndex, int rightIndex) {
+	    	if (leftIndex == rightIndex) {
+	    		return getValue(citations, leftIndex);
+	    	}
+	        int midIndex = 0;
+	        if (upTowardsRight(citations, leftIndex, leftIndex + 1) < 0) return getValue(citations, leftIndex);
+	        if (upTowardsLeft(citations, rightIndex - 1, rightIndex) < 0) return getValue(citations, rightIndex);
+	        while (leftIndex <= rightIndex) {
+	        	midIndex = (leftIndex + rightIndex) / 2;
+	        	int upTR = midIndex + 1 < citations.length ? upTowardsRight(citations, midIndex, midIndex + 1) : 0;
+	        	if (upTR == 0) {
+	        		return Math.max(getMaxValue(citations, leftIndex, midIndex)
+	        				, getMaxValue(citations, midIndex + 1, rightIndex));
+	        	}
+	        	int upTL = midIndex > 0 ? 0 : upTowardsLeft(citations, midIndex - 1, midIndex);
+	        	if (upTL == 0) {
+	        		return Math.max(getMaxValue(citations, leftIndex, midIndex - 1)
+	        				, getMaxValue(citations, midIndex, rightIndex));
+	        	}
+	        	int upValue = 10 * upTR + upTL;
+	        	switch (upValue) {
+				case 0:
+				case -11:
+				case -1:
+				case -10:
+					return getValue(citations, midIndex);
+				case 1:
+				case -9:
+					rightIndex = midIndex - 1;
+					break;
+				case 10:
+				case 9:
+					leftIndex = midIndex + 1;
+					break;
+				case 11:
+				default:
+					break;
+				}
+	        }
+	        return 0;
+	    }
+	    private int upTowardsRight(int[] citations, int smallIndex, int largeIndex) {
+	    	if (getValue(citations, largeIndex) > getValue(citations, smallIndex)) {
+	    		return 1;
+	    	} else if (getValue(citations, largeIndex) == getValue(citations, smallIndex)) {
+	    		return 0;
+	    	}
+	    	return -1;
+	    }
+	    private int upTowardsLeft(int[] citations, int smallIndex, int largeIndex) {
+	    	if (getValue(citations, largeIndex) < getValue(citations, smallIndex)) {
+	    		return 1;
+	    	} else if (getValue(citations, largeIndex) < getValue(citations, smallIndex)) {
+	    		return 0;
+	    	}
+	    	return -1;
+	    }
+	    private int getValue(int[] citations, int index) {
+	    	return Math.min(citations.length - index, citations[index]);
+	    }
+	}
 }
