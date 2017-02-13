cae0044804730f95262c7fa75183777bde88774e
zxwtry <zxwtry@qq.com>
2017-02-13 09:34:35 +0800

    添加 leetcode P493_ReversePairs

---
+++ src/leetcode/P493_ReversePairs.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+/**
+ *	Given an array nums, we call (i, j) an important reverse pair if i < j and 
+ *	nums[i] > 2*nums[j].
+ *	You need to return the number of important reverse pairs in the given array.
+ *	Example1:
+ *	Input: [1,3,2,3,1]
+ *	Output: 2
+ *	Example2:
+ *	Input: [2,4,3,5,1]
+ *	Output: 3
+ *	Note:
+ *	The length of the given array will not exceed 50,000.
+ *	All the numbers in the input array are in the range of 32-bit integer.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P493_ReversePairs.java
+ * @type        P493_ReversePairs
+ * @date        2017年2月12日 下午4:03:34
+ * @details     Solution1: AC 880 ms
+ */
+public class P493_ReversePairs {}
