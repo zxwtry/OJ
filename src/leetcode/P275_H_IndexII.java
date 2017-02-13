41a4b8b05cb72c52ab8a9f19d80c51b90aca6978
zxwtry <zxwtry@qq.com>
2017-02-13 16:39:51 +0800

    添加 leetcode P275_H_IndexII Solution2:  AC  13ms   41.32%

--- src/leetcode/P275_H_IndexII.java
+++ src/leetcode/P275_H_IndexII.java
@@ -104,4 +104,25 @@ public class P275_H_IndexII {
 	    	return Math.min(citations.length - index, citations[index]);
 	    }
 	}
+	static class Solution2 {
+	    public int hIndex(int[] citations) {
+	        int len = citations.length;
+	        int first = 0;
+	        int mid;
+	        int count = len;
+	        int step;
+	        while (count > 0) {
+	            step = count / 2;
+	            mid = first + step;
+	            if (citations[mid] < len - mid) {
+	                first = mid + 1;
+	                count -= (step + 1);
+	            }
+	            else {
+	                count = step;
+	            }
+	        }
+	        return len - first;
+	    }
+	}
 }
