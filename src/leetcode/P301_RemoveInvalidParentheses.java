0935989204a196f6749fc4f2aa819ae760649924
zxwtry <zxwtry@qq.com>
2017-02-15 11:48:18 +0800

    添加 P301_RemoveInvalidParentheses　Solution1: AC 4ms 69.26%

--- src/leetcode/P301_RemoveInvalidParentheses.java
+++ src/leetcode/P301_RemoveInvalidParentheses.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 	Remove the minimum number of invalid parentheses in order to make the 
  * 	input string valid. Return all possible results.
@@ -22,12 +25,30 @@ package leetcode;
  * @file        P301_RemoveInvalidParentheses.java
  * @type        P301_RemoveInvalidParentheses
  * @date        2016年12月28日 下午10:22:08
- * @details     
+ * @details     Solution1: AC 4ms 69.26%
  */
 public class P301_RemoveInvalidParentheses {
-	static class Solution {
-	    public List<String> removeInvalidParentheses(String s) {
-	        
-	    }
+	static class Solution1 {
+		public List<String> removeInvalidParentheses(String s) {
+		    List<String> ans = new LinkedList<>();
+		    remove(s, ans, 0, 0, new char[]{'(', ')'});
+		    return ans;
+		}
+		public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
+		    for (int stack = 0, i = last_i; i < s.length(); ++i) {
+		        if (s.charAt(i) == par[0]) stack++;
+		        if (s.charAt(i) == par[1]) stack--;
+		        if (stack >= 0) continue;
+		        for (int j = last_j; j <= i; ++j)
+		            if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
+		                remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+		        return;
+		    }
+		    String reversed = new StringBuilder(s).reverse().toString();
+		    if (par[0] == '(') // finished left to right
+		        remove(reversed, ans, 0, 0, new char[]{')', '('});
+		    else // finished right to left
+		        ans.add(reversed);
+		}
 	}
 }
