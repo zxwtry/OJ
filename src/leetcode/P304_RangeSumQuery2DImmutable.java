f23ab4aac0191ef8b0e761e071decd377553fc60
zxwtry <zxwtry@qq.com>
2017-02-15 14:12:42 +0800

    添加 leetcode P304_RangeSumQuery2DImmutable NumMatrix1: AC 127ms 47.54%

--- src/leetcode/P304_RangeSumQuery2DImmutable.java
+++ src/leetcode/P304_RangeSumQuery2DImmutable.java
@@ -33,17 +33,37 @@ package leetcode;
  * @file        P304_RangeSumQuery2DImmutable.java
  * @type        P304_RangeSumQuery2DImmutable
  * @date        2016年12月28日 下午10:27:47
- * @details     
+ * @details     NumMatrix1: AC 127ms 47.54%
  */
 public class P304_RangeSumQuery2DImmutable {
-	static class NumMatrix {
-
-	    public NumMatrix(int[][] matrix) {
-	        
+	static class NumMatrix1 {
+		long[][] sum = null;
+	    public NumMatrix1(int[][] matrix) {
+	    	if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+	        sum = new long[matrix.length][matrix[0].length];
+	        sum[0][0] = matrix[0][0];
+	        for (int i = 1; i < matrix.length; i ++)
+	        	sum[i][0] = matrix[i][0] + sum[i - 1][0];
+	        for (int j = 1; j < matrix[0].length; j ++)
+	        	sum[0][j] = matrix[0][j] + sum[0][j - 1];
+	        for (int i = 1; i < matrix.length; i ++) {
+	        	for (int j = 1; j < matrix[0].length; j ++) {
+	        		sum[i][j] = sum[i - 1][ j] + sum[i][ j - 1] - sum[i - 1][ j - 1] + matrix[i][j];
+	        	}
+	        }
+	        for (int i = 0; i < sum.length; i ++) {
+	        	for (int j = 0; j < sum[i].length; j ++) {
+	        		System.out.print(sum[i][j] + "\t");
+	        	}
+	        	System.out.println();
+	        }
 	    }
-
+	    public long accessSum(int i, int j) {
+	    	if (i < 0 || j < 0) return 0;
+	    	return sum[i][j];
+	    }
 	    public int sumRegion(int row1, int col1, int row2, int col2) {
-	        
+	        return (int) (sum[row2][col2] + accessSum(row1 - 1,col1 - 1) - accessSum(row2,col1 - 1) - accessSum(row1 - 1,col2));
 	    }
 	}
 }
