bca56e8c504da3d26d690255180e21702d6d5704
zxwtry <zxwtry@qq.com>
2017-02-15 11:54:25 +0800

    添加 leetcode P303_RangeSumQueryImmutable NumArray1: AC 241ms 22.91%

--- src/leetcode/P303_RangeSumQueryImmutable.java
+++ src/leetcode/P303_RangeSumQueryImmutable.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+
 /**
  * 	Given an integer array nums, find the sum of the elements between 
  * 	indices i and j (i ≤ j), inclusive.
@@ -23,17 +24,23 @@ package leetcode;
  * @file        P303_RangeSumQueryImmutable.java
  * @type        P303_RangeSumQueryImmutable
  * @date        2016年12月28日 下午10:25:59
- * @details     
+ * @details     NumArray1: AC 241ms 22.91%
  */
 public class P303_RangeSumQueryImmutable {
-	static class NumArray {
-
-	    public NumArray(int[] nums) {
-	        
+	static class NumArray1 {
+		long[] sums = null;
+	    public NumArray1(int[] nums) {
+	    	if (nums == null || nums.length == 0) return;
+	        sums = new long[nums.length + 1];
+	        long sum = 0;
+	        for (int index = 0; index < nums.length; index ++) {
+	        	sum += nums[index];
+	        	sums[index + 1] = sum;
+	        }
 	    }
-
 	    public int sumRange(int i, int j) {
-	        
+	        if (sums == null) return 0;
+	        return (int)(sums[j + 1] - sums[i]);
 	    }
 	}
 }
