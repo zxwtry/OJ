14c06459cc207560d741e4f7b5bc09e8ee5f7c18
zxwtry <zxwtry@qq.com>
2017-02-16 21:09:04 +0800

    添加 leetcode P312_BurstBalloons Solution: AC 15ms 44.19%

--- src/leetcode/P312_BurstBalloons.java
+++ src/leetcode/P312_BurstBalloons.java
@@ -32,12 +32,30 @@ package leetcode;
  * @file        P312_BurstBalloons.java
  * @type        P312_BurstBalloons
  * @date        2016年12月29日 下午8:56:36
- * @details     
+ * @details     Solution: AC 15ms 44.19%
  */
 public class P312_BurstBalloons {
 	static class Solution {
-	    public int maxCoins(int[] nums) {
-	        
-	    }
+		public int maxCoins(int[] iNums) {
+		    int[] nums = new int[iNums.length + 2];
+		    int n = 1;
+		    for (int x : iNums) if (x > 0) nums[n++] = x;
+		    nums[0] = nums[n++] = 1;
+
+
+		    int[][] memo = new int[n][n];
+		    return burst(memo, nums, 0, n - 1);
+		}
+
+		public int burst(int[][] memo, int[] nums, int left, int right) {
+		    if (left + 1 == right) return 0;
+		    if (memo[left][right] > 0) return memo[left][right];
+		    int ans = 0;
+		    for (int i = left + 1; i < right; ++i)
+		        ans = Math.max(ans, nums[left] * nums[i] * nums[right] 
+		        + burst(memo, nums, left, i) + burst(memo, nums, i, right));
+		    memo[left][right] = ans;
+		    return ans;
+		}
 	}
 }
