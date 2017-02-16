b56f0c7871bb8f2292d0528730d1c3a3baa39aa8
zxwtry <zxwtry@qq.com>
2017-02-16 21:08:04 +0800

    添加 leetcode P310_MinimumHeightTrees Solution: AC 63ms 43.24%

--- src/leetcode/P310_MinimumHeightTrees.java
+++ src/leetcode/P310_MinimumHeightTrees.java
@@ -1,5 +1,11 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * 	For a undirected graph with tree characteristics, we can choose any 
  * node as the root. The result graph is then a rooted tree. Among all 
@@ -62,12 +68,35 @@ package leetcode;
  * @file        P310_MinimumHeightTrees.java
  * @type        P310_MinimumHeightTrees
  * @date        2016年12月29日 下午8:54:46
- * @details     
+ * @details     Solution: AC 63ms 43.24%
  */
 public class P310_MinimumHeightTrees {
 	static class Solution {
-	    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-	        
-	    }
+		public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+		    if (n == 1) return Collections.singletonList(0);
+
+		    List<Set<Integer>> adj = new ArrayList<>(n);
+		    for (int i = 0; i < n; ++i) adj.add(new HashSet<>());
+		    for (int[] edge : edges) {
+		        adj.get(edge[0]).add(edge[1]);
+		        adj.get(edge[1]).add(edge[0]);
+		    }
+
+		    List<Integer> leaves = new ArrayList<>();
+		    for (int i = 0; i < n; ++i)
+		        if (adj.get(i).size() == 1) leaves.add(i);
+
+		    while (n > 2) {
+		        n -= leaves.size();
+		        List<Integer> newLeaves = new ArrayList<>();
+		        for (int i : leaves) {
+		            int j = adj.get(i).iterator().next();
+		            adj.get(j).remove(i);
+		            if (adj.get(j).size() == 1) newLeaves.add(j);
+		        }
+		        leaves = newLeaves;
+		    }
+		    return leaves;
+		}
 	}
 }
