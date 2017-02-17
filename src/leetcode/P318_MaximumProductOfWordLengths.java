72c2da0ffca536421df28cb73f778ae320190ebd
zxwtry <zxwtry@qq.com>
2017-02-17 20:02:27 +0800

    添加  leetcode P318_MaximumProductOfWordLengths

--- src/leetcode/P318_MaximumProductOfWordLengths.java
+++ src/leetcode/P318_MaximumProductOfWordLengths.java
@@ -6,21 +6,21 @@ package leetcode;
  * 	do not share common letters. You may assume that each 
  * 	word will contain only lower case letters. If no such 
  * 	two words exist, return 0.
-
-	Example 1:
-	Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
-	Return 16
-	The two words can be "abcw", "xtfn".
-	
-	Example 2:
-	Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
-	Return 4
-	The two words can be "ab", "cd".
-	
-	Example 3:
-	Given ["a", "aa", "aaa", "aaaa"]
-	Return 0
-	No such pair of words.
+ *
+ *	Example 1:
+ *	Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+ *	Return 16
+ *	The two words can be "abcw", "xtfn".
+ *	
+ *	Example 2:
+ *	Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
+ *	Return 4
+ *	The two words can be "ab", "cd".
+ *	
+ *	Example 3:
+ *	Given ["a", "aa", "aaa", "aaaa"]
+ *	Return 0
+ *	No such pair of words.
  */
 
 /**
