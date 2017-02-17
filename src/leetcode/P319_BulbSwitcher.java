73579e2282ba2da133dd40d6757ecbdc546e85d2
zxwtry <zxwtry@qq.com>
2017-02-17 21:32:00 +0800

    添加 leetcode P319_BulbSwitcher

--- src/leetcode/P319_BulbSwitcher.java
+++ src/leetcode/P319_BulbSwitcher.java
@@ -6,17 +6,17 @@ package leetcode;
  *  (turning on if it's off or turning off if it's on). For the ith round, you toggle every
  *   i bulb. For the nth round, you only toggle the last bulb. Find how many bulbs are on after
  *    n rounds.
-
-	Example:
-	
-	Given n = 3. 
-	
-	At first, the three bulbs are [off, off, off].
-	After first round, the three bulbs are [on, on, on].
-	After second round, the three bulbs are [on, off, on].
-	After third round, the three bulbs are [on, off, off]. 
-	
-	So you should return 1, because there is only one bulb is on.
+ *
+ *	Example:
+ *	
+ *	Given n = 3. 
+ *	
+ *	At first, the three bulbs are [off, off, off].
+ *	After first round, the three bulbs are [on, on, on].
+ *	After second round, the three bulbs are [on, off, on].
+ *	After third round, the three bulbs are [on, off, off]. 
+ *	
+ *	So you should return 1, because there is only one bulb is on.
  */
 
 /**
