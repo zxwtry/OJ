b0d0f4fb31e0939e88f4978ad1202783de1bddf4
zxwtry <zxwtry@qq.com>
2017-02-18 23:49:11 +0800

    添加 leetcode P349_IntersectionOfTwoArrays Solution1: AC 7ms 26.37%

--- src/leetcode/P349_IntersectionOfTwoArrays.java
+++ src/leetcode/P349_IntersectionOfTwoArrays.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 /**
  * 	Given two arrays, write a function to compute their intersection.
  *	
@@ -19,15 +22,29 @@ package leetcode;
  * @file        P349_IntersectionOfTwoArrays.java
  * @type        P349_IntersectionOfTwoArrays
  * @date        2017年2月3日 下午11:49:03
- * @details     
+ * @details     Solution1: AC 7ms 26.37%
  */
 public class P349_IntersectionOfTwoArrays {
-	public static void main(String[] args) {
-		
-	}
 	static class Solution1 {
 	    public int[] intersection(int[] nums1, int[] nums2) {
-	        
+	        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0)
+	            return new int[0];
+	        if (nums1.length > nums2.length) return intersection(nums2, nums1);
+	        HashSet<Integer> set1 = new HashSet<Integer>(nums1.length);
+	        for (int num : nums1) set1.add(num);
+	        LinkedList<Integer> list = new LinkedList<>();
+	        for (int num : nums2) {
+	            if (set1.contains(num)) {
+	                list.add(num);
+	                set1.remove(num);
+	            }
+	        }
+	        int[] arr = new int[list.size()];
+	        int arrIndex = 0;
+	        for (int val : list) {
+	            arr[arrIndex ++] = val;
+	        }
+	        return arr;
 	    }
 	}
 }
