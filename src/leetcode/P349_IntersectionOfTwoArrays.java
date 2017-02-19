405664c00b4eae24c6244f5769ceb8650ad8f6be
zxwtry <zxwtry@qq.com>
2017-02-19 09:21:58 +0800

    添加 leetcode P349_IntersectionOfTwoArrays Solution2: AC 6ms 46.04%

--- src/leetcode/P349_IntersectionOfTwoArrays.java
+++ src/leetcode/P349_IntersectionOfTwoArrays.java
@@ -23,6 +23,7 @@ import java.util.LinkedList;
  * @type        P349_IntersectionOfTwoArrays
  * @date        2017年2月3日 下午11:49:03
  * @details     Solution1: AC 7ms 26.37%
+ * @details     Solution2: AC 6ms 46.04%
  */
 public class P349_IntersectionOfTwoArrays {
 	static class Solution1 {
@@ -37,7 +38,8 @@ public class P349_IntersectionOfTwoArrays {
 	            if (set1.contains(num)) {
 	                list.add(num);
 	                set1.remove(num);
-	            }
+	            } 
+	            if (set1.isEmpty()) break;
 	        }
 	        int[] arr = new int[list.size()];
 	        int arrIndex = 0;
@@ -45,6 +47,21 @@ public class P349_IntersectionOfTwoArrays {
 	            arr[arrIndex ++] = val;
 	        }
 	        return arr;
+	    }
+	}
+	static class Solution2 {
+	    public int[] intersection(int[] nums1, int[] nums2) {
+            if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0)
+                return new int[0];
+            if (nums1.length > nums2.length) return intersection(nums2, nums1);
+            HashSet<Integer> set1 = new HashSet<Integer>(nums1.length);
+            HashSet<Integer> set2 = new HashSet<Integer>(nums1.length);
+            for (int num : nums1) set1.add(num);
+            for (int num : nums2) if (set1.contains(num)) set2.add(num);
+            int[] answerArray = new int[set2.size()];
+            int answerArrayIndex = 0;
+            for (int num : set2) answerArray[answerArrayIndex ++] = num;
+            return answerArray;
 	    }
 	}
 }
