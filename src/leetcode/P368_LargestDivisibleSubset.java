d04179ae7801cd36a3682765c23b2c58a1ad07e9
zxwtry <zxwtry@qq.com>
2017-02-19 10:30:44 +0800

    添加 leetcode P368_LargestDivisibleSubset Solution1: AC 36ms 51.74%

--- src/leetcode/P368_LargestDivisibleSubset.java
+++ src/leetcode/P368_LargestDivisibleSubset.java
@@ -1,7 +1,12 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Given a set of distinct positive integers, find the largest 
+ *  Given a set of distinct positive integers, find the largest 
  * 	subset such that every pair (Si, Sj) of elements in this subset
  *  satisfies: Si % Sj = 0 or Sj % Si = 0.
  *	
@@ -29,12 +34,37 @@ package leetcode;
  * @file        P368_LargestDivisibleSubset.java
  * @type        P368_LargestDivisibleSubset
  * @date        2016年12月8日 下午10:27:41
- * @details     
+ * @details     Solution1: AC 36ms 51.74%
  */
 public class P368_LargestDivisibleSubset {
-	static class Solution {
+	static class Solution1 {
 	    public List<Integer> largestDivisibleSubset(int[] nums) {
-	        
+	        if (nums == null || nums.length == 0) return new LinkedList<Integer>();
+	        else if (nums.length == 1) return Collections.singletonList(nums[0]);
+	        Arrays.sort(nums);
+	        int[] dp = new int[nums.length];
+	        for (int dpIndexRow = 1; dpIndexRow < nums.length; dpIndexRow ++)
+	            for (int dpIndexCol = dpIndexRow - 1; dpIndexCol > -1; dpIndexCol --)
+	                if (nums[dpIndexCol] != 0 && nums[dpIndexRow] % nums[dpIndexCol] == 0)
+	                    dp[dpIndexRow] = Math.max(dp[dpIndexRow], dp[dpIndexCol] + 1);
+	        int dpMaxIndex = 0;
+	        boolean hasZero = false;
+	        for (int dpIndex = 0; dpIndex < dp.length; dpIndex ++) {
+	            dpMaxIndex = dp[dpMaxIndex] > dp[dpIndex] ? dpMaxIndex : dpIndex;
+	            hasZero |= nums[dpIndex] == 0;
+	        }
+	        int dpCurrentValue = dp[dpMaxIndex];
+	        int num = nums[dpMaxIndex];
+	        List<Integer> answerList = new LinkedList<Integer>();
+	        for (int dpIndex = dpMaxIndex; dpIndex > -1; dpIndex --) {
+	            if (nums[dpIndex] != 0 && num % nums[dpIndex] == 0 && dp[dpIndex] == dpCurrentValue) {
+	                answerList.add(nums[dpIndex]);
+	                num = nums[dpIndex];
+	                dpCurrentValue --;
+	            }
+	        }
+	        if (hasZero) answerList.add(0);
+	        return answerList;
 	    }
 	}
 }
