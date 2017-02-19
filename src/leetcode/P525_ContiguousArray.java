28b5586e454a7162427a19e40a6a68456902c897
zxwtry <zxwtry@qq.com>
2017-02-19 21:46:55 +0800

    添加 leetcode P525_ContiguousArray

---
+++ src/leetcode/P525_ContiguousArray.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.text.DecimalFormat;
+
+/**
+ *  Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+ *  
+ *  Example 1:
+ *  Input: [0,1]
+ *  Output: 2
+ *  Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+ *  Example 2:
+ *  Input: [0,1,0]
+ *  Output: 2
+ *  Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+ *  Note: The length of the given binary array will not exceed 50,000.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P525_ContiguousArray.java
+ * @type        P525_ContiguousArray
+ * @date        2017年2月19日 上午11:17:38
+ * @details     
+ */
+public class P525_ContiguousArray {}
