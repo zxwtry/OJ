64c02c6948cd21510815479c7d364c54a45ddc85
zxwtry <zxwtry@qq.com>
2017-02-19 22:13:52 +0800

    添加 leetcode P324_WiggleSortII Solution1: WA

--- src/leetcode/P324_WiggleSortII.java
+++ src/leetcode/P324_WiggleSortII.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * 	Given an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
  * 	
@@ -23,12 +25,33 @@ package leetcode;
  * @file        P324_WiggleSortII.java
  * @type        P324_WiggleSortII
  * @date        2017年1月9日 下午9:09:56
- * @details     
+ * @details     Solution1: WA
  */
 public class P324_WiggleSortII {
-	static class Solution {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 3, 3, 3, 3};
+        Solution1 solution1 = new Solution1();
+        solution1.wiggleSort(nums);
+        tools.Utils.printArray(nums, nums.length);
+    }
+	static class Solution1 {
+	    //0, 1, 2, 3, 4, 5, 6, 7, 3, 3, 3, 3
 	    public void wiggleSort(int[] nums) {
-	        return
+	        if (nums == null || nums.length < 2) return;
+	        Arrays.sort(nums);
+	        int startIndex = 1, middleIndex = nums.length / 2;
+	        middleIndex = (middleIndex % 2 == 0 ? middleIndex : middleIndex + 1);
+	        while (middleIndex < nums.length) {
+	            swap(nums, startIndex, middleIndex);
+	            startIndex += 2;
+	            middleIndex += 2;
+	        }
+	    }
+	    private void swap(int[] nums, int i, int j) {
+	        int temp = nums[i];
+	        nums[i] = nums[j];
+	        nums[j] = temp;
 	    }
 	}
+	
 }
