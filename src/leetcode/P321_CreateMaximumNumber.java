50866a18ef39d02c2c3df83a3b7b8b259c10605d
zxwtry <zxwtry@qq.com>
2017-02-20 23:11:40 +0800

    添加 leetcode P321_CreateMaximumNumber Solution1

--- src/leetcode/P321_CreateMaximumNumber.java
+++ src/leetcode/P321_CreateMaximumNumber.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * 	Given two arrays of length m and n with digits 0-9 representing 
  * 	two numbers. Create the maximum number of length k <= m + n from digits of the two. 
@@ -36,11 +39,29 @@ package leetcode;
  * @details     
  */
 public class P321_CreateMaximumNumber {
-	static class Solution {
+    public static void main(String[] args) {
+        int[] nums1 = {3, 4, 6, 5};
+        int[] nums2 = {9, 1, 2, 5, 8, 3};
+        Solution1 solution1 = new Solution1();
+        for (int k = 1; k <= nums1.length + nums2.length; k ++)
+            tools.Utils.printArray(solution1.maxNumber(nums1, nums2, k), k);
+    }
+	static class Solution1 {
 	    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
 	        if (nums1 == null || nums2 == null || k < 0 || k > nums1.length + nums2.length)
 	        	return new int[0];
-	        
+	        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o2 - o1;
+                }
+            });
+	        for (int num : nums1) priorityQueue.add(num);
+	        for (int num : nums2) priorityQueue.add(num);
+	        int[] maxNumber = new int[k];
+	        for (int maxNumberIndex = 0; maxNumberIndex < maxNumber.length; maxNumberIndex ++)
+	            maxNumber[maxNumberIndex] = priorityQueue.poll();
+	        return maxNumber;
 	    }
 	}
 }
