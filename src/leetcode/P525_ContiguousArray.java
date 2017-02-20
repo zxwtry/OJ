05f2d48903f538b46b82ae8527bf3ea5f7605bf3
zxwtry <zxwtry@qq.com>
2017-02-20 23:15:30 +0800

    添加 P525_ContiguousArray Solution1

--- src/leetcode/P525_ContiguousArray.java
+++ src/leetcode/P525_ContiguousArray.java
@@ -1,6 +1,5 @@
 package leetcode;
 
-import java.text.DecimalFormat;
 
 /**
  *  Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
@@ -26,4 +25,36 @@ import java.text.DecimalFormat;
  * @date        2017年2月19日 上午11:17:38
  * @details     
  */
-public class P525_ContiguousArray {}
+public class P525_ContiguousArray {
+    public static void main(String[] args) {
+        double value = 234234234.1231234324;
+        String s = String.format("%f", value);
+        System.out.println(s);
+    }
+    static class Solution {
+        public int findMaxLength(int[] nums) {
+            int len = nums.length;
+            int numOf1 = 0;
+            int[] left1Count = new int[len];
+            for (int numsIndex = 0; numsIndex < len; numsIndex ++) {
+                numOf1 += nums[numsIndex];
+                left1Count[numsIndex] = numOf1;
+            }
+            int[] right1Count = new int[len];
+            int tmp = 0;
+            for (int numsIndex = len-1; numsIndex > -1; numsIndex --) {
+                tmp += nums[numsIndex];
+                right1Count[numsIndex] = tmp;
+            }
+            int numOf0 = len - numOf1;
+            int leftIndex = 0, rightIndex = nums.length - 1;
+            if (len / 2 == numOf1)  {
+                if (len % 2 == 0)
+                    return len;
+                else if (len % 2 == 1 && (nums[0] == 1 || nums[len - 1] == 1))
+                    return len - 1;
+            }
+            
+        }
+    }
+}
