172a8b67e77ce121b277e54d2a3be28d19ae1535
zxwtry <zxwtry@qq.com>
2017-02-22 23:24:24 +0800

    添加 leetcode P331_VerifyPreorderSerializationOfABinaryTree Solution4 22ms

--- src/leetcode/P331_VerifyPreorderSerializationOfABinaryTree.java
+++ src/leetcode/P331_VerifyPreorderSerializationOfABinaryTree.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * 	One way to serialize a binary tree is to use pre-order traversal. 
  * 	When we encounter a non-null node, we record the node's value. 
@@ -79,6 +81,33 @@ public class P331_VerifyPreorderSerializationOfABinaryTree {
 	            }  
 	        }
 	        return degree == 0;
+	    }
+	}
+	static class Solution4 {
+	    public boolean isValidSerialization(String preorder) {
+	        // using a stack, scan left to right
+	        // case 1: we see a number, just push it to the stack
+	        // case 2: we see #, check if the top of stack is also #
+	        // if so, pop #, pop the number in a while loop, until top of stack is not #
+	        // if not, push it to stack
+	        // in the end, check if stack size is 1, and stack top is #
+	        if (preorder == null) {
+	            return false;
+	        }
+	        Stack<String> st = new Stack<>();
+	        String[] strs = preorder.split(",");
+	        for (int pos = 0; pos < strs.length; pos++) {
+	            String curr = strs[pos];
+	            while (curr.equals("#") && !st.isEmpty() && st.peek().equals(curr)) {
+	                st.pop();
+	                if (st.isEmpty()) {
+	                    return false;
+	                }
+	                st.pop();
+	            }
+	            st.push(curr);
+	        }
+	        return st.size() == 1 && st.peek().equals("#");
 	    }
 	}
 }
