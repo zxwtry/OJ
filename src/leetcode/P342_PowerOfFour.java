df40fd3dcb05c0288f5bfad915e1b06e68870eca
zxwtry <zxwtry@qq.com>
2017-02-22 10:49:18 +0800

    添加 leetcode P342_PowerOfFour Solution2: AC 2ms 20.06%

--- src/leetcode/P342_PowerOfFour.java
+++ src/leetcode/P342_PowerOfFour.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Single;
-
 /**
  * 	Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
  * 	
@@ -19,7 +17,8 @@ import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Single;
  * @file        P342_PowerOfFour.java
  * @type        P342_PowerOfFour
  * @date        2017年2月2日 下午9:16:18
- * @details     
+ * @details     Solution1: AC 2ms 19.47% 
+ * @details     Solution2: AC 2ms 20.06%
  */
 public class P342_PowerOfFour {
 	static class Solution1 {
@@ -35,10 +34,12 @@ public class P342_PowerOfFour {
 	}
 	static class Solution2 {
 	    public boolean isPowerOfFour(int num) {
-            if (num < 0) return false;
+            if (num < 2) return num == 1;
             int sign = num & (num - 1);
             if (sign != 0) return false;
-            if 
+            sign = num & 0xAAAAAAAA;
+            if (sign != 0) return false;
+            return true;
 	    }
 	}
 }
