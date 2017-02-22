7ce4bcdc803b8733eafdd18e4af9bfbaaeff037e
zxwtry <zxwtry@qq.com>
2017-02-22 23:30:52 +0800

    添加 leetcode P332_ReconstructItinerary  Solution1  AC

--- src/leetcode/P332_ReconstructItinerary.java
+++ src/leetcode/P332_ReconstructItinerary.java
@@ -1,5 +1,11 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /**
  * 	Given a list of airline tickets represented by pairs of departure and arrival 
  * 	airports [from, to], reconstruct the itinerary in order. 
@@ -37,6 +43,23 @@ public class P332_ReconstructItinerary {
 	static class Solution {
 	    public List<String> findItinerary(String[][] tickets) {
 	        
+	    }
+	}
+	static class Solution1 {
+	    public List<String> findItinerary(String[][] tickets) {
+	        for (String[] ticket : tickets)
+	            targets.computeIfAbsent(ticket[0], k -> new PriorityQueue()).add(ticket[1]);
+	        visit("JFK");
+	        return route;
+	    }
+
+	    Map<String, PriorityQueue<String>> targets = new HashMap<>();
+	    List<String> route = new LinkedList();
+
+	    void visit(String airport) {
+	        while(targets.containsKey(airport) && !targets.get(airport).isEmpty())
+	            visit(targets.get(airport).poll());
+	        route.add(0, airport);
 	    }
 	}
 }
