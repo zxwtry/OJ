3d1deb8a210b1c3a7c71fb2710bada108f372a8f
zxwtry <zxwtry@qq.com>
2017-02-22 23:33:00 +0800

    添加 leetcode P332_ReconstructItinerary Solution2 AC 15ms

--- src/leetcode/P332_ReconstructItinerary.java
+++ src/leetcode/P332_ReconstructItinerary.java
@@ -1,10 +1,12 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 /**
  * 	Given a list of airline tickets represented by pairs of departure and arrival 
@@ -60,6 +62,31 @@ public class P332_ReconstructItinerary {
 	        while(targets.containsKey(airport) && !targets.get(airport).isEmpty())
 	            visit(targets.get(airport).poll());
 	        route.add(0, airport);
+	    }
+	}
+	static class Solution2 {
+	    public List<String> findItinerary(String[][] tickets) {
+	        List<String> ans = new ArrayList<String>();
+	        if(tickets == null || tickets.length == 0) return ans;
+	        Map<String, PriorityQueue<String>> ticketsMap = new HashMap<>();
+	        for(int i = 0; i < tickets.length; i++) {
+	            if(!ticketsMap.containsKey(tickets[i][0])) ticketsMap.put(tickets[i][0], new PriorityQueue<String>());
+	            ticketsMap.get(tickets[i][0]).add(tickets[i][1]);
+	        }
+
+	        String curr = "JFK";
+	        Stack<String> drawBack = new Stack<String>();
+	        for(int i = 0; i < tickets.length; i++) {
+	            while(!ticketsMap.containsKey(curr) || ticketsMap.get(curr).isEmpty()) {
+	                drawBack.push(curr);
+	                curr = ans.remove(ans.size()-1);
+	            }
+	            ans.add(curr);
+	            curr = ticketsMap.get(curr).poll();
+	        }
+	        ans.add(curr);
+	        while(!drawBack.isEmpty()) ans.add(drawBack.pop());
+	        return ans;
 	    }
 	}
 }
