9a3754135ff88a693b9df45d927c1fe71c2236ca
zxwtry <zxwtry@qq.com>
2017-02-22 23:22:47 +0800

    添加 leetcode P331_VerifyPreorderSerializationOfABinaryTree Solution3 19ms

--- src/leetcode/P331_VerifyPreorderSerializationOfABinaryTree.java
+++ src/leetcode/P331_VerifyPreorderSerializationOfABinaryTree.java
@@ -65,4 +65,20 @@ public class P331_VerifyPreorderSerializationOfABinaryTree {
 	        return diff == 0;
 	    }
 	}
+	static class Solution3 {
+	    public boolean isValidSerialization(String preorder) {
+	        String[] strs = preorder.split(",");
+	        int degree = -1;         // root has no indegree, for compensate init with -1
+	        for (String str: strs) {
+	            degree++;             // all nodes have 1 indegree (root compensated)
+	            if (degree > 0) {     // total degree should never exceeds 0
+	                return false;
+	            }      
+	            if (!str.equals("#")) {// only non-leaf node has 2 outdegree
+	                degree -= 2;
+	            }  
+	        }
+	        return degree == 0;
+	    }
+	}
 }
