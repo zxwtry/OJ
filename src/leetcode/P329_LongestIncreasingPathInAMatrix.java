c391376b8a53c83b00b2b1fc343128dd400437d3
zxwtry <zxwtry@qq.com>
2017-03-02 23:31:52 +0800

    添加 leetcode P329_LongestIncreasingPathInAMatrix Solution1: AC 20ms 38.61%

--- src/leetcode/P329_LongestIncreasingPathInAMatrix.java
+++ src/leetcode/P329_LongestIncreasingPathInAMatrix.java
@@ -36,69 +36,51 @@ package leetcode;
  * @file        P329_LongestIncreasingPathInAMatrix.java
  * @type        P329_LongestIncreasingPathInAMatrix
  * @date        2017年1月10日 下午10:02:26
- * @details     
+ * @details     Solution1: AC 20ms 38.61% *
+ * @details     Solution3: AC 16ms 62.52% *
  */
 public class P329_LongestIncreasingPathInAMatrix {
 	static class Solution1 {
+	    int row = 0, col = 0;
+	    int[][] record = null;
+	    boolean[][] traveled = null;
+	    int[] xs = new int[] {0,   1,  0,  -1};
+	    int[] ys = new int[] {-1,  0,  1,  0};
+	    int max = 0;
 	    public int longestIncreasingPath(int[][] matrix) {
 	        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
 	            return 0;
-	        int row = matrix.length - 1;
-	        int col = matrix[0].length - 1;
-	        int[][] dp = new int[row + 1][col + 1];
-	        
-	        
-	        
-	    }
-	}
-	static class Solution2 {
-	    int[][] max = null;
-	    int[][] matrix = null;
-	    int row, col;
-	    public int longestIncreasingPath(int[][] matrix) {
-            if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-                return 0;
-            row = matrix.length;
-            col = matrix[0].length;
-            max = new int[row][col];
-            this.matrix = matrix;
-            int answer = 0;
-            for (int rowIndex = 0; rowIndex < row; rowIndex ++) {
-                for (int colIndex = 0; colIndex < col; colIndex ++) {
-                    answer = Math.max(answer, longestIncreasingPath(rowIndex, colIndex));
-                }
-            }
-	    }
-        private int longestIncreasingPath(int rowIndex, int colIndex) {
-            return 0;
-        }
-	}
-	static class Solution3 {
-	    public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-	    public int longestIncreasingPath(int[][] matrix) {
-	        if(matrix.length == 0) return 0;
-	        int m = matrix.length, n = matrix[0].length;
-	        int[][] cache = new int[m][n];
-	        int max = 1;
-	        for(int i = 0; i < m; i++) {
-	            for(int j = 0; j < n; j++) {
-	                int len = dfs(matrix, i, j, m, n, cache);
-	                max = Math.max(max, len);
+	        row = matrix.length;
+	        col = matrix[0].length;
+	        record = new int[row][col]; 
+	        traveled = new boolean[row][col];
+	        for (int xi = 0; xi < row; xi ++) {
+	            for (int yi = 0; yi < col; yi ++) {
+	                visit(matrix, xi, yi);
 	            }
-	        }   
-	        return max;
-	    }
-	    public int dfs(int[][] matrix, int i, int j, int m, int n, int[][] cache) {
-	        if(cache[i][j] != 0) return cache[i][j];
-	        int max = 1;
-	        for(int[] dir: dirs) {
-	            int x = i + dir[0], y = j + dir[1];
-	            if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
-	            int len = 1 + dfs(matrix, x, y, m, n, cache);
-	            max = Math.max(max, len);
 	        }
-	        cache[i][j] = max;
 	        return max;
 	    }
+        private void visit(int[][] matrix, int xi, int yi) {
+            if (record[xi][yi] != 0) return;
+            record[xi][yi] = 1;
+            traveled[xi][yi] = true;
+            for (int xsIndex = 0; xsIndex < xs.length; xsIndex ++) {
+                int newX = xi + xs[xsIndex];
+                if (newX < 0 || newX >= row) continue;
+                int newY = yi + ys[xsIndex];
+                if (newY < 0 || newY >= col) continue;
+                if (matrix[newX][newY] <= matrix[xi][yi]) continue;
+                if (record[newX][newY] != 0) {
+                    record[xi][yi] = Math.max(record[xi][yi], 1 + record[newX][newY]);
+                    continue;
+                }
+                if (traveled[newX][newY]) continue;
+                visit(matrix, newX, newY);
+                record[xi][yi] = Math.max(record[xi][yi], 1 + record[newX][newY]);
+            }
+            max = Math.max(max, record[xi][yi]);
+            traveled[xi][yi] = false;
+        }
 	}
 }
