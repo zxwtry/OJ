3f1c512fc6045c0aa3cf25b102f9886302c5a352
zxwtry <zxwtry@qq.com>
2017-03-03 23:39:45 +0800

    添加 leetcode P341_FlattenNestedListIterator NestedIterator2: AC  9ms 57.64%

--- src/leetcode/P341_FlattenNestedListIterator.java
+++ src/leetcode/P341_FlattenNestedListIterator.java
@@ -57,6 +57,32 @@ public class P341_FlattenNestedListIterator {
 	    }
 	}
 	
+	static class NestedIterator2 implements Iterator<Integer> {
+	    Stack<NestedInteger> stack = new Stack<>();
+	    public NestedIterator2(List<NestedInteger> nestedList) {
+	        for(int i = nestedList.size() - 1; i >= 0; i--) {
+	            stack.push(nestedList.get(i));
+	        }
+	    }
+	    @Override
+	    public Integer next() {
+	        return stack.pop().getInteger();
+	    }
+	    @Override
+	    public boolean hasNext() {
+	        while(!stack.isEmpty()) {
+	            NestedInteger curr = stack.peek();
+	            if(curr.isInteger()) {
+	                return true;
+	            }
+	            stack.pop();
+	            for(int i = curr.getList().size() - 1; i >= 0; i--) {
+	                stack.push(curr.getList().get(i));
+	            }
+	        }
+	        return false;
+	    }
+	}
 	
 	static class NestedInteger {
 	    public boolean isInteger() {
