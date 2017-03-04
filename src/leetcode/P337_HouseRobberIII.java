c17861f67865fcf562d5ebcefa3e1bc2ecd441a3
zxwtry <zxwtry@qq.com>
2017-03-04 23:22:23 +0800

    添加 leetcode P337_HouseRobberIII Solution2: TLE

--- src/leetcode/P337_HouseRobberIII.java
+++ src/leetcode/P337_HouseRobberIII.java
@@ -72,4 +72,17 @@ public class P337_HouseRobberIII {
 	        return Math.max(sum[0], sum[1]);
 	    }
 	}
+	static class Solution2 {
+	    public int rob(TreeNode root) {
+	        if (root == null) return 0;
+	        int val = 0;
+	        if (root.left != null) {
+	            val += rob(root.left.left) + rob(root.left.right);
+	        }
+	        if (root.right != null) {
+	            val += rob(root.right.left) + rob(root.right.right);
+	        }
+	        return Math.max(val + root.val, rob(root.left) + rob(root.right));
+	    }
+	}
 }
