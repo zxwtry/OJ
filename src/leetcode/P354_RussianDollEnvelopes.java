583831b5fce695286ab423a9241c67b2f754dc48
zxwtry <zxwtry@qq.com>
2017-03-04 23:28:22 +0800

    添加 leetcode P354_RussianDollEnvelopes Solution2: AC  23ms 75.62%

--- src/leetcode/P354_RussianDollEnvelopes.java
+++ src/leetcode/P354_RussianDollEnvelopes.java
@@ -56,4 +56,29 @@ public class P354_RussianDollEnvelopes {
 	        return max;
 	    }
 	}
+	static class Solution2 {
+	    public int maxEnvelopes(int[][] envelopes) {
+	        if(envelopes == null || envelopes.length == 0 
+	           || envelopes[0] == null || envelopes[0].length != 2)
+	            return 0;
+	        Arrays.sort(envelopes, new Comparator<int[]>(){
+	            public int compare(int[] val1, int[] val2){
+	                long cut = (long)val1[0] - val2[0];
+                    if (cut != 0) return cut > 0 ? 1 : -1;
+                    cut = (long)val1[1] - val2[1];
+                    if (cut != 0) return cut > 0 ? -1 : 1;
+                    return 0;
+	           } 
+	        });
+	        int dp[] = new int[envelopes.length];
+	        int len = 0;
+	        for(int[] envelope : envelopes){
+	            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+	            index = index < 0 ? -(index + 1) : index;
+	            dp[index] = envelope[1];
+	            len += (index == len ? 1 : 0);
+	        }
+	        return len;
+	    }
+	}
 }
