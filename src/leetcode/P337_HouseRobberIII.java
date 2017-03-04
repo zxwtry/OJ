dccc91d8b7e38270100ee09e0bf42132fa00e078
zxwtry <zxwtry@qq.com>
2017-03-04 23:22:58 +0800

    添加 leetcode P337_HouseRobberIII Solution3: AC 11ms 26.93%

--- src/leetcode/P337_HouseRobberIII.java
+++ src/leetcode/P337_HouseRobberIII.java
@@ -85,4 +85,24 @@ public class P337_HouseRobberIII {
 	        return Math.max(val + root.val, rob(root.left) + rob(root.right));
 	    }
 	}
+	static class Solution3 {
+	    public int rob(TreeNode root) {
+	        return robSub(root, new HashMap<>());
+	    }
+	    private int robSub(TreeNode root, Map<TreeNode, Integer> map) {
+	        if (root == null) return 0;
+	        if (map.containsKey(root)) return map.get(root);
+	        int val = 0;
+	        if (root.left != null) {
+	            val += robSub(root.left.left, map) + robSub(root.left.right, map);
+	        }
+	        
+	        if (root.right != null) {
+	            val += robSub(root.right.left, map) + robSub(root.right.right, map);
+	        }
+	        val = Math.max(val + root.val, robSub(root.left, map) + robSub(root.right, map));
+	        map.put(root, val);
+	        return val;
+	    }
+	}
 }
