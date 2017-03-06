59ae2e48707638936a7df771016de435ff6a5b71
zxwtry <zxwtry@qq.com>
2017-03-06 22:12:27 +0800

    添加  leetcode P372_SuperPow

---
+++ src/leetcode/P372_SuperPow.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/**
+ *  Your task is to calculate ab mod 1337 where a is a positive integer and b is
+ *  an extremely large positive integer given in the form of an array.
+ *  
+ *  Example1:
+ *  
+ *  a = 2
+ *  b = [3]
+ *  
+ *  Result: 8
+ *  Example2:
+ *  
+ *  a = 2
+ *  b = [1,0]
+ *  
+ *  Result: 1024
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P372_SuperPow.java
+ * @type        P372_SuperPow
+ * @date        2017年3月6日 下午7:15:56
+ * @details
+ */
+public class P372_SuperPow {
+    
+}
