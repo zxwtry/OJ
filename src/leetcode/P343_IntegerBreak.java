806ec50d6db1b9465fee604f31311f6a3b444a45
zxwtry <zxwtry@qq.com>
2017-03-06 23:21:54 +0800

    添加 leetcode P343_IntegerBreak Solution1: AC 2ms 16.91%

--- src/leetcode/P343_IntegerBreak.java
+++ src/leetcode/P343_IntegerBreak.java
@@ -1,7 +1,8 @@
 package leetcode;
 
 /**
- * 	Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
+ * 	Given a positive integer n, break it into the sum of at least two positive integers
+ *  and maximize the product of those integers. Return the maximum product you can get.
  *	
  *	For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
  *	
@@ -21,12 +22,21 @@ package leetcode;
  * @file        P343_IntegerBreak.java
  * @type        P343_IntegerBreak
  * @date        2017年2月2日 下午9:17:23
- * @details     
+ * @details     Solution1: AC 2ms 16.91%
+ * @details     Solution1: AC 0ms 49.43%
  */
 public class P343_IntegerBreak {
-	static class Solution {
+	static class Solution1 {
 	    public int integerBreak(int n) {
-	        
+	        int[] dp = new int[n + 1];
+	        dp[1] = 1;
+	        for (int i = 2; i <= n; i ++) {
+	            dp[i] = i == n ? 0 : i;
+	            for (int j = 1; j < i; j ++) {
+	                dp[i] = Math.max(dp[j] * dp[i - j], dp[i]);
+	            }
+	        }
+	        return dp[n];
 	    }
 	}
 }
