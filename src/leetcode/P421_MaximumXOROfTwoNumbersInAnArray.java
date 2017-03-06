e675d15213712d713260266973ec20181fa3d312
zxwtry <zxwtry@qq.com>
2017-03-06 23:19:52 +0800

    添加 leetcode P421_MaximumXOROfTwoNumbersInAnArray Solution4: AC 122ms 16.18%

--- src/leetcode/P421_MaximumXOROfTwoNumbersInAnArray.java
+++ src/leetcode/P421_MaximumXOROfTwoNumbersInAnArray.java
@@ -63,4 +63,44 @@ public class P421_MaximumXOROfTwoNumbersInAnArray {
 	        return max;
 	    }
 	}
+	static class Trie {
+        Trie[] children;
+        public Trie() {
+            children = new Trie[2];
+        }
+    }
+	static class Solution4 {
+	    public int findMaximumXOR(int[] nums) {
+    	    if(nums == null || nums.length == 0) {
+                return 0;
+            }
+            Trie root = new Trie();
+            for(int num: nums) {
+                Trie curNode = root;
+                for(int i = 31; i >= 0; i --) {
+                    int curBit = (num >>> i) & 1;
+                    if(curNode.children[curBit] == null) {
+                        curNode.children[curBit] = new Trie();
+                    }
+                    curNode = curNode.children[curBit];
+                }
+            }
+            int max = Integer.MIN_VALUE;
+            for(int num: nums) {
+                Trie curNode = root;
+                int curSum = 0;
+                for(int i = 31; i >= 0; i --) {
+                    int curBit = (num >>> i) & 1;
+                    if(curNode.children[curBit ^ 1] != null) {
+                        curSum += (1 << i);
+                        curNode = curNode.children[curBit ^ 1];
+                    }else {
+                        curNode = curNode.children[curBit];
+                    }
+                }
+                max = Math.max(curSum, max);
+            }
+            return max;
+        }
+	}
 }
