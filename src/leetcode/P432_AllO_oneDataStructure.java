0f2e5782dea6c8521908956795221ba65db9ecb6
zxwtry <zxwtry@qq.com>
2017-03-06 23:20:35 +0800

    添加 leetcode P432_AllO_oneDataStructure AllOne: AC 152ms 67.14%

--- src/leetcode/P432_AllO_oneDataStructure.java
+++ src/leetcode/P432_AllO_oneDataStructure.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * 	Implement a data structure supporting the following operations:
  *	
@@ -19,31 +23,71 @@ package leetcode;
  *	Challenge: Perform all these in O(1) time complexity.
  */
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P432_AllO_oneDataStructure.java
+ * @type        P432_AllO_oneDataStructure
+ * @date        2017年3月6日 下午10:07:06
+ * @details     AllOne: AC 152ms 67.14%
+ */
 public class P432_AllO_oneDataStructure {
-	
 	static class AllOne {
-
-	    /** Initialize your data structure here. */
+	     ArrayList<HashSet<String>> list;
+	     HashMap<String, Integer> map;
 	    public AllOne() {
-	        
+	        list = new ArrayList<HashSet<String>>();
+	        map = new HashMap<String, Integer>();
 	    }
-	    
-	    /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
 	    public void inc(String key) {
-	        
+	        Integer set = map.get(key);
+	        if (set == null) {
+	            checkList(0);
+	            list.get(0).add(key);
+	            map.put(key, 0);
+	        } else {
+	            checkList(set + 1);
+	            list.get(set).remove(key);
+	            list.get(set + 1).add(key);
+	            map.put(key, set + 1);
+	        }
 	    }
-	    
-	    /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+	    private void checkList(int index) {
+	        while(list.size() <= index + 1)
+	            list.add(new HashSet<String>());
+	    }
 	    public void dec(String key) {
-	        
+	        Integer val = map.get(key);
+	        if (val == null) return;
+	        if (val == 0) {
+	            list.get(0).remove(key);
+	            if (list.size() == 1 && list.get(0).size() == 0)
+	                list.remove(0);
+	            map.remove(key);
+	        } else {
+	            list.get(val).remove(key);
+	            if (list.size() == val + 1 && list.get(val).size() == 0)
+	                list.remove(val);
+	            list.get(val - 1).add(key);
+	            map.put(key, val - 1);
+	        }
 	    }
-	    
-	    /** Returns one of the keys with maximal value. */
 	    public String getMaxKey() {
+	        while (list.size() != 0 && list.get(list.size() - 1).size() == 0)
+	            list.remove(list.size() - 1);
+	        if (list.size() != 0 && list.get(list.size() - 1).size() != 0)
+	            return list.get(list.size() - 1).iterator().next();
+	        return "";
 	    }
-	    
-	    /** Returns one of the keys with Minimal value. */
 	    public String getMinKey() {
+	        int index = 0;
+	        while (index < list.size()) {
+	            if (list.get(index).size() != 0)
+	                return list.get(index).iterator().next();
+	            index ++;
+	        }
 	        return "";
 	    }
 	}
