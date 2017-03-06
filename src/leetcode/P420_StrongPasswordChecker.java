e4efa5fe1fd41ddf9e59d95272865e779c079e3a
zxwtry <zxwtry@qq.com>
2017-03-06 23:17:15 +0800

    添加 leetcode P420_StrongPasswordChecker Solution2: AC 4ms 67.48%

--- src/leetcode/P420_StrongPasswordChecker.java
+++ src/leetcode/P420_StrongPasswordChecker.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 /**
- * A password is considered strong if below conditions are all met:
+ *  A password is considered strong if below conditions are all met:
  *	
  *	It has at least 6 characters and at most 20 characters.
  *	It must contain at least one lowercase letter, at least one uppercase letter,
@@ -26,16 +26,39 @@ package leetcode;
  * @file        P420_StrongPasswordChecker.java
  * @type        P420_StrongPasswordChecker
  * @date        2016年12月8日 下午10:25:31
- * @details     
+ * @details     Solution2: AC 4ms 67.48%
  */
 public class P420_StrongPasswordChecker {
-	public static void main(String[] args) {
-		
-	}
-	
-	static class Solution {
+	static class Solution2 {
 	    public int strongPasswordChecker(String s) {
-	        
+	        if(s.length()<2) return 6-s.length();
+	        char end = s.charAt(0);
+	        boolean upper = end>='A'&&end<='Z', lower = end>='a'&&end<='z', digit = end>='0'&&end<='9';
+	        int end_rep = 1, change = 0;
+	        int[] delete = new int[3];
+	        for(int i = 1;i<s.length();++i){
+	            if(s.charAt(i)==end) ++end_rep;
+	            else{
+	                change+=end_rep/3;
+	                if(end_rep/3>0) ++delete[end_rep%3];
+	                end = s.charAt(i);
+	                upper = upper||end>='A'&&end<='Z';
+	                lower = lower||end>='a'&&end<='z';
+	                digit = digit||end>='0'&&end<='9';
+	                end_rep = 1;
+	            }
+	        }
+	        change+=end_rep/3;
+	        if(end_rep/3>0) ++delete[end_rep%3];
+	        int check_req = (upper?0:1)+(lower?0:1)+(digit?0:1);
+	        if(s.length()>20){
+	            int del = s.length()-20;
+	            if(del<=delete[0]) change-=del;
+	            else if(del-delete[0]<=2*delete[1]) change-=delete[0]+(del-delete[0])/2;
+	            else change-=delete[0]+delete[1]+(del-delete[0]-2*delete[1])/3;
+	            return del+Math.max(check_req,change);
+	        }
+	        else return Math.max(6-s.length(), Math.max(check_req, change));
 	    }
 	}
 }
