1e876aad36d4f510d0c688cd17ea6584c2450fd2
zxwtry <zxwtry@qq.com>
2017-03-07 23:57:43 +0800

    添加 leetcode P466_CountTheRepetitions Solution1: AC 8ms 83.39%

--- src/leetcode/P466_CountTheRepetitions.java
+++ src/leetcode/P466_CountTheRepetitions.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * 	Define S = [s,n] as the string S which consists of n connected strings s.
  *  For example, ["abc", 3] ="abcabcabc".
@@ -10,9 +12,9 @@ package leetcode;
  *	but it can not be obtained from “acbbe”.
  *	
  *	You are given two non-empty strings s1 and s2 (each at most 100 characters long)
- *	 and two integers 0 ≤ n1 ≤ 106 and 1 ≤ n2 ≤ 106. Now consider the strings S1 and S2,
- *	  where S1=[s1,n1] and S2=[s2,n2]. Find the maximum integer M such that [S2,M] 
- *	  can be obtained from S1.
+ *	and two integers 0 ≤ n1 ≤ 106 and 1 ≤ n2 ≤ 106. Now consider the strings S1 and S2,
+ *	where S1=[s1,n1] and S2=[s2,n2]. Find the maximum integer M such that [S2,M] 
+ *	can be obtained from S1.
  *	
  *	Example:
  *	
@@ -32,17 +34,42 @@ package leetcode;
  * @file        P466_CountTheRepetitions.java
  * @type        P466_CountTheRepetitions
  * @date        2016年12月4日 下午8:31:07
- * @details     
+ * @details     Solution1: AC 8ms 83.39%
  */
 public class P466_CountTheRepetitions {
-	public static void main(String[] args) {
-		
-	}
-	
-	static class Solution {
+	static class Solution1 {
 	    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
-	        
+	        int[] rapport = new int[102], rest = new int[102];
+	        Arrays.fill(rapport, -1);
+	        Arrays.fill(rest, -1);
+	        int b = -1, posRest = 0, rap = 0, last = -1;
+	        rapport[0] = rest[0] = 0;
+	        for (int i = 1; i <= s2.length() + 1; i ++) {
+	            int j = 0;
+	            for (j = 0; j < s1.length(); j ++) {
+	                if (s2.charAt(posRest) == s1.charAt(j)) {
+	                    posRest ++;
+	                    if (posRest == s2.length()) {
+	                        rap ++;
+	                        posRest = 0;
+	                    }
+	                }
+	            }
+	            for (int k = 0; k < i; k ++)
+	                if (posRest == rest[k]) {
+	                    b = k;
+	                    last = i;
+	                    break;
+	                }
+	            rapport[i] = rap;
+	            rest[i] = posRest;
+	            if (b >= 0) break;
+	        }
+	        int interval = last - b;
+	        if (b >= n1) return rapport[n1] / n2;
+	        return ((n1-b)/interval*(rapport[last]-rapport[b])+rapport[(n1-b)%interval+b])/n2;
 	    }
+	    
 	}
 	
 }
