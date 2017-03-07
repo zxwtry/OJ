3c7ba2d4e82308ed96c72e456d9cc0fcb69cdd87
zxwtry <zxwtry@qq.com>
2017-03-07 23:43:07 +0800

    添加 leetcode P372_SuperPow Solution1: AC 14ms 18.60%

--- src/leetcode/P372_SuperPow.java
+++ src/leetcode/P372_SuperPow.java
@@ -26,8 +26,26 @@ package leetcode;
  * @file        P372_SuperPow.java
  * @type        P372_SuperPow
  * @date        2017年3月6日 下午7:15:56
- * @details
+ * @details     Solution1: AC 14ms 18.60%
+ * @details     Solution2: AC  8ms 58.93%
  */
 public class P372_SuperPow {
-    
+    //ab%k = ((a%k)*(b%k)) % k
+    static class Solution1 {
+        final int base = 1337;
+        public int superPow(int a, int[] b) {
+            return superPow(a, b, 0, b.length - 1);
+        }
+        public int superPow(int a, int[] b, int i, int j) {
+            if (i > j) return 1;
+            return mod(superPow(a, b, i, j - 1), 10) * mod(a, b[j]) % base;
+        }
+        public int mod(int a, int b) {
+            a %= base;
+            int j = 1;
+            for (int i = 0; i < b; i ++)
+                j = (a * j) % base;
+            return j;
+        }
+    }
 }
