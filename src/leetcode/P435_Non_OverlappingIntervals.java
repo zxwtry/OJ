5ec4a9de89054121fd3a33777c1f08e8606bed57
zxwtry <zxwtry@qq.com>
2017-03-07 23:48:00 +0800

    添加 leetcode P435_Non_OverlappingIntervals Solution1: AC 8ms 83.78%

--- src/leetcode/P435_Non_OverlappingIntervals.java
+++ src/leetcode/P435_Non_OverlappingIntervals.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * 	Given a collection of intervals, find the minimum number of intervals
  *  you need to remove to make the rest of the intervals non-overlapping.
@@ -29,12 +32,36 @@ package leetcode;
  */
 
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P435_Non_OverlappingIntervals.java
+ * @type        P435_Non_OverlappingIntervals
+ * @date        2017年3月6日 下午10:11:27
+ * @details     Solution1: AC 8ms 83.78%
+ * @details     Solution2: AC 8ms 83.78%
+ */
 public class P435_Non_OverlappingIntervals {
-	public static void main(String[] args) {
-		
-	}
-	static class Solution {
+	static class Solution1 {
 	    public int eraseOverlapIntervals(Interval[] intervals) {
+	        if (intervals.length == 0)  return 0;
+	        Arrays.sort(intervals, new myComparator());
+	        int end = intervals[0].end;
+	        int count = 1;
+	        for (int i = 1; i < intervals.length; i++) {
+	            if (intervals[i].start >= end) {
+	                end = intervals[i].end;
+	                count++;
+	            }
+	        }
+	        return intervals.length - count;
+	    }
+	    class myComparator implements Comparator<Interval> {
+	        public int compare(Interval a, Interval b) {
+	            return a.end - b.end;
+	        }
 	    }
 	}
 	static class Interval {
