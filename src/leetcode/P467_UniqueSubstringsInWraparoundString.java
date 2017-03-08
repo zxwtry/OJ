37e67d716dd7e3d47be98fe8f34652b304855c98
zxwtry <zxwtry@qq.com>
2017-03-08 09:36:33 +0800

    添加 leetcode P467_UniqueSubstringsInWraparoundString Solution2: AC 24ms 44.69%

--- src/leetcode/P467_UniqueSubstringsInWraparoundString.java
+++ src/leetcode/P467_UniqueSubstringsInWraparoundString.java
@@ -37,9 +37,9 @@ package leetcode;
  * @type        P467_UniqueSubstringsInWraparoundString
  * @date        2016年12月4日 下午8:29:13
  * @details     Solution1: WA
+ * @details     Solution2: AC 24ms 44.69%
  */
 public class P467_UniqueSubstringsInWraparoundString {
-	
 	static class Solution1 {
 	    public int findSubstringInWraproundString(String p) {
 	        boolean[] map = new boolean[26];
@@ -59,6 +59,26 @@ public class P467_UniqueSubstringsInWraparoundString {
 	        return answer;
 	    }
 	}
-	
-	
+	static class Solution2 {
+	    public int findSubstringInWraproundString(String p) {
+	        int[] count = new int[26];
+	        int maxLengthCur = 0; 
+	        for (int i = 0; i < p.length(); i++) {
+	            if (i > 0 && (p.charAt(i) - p.charAt(i - 1) == 1 || (p.charAt(i - 1) - p.charAt(i) == 25))) {
+	                maxLengthCur++;
+	            }
+	            else {
+	                maxLengthCur = 1;
+	            }
+	            
+	            int index = p.charAt(i) - 'a';
+	            count[index] = Math.max(count[index], maxLengthCur);
+	        }
+	        int sum = 0;
+	        for (int i = 0; i < 26; i++) {
+	            sum += count[i];
+	        }
+	        return sum;
+	    }
+	}
 }
