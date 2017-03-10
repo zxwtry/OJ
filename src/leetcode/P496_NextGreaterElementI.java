631545caeea4cda672195cec36be90018d18d1fd
zxwtry <zxwtry@qq.com>
2017-03-10 20:34:22 +0800

    添加 leetcode P496_NextGreaterElementI Solution1: AC 8ms 89.13%

--- src/leetcode/P496_NextGreaterElementI.java
+++ src/leetcode/P496_NextGreaterElementI.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashMap;
+
 /**
  * 	You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. 
  * 	Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
@@ -34,8 +36,31 @@ package leetcode;
  * @file        P496_NextGreaterElementI.java
  * @type        P496_NextGreaterElementI
  * @date        2017年2月8日 下午11:22:26
- * @details     
+ * @details     Solution1: AC 8ms 89.13%
  */
 public class P496_NextGreaterElementI {
-	
+    static class Solution1 {
+        public int[] nextGreaterElement(int[] findNums, int[] nums) {
+            int max = Integer.MIN_VALUE;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = nums.length - 1; i > -1; i --) {
+                if (max < nums[i]) {
+                    map.put(nums[i], -1);
+                } else {
+                    for (int j = i + 1; j < nums.length; j ++) {
+                        if (nums[j] > nums[i]) {
+                            map.put(nums[i], nums[j]);
+                            break;
+                        }
+                    }
+                }
+                max = Math.max(max, nums[i]);
+            }
+            int[] answer = new int[findNums.length];
+            for (int i = 0; i < answer.length; i ++) {
+                answer[i] = map.get(findNums[i]);
+            }
+            return answer;
+        }
+    }
 }
