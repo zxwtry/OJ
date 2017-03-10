bb613f4f42df729e348e10e87adeced7e6d2e531
zxwtry <zxwtry@qq.com>
2017-03-10 20:36:33 +0800

    添加 leetcode P501_FindModeinBinarySearchTree

--- src/leetcode/P501_FindModeinBinarySearchTree.java
+++ src/leetcode/P501_FindModeinBinarySearchTree.java
@@ -2,24 +2,24 @@ package leetcode;
 
 /**
  * 	Given a binary search tree (BST) with duplicates, find all the mode(s) (the most frequently occurred element) in the given BST.
-
-Assume a BST is defined as follows:
-
-The left subtree of a node contains only nodes with keys less than or equal to the node's key.
-The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
-Both the left and right subtrees must also be binary search trees.
-For example:
-Given BST [1,null,2,2],
-   1
-    \
-     2
-    /
-   2
-return [2].
-
-Note: If a tree has more than one mode, you can return them in any order.
-
-Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count).
+ *  
+ *  Assume a BST is defined as follows:
+ *  
+ *  The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+ *  The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+ *  Both the left and right subtrees must also be binary search trees.
+ *  For example:
+ *  Given BST [1,null,2,2],
+ *     1
+ *      \
+ *       2
+ *      /
+ *     2
+ *  return [2].
+ *  
+ *  Note: If a tree has more than one mode, you can return them in any order.
+ *  
+ *  Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count).
  */
 
 /**
@@ -33,7 +33,7 @@ Follow up: Could you do that without using any extra space? (Assume that the imp
  * @details     
  */
 public class P501_FindModeinBinarySearchTree {
-	class Solution {
+	class Solution1 {
 	    public int[] findMode(TreeNode root) {
 	        
 	    }
