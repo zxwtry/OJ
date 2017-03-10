60c0412a4ce6c2bfb8f06a17e79aa98435525bc0
zxwtry <zxwtry@qq.com>
2017-03-10 19:52:00 +0800

    添加 leetcode P506_RelativeRanks

---
+++ src/leetcode/P506_RelativeRanks.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ *  Given scores of N athletes, find their relative ranks and the people with 
+ *  the top three highest scores, who will be awarded medals: "Gold Medal", 
+ *  "Silver Medal" and "Bronze Medal".
+ *  
+ *  Example 1:
+ *  Input: [5, 4, 3, 2, 1]
+ *  Output: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+ *  Explanation: The first three athletes got the top three highest scores, 
+ *  so they got "Gold Medal", "Silver Medal" and "Bronze Medal". 
+ *  For the left two athletes, you just need to output their relative ranks 
+ *  according to their scores.
+ *  Note:
+ *  N is a positive integer and won't exceed 10,000.
+ *  All the scores of athletes are guaranteed to be unique.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P506_RelativeRanks.java
+ * @type        P506_RelativeRanks
+ * @date        2017年3月10日 下午7:50:53
+ * @details     
+ */
+public class P506_RelativeRanks {
+
+}
