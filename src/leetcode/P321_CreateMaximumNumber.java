fc690bcb47e4e63f5248a1243727dbf7b1e28b05
zxwtry <zxwtry@qq.com>
2017-03-11 21:31:29 +0800

    添加 leetcode P321_CreateMaximumNumber Solution2: AC 25 46.78%

--- src/leetcode/P321_CreateMaximumNumber.java
+++ src/leetcode/P321_CreateMaximumNumber.java
@@ -36,16 +36,9 @@ import java.util.PriorityQueue;
  * @file        P321_CreateMaximumNumber.java
  * @type        P321_CreateMaximumNumber
  * @date        2017年1月9日 下午9:07:53
- * @details     
+ * @details     Solution2: AC 25 46.78%
  */
 public class P321_CreateMaximumNumber {
-    public static void main(String[] args) {
-        int[] nums1 = {3, 4, 6, 5};
-        int[] nums2 = {9, 1, 2, 5, 8, 3};
-        Solution1 solution1 = new Solution1();
-        for (int k = 1; k <= nums1.length + nums2.length; k ++)
-            tools.Utils.printArray(solution1.maxNumber(nums1, nums2, k), k);
-    }
 	static class Solution1 {
 	    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
 	        if (nums1 == null || nums2 == null || k < 0 || k > nums1.length + nums2.length)
@@ -62,6 +55,40 @@ public class P321_CreateMaximumNumber {
 	        for (int maxNumberIndex = 0; maxNumberIndex < maxNumber.length; maxNumberIndex ++)
 	            maxNumber[maxNumberIndex] = priorityQueue.poll();
 	        return maxNumber;
+	    }
+	}
+	static class Solution2 {
+	    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+	        int n = nums1.length;
+	        int m = nums2.length;
+	        int[] ans = new int[k];
+	        for (int i = Math.max(0, k - m); i <= k && i <= n; ++i) {
+	            int[] candidate = merge(maxArray(nums1, i), maxArray(nums2, k - i), k);
+	            if (greater(candidate, 0, ans, 0)) ans = candidate;
+	        }
+	        return ans;
+	    }
+	    private int[] merge(int[] nums1, int[] nums2, int k) {
+	        int[] ans = new int[k];
+	        for (int i = 0, j = 0, r = 0; r < k; ++r)
+	            ans[r] = greater(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
+	        return ans;
+	    }
+	    public boolean greater(int[] nums1, int i, int[] nums2, int j) {
+	        while (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
+	            i++;
+	            j++;
+	        }
+	        return j == nums2.length || (i < nums1.length && nums1[i] > nums2[j]);
+	    }
+	    public int[] maxArray(int[] nums, int k) {
+	        int n = nums.length;
+	        int[] ans = new int[k];
+	        for (int i = 0, j = 0; i < n; ++i) {
+	            while (n - i + j > k && j > 0 && ans[j - 1] < nums[i]) j--;
+	            if (j < k) ans[j++] = nums[i];
+	        }
+	        return ans;
 	    }
 	}
 }
