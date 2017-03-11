a37c988cf2c5e1e5d0ee292fd8af9358a2c96755
zxwtry <zxwtry@qq.com>
2017-03-11 21:13:44 +0800

    添加 leetcode P483_SmallestGoodBase Solution1: AC 11ms 64.49%

--- src/leetcode/P483_SmallestGoodBase.java
+++ src/leetcode/P483_SmallestGoodBase.java
@@ -31,12 +31,36 @@ package leetcode;
  * @file        P483_SmallestGoodBase.java
  * @type        P483_SmallestGoodBase
  * @date        2017年2月5日 下午2:18:07
- * @details     
+ * @details     Solution1: AC 11ms 64.49%
  */
 public class P483_SmallestGoodBase {
 	static class Solution1 {
 	    public String smallestGoodBase(String n) {
-	        
+	        long num = 0;
+	        for (char c : n.toCharArray()) num = num * 10 + c - '0';
+	        long x = 1;
+	        for (int p = 64; p >= 1; p--) {
+	            if ((x << p) < num) {
+	                long k = helper(num, p);
+	                if (k != -1) return String.valueOf(k);
+	            }
+	        }
+	        return String.valueOf(num - 1);
+	    }
+	    private long helper(long num, int p) {
+	        long l = 1, r = (long)(Math.pow(num, 1.0/p) + 1);
+	        while (l < r) {
+	            long mid = l + (r - l) / 2;
+	            long sum = 0, cur = 1;
+	            for (int i = 0; i <= p; i++) {
+	                sum += cur;
+	                cur *= mid;
+	            }
+	            if (sum == num) return mid;
+	            else if (sum > num) r = mid;
+	            else l = mid + 1;
+	        }
+	        return -1;
 	    }
 	}
 }
