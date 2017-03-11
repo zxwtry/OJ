d5866eea2e5b5151713c51c300080e2a12a62fd4
zxwtry <zxwtry@qq.com>
2017-03-11 21:37:45 +0800

    添加 leetcode P508_MostFrequentSubtreeSum Solution1: AC 17ms 84.77%

--- src/leetcode/P508_MostFrequentSubtreeSum.java
+++ src/leetcode/P508_MostFrequentSubtreeSum.java
@@ -1,5 +1,12 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import tools.TreeNode辅助.TreeNode;
+
 /**
  *  Given the root of a tree, you are asked to find the most frequent subtree sum. 
  *  The subtree sum of a node is defined as the sum of all the node values formed 
@@ -32,8 +39,46 @@ package leetcode;
  * @file        P508_MostFrequentSubtreeSum.java
  * @type        P508_MostFrequentSubtreeSum
  * @date        2017年3月11日 下午9:34:33
- * @details     
+ * @details     Solution1: AC 17ms 84.77% 
  */
 public class P508_MostFrequentSubtreeSum {
-
+    static class Solution1 {
+        Map<Integer, Integer> sumToCount;
+        int maxCount;
+        
+        public int[] findFrequentTreeSum(TreeNode root) {
+            maxCount = 0;
+            sumToCount = new HashMap<Integer, Integer>();
+            
+            postOrder(root);
+            
+            List<Integer> res = new ArrayList<>();
+            for (int key : sumToCount.keySet()) {
+                if (sumToCount.get(key) == maxCount) {
+                    res.add(key);
+                }
+            }
+            
+            int[] result = new int[res.size()];
+            for (int i = 0; i < res.size(); i++) {
+                result[i] = res.get(i);
+            }
+            return result;
+        }
+        
+        private int postOrder(TreeNode root) {
+            if (root == null) return 0;
+            
+            int left = postOrder(root.left);
+            int right = postOrder(root.right);
+            
+            int sum = left + right + root.val;
+            int count = sumToCount.getOrDefault(sum, 0) + 1;
+            sumToCount.put(sum, count);
+            
+            maxCount = Math.max(maxCount, count);
+            
+            return sum;
+        }
+    }
 }
