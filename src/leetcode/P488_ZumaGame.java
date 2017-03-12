5c17f942be14e368779534537fcddb07fc8cd6e6
zxwtry <zxwtry@qq.com>
2017-03-12 20:40:52 +0800

    添加 leetcode P488_ZumaGame Solution2: AC 19ms 64.73%

--- src/leetcode/P488_ZumaGame.java
+++ src/leetcode/P488_ZumaGame.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 	Think about Zuma Game. You have a row of balls on the table, 
  *  colored red(R), yellow(Y), blue(B), green(G), and white(W). 
@@ -50,6 +55,7 @@ package leetcode;
  * @type        P488_ZumaGame
  * @date        2017年2月6日 下午11:28:03
  * @details     Solution1: AC 16ms 84.25%
+ * @details     Solution2: AC 19ms 64.73%
  */
 public class P488_ZumaGame {
 	static class Solution1 {
@@ -83,6 +89,79 @@ public class P488_ZumaGame {
 	            else i = j;
 	        }
 	        return board;
+	    }
+	}
+	static class Solution2 {
+	    public int findMinStep(String board, String hand) {
+	        List<Character> boardList = new ArrayList<Character>();
+	        for (char c : board.toCharArray()) {
+	            boardList.add(c);
+	        }
+	        Map<Character,Integer> handMap = new HashMap<>();
+	        handMap.put('R',0);
+	        handMap.put('Y',0);
+	        handMap.put('B',0);
+	        handMap.put('G',0);
+	        handMap.put('W',0);
+	        for (char h : hand.toCharArray()) {
+	            handMap.put(h, handMap.get(h) + 1);
+	        }
+	        return find(boardList, handMap);
+	    }
+	    private int find(List<Character> board, Map<Character, Integer> hand) {
+	        cleanupBoard(board);
+	        if (board.size() == 0) return 0;
+	        if (empty(hand)) return -1;
+	        int count = 0;
+	        int min = Integer.MAX_VALUE;
+	        for (int i = 0; i<board.size(); i++) {
+	            char c = board.get(i);
+	            count++;
+	            if (i == board.size() - 1 || board.get(i+1) != c) {
+	                int missing = 3 - count;
+	                if (hand.get(c) >= missing) {
+	                    hand.put(c, hand.get(c) - missing);
+	                    List<Character> smallerBoard = new ArrayList<>(board);
+	                    for (int j = 0; j<count; j++) {
+	                        smallerBoard.remove(i-j);
+	                    }
+	                    int smallerFind = find(smallerBoard, hand);
+	                    if ( smallerFind != -1 ) {
+	                        min = Math.min(smallerFind + missing, min);
+	                    }
+	                    hand.put(c, hand.get(c) + missing);
+	                }
+	                count = 0;
+	            }
+	        }
+	        return (min == Integer.MAX_VALUE) ? -1 : min;
+	    }
+	    private void cleanupBoard(List<Character> board) {
+	        int count = 0;
+	        boolean cleaned = false;
+	        for (int i = 0; i<board.size(); i++) {
+	            char c = board.get(i);
+	            count++;
+	            if (i == board.size() - 1 || board.get(i+1) != c) {
+	                if (count >= 3) {
+	                    for (int j = 0; j<count; j++) {
+	                        board.remove(i-j);
+	                    }
+	                    cleaned = true;
+	                    break;
+	                }
+	                count = 0;
+	            }
+	        }
+	        if (cleaned) {
+	            cleanupBoard(board);
+	        }
+	    }
+	    private boolean empty(Map<Character,Integer> hand) {
+	        for (int val : hand.values()) {
+	            if (val > 0) return false;
+	        }
+	        return true;
 	    }
 	}
 }
