c1f633374bfb7f771d67bda1a17c06bb9f01bb97
zxwtry <zxwtry@qq.com>
2017-03-14 22:12:34 +0800

    添加 leetcode P541_ReverseStringII

---
+++ src/leetcode/P541_ReverseStringII.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ *  Given a string and an integer k, you need to reverse the first k characters 
+ *  for every 2k characters counting from the start of the string. 
+ *  If there are less than k characters left, reverse all of them. 
+ *  If there are less than 2k but greater than or equal to k characters, 
+ *  then reverse the first k characters and left the other as original.
+ *  Example:
+ *  Input: s = "abcdefg", k = 2
+ *  Output: "bacdfeg"
+ *  Restrictions:
+ *  The string consists of lower English letters only.
+ *  Length of the given string and k will in the range [1, 10000]
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P541_ReverseStringII.java
+ * @type        P541_ReverseStringII
+ * @date        2017年3月14日 下午10:07:36
+ * @details     
+ */
+public class P541_ReverseStringII {
+
+}
