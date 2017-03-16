68a8b8a5fbf3b0d1ad1ed0d5d8a9b0440c5936b4
zxwtry <zxwtry@qq.com>
2017-03-16 20:31:13 +0800

    添加 leetcode P387_FirstUniqueCharacterInAString Solution1: AC 37ms 42.43%

--- src/leetcode/P387_FirstUniqueCharacterInAString.java
+++ src/leetcode/P387_FirstUniqueCharacterInAString.java
@@ -22,8 +22,19 @@ package leetcode;
  * @file        P387_FirstUniqueCharacterInAString.java
  * @type        P387_FirstUniqueCharacterInAString
  * @date        2017年3月16日 下午8:25:02
- * @details     
+ * @details     Solution1: AC 37ms 42.43%
  */
 public class P387_FirstUniqueCharacterInAString {
-
+    static class Solution1 {
+        public int firstUniqChar(String s) {
+            int[] map = new int[26];
+            int n = s.length();
+            for (int i = 0; i < n; i ++)
+                map[s.charAt(i) - 'a'] ++;
+            for (int i = 0; i < n; i ++)
+                if (map[s.charAt(i) - 'a'] > 1)
+                    return i;
+            return -1;
+        }
+    }
 }
