b36705c9d09052fffc771be4e8a8399ade4fd80b
zxwtry <zxwtry@qq.com>
2017-03-24 15:50:59 +0800

    添加 leetcode 未知_最长公共连续子串 solve

--- src/nowcoder/com/未知_最长公共连续子串.java
+++ src/nowcoder/com/未知_最长公共连续子串.java
@@ -1,5 +1,7 @@
 package nowcoder.com;
 
+import java.util.Scanner;
+
 /**
  * 牛牛有两个字符串（可能包含空格）,牛牛想找出其中最长的公共连续子串,希望你能帮助他,并输出其长度。 
  * 输入描述:
@@ -23,8 +25,29 @@ package nowcoder.com;
  * @file        未知_最长公共连续子串.java
  * @type        未知_最长公共连续子串
  * @date        2017年3月24日 下午12:08:15
- * @details     
+ * @details     AC
  */
 public class 未知_最长公共连续子串 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        System.out.println(solve(s1, s2));
+        scanner.close();
+    }
+    private static int solve(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int max = 0;
+        int[][] m = new int[n1 + 1][n2 + 1];
+        for (int i1 = 0; i1 < n1; i1 ++) {
+            for (int i2 = 0; i2 < n2; i2 ++) {
+                if (s1.charAt(i1) == s2.charAt(i2)) {
+                    m[i1 + 1][i2 + 1] = m[i1][i2] + 1;
+                    max = Math.max(max, m[i1 + 1][i2 + 1]);
+                }
+            }
+        }
+        return max;
+    }
 }
