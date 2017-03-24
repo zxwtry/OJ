c09262ccca266988e5e0634fd895eeaffc4a03b5
zxwtry <zxwtry@qq.com>
2017-03-24 15:54:44 +0800

    添加 nowcoder com 未知_找整除

---
+++ src/nowcoder/com/未知_找整除.java
@@ -0,0 +1,32 @@
+package nowcoder.com;
+
+import java.util.Scanner;
+
+/**
+ * 牛牛想在[a, b]区间内找到一些数满足可以被一个整数c整除,现在你需要帮助牛牛统计区间内一共有多少个这样的数满足条件？ 
+ * 输入描述:
+ * 首先输入两个整数a,b,（-5*10^8 ≤ a ≤ b ≤ 5*10^8)
+ * 接着是一个正整数c（1 <= c <= 1000）
+ * 
+ * 
+ * 输出描述:
+ * 输出一个整数表示个数。
+ * 
+ * 输入例子:
+ * 0 14 5
+ * 
+ * 输出例子:
+ * 3
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        未知_找整除.java
+ * @type        未知_找整除
+ * @date        2017年3月24日 下午12:19:00
+ * @details     AC
+ */
+public class 未知_找整除 {}
