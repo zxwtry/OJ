ab9e1accfba65fbe3af1298e7238d024ab0f0ca8
zxwtry <zxwtry@qq.com>
2017-03-24 12:08:57 +0800

    添加 nowcoder.com 未知_最长公共连续子串

---
+++ src/nowcoder/com/未知_最长公共连续子串.java
@@ -0,0 +1,30 @@
+package nowcoder.com;
+
+/**
+ * 牛牛有两个字符串（可能包含空格）,牛牛想找出其中最长的公共连续子串,希望你能帮助他,并输出其长度。 
+ * 输入描述:
+ * 输入为两行字符串（可能包含空格），长度均小于等于50.
+ * 输出描述:
+ * 输出为一个整数，表示最长公共连续子串的长度。
+ * 
+ * 输入例子:
+ * abcde
+ * abgde
+ * 
+ * 输出例子:
+ * 2
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        未知_最长公共连续子串.java
+ * @type        未知_最长公共连续子串
+ * @date        2017年3月24日 下午12:08:15
+ * @details     
+ */
+public class 未知_最长公共连续子串 {
+    
+}
