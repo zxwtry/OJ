9fcbd521950ade2f21bc472ac0d4b3e010f7772e
zxwtry <zxwtry@qq.com>
2017-03-25 21:58:35 +0800

    添加 nowcoder com 未知_平衡数

---
+++ src/nowcoder/com/未知_平衡数.java
@@ -0,0 +1,15 @@
+package nowcoder.com;
+
+import java.util.Scanner;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        未知_平衡数.java
+ * @type        未知_平衡数
+ * @date        2017年3月24日 下午1:36:19
+ * @details     AC
+ */
+public class 未知_平衡数 {}
