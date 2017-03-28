e8f69527213d6c372de7cd1f6b2e9e4d8590e15e
zxwtry <zxwtry@qq.com>
2017-03-28 20:21:40 +0800

    添加 nowcoder com 未知_创造新世界

---
+++ src/nowcoder/com/未知_创造新世界.java
@@ -0,0 +1,34 @@
+package nowcoder.com;
+
+import java.util.ArrayList;
+
+/**
+ *  众所周知计算机代码底层计算都是0和1的计算，
+ *  牛牛知道这点之后就想使用0和1创造一个新世界！
+ *  牛牛现在手里有n个0和m个1，给出牛牛可以创造的x种物品，
+ *  每种物品都由一个01串表示。牛牛想知道当前手中的0和1可以
+ *  最多创造出多少种物品。
+ *  
+ *  x n m
+ *  
+ *  3 3 1
+ *  1
+ *  00
+ *  100
+ */
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        未知_创造新世界.java
+ * @type        未知_创造新世界
+ * @date        2017年3月24日 下午1:53:16
+ * @details     AC
+ */
+public class 未知_创造新世界 {}
