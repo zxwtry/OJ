28cc920ce3772bcec2acf7a8f2b8538040ed758f
zxwtry <zxwtry@qq.com>
2017-03-28 20:23:31 +0800

    添加 nowcoder com 未知_创造新世界 solve1

--- src/nowcoder/com/未知_创造新世界.java
+++ src/nowcoder/com/未知_创造新世界.java
@@ -31,4 +31,81 @@ import java.util.Scanner;
  * @date        2017年3月24日 下午1:53:16
  * @details     AC
  */
-public class 未知_创造新世界 {}
+public class 未知_创造新世界 {
+    public static void main(String[] args) {
+        solve2();
+    }
+    static void solve1() {
+        Scanner scanner = new Scanner(System.in);
+        int times = scanner.nextInt();
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        A[] as = new A[times];
+        for (int i = 0; i < times; i ++) {
+            String s = scanner.next();
+            int nn = 0;
+            int nm = 0;
+            for (int j = 0; j < s.length(); j ++) {
+                if (s.charAt(j) == '0') {
+                    nn ++;
+                } else {
+                    nm ++;
+                }
+            }
+            A a = new A();
+            a.n = nn;
+            a.m = nm;
+            as[i] = a;
+        }
+        Arrays.sort(as, new Comparator<A>() {
+            @Override
+            public int compare(A a1, A a2) {
+                if (a1.n < a2.n) return -1;
+                if (a1.n > a2.n) return 1;
+                if (a1.m < a2.m) return -1;
+                if (a1.m > a2.m) return 1;
+                return 0;
+            }
+        });
+        int count = 0;
+        int max = 0;
+        int nn = n;
+        int nm = m;
+        for (int i = 0; i < times; i ++) {
+            if (nn < as[i].n) break;
+            if (nm >= as[i].m) {
+                count ++;
+                nm -= as[i].m;
+                nn -= as[i].n;
+            }
+        }
+        max = Math.max(max, count);
+        Arrays.sort(as, new Comparator<A>() {
+            @Override
+            public int compare(A a1, A a2) {
+                if (a1.m < a2.m) return -1;
+                if (a1.m > a2.m) return 1;
+                if (a1.n < a2.n) return -1;
+                if (a1.n > a2.n) return 1;
+                return 0;
+            }
+        });
+        nn = n;
+        nm = m;
+        count = 0;
+        for (int i = 0; i < times; i ++) {
+            if (nm < as[i].m) break;
+            if (nn >= as[i].n) {
+                count ++;
+                nm -= as[i].m;
+                nn -= as[i].n;
+            }
+        }
+        max = Math.max(max, count);
+        System.out.println(max);
+        scanner.close();
+    }
+    static class A{
+        int n, m;
+    }
+}
