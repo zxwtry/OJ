28ce85b9ecb9e7af35984be44141d5d856314925
zxwtry <zxwtry@qq.com>
2017-04-23 20:42:27 +0800

    添加 leetcode P562_LongestLineOfConsecutiveOneInMatrix

---
+++ src/leetcode/P562_LongestLineOfConsecutiveOneInMatrix.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ *  Given a 01 matrix M, find the longest line of consecutive one in the matrix. 
+ *  The line could be horizontal, vertical, diagonal or anti-diagonal.
+
+    Example:
+    Input:
+    [[0,1,1,0],
+     [0,1,1,0],
+     [0,0,0,1]]
+    Output: 3
+    Hint: The number of elements in the given matrix will not exceed 10,000.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P562_LongestLineOfConsecutiveOneInMatrix.java
+ * @type        P562_LongestLineOfConsecutiveOneInMatrix
+ * @date        2017年4月23日 上午10:06:20
+ */
+public class P562_LongestLineOfConsecutiveOneInMatrix {}
