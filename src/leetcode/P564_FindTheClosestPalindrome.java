05ddd8a14da0f4a332152a425d060b6b4355fa09
zxwtry <zxwtry@qq.com>
2017-04-23 20:44:35 +0800

    添加 leetcode P563_BinaryTreeTilt

---
+++ src/leetcode/P564_FindTheClosestPalindrome.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ *  Given an integer n, find the closest integer (not including itself), which is a palindrome.
+
+    The 'closest' is defined as absolute difference minimized between two integers.
+    
+    Example 1:
+    Input: "123"
+    Output: "121"
+    Note:
+    The input n is a positive integer represented by string, whose length will not exceed 18.
+    If there is a tie, return the smaller one as answer.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P564_FindTheClosestPalindrome.java
+ * @type        P564_FindTheClosestPalindrome
+ * @date        2017年4月23日 上午10:25:25
+ */
+public class P564_FindTheClosestPalindrome {}
