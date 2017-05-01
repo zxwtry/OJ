c8826b5d3046623653a5df97dae0e866e4508903
zxwtry <zxwtry@qq.com>
2017-05-01 20:57:32 +0800

    添加 nowcoder.com NOD51_小C的多边形

---
+++ src/nowcoder/com/NOD51_小C的多边形.java
@@ -0,0 +1,26 @@
+package nowcoder.com;
+
+import java.util.Scanner;
+
+/**
+ *  大意：凸多边形和中间一点
+ *  n：是输入，是所有点数
+ *  凸边：1~n-1
+ *  中点：1~n-1
+ *  一条凸边+两条相邻中点边=一个三角形
+ *  要求所有三角形的边长和相同
+ *  有方案输出：先输出凸边，后输出中边
+ *  无方案输出：输出0
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        NOD51_小C的多边形.java
+ * @type        NOD51_小C的多边形
+ * @date        2017年5月1日 下午8:53:38
+ * @details     
+ */
+public class NOD51_小C的多边形 {}
