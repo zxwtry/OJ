992b09ca4646f1d568ce30953205301faadeb488
zxwtry <zxwtry@qq.com>
2017-05-04 22:24:37 +0800

    添加 leetcode P560_SubarraySumEqualsK

---
+++ src/leetcode/P560_SubarraySumEqualsK.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ *  Given an array of integers and an integer k, you need to find the total number of continuous
+ *   subarrays whose sum equals to k.
+    
+    Example 1:
+    Input:nums = [1,1,1], k = 2
+    Output: 2
+    Note:
+    The length of the array is in range [1, 20,000].
+    The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P560_SubarraySumEqualsK.java
+ * @type        P560_SubarraySumEqualsK
+ * @date        2017年4月30日 上午9:40:41
+ * @details     
+ */
+public class P560_SubarraySumEqualsK {}
