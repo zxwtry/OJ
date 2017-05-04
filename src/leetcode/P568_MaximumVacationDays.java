8c21c4a49ca6e7a051a293ecf15ad5ec1a302fcc
zxwtry <zxwtry@qq.com>
2017-05-04 22:28:27 +0800

    添加 leetcode P568_MaximumVacationDays

---
+++ src/leetcode/P568_MaximumVacationDays.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+    f代表能够从i飞到j
+    d代表在i地方，在j星期，休息d[i][j]天
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P568_MaximumVacationDays.java
+ * @type        P568_MaximumVacationDays
+ * @date        2017年5月4日 下午10:26:15
+ * @details
+ */
+public class P568_MaximumVacationDays {}
