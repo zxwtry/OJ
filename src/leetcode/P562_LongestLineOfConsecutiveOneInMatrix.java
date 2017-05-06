f93456e5eedb8909c226607cbffe50a344030c8e
zxwtry <zxwtry@qq.com>
2017-05-06 22:23:25 +0800

    添加 leetcode P562_LongestLineOfConsecutiveOneInMatrix

--- src/leetcode/P562_LongestLineOfConsecutiveOneInMatrix.java
+++ src/leetcode/P562_LongestLineOfConsecutiveOneInMatrix.java
@@ -21,5 +21,65 @@ package leetcode;
  * @file        P562_LongestLineOfConsecutiveOneInMatrix.java
  * @type        P562_LongestLineOfConsecutiveOneInMatrix
  * @date        2017年4月23日 上午10:06:20
+ * @details     Solution: AC 35ms 50.00%
  */
-public class P562_LongestLineOfConsecutiveOneInMatrix {}
+public class P562_LongestLineOfConsecutiveOneInMatrix {
+    public static void main(String[] args) {
+        int[][] M = {
+                {1,1,1,1},
+//                {0,1,0,0},
+//                {0,1,0,0},
+        };
+        System.out.println(new Solution().longestLine(M));
+    }
+    static class Solution {
+        public int longestLine(int[][] M) {
+            int rn = M == null ? 0 : M.length;
+            if (rn == 0) return 0;
+            int cn = M[0] == null ? 0 : M[0].length;
+            if (cn == 0) return 0;
+            int ans = 0, cnt = 0;
+            for (int i = 0; i < rn; i ++) {
+                cnt = 0;
+                for (int j = 0; j < cn; j ++) {
+                    if (M[i][j] == 0) cnt = 0;
+                    else cnt ++;
+                    ans = Math.max(ans, cnt);
+                }
+            }
+            for (int j = 0; j < cn; j ++) {
+                cnt = 0;
+                for (int i = 0; i < rn; i ++) {
+                    if (M[i][j] == 0) cnt = 0;
+                    else cnt ++;
+                    ans = Math.max(ans, cnt);
+                }
+            }
+            for (int i = 0; i < rn+cn-1; i ++) {
+                int ri = i < rn ? i : rn-1;
+                int ci = i < rn ? 0 : i-rn+1;
+                cnt = 0;
+                while (ri > -1 && ci < cn) {
+                    if (M[ri][ci] == 0) cnt = 0;
+                    else cnt ++;
+                    ans = Math.max(ans, cnt);
+                    ri --;
+                    ci ++;
+                }
+            }
+            for (int i = 0; i < rn+cn-1; i ++) {
+                int ri = i < rn ? rn-1-i : 0;
+                int ci = i < rn ? 0 : i-rn+1;
+                cnt = 0;
+                while (ri < rn && ci < cn) {
+                    if (M[ri][ci] == 0) cnt = 0;
+                    else cnt ++;
+                    ans = Math.max(ans, cnt);
+                    ri ++;
+                    ci ++;
+                }
+            }
+            return ans;
+        }
+    }
+}
