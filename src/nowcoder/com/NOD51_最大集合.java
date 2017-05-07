e45925d37c0756de1a391fb80da9a8be42da24c7
zxwtry <zxwtry@qq.com>
2017-05-07 22:24:57 +0800

    添加 nowcoder com NOD51_最大集合

---
+++ src/nowcoder/com/NOD51_最大集合.java
@@ -0,0 +1,41 @@
+package nowcoder.com;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+
+/**
+    描述
+    给定一个1-N的排列A[1], A[2], ... A[N]，定义集合S[K] = {A[K], A[A[K]], A[A[A[K]]] ... }。  
+    
+    显然对于任意的K=1..N，S[K]都是有限集合。  
+    
+    你能求出其中包含整数最多的S[K]的大小吗？
+    
+    输入
+    第一行包含一个整数N。(1 <= N <= 100000)  
+    
+    第二行包含N个两两不同的整数，A[1], A[2], ... A[N]。(1 <= A[i] <= N)
+    
+    输出
+    最大的S[K]的大小。
+    
+    样例输入
+    7  
+    6 5 1 4 2 7 3
+    样例输出
+    4
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        NOD51_最大集合.java
+ * @type        NOD51_最大集合
+ * @date        2017年5月7日 下午12:45:31
+ * @details     
+ */
+public class NOD51_最大集合 {}
