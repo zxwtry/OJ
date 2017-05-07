d817a6534e09a02a115af9fc490a28c866f00684
zxwtry <zxwtry@qq.com>
2017-05-07 22:11:14 +0800

    添加 leetcode P572_SubtreeOfAnotherTree

---
+++ src/leetcode/P572_SubtreeOfAnotherTree.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map.Entry;
+
+
+import java.util.Queue;
+
+import tools.TreeNode辅助.TreeNode;
+
+/**
+    Given two non-empty binary trees s and t, check whether tree t has exactly 
+    the same structure and node values with a subtree of s. A subtree of s is 
+    a tree consists of a node in s and all of this node's descendants. 
+    The tree s could also be considered as a subtree of itself.
+    
+    Example 1:
+    Given tree s:
+    
+         3
+        / \
+       4   5
+      / \
+     1   2
+    Given tree t:
+       4 
+      / \
+     1   2
+    Return true, because t has the same structure and node values with a subtree of s.
+    Example 2:
+    Given tree s:
+    
+         3
+        / \
+       4   5
+      / \
+     1   2
+        /
+       0
+    Given tree t:
+       4
+      / \
+     1   2
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P572_SubtreeOfAnotherTree.java
+ * @type        P572_SubtreeOfAnotherTree
+ * @date        2017年5月7日 上午9:46:43
+ * @details     
+ */
+public class P572_SubtreeOfAnotherTree {}
