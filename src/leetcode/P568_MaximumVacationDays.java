0291ecf3e10f0c3dc56f90f00a3c6f68ac803054
zxwtry <zxwtry@qq.com>
2017-05-07 22:29:27 +0800

    添加 leetcode P568_MaximumVacationDays

--- src/leetcode/P568_MaximumVacationDays.java
+++ src/leetcode/P568_MaximumVacationDays.java
@@ -15,4 +15,78 @@ package leetcode;
  * @date        2017年5月4日 下午10:26:15
  * @details
  */
-public class P568_MaximumVacationDays {}
+public class P568_MaximumVacationDays {
+    public static void main(String[] args) {
+        int[][] f = {
+                {0,1,1},
+                {1,0,1},
+                {1,1,0},
+//                {0, 0, 0},
+//                {0, 0, 0},
+//                {0, 0, 0},
+        };
+        int[][] d = {
+//                {1,3,1},
+//                {6,0,3},
+//                {3,3,3},
+//                {1, 1, 1},
+//                {7, 7, 7},
+//                {7, 7, 7},
+
+                {7, 0, 0},
+                {0, 7, 0},
+                {0, 0, 7},
+        };
+        System.out.println(new Solution2().maxVacationDays(f, d));
+    }
+    
+    static public class Solution {
+        public int maxVacationDays(int[][] f, int[][] d) {
+            int n = f == null ? 0 : f.length;
+            if (n == 0) return 0;
+            int k = d[0] == null ? 0 : d[0].length;
+            if (k == 0) return 0;
+            int[] ans = {0};
+            search(f, d, n, k, 0, 0, ans, 0);
+            return ans[0];
+        }
+        private void search(int[][] f, int[][] d, int n, int k, int ni, int ki, int[] a, int c) {
+            a[0] = Math.max(a[0], c);
+            if (ki == k) return;
+            for(int nj = 0; nj < n; nj ++) {
+                if (ni == nj || f[ni][nj] == 1) {
+                    search(f, d, n, k, nj, ki+1, a, c+d[nj][ki]);
+                }
+            }
+        }
+    }
+    
+    static public class Solution2 {
+        public int maxVacationDays(int[][] f, int[][] d) {
+            int n = f == null ? 0 : f.length;
+            if (n == 0) return 0;
+            int k = d[0] == null ? 0 : d[0].length;
+            if (k == 0) return 0;
+            int ans = 0;
+            int[][] m = new int[n+1][k];
+            boolean[][] a = new boolean[k][k];
+            a[0][0] = true;
+            for (int ni = 1; ni <= n; ni ++) {
+                boolean[][] aa = new boolean[k][k];
+                for (int ki = 0; ki < k; ki ++) {
+                    if (a[m])
+                    aa[ki][ki] = true;
+                    for (int kj = 0; kj < k; kj ++) {
+                        
+                    }
+                }
+                a = aa;
+            }
+            for (int ni = 0; ni <= n; ni ++)
+                ans = Math.max(m[ni][k-1], ans);
+            return ans;
+            
+        }
+    }
+    
+}
