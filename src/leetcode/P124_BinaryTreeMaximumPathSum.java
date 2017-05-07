cb50f70d22688533147ba1138363a6361b954f74
zxwtry <zxwtry@qq.com>
2017-05-07 22:03:20 +0800

    添加 leetcode P124_BinaryTreeMaximumPathSum

--- src/leetcode/P124_BinaryTreeMaximumPathSum.java
+++ src/leetcode/P124_BinaryTreeMaximumPathSum.java
@@ -3,7 +3,7 @@ package leetcode;
 import tools.TreeNode辅助.TreeNode;
 
 /*
- * 	Given a binary tree, find the maximum path sum.
+    Given a binary tree, find the maximum path sum.
 
 	For this problem, a path is defined as any sequence of nodes
 	 from some starting node to any node in the tree 
@@ -20,67 +20,30 @@ import tools.TreeNode辅助.TreeNode;
  */
 
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P124_BinaryTreeMaximumPathSum.java
+ * @type        P124_BinaryTreeMaximumPathSum
+ * @date        2017年5月7日 下午10:02:15
+ * @details     Solution: AC 3ms 19.84%
+ */
 public class P124_BinaryTreeMaximumPathSum {
-	static int N = Integer.MIN_VALUE;
-	public static void main(String[] args) {
-		TreeNode root = tools.TreeNode辅助.A_生成满二叉树(new int[] {
-				-1,
-				-2,-3,
-//				40, 60, 800, 100,
-//				1,2,3,40,600,800,100,0
-//				-6,
-//				N, 3,
-//				N,N,2,N
-				
-		});
-		Solution s = new Solution();
-		System.out.println(s.maxPathSum(root));
-	}
-	/*
-	 * 	3 ms
-	 * 	20.74%
-	 */
 	static class Solution {
-		int max = Integer.MIN_VALUE;
-		int single_max = Integer.MIN_VALUE;
-		boolean isCut = false;
 	    public int maxPathSum(TreeNode root) {
-	    	if (root == null) {
-	    		return Integer.MIN_VALUE;
-	    	}
-	    	search(root);
-	        return max;
+	        if (root == null) return 0;
+	        int[] ans = {Integer.MIN_VALUE};
+	    	search(root, ans);
+	        return ans[0];
 	    }
-		private int search(TreeNode root) {
-			if (root == null) {
-				return 0;
-			}
-			isCut = false;
-			max = Math.max(max, root.val);
-			single_max = Math.max(single_max, root.val);
-			int left_val = 0;
-			if (root.left != null) {
-				left_val = search(root.left);
-				if (! isCut) {
-					max = Math.max(max, left_val);
-				}
-			}
-			int right_val = 0;
-			if (root.right != null) {
-				right_val = search(root.right);
-				if (! isCut) {
-					max = Math.max(max, right_val);
-				}
-			}
-			max = Math.max(max, root.val + left_val + right_val);
-			int val = root.val + Math.max(left_val, right_val);
-			max = Math.max(max, val);
-			if (val < 0) {
-				isCut = true;
-				return 0;
-			} else {
-				return val;
-			}
+		private int search(TreeNode root, int[] ans) {
+		    if (root == null) return 0;
+		    int lv = Math.max(0, search(root.left, ans));
+		    int rv = Math.max(0, search(root.right, ans));
+		    ans[0] = Math.max(ans[0], lv+rv+root.val);
+		    return Math.max(lv, rv) + root.val;
 		}
 	}
 }
