a737b0cac2a3ff71b71b5127cd1769c4ac73523e
zxwtry <zxwtry@qq.com>
2017-05-09 22:26:23 +0800

    添加  leetcode template RECITE_Graph_Kruskal

--- src/template/RECITE_Graph_Kruskal.java
+++ src/template/RECITE_Graph_Kruskal.java
@@ -20,4 +20,80 @@ import java.util.PriorityQueue;
  * @date        2017年4月24日 下午3:39:04
  * @details     
  */
-public class RECITE_Graph_Kruskal {}
+public class RECITE_Graph_Kruskal {
+    
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE);
+        int[][] arr = {
+                {0, 1, 2},
+                {0, 2, 4},
+                {0, 3, 1},
+                {1, 3, 3},
+                {1, 4, 10},
+                {2, 3, 2},
+                {2, 5, 5},
+                {3, 5, 8},
+                {3, 4, 7},
+                {3, 6, 4},
+                {4, 6, 6},
+                {5, 6, 1},
+        };
+        List<Edge> edges = new ArrayList<Edge>(arr.length);
+        for (int[] a : arr) {
+            edges.add(new Edge(a[0], a[1], a[2]));
+        }
+        int numVertices = 7;
+        List<Edge> ans = kruskal(edges, numVertices);
+        System.out.println(ans);
+    }
+    
+    public static List<Edge> kruskal(List<Edge> edges, int numVertices) {
+        DisjSets set = new DisjSets(numVertices);
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge>(edges);
+        List<Edge> mst = new ArrayList<Edge>();
+        while (mst.size() != numVertices - 1) {
+            Edge e = pq.poll();
+            int u = set.find(e.u);
+            int v = set.find(e.v);
+            if (u != v) {
+                mst.add(e);
+                set.union(u, v);
+            }
+        }
+        return mst;
+    }
+    
+    static class Edge implements Comparable<Edge> {
+        public int u, v, dist;
+        public Edge(int u, int v, int dist) {
+            this.u = Math.min(u, v);
+            this.v = Math.max(u, v);
+            this.dist = dist;
+        }
+        @Override
+        public int compareTo(Edge e) {
+            return this.dist - e.dist;
+        }
+        @Override
+        public String toString() {
+            return String.format("u:%d v:%d d:%d", u, v, dist);
+        }
+    }
+    
+    static class DisjSets {
+        private int[] arr = null;
+        public DisjSets(int numOfElements) {
+            super();
+            arr = new int[numOfElements];
+            Arrays.fill(arr, -1);
+        }
+        public void union(int root1, int root2) {
+            arr[root2] = root1;
+        }
+        public int find(int x) {
+            while (arr[x] >= 0)
+                x = arr[x];
+            return x;
+        }
+    }
+}
