6eeaf8b3cd53f6f516f3053638d59f7b6b6ffa84
zxwtry <zxwtry@qq.com>
2017-05-13 22:25:42 +0800

    添加 leetcode P129_SumRoottoLeafNumbers Solution1

--- src/leetcode/P129_SumRoottoLeafNumbers.java
+++ src/leetcode/P129_SumRoottoLeafNumbers.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 /*
  * 	Given a binary tree containing digits from 0-9 only, 
  * 	each root-to-leaf path could represent a number.
@@ -14,37 +18,27 @@ package leetcode;
 	   / \
 	  2   3
 	The root-to-leaf path 1->2 represents the number 12.
-	The root-to-leaf path 1->3 represents the number 13.
-	
+	The root-to-leaf path 1->3 represents the number 13.	
 	Return the sum = 12 + 13 = 25.
  */
 
 import tools.TreeNode辅助.TreeNode;
 
 public class P129_SumRoottoLeafNumbers {
-	public static void main(String[] args) {
-		
-	}
-	/*
-	 * 	1 ms
-	 * 	27.61%
-	 */
-	static class Solution {
-		int sum = 0;
+	static class Solution1 {
 	    public int sumNumbers(TreeNode root) {
-	    	if (root == null) {
-	    		return sum;
-	    	}
-	    	search(root, 0);
-	        return sum;
+	    	if (root == null) return 0;
+	    	int[] sum = {0};
+	    	search(root, 0, sum);
+	        return sum[0];
 	    }
-		private void search(TreeNode root, int val) {
-			if (root == null) {
-				sum += val;
-				return;
-			}
-			search(root.left, val + root.val);
-			search(root.right, val + root.val);
+		private void search(TreeNode root, int val, int[] sum) {
+		    if (root.left == null && root.right == null)
+		        sum[0] += val * 10 + root.val;
+			if (root.left != null)
+			    search(root.left, val * 10 + root.val, sum);
+			if (root.right != null)
+			    search(root.right, val * 10 + root.val, sum);
 		}
 	}
 }
