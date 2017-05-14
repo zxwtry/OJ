e56669f523f4b5759e457808b0208f1b42b50549
zxwtry <zxwtry@qq.com>
2017-05-14 22:24:01 +0800

    添加 leetcode P587_ErectTheFence

---
+++ src/leetcode/P587_ErectTheFence.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+
+/**
+    
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P587_ErectTheFence.java
+ * @type        P587_ErectTheFence
+ * @date        2017年5月14日 上午10:28:43
+ * @details     
+ */
+public class P587_ErectTheFence {}
