4ec86332d23e7a0380f22c8ae582515ae28fe8ca
zxwtry <zxwtry@qq.com>
2017-05-14 22:32:26 +0800

    添加 leetcode 爱奇艺_170514_B

---
+++ src/nowcoder/com/爱奇艺_170514_B.java
@@ -0,0 +1,18 @@
+package nowcoder.com;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        爱奇艺_170514_B.java
+ * @type        爱奇艺_170514_B
+ * @date        2017年5月14日 下午10:30:02
+ * @details     AC
+ */
+public class 爱奇艺_170514_B {}
