16a34d88dcad1740d51aedf6eac3e698139d7006
zxwtry <zxwtry@qq.com>
2017-05-14 22:23:18 +0800

    添加 leetcode P583_DeleteOperationForTwoStrings

---
+++ src/leetcode/P583_DeleteOperationForTwoStrings.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+    Given two words word1 and word2, find the minimum number of steps required to 
+    make word1 and word2 the same, where in each step you can delete one character in either string.
+    
+    Example 1:
+    Input: "sea", "eat"
+    Output: 2
+    Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+    Note:
+    The length of given words won't exceed 500.
+    Characters in given words can only be lower-case letters.
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P583_DeleteOperationForTwoStrings.java
+ * @type        P583_DeleteOperationForTwoStrings
+ * @date        2017年5月14日 上午10:14:54
+ * @details     
+ */
+public class P583_DeleteOperationForTwoStrings {}
