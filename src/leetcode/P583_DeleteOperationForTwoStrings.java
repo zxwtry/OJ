065dd3cfa9362e568072439ceb75c5d10daaada4
zxwtry <zxwtry@qq.com>
2017-05-16 22:27:09 +0800

    添加 leetcode P583_DeleteOperationForTwoStrings

--- src/leetcode/P583_DeleteOperationForTwoStrings.java
+++ src/leetcode/P583_DeleteOperationForTwoStrings.java
@@ -21,14 +21,10 @@ package leetcode;
  * @file        P583_DeleteOperationForTwoStrings.java
  * @type        P583_DeleteOperationForTwoStrings
  * @date        2017年5月14日 上午10:14:54
- * @details     
+ * @details     Solution: AC 76ms 23.53%
  */
 public class P583_DeleteOperationForTwoStrings {
-    public static void main(String[] args) {
-        String s1 = "abc";
-        String s2 = "adc";
-        System.out.println(new Solution().minDistance(s1, s2));
-    }
+    
     static public class Solution {
         public int minDistance(String s1, String s2) {
             int n1 = s1 == null ? 0 : s1.length();
@@ -43,31 +39,26 @@ public class P583_DeleteOperationForTwoStrings {
             int n1 = c1 == null ? 0 : c1.length;
             int n2 = c2 == null ? 0 : c2.length;
             if (n1 < n2) return editDistance(c2, c1, delete, insert, replace);
-            int[] d = new int[n2+1];
-            int[] e = new int[n2+1];
+            int[][] d = new int[2][n2+1];
             int INF = Integer.MAX_VALUE / 3;
             for (int i1 = 0; i1 <= n1; i1 ++) {
                 for (int i2 = 0; i2 <= n2; i2 ++) {
                     if (i1 == 0 && i2 == 0) continue;
-                    if (i1 == n1 && i2 == n2) {
-                        System.out.println();
-                    }
                     int min = INF;
-                    if (i1-1 >= 0) min = Math.min(min, d[i2] + delete);
-                    if (i2-1 >= 0) min = Math.min(min, e[i2-1] + insert);
+                    if (i1-1 >= 0) min = Math.min(min, d[0][i2] + delete);
+                    if (i2-1 >= 0) min = Math.min(min, d[1][i2-1] + insert);
                     if (i1-1 >= 0 && i2-1 >= 0) min = Math.min(min, 
-                            d[i2-1] + (c1[i1-1] == c2[i2-1] ? 0 : replace));
-                    e[i2] = min;
+                            d[0][i2-1] + (c1[i1-1] == c2[i2-1] ? 0 : replace));
+                    d[1][i2] = min;
                 }
-                tools.Utils.printArray(e, e.length);
-                swap(d, e);
+                swap(d);
             }
-            return d[n1];
+            return d[0][n2];
         }
-        public void swap(int[] d, int[] e) {
-            int[] t = d;
-            d = e;
-            e = t;
+        public void swap(int[][] d) {
+            int[] t = d[0];
+            d[0] = d[1];
+            d[1] = t;
         }
     }
 }
