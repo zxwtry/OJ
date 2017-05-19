73622c004d68cbe58396a2a5df31c2b15a8a5ca9
zxwtry <zxwtry@qq.com>
2017-05-19 22:30:34 +0800

    添加 template EditDistSep

---
+++ src/template/EditDistSep.java
@@ -0,0 +1,17 @@
+package template;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     template
+ * @file        EditDistSep.java
+ * @type        EditDistSep
+ * @date        2017年5月19日 下午10:30:08
+ * @details     
+ */
+public class EditDistSep {}
