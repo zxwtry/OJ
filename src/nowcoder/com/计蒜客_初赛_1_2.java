5386b6a97f2ec56b161bc7c7296dd4da396bc1b6
zxwtry <zxwtry@qq.com>
2017-05-20 21:41:24 +0800

    添加 leetcode com 计蒜客_初赛_1_2 AC

--- src/nowcoder/com/计蒜客_初赛_1_2.java
+++ src/nowcoder/com/计蒜客_初赛_1_2.java
@@ -130,4 +130,93 @@ ACTG
 
 import java.util.Scanner;
 
-public class 计蒜客_初赛_1_2 {}
+public class 计蒜客_初赛_1_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int L = sc.nextInt();
+        int R = sc.nextInt();
+        char[] t = sc.next().toCharArray();
+        char[] s = new char[n];
+        int pw = 0, w = 0;
+        for (int i = 0; i < n; i ++) {
+            if (i == 0) {
+                w = b;
+                pw = w;
+            } else {
+                w = (pw + a) % n;
+                pw = w;
+            }
+            boolean bl = w < L;
+            boolean br = w > R;
+            boolean bw = w % 2 == 0;
+            if ((! bl) && (! br) ) {
+                if (bw) {
+                    s[i] = 'A';
+                } else {
+                    s[i] = 'T';
+                }
+            } else {
+                if (bw) {
+                    s[i] = 'G';
+                } else {
+                    s[i] = 'C';
+                }
+            }
+        }
+        
+//        System.out.println(new String(s));
+//        System.out.println(t);
+//        System.out.println(t.length);
+        System.out.println(kmp(s, t, n, t.length));
+//        char[] uu = "ABCABC".toCharArray();
+//        char[] vv = "ABCABCABCABCABCABCABCABC".toCharArray();
+//        System.out.println(uu.length);
+//        System.out.println(vv.length);
+//        
+//        System.out.println(kmp(vv, uu, vv.length, uu.length));
+        
+        
+        sc.close();
+    }
+    
+    private static int kmp(char[] s, char[] p, int sn, int pn) {
+        if (pn == 0) return 0;
+        if (sn == 0) return -1;
+        int[] next = getNext(p, pn);
+//        tools.Utils.printArray(next, next.length);
+        int pi = 0, si = 0;
+        int ans = 0;
+        while (si < sn)
+            if (s[si] == p[pi]) {
+                si ++;
+                pi ++;
+                if (pi == pn) {
+                    ans ++;
+                    pi = next[pn - 1];
+                    si --;
+                }
+            } else if (next[pi] == -1) {
+                si ++;
+            } else pi = next[pi];
+        return ans;
+    }
+    private static int[] getNext(char[] p, int pn) {
+        //return short p
+        if (pn < 2) return new int[] {-1};
+        int[] next = new int[pn];
+        next[0] = -1;
+        next[1] = 0;
+        int fi = 2, bi = 0;
+        while (fi < pn)
+            if (p[fi - 1] == p[bi]) {
+                next[fi ++] = ++ bi;
+            } else if (bi <= 0) {
+                next[fi ++] = 0;
+            } else bi = next[bi];;
+        return next;
+    }
+}
