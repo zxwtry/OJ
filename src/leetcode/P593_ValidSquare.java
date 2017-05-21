e7369ede6f8c969df11a191d00719f292b249722
zxwtry <zxwtry@qq.com>
2017-05-21 11:58:46 +0800

    添加 leetcode P593_ValidSquare Solution AC

--- src/leetcode/P593_ValidSquare.java
+++ src/leetcode/P593_ValidSquare.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 /**
 Given the coordinates of four points in 2D space, return whether the four points could construct a square.
 
@@ -27,9 +30,32 @@ Input points have no order.
  * @details     
  */
 public class P593_ValidSquare {
+    public static void main(String[] args) {
+        int[] p1 = {1134,-2539};
+        int[] p2 = {492,-1255};
+        int[] p3 = {-792,-1897};
+        int[] p4 = {-150,-3181};
+        System.out.println(new Solution().validSquare(p1, p2, p3, p4));
+    }
     static public class Solution {
         public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-            
+            HashSet<Long> set = new HashSet<>();
+            int[][] p = new int[] [] {p1, p2, p3, p4};
+            for (int i = 0; i < 4; i ++) {
+                for (int j = i+1; j < 4; j ++) {
+                    set.add(calc(p[i], p[j]));
+                }
+            }
+            if (set.size() != 2) return false;
+            Iterator<Long> it = set.iterator();
+            long v1 = it.next();
+            long v2 = it.next();
+            long min = Math.min(v1, v2);
+            long max = Math.max(v1, v2);
+            return min + min == max;
+        }
+        long calc(int[] p1, int[] p2) {
+            return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
         }
     }
 }
