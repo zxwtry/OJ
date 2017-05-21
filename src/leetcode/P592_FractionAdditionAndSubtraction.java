9181671f30d343ee9741b38e3aac676df24693e6
zxwtry <zxwtry@qq.com>
2017-05-21 11:58:26 +0800

    添加 leetcode P592_FractionAdditionAndSubtraction Solution AC

--- src/leetcode/P592_FractionAdditionAndSubtraction.java
+++ src/leetcode/P592_FractionAdditionAndSubtraction.java
@@ -47,4 +47,89 @@ range of 32-bit int.
  * @date        2017年5月21日 上午10:31:37
  * @details     Solution: AC
  */
-public class P592_FractionAdditionAndSubtraction {}
+public class P592_FractionAdditionAndSubtraction {
+    public static void main(String[] args) {
+        String e = "-1/2-1/2-1/3";
+        e = "-6/1";
+        System.out.println(new Solution().fractionAddition(e));
+//        System.out.println(new Solution().minMultiple(-6, 8));
+    }
+    static public class Solution {
+        public String fractionAddition(String e) {
+            return plus(e);
+        }
+        
+        public String plus(String e) {
+            if (e.length() == 0) {
+                return "0/1";
+            }
+            ArrayList<String> es = new ArrayList<>();
+            ArrayList<Boolean> b = new ArrayList<>();
+            int p = (e.charAt(0) == '-' || e.charAt(0) == '+') ? 1 : 0;
+            if (e.charAt(0) == '-' || e.charAt(0) == '+') {
+                es.add("");
+                b.add(e.charAt(0) == '+');
+            }
+            for (int i = (e.charAt(0) == '-' || e.charAt(0) == '+') ? 1 : 0; i <= e.length(); i ++) {
+                char c = i == e.length() ? '+' : e.charAt(i);
+                if (c == '-' || c == '+') {
+                    es.add(e.substring(p, i));
+                    p = i + 1;
+                    b.add(c == '+');
+                }
+            }
+            
+//            System.out.println(es);
+//            System.out.println(b);
+            int[][] m = new int[es.size()][];
+            for (int i = 0; i < es.size(); i ++) {
+                String[] ps = es.get(i).split("/");
+                if (ps.length != 2) {
+                    m[i] = new int[] {0, 1};
+                } else {
+                    m[i] = new int[] {Integer.parseInt(ps[0]), Integer.parseInt(ps[1])};
+                }
+            }
+            long minM = 1;
+            for (int i = 0; i < es.size(); i ++) {
+                minM = minMultiple(minM, m[i][1]);
+            }
+            long calc = 0;
+            for (int i = 0; i < es.size(); i ++) {
+                if (i == 0 || b.get(i-1)) {
+                    calc += (minM* m[i][0]) / m[i][1];
+                } else {
+                    calc -= (minM* m[i][0]) / m[i][1];
+                }
+//                System.out.println(i + "..." + calc);
+            }
+            long gys = gcd(Math.abs(minM), Math.abs(calc));
+            return String.valueOf((int)(calc / gys)) + "/"+ String.valueOf( (int)(minM / gys));
+        }
+        
+        long minMultiple(long a, long b) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            if (a < b) return minMultiple(b, a);
+            
+            long r = a, s = a, t = b;
+            while (r != 0) {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            return s * t / a;
+        }
+        public long gcd(long a, long b) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            if (a > b) return gcd(b, a);
+            while(b != 0) {
+                 long temp = a%b;
+                 a = b;
+                 b = temp;
+            }
+            return a;
+        }
+    }
+}
