19c29d477fea7c9e6f6376a0b1f18ff28bc336bc
zxwtry <zxwtry@qq.com>
2017-05-21 10:22:10 +0800

    添加 leetcode P593_ValidSquare

---
+++ src/leetcode/P593_ValidSquare.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+Given the coordinates of four points in 2D space, return whether the four points could construct a square.
+
+The coordinate (x,y) of a point is represented by an integer array with two integers.
+
+Example:
+Input: p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,1]
+Output: True
+Note:
+
+All the input integers are in the range [-10000, 10000].
+A valid square has four equal sides with positive length and four equal angles (90-degree angles).
+Input points have no order.
+ */
+
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P593_ValidSquare.java
+ * @type        P593_ValidSquare
+ * @date        2017年5月21日 上午10:21:26
+ * @details     
+ */
+public class P593_ValidSquare {
+    static public class Solution {
+        public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+            
+        }
+    }
+}
