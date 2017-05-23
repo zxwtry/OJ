37c1e75cd6dab051fff03c2f03320583df46e087
zxwtry <zxwtry@qq.com>
2017-05-23 23:44:46 +0800

    添加 hiho 训练17_题目3_逆序对

--- src/hiho/训练17_题目3_逆序对.java
+++ src/hiho/训练17_题目3_逆序对.java
@@ -48,4 +48,185 @@ import java.util.Scanner;
 
 import com.sun.swing.internal.plaf.basic.resources.basic;
 
-public class 训练17_题目3_逆序对 {}
+public class 训练17_题目3_逆序对 {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+//        StringBuilder st = new StringBuilder();
+//        for (int index = 0; index <= Character.MAX_VALUE; index ++) {
+//            st.append((char)index);
+//        }
+//        System.out.println(st.toString().getBytes("UTF-8").length);
+        
+//        System.out.println(1 << 14);
+//        
+//        System.out.println(3 << 14 < 1 << 16);
+        
+//        byte b = -1;
+//        int v = b & 0xff;
+//        System.out.println(v);
+        
+//        int v = 0xd811;
+//        char c = (char) v;
+//        System.out.println(Integer.toHexString((int)v));
+        
+//        System.out.println(0xffff);
+        
+//        int val = 0xdcba;
+//        System.out.println((val >> 0) & 0xf);
+//        System.out.println((val >> 4) & 0xf);
+//        System.out.println((val >> 8) & 0xf);
+//        System.out.println((val >> 12) & 0xf);
+        
+//        int v = 127;
+//        v = v << 7;
+//        v += 127;
+//        v = v << 7;
+//        v += 127;
+//        System.out.println(Integer.toHexString(v));
+//        System.out.println(v);
+        
+//        int v = 255;
+//        byte b = (byte) (v & 0xff);
+//        System.out.println(b);
+        
+        
+//        int v = 0x12345657;
+//        System.out.println(Integer.toHexString((v ) & 0xff));
+//        System.out.println(Integer.toHexString((v >> 8) & 0xff));
+//        System.out.println(Integer.toHexString((v >> 16) & 0xff));
+        
+//        int v = 1;
+//        System.out.println(v << 1);
+        
+        int v = 0;
+        v = (v << 8) + 255;
+        v = (v << 8) + 255;
+        v = (v << 8) + 255;
+        v = (v << 8) + 255;
+        System.out.println(v);
+        
+//        int v = 0xffffff;
+//        System.out.println(v);
+        
+        
+        int ssssssss = 1;
+        if (ssssssss == 1) return;
+        
+        
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int[] a = new int[n];
+        int max = 0;
+        long ans = 0;
+        
+        
+        for (int i = 0; i < n; i ++) 
+            a[i] = sc.nextInt();
+        
+        System.out.println(InversePairs(a));
+//            a[i] =sc.nextInt();
+        
+        sc.close();
+        
+        
+    }
+    
+    public static long InversePairs(int [] array) {
+        if(array == null || array.length == 0) return 0;
+        int[] hash = new int[200000];
+        int i = array.length-1;
+        long ans = 0;
+        for(; i>= 0; i--){
+            hash[array[i]] ++;
+            ans += countInversePairs(hash, array[i]);
+        }
+        return ans;
+    }
+    
+    private static long countInversePairs(int[] hash, int target){
+        long count = 0;
+        for(int i = 0; i < target; i++){
+            count += hash[i];
+        }
+        return count;
+    }
+
+/**
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+long a[100001],t[100001];
+long long ans=0;
+
+void mergesort(long l,long r)
+{
+    if (l==r)
+        return ;
+    long mid,x,y,z,i;
+    mid=(l+r)/2;
+    mergesort(l,mid);
+    mergesort(mid+1,r);
+    for (i=l;i<=r;i++)
+        t[i]=a[i];
+    x=l;
+    y=mid+1;
+    z=l;
+    while (x<=mid && y<=r)
+    {
+        //1~n not the same
+        if (t[x]<t[y])
+        {
+            a[z]=t[x];
+            x++;
+            //a[x] > a[mid+1]~a[y-1]
+            ans+=(y-mid-1);
+        }
+        else
+        {
+
+            a[z]=t[y];
+            y++;
+        }
+        z++;
+    }
+    if (x<=mid)
+    {
+        //a[x] > a[mid+1]~a[r]
+        ans+=(mid-x+1)*(r-mid);
+        while (z<=r)
+        {
+            a[z]=t[x];
+            x++;
+            z++;
+        }
+    }
+    else
+    {
+        while (z<=r)
+        {
+            a[z]=t[y];
+            y++;
+            z++;
+        }
+    }
+}
+
+int main()
+{
+    long n,i;
+    scanf("%ld",&n);
+    for (i=1;i<=n;i++)
+        scanf("%ld",&a[i]);
+    mergesort(1,n);
+    printf("%lld\n",ans);
+    return 0;
+}
+
+
+ */
+    
+}
