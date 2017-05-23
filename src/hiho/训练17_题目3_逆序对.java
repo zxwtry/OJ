cda9a295675674528074f863de597f5a5b9f4f0e
zxwtry <zxwtry@qq.com>
2017-05-23 23:44:31 +0800

    添加 hiho 训练17_题目3_逆序对

---
+++ src/hiho/训练17_题目3_逆序对.java
@@ -0,0 +1,51 @@
+package hiho;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+
+时间限制:10000ms
+单点时限:1000ms
+内存限制:256MB
+描述
+给定一个1-N的排列A1, A2, ... AN，每次操作小Hi可以选择一个数，把它放到数组的最左边。
+
+请计算小Hi最少进行几次操作就能使得新数组是递增排列的。
+
+输入
+第一行包含一个整数N。
+
+第二行包含N个两两不同整数A1, A2, ... AN。(1 <= Ai <= N)
+
+对于60%的数据 1 <= N <= 20
+
+对于100%的数据 1 <= N <= 100000
+
+输出
+一个整数代表答案
+
+样例输入
+5
+2 3 1 4 5
+样例输出
+1
+
+ */
+
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     hiho
+ * @file        训练17_题目1_F1Score.java
+ * @type        训练17_题目1_F1Score
+ * @date        2017年5月21日 下午12:00:03
+ * @details     AC 90%
+ */
+
+import java.util.Scanner;
+
+import com.sun.swing.internal.plaf.basic.resources.basic;
+
+public class 训练17_题目3_逆序对 {}
