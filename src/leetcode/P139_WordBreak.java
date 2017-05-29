384dba8a115917a626d3202b9329b38529c0685f
zxwtry <zxwtry@qq.com>
2017-05-29 16:22:27 +0800

    添加 leetcode P139_WordBreak Solution1 AC

--- src/leetcode/P139_WordBreak.java
+++ src/leetcode/P139_WordBreak.java
@@ -1,7 +1,7 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Set;
+ import java.util.HashMap;
+import java.util.List;
 
 /*
     Given a string s and a dictionary of words dict, 
@@ -15,45 +15,37 @@ import java.util.Set;
 	Return true because "leetcode" can be segmented as "leet code".
  */
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P139_WordBreak.java
+ * @date        2017年5月29日 上午10:20:42
+ * @details     Solution1: AC 18ms 41.08%
+ */
 public class P139_WordBreak {
-	/*
-	 * 	14.05%
-	 * 	16 ms
-	 */
-	static class Solution {
-		HashMap<String, Boolean> map = new HashMap<>();
-	    public boolean wordBreak(String s, Set<String> wordDict) {
-	    	if (s == null || s.length() == 0 || wordDict.size() == 0) {
-	    		return false;
-	    	}
-	    	char[] cs = s.toCharArray();
-	    	for (String str : wordDict) {
-	    		map.put(str, true);
-	    	}
-	    	return wordBreak(cs, 0, cs.length - 1, wordDict);
-	    }
-		private boolean wordBreak(char[] cs, int i, int j, Set<String> wordDict) {
-			if (i == cs.length) {
-				return true;
-			}
-			String all = new String(cs, i, j + 1 - i);
-			if (map.containsKey(all)) {
-				if (! map.get(all)) {
-					return false;
-				}
-			}
-			boolean isTrue = false;
-			for (int k = i; k <= j; k ++) {
-				String this_part = new String(cs, i, k + 1 - i);
-				if (wordDict.contains(this_part) || ((map.containsKey(this_part) && map.get(this_part)))) {
-					isTrue |= wordBreak(cs, k + 1, j, wordDict);
-				}
-				if (isTrue) {
-					break;
-				}
-			}
-			map.put(new String(cs, i, j + 1 - i), isTrue);
-			return isTrue;
-		}
-	}
+    static class Solution1 {
+        public boolean wordBreak(String s, List<String> wd) {
+            int sn = s == null ? 0 : s.length();
+            HashMap<String, Boolean> nm = new HashMap<>(sn);
+            for (String w : wd) nm.put(w, true);
+            return search(s, 0, sn, nm);
+        }
+        private boolean search(String s, int si, int sn, HashMap<String, Boolean> nm) {
+            if (si == sn) return true;
+            String res = s.substring(si, sn);
+            Boolean resVal = nm.get(res);
+            if (resVal != null && (!resVal)) return false;
+            boolean ans = false;
+            for (int i = si+1; (!ans) && i <= sn; i ++) {
+                String n = s.substring(si, i);
+                Boolean nVal = nm.get(n);
+                if (nVal != null && nVal)
+                    ans |= search(s, i, sn, nm);
+            }
+            nm.put(res, ans);
+            return ans;
+        }
+    }
 }
