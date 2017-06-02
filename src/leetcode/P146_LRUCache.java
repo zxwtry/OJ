b853de3be4ca01093172e666de1c0b15b543b526
zxwtry <zxwtry@qq.com>
2017-06-02 20:55:52 +0800

    添加 leetcode P146_LRUCache

--- src/leetcode/P146_LRUCache.java
+++ src/leetcode/P146_LRUCache.java
@@ -103,4 +103,179 @@ public class P146_LRUCache {
             }
 	    }
 	}
+	static class LRUCache3 {
+	    static class Node {
+	        Node pre, nxt; 
+	        int val;
+	        public Node(int val) {
+	            this.val = val;
+	        }
+	    }
+	    int cap, size;
+	    Node head, tail;
+	    HashMap<Integer, Node> map;
+	    public LRUCache3(int capacity) {
+	        this.cap  = capacity;
+	        this.size = 0;
+	        this.head = null;
+	        this.tail = null;
+	        this.map  = new HashMap<>();
+	    }
+	    public int get(int key) {
+	        Node nodeVal = map.get(key);
+	        if (nodeVal == null) return -1;
+	        moveNodeToHead(nodeVal);
+	        return nodeVal.val;
+	    }
+	    private void moveNodeToHead(Node nodeVal) {
+            Node nodePre = nodeVal.pre;
+            Node nodeNxt = nodeVal.nxt;
+            if (nodePre != null) nodePre.nxt = nodeNxt;
+            if (nodeNxt != null) nodeNxt.pre = nodePre;
+            if (nodeVal == tail && head != tail) tail = nodePre;
+            if (size == cap + 1 && tail != null) {
+                if (tail.pre != null)   tail = tail.pre;
+                tail.nxt = null;
+                size --;
+            }
+            if (nodeVal != head) {
+                nodeVal.nxt = head;
+                head = nodeVal;
+            }
+	    }
+ 	    public void put(int key, int val) {
+ 	        Node nodeVal = map.get(key);
+ 	        if (nodeVal == null) {
+ 	            size ++;
+ 	            nodeVal = new Node(val);
+ 	            map.put(key, nodeVal);
+ 	            
+ 	        } else {
+ 	            
+ 	        }
+ 	    }
+	}
+
+	static public class LRU<E> {
+	    private int size;
+	    private int occupiedBlocks = 0;
+	    private Node<E> leastRecentlyUsedElement;
+	    private Node<E> mostRecentlyUsedElement;
+	    private Map<E, Node<E>> cache = new HashMap<E, Node<E>>();
+	    public LRU(int size) {
+	        this.size = size;
+	    }
+
+	    public void add(E element) {
+	        if (leastRecentlyUsedElement == null) {
+	            Node<E> node = createNode(element);
+	            leastRecentlyUsedElement = node;
+	            mostRecentlyUsedElement = node;
+	            cache.put(element, node);
+	            occupiedBlocks++;
+	        } else {
+	            if (cache.containsKey(element)) {
+	                Node<E> elementNode = cache.get(element);
+	                addElementToFront(elementNode);
+	            } else {
+	                Node<E> node = createNode(element, mostRecentlyUsedElement, null);
+	                if (occupiedBlocks >= size) {
+	                    cache.remove(leastRecentlyUsedElement.value);
+	                    leastRecentlyUsedElement = leastRecentlyUsedElement.next;
+	                    leastRecentlyUsedElement.prev = null;
+	                    occupiedBlocks--;
+	                }
+	                mostRecentlyUsedElement.next = node;
+	                mostRecentlyUsedElement = node;
+	                cache.put(element, node);
+	                occupiedBlocks++;
+	            }
+	        }
+	    }
+
+	    private void addElementToFront(Node<E> elementNode) {
+	        if (elementNode.next != null) {
+	            if (elementNode.prev == null) {
+	                mostRecentlyUsedElement.next = leastRecentlyUsedElement;
+	                leastRecentlyUsedElement = leastRecentlyUsedElement.next;
+	                mostRecentlyUsedElement.next = null;
+	            } else {
+	                elementNode.prev.next = elementNode.next;
+	                elementNode.next.prev = elementNode.prev;
+	                mostRecentlyUsedElement.next = elementNode;
+	                elementNode.prev = mostRecentlyUsedElement;
+	                mostRecentlyUsedElement = mostRecentlyUsedElement.next;
+	            }
+	        }
+	    }
+
+	    private LRU<E>.Node<E> createNode(E element, Node<E> prev, Node<E> next) {
+	        Node<E> node = new Node<E>(element, prev, next);
+	        return node;
+	    }
+
+	    private Node<E> createNode(E element) {
+	        return createNode(element, null, null);
+	    }
+
+	    public void printCache() {
+	        Node<E> temp = mostRecentlyUsedElement;
+	        System.out.println("==============");
+	        do {
+	            System.out.println(temp.value);
+	            temp = temp.prev;
+	        } while (temp != null);
+	        System.out.println("==============");
+	    }
+
+	    @SuppressWarnings("hiding")
+        public class Node<E> {
+	        private Node<E> next;
+	        private Node<E> prev;
+	        private E value;
+
+	        public Node(E element, Node<E> prev, Node<E> next) {
+	            this.value = element;
+	            this.prev = prev;
+	            this.next = next;
+	        }
+
+	        public Node<E> getNext() {
+	            return next;
+	        }
+
+	        public void setNext(Node<E> next) {
+	            this.next = next;
+	        }
+
+	        public Node<E> getPrev() {
+	            return prev;
+	        }
+
+	        public void setPrev(Node<E> prev) {
+	            this.prev = prev;
+	        }
+
+	        public E getValue() {
+	            return value;
+	        }
+
+	        public void setValue(E value) {
+	            this.value = value;
+	        }
+
+	        @Override
+	        public String toString() {
+	            return "Node [next=" + next + ", prev=" + prev + ", value=" + value + "]";
+	        }
+
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "LRU [size=" + size + ", occupiedBlocks=" + occupiedBlocks + ", leastRecentlyUsedElement=" + leastRecentlyUsedElement + ", mostRecentlyUsedElement=" + mostRecentlyUsedElement
+	                + ", cache=" + cache + "]";
+	    }
+
+	}
 }
