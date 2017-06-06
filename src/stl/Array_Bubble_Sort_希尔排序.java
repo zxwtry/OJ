f2f4afab98fa7d9e97e1b8d3679f1c256a4aa4c0
zxwtry <zxwtry@qq.com>
2017-06-06 22:14:43 +0800

    添加 stl Array_Bubble_Sort_希尔排序

--- src/stl/Array_Bubble_Sort_希尔排序.java
+++ src/stl/Array_Bubble_Sort_希尔排序.java
@@ -1,16 +1,19 @@
 package stl;
 
+import tools.ListNode辅助.ListNode;
+
 public class Array_Bubble_Sort_希尔排序 {
     public static void main(String[] args) {
         int len = 50;
         int[] arr = tools.Random随机生成器.A_生成一个不重复随机数据(len, 0, len-1);
-        help = new int[arr.length];
+//        help = new int[arr.length];
         shellSort(arr, 0, arr.length - 1);
         tools.Utils.printArray(arr, 10, 3);
     }
     static int[] help = null;
     //[i, j]
     public static void shellSort(int[] arr, int i, int j) {
+        if (help == null) help = new int[arr.length];
         if (i == j) return;
         if (i > j) return;
         if (i == j-1) {
@@ -31,5 +34,8 @@ public class Array_Bubble_Sort_希尔排序 {
         }
         if (p <= m) System.arraycopy(help, p, arr, t, m-p+1);
         if (q <= j) System.arraycopy(help, q, arr, t, j-q+1);
+    }
+    public static void shellSort(ListNode head) {
+        
     }
 }
