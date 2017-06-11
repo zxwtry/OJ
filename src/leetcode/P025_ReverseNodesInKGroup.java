e54fc09f8924e787634dce58762723d07e6152e3
zxwtry <zxwtry@qq.com>
2017-06-11 22:21:46 +0800

    添加 leetcode P025_ReverseNodesInKGroup

--- src/leetcode/P025_ReverseNodesInKGroup.java
+++ src/leetcode/P025_ReverseNodesInKGroup.java
@@ -26,11 +26,6 @@ package leetcode;
 import tools.ListNode辅助.ListNode;;
 
 public class P025_ReverseNodesInKGroup {
-	public static void main(String[] args) {
-		ListNode input = tools.ListNode辅助.A_一维生成器(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17});
-//		ListNode input = tools.ListNode辅助.A_一维生成器(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
-		tools.ListNode辅助.B_打印链表(new Solution1().reverseKGroup(input, 2));
-	}
 	/*
 	 * 	花了很长时间
 	 * 	代码写得非常乱，今后需要整理一下
