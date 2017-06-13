1cc9d203c6f0f26c6c410d45ab85e6299e0632be
zxwtry <zxwtry@qq.com>
2017-06-13 21:56:11 +0800

    添加 leetcode P604_DesignCompressedStringIterator

---
+++ src/leetcode/P604_DesignCompressedStringIterator.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
+
+/**
+
+Design and implement a data structure for a compressed string iterator. 
+It should support the following operations: next and hasNext.
+
+The given compressed string will be in the form of each letter followed 
+by a positive integer representing the number of this letter existing 
+in the original uncompressed string.
+
+next() - if the original string still has uncompressed characters, 
+return the next letter; Otherwise return a white space.
+hasNext() - Judge whether there is any letter needs to be uncompressed.
+
+Note:
+Please remember to RESET your class variables declared in StringIterator, 
+as static/class variables are persisted across multiple test cases. 
+Please see here for more details.
+
+Example:
+
+StringIterator iterator = new StringIterator("L1e2t1C1o1d1e1");
+
+iterator.next(); // return 'L'
+iterator.next(); // return 'e'
+iterator.next(); // return 'e'
+iterator.next(); // return 't'
+iterator.next(); // return 'C'
+iterator.next(); // return 'o'
+iterator.next(); // return 'd'
+iterator.hasNext(); // return true
+iterator.next(); // return 'e'
+iterator.hasNext(); // return false
+iterator.next(); // return ' '
+
+ */
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P604_DesignCompressedStringIterator.java
+ * @date        2017年6月13日 下午7:35:50
+ * @details     Solution: AC 201ms 5.43%
+ */
+public class P604_DesignCompressedStringIterator {}
