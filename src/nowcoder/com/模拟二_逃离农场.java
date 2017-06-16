8d979ea8fa49eeb4e39a12670e5e3c1ff6a1c882
zxwtry <zxwtry@qq.com>
2017-06-16 22:32:03 +0800

    添加 leetcode 模拟二_随机的机器人

---
+++ src/nowcoder/com/模拟二_逃离农场.java
@@ -0,0 +1,16 @@
+package nowcoder.com;
+
+import java.util.Scanner;
+
+public class 模拟二_逃离农场 {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+//    while (in.hasNextInt()) {//注意while处理多个case
+//        int a = in.nextInt();
+//        int b = in.nextInt();
+//        System.out.println(a + b);
+//    }
+        int n = in.nextInt();
+        int k = in.nextInt();
+    }
+}
