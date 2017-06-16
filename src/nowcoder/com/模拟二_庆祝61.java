6c04733f71f77f5c82add62d000143dcc3aef733
zxwtry <zxwtry@qq.com>
2017-06-16 21:15:41 +0800

    添加 leetcode 模拟二_庆祝61

---
+++ src/nowcoder/com/模拟二_庆祝61.java
@@ -0,0 +1,15 @@
+package nowcoder.com;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        模拟二_庆祝61.java
+ * @date        2017年6月16日 下午9:15:00
+ * @details     
+ */
+public class 模拟二_庆祝61 {}
