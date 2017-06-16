5deca14a9d7627ec47e0abc8375bf84fb013858e
zxwtry <zxwtry@qq.com>
2017-06-16 22:31:49 +0800

    添加 leetcode 模拟二_庆祝61

--- src/nowcoder/com/模拟二_庆祝61.java
+++ src/nowcoder/com/模拟二_庆祝61.java
@@ -10,6 +10,45 @@ import java.util.Scanner;
  * @package     nowcoder.com
  * @file        模拟二_庆祝61.java
  * @date        2017年6月16日 下午9:15:00
- * @details     
+ * @details     AC
  */
-public class 模拟二_庆祝61 {}
+public class 模拟二_庆祝61 {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNextInt()) {//注意while处理多个case
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            System.out.println(a + b);
+//        }
+//    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        while (in.hasNextInt()) {//注意while处理多个case
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            System.out.println(a + b);
+//        }
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i ++) {
+            arr[i] = in.nextInt();
+        }
+        int[] cpy = new int[n];
+        Arrays.sort(arr);
+        int ci = 0;
+        for (int i = 0; i < n; i += 2) {
+            cpy[ci ++] = arr[i];
+        }
+        ci = n-1;
+        for (int i = 1; i < n; i += 2) {
+            cpy[ci --] = arr[i];
+        }
+        int ans = 0;
+        for (int i = 1; i < n; i ++) {
+            ans = Math.max(Math.abs(cpy[i] - cpy[i-1]), ans);
+        }
+        ans = Math.max(Math.abs(cpy[n-1] - cpy[0]), ans);
+        System.out.println(ans);
+        in.close();
+    }
+}
