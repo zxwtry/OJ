4c5b45a1d6f951b058f747bb5547ca0066672417
zxwtry <zxwtry@qq.com>
2017-06-17 22:22:23 +0800

    添加 nowcoder com 模拟二_逃离农场

--- src/nowcoder/com/模拟二_逃离农场.java
+++ src/nowcoder/com/模拟二_逃离农场.java
@@ -2,6 +2,15 @@ package nowcoder.com;
 
 import java.util.Scanner;
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        模拟二_逃离农场.java
+ * @date        2017年6月17日 下午10:21:53
+ * @details     
+ */
 public class 模拟二_逃离农场 {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
