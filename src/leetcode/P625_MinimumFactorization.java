9eee3897d828cb4167c8a95533fdeee250ceb285
zxwtry <zxwtry@qq.com>
2017-06-18 22:23:33 +0800

    添加 leetcode P625_MinimumFactorization AC

--- src/leetcode/P625_MinimumFactorization.java
+++ src/leetcode/P625_MinimumFactorization.java
@@ -31,4 +31,34 @@ Output:
  * @date        2017年6月18日 上午10:28:25
  * @details     AC
  */
-public class P625_MinimumFactorization {}
+public class P625_MinimumFactorization {
+    static public class Solution {
+        public int smallestFactorization(int a) {
+            if (a <= 0) return 0;
+            if (a == 1) return 1;
+            int facLength = 64;
+            int[] fac = new int[facLength];
+            int facIndex = 0;
+            int acpy = a;
+            while (acpy != 1) {
+                int preAcpy = acpy;
+                for (int facValue = 9; facValue > 1; facValue --) {
+                    if (acpy % facValue == 0) {
+                        acpy /= facValue;
+                        fac[facIndex ++] = facValue;
+                        break;
+                    }
+                }
+                if (preAcpy == acpy) break;
+            }
+            if (acpy != 1) return 0;
+            long ans = 0;
+            for (int facRealIndex = facIndex - 1; facRealIndex > -1; facRealIndex --) {
+                ans = ans * 10 + fac[facRealIndex];
+            }
+            if (ans > Integer.MAX_VALUE || ans < 0) return 0;
+            return (int)ans;
+        }
+    }
+    
+}
