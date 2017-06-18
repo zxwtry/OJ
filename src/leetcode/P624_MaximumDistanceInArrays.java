a50c97545622acf79c5ce5a3dba8486183f38e62
zxwtry <zxwtry@qq.com>
2017-06-18 09:55:48 +0800

    添加 leetcode P624_MaximumDistanceInArrays

--- src/leetcode/P624_MaximumDistanceInArrays.java
+++ src/leetcode/P624_MaximumDistanceInArrays.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
 
 Given m arrays, and each array is sorted in ascending order. Now you can pick up two 
@@ -30,8 +33,28 @@ The integers in the m arrays will be in the range of [-10000, 10000].
  * @package     leetcode
  * @file        P624_MaximumDistanceInArrays.java
  * @date        2017年6月18日 上午9:31:42
- * @details     
+ * @details     AC
  */
 public class P624_MaximumDistanceInArrays {
-
+    static public class Solution {
+        public int maxDistance(int[][] arrays) {
+            if (arrays == null || arrays.length == 0) return 0;
+            Arrays.sort(arrays, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return o1[0] - o2[0];
+                }
+            });
+            int ans = 0;
+            int minCut = arrays[0][0];
+            for (int arraysIndex = 1; arraysIndex < arrays.length; arraysIndex ++) {
+                ans = Math.max(arrays[arraysIndex][arrays[arraysIndex].length - 1] - minCut, ans);
+            }
+            int maxCut = arrays[0][arrays[0].length - 1];
+            for (int arraysIndex = 1; arraysIndex < arrays.length; arraysIndex ++) {
+                ans = Math.max(maxCut - arrays[arraysIndex][0], ans);
+            }
+            return ans;
+        }
+    }
 }
