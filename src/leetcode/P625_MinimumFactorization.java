0a5fdba39be49d604bd3f557e297403f706f7029
zxwtry <zxwtry@qq.com>
2017-06-22 22:34:28 +0800

    添加 leetcode P625_MinimumFactorization

--- src/leetcode/P625_MinimumFactorization.java
+++ src/leetcode/P625_MinimumFactorization.java
@@ -5,6 +5,8 @@ package leetcode;
 Given a positive integer a, find the smallest positive integer b whose 
 multiplication of each digit equals to a.
 
+
+
 If there is no answer or the answer is not fit in 32-bit signed integer, then return 0.
 
 Example 1
