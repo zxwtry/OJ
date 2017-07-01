f96a32db220b137219867f5b906cf979e3bd772c
zxwtry <zxwtry@qq.com>
2017-07-01 21:43:05 +0800

    添加 nowcoder leetcode 知识迁移能力_二叉树深度

--- src/nowcoder/leetcode/知识迁移能力_二叉树深度.java
+++ src/nowcoder/leetcode/知识迁移能力_二叉树深度.java
@@ -1,5 +1,35 @@
 package nowcoder.leetcode;
 
-public class 知识迁移能力_二叉树深度 {
+import java.util.LinkedList;
 
+import tools.TreeNode辅助.TreeNode;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.leetcode
+ * @file        知识迁移能力_二叉树深度.java
+ * @date        2017年7月1日 下午9:42:19
+ * @details     剑指Offer
+ */
+public class 知识迁移能力_二叉树深度 {
+    static public class Solution {
+        public int TreeDepth(TreeNode root) {
+            int len = 0;
+            if (root == null) return len;
+            LinkedList<TreeNode> ll = new LinkedList<>();
+            ll.add(root);
+            while (ll.size() != 0) {
+                int size = ll.size();
+                for (int i = 0; i < size; i ++) {
+                    TreeNode now = ll.poll();
+                    if (now.left != null) ll.add(now.left);
+                    if (now.right != null) ll.add(now.right);
+                }
+                len ++;
+            }
+            return len;
+        }
+    }
 }
