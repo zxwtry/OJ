751eccd4b45f002fb1d121b356e1b8ebadcdc817
zxwtry <zxwtry@qq.com>
2017-07-02 09:28:47 +0800

    添加 nowcoder leetcode 知识迁移能力_孩子们的游戏约瑟夫环

--- src/nowcoder/leetcode/知识迁移能力_孩子们的游戏约瑟夫环.java
+++ src/nowcoder/leetcode/知识迁移能力_孩子们的游戏约瑟夫环.java
@@ -13,11 +13,21 @@ public class 知识迁移能力_孩子们的游戏约瑟夫环 {
     static public class Solution {
         public int LastRemaining_Solution(int n, int m) {
             if (n == 0) return -1;
-            return get(n, m) - 1;
+            return realGet(n, m) - 1;
         }
-        int get(int l, int m) {
-            if (l == 1) return 1;
-            return (get(l - 1, m) + m - 1) % l + 1;
+        int get(int n, int m) {
+            if (n == 1) return 1;
+            return (get(n - 1, m) + m - 1) % n + 1;
+        }
+        int realGet(int n, int m) {
+            if (n <= 0) return 0;
+            int[] arr = new int[n + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+            for (int i = 2; i <= n; i ++) {
+                arr[i] = (arr[i - 1] + m - 1) % i + 1;
+            }
+            return arr[n];
         }
     }
 }
