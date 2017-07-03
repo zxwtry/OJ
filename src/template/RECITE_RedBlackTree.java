d5df83e2bda4a8087389472180f4351510782831
zxwtry <zxwtry@qq.com>
2017-07-03 18:35:53 +0800

    添加 template RECITE_RedBlackTree

--- src/template/RECITE_RedBlackTree.java
+++ src/template/RECITE_RedBlackTree.java
@@ -10,6 +10,43 @@ package template;
  * @date        2017年3月14日 下午9:10:51
  * @details     
  */
-public class RECITE_RedBlackTree {
-    
+public class RECITE_RedBlackTree<T extends Comparable<? super T>> {
+    private static final int BLACK = 1;     //BLACK must be 1
+    private static final int RED = 0;
+    private RedBlackNode<T> header;
+    private RedBlackNode<T> nullNode;
+    public RECITE_RedBlackTree( ) {
+        
+    }
+    private static class RedBlackNode<T> {
+        RedBlackNode(T e) {
+            this(e, null, null);
+        }
+        RedBlackNode(T e, RedBlackNode<T> lt, RedBlackNode<T> rt) {
+            this.element = e;
+            this.left = lt;
+            this.right = rt;
+            this.color = BLACK;
+        }
+        T element;
+        RedBlackNode<T> left;
+        RedBlackNode<T> right;
+        int color;
+    }
+    private RedBlackNode<T> rotate(T item, RedBlackNode<T> parent) {
+        return compare(item, parent) < 0 ? 
+            (
+                parent.left = compare(item, parent.left) < 0 ? 
+                rotateWithLeftChild(parent.left) :  //LL
+                rotateWithRightChild(parent.left);  //LR
+            ) : 
+            (
+                parent.right = compare(item, parent.right) < 0 ?
+                rotateWithLeftChild(parent.right) : //RL
+                rotateWithRightChild(parent.right); //RR    
+            );
+    }
+    private final int compare(T t, RedBlackNode<T> n) {
+        return t == header ? 1 : t.compareTo(n.element);
+    }
 }
