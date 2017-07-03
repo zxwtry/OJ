8eb826dfe9a40d9258d117c2e2ed8aadb7504fb3
zxwtry <zxwtry@qq.com>
2017-07-03 18:59:00 +0800

    添加 book.编程之法 Code201寻找最小的k个数_线性选择算法

--- src/book/编程之法/Code201寻找最小的k个数_线性选择算法.java
+++ src/book/编程之法/Code201寻找最小的k个数_线性选择算法.java
@@ -1,27 +1,29 @@
 package book.编程之法;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Code201寻找最小的k个数_线性选择算法 {
 	static int[] find(int[] arr, int k) {
 	    int arrLen = arr == null ? 0 : arr.length;
 	    if (arrLen <= k) return arr;
-	    PriorityQueue<Integer> pq = new PriorityQueue<>(
-            new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o2.compareTo(o1);
-                }
-            }
-	    );
+	    PriorityQueue<T> pq = new PriorityQueue<>();
 	    for (int v : arr) {
-	        pq.add(v);
+	        pq.add(new T(v));
 	        if (pq.size() > k) pq.poll();
 	    }
 	    int[] ans = new int[k];
 	    for (int i = k-1; i > -1; i --)
-	        ans[i] = pq.poll();
+	        ans[i] = pq.poll().val;
 	    return ans;
+	}
+	private static class T implements Comparable<T> {
+		int val;
+		T(int val) {
+			this.val = val;
+		}
+		@Override
+        public int compareTo(T t) {
+	        return Integer.compare(t.val, this.val);
+        }
 	}
 }
