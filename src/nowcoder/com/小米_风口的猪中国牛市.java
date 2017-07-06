4c6ff24be6575956291d61f87ccfaadb7747bbfe
zxwtry <zxwtry@qq.com>
2017-07-06 22:28:38 +0800

    添加 nowcoder com 小米_风口的猪中国牛市

--- src/nowcoder/com/小米_风口的猪中国牛市.java
+++ src/nowcoder/com/小米_风口的猪中国牛市.java
@@ -10,10 +10,6 @@ package nowcoder.com;
  * @details     
  */
 public class 小米_风口的猪中国牛市 {
-    public static void main(String[] args) {
-        int[] p = {5,15,56,26,62,65,57,69};
-        System.out.println(new Solution().calculateMax(p));
-    }
     static public class Solution {
         /**
          * 计算你能获得的最大收益
@@ -24,30 +20,27 @@ public class 小米_风口的猪中国牛市 {
         public int calculateMax(int[] p) {
             int pl = p == null ? 0 : p.length;
             if (pl == 0) return 0;
-            int[] arr = {0, 0};
-            int pv = p[0];
-            for (int i = 1; i <= pl; i ++) {
-                if (i == pl || p[i - 1] > p[i]) {
-                    add(arr, p[i - 1] - pv);
-                    pv = i == pl ? pv : p[i];
+            int[] fMin = new int[pl + 1];
+            int[] bMax = new int[pl + 1];
+            int t = Integer.MAX_VALUE;
+            for (int i = 0; i < pl; i ++) {
+                t = Math.min(t, p[i]);
+                if (i != 0) {
+                    fMin[i] = Math.max(p[i] - t, fMin[i - 1]);
                 }
             }
-            System.out.println(arr[0] + "..." + arr[1]);
-            return arr[0] + arr[1];
-        }
-        void add(int[] arr, int v) {
-            if (arr[0] == 0) {
-                arr[0] = v;
-                return;
+            t = Integer.MIN_VALUE;
+            for (int i = pl - 1; i > -1; i --) {
+                t = Math.max(t, p[i]);
+                if (i != pl - 1) {
+                    bMax[i] = Math.max(t - p[i], bMax[i + 1]);
+                }
             }
-            if (arr[1] == 0) {
-                arr[1] = v;
-                return;
+            int ans = 0;
+            for (int i = 0; i < pl; i ++) {
+                ans = Math.max(ans, fMin[i] + bMax[i]);
             }
-            int i = arr[0] < arr[1] ? 0 : 1;
-            if (arr[i] < v) {
-                arr[i] = v;
-            }
+            return ans;
         }
     }
 }
