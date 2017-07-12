a8885fffc1347e2c017fea42c6b0c091d84e71b0
zxwtry <zxwtry@qq.com>
2017-07-12 22:20:59 +0800

    add nowcoder com 华为_删数

---
+++ src/nowcoder/com/华为_删数.java
@@ -0,0 +1,26 @@
+package nowcoder.com;
+
+import java.util.Scanner;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        华为_删数.java
+ * @date        2017年7月12日 下午9:53:20
+ * @details     
+ */
+public class 华为_删数 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            sovle(sc);
+        }
+        sc.close();
+    }
+
+    private static void sovle(Scanner sc) {
+        
+    }
+}
