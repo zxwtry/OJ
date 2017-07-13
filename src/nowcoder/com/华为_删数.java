7c32cbc4b5e6b93b29844af6bc6f468ecde96564
zxwtry <zxwtry@qq.com>
2017-07-13 22:24:20 +0800

    添加 nowcoder com 华为_删数

--- src/nowcoder/com/华为_删数.java
+++ src/nowcoder/com/华为_删数.java
@@ -21,6 +21,31 @@ public class 华为_删数 {
     }
 
     private static void sovle(Scanner sc) {
-        
+        int n = sc.nextInt();
+        T[] ts = new T[n];
+        T next = null;
+        for (int i = n - 1; i > -1; i --) {
+            ts[i] = new T(i, next);
+            next = ts[i];
+        }
+        ts[n - 1].next = ts[0];
+        T head = ts[0];
+        while (head.next != head) {
+            head = head.next;
+            head.next = head.next.next;
+            head = head.next;
+        }
+        System.out.println( head.val );
+    }
+    static class T {
+        public T(int val, T next) {
+            this.next = next;
+            this.val = val;
+        }
+        public T(int val) {
+            this(val, null);
+        }
+        int val;
+        T next;
     }
 }
