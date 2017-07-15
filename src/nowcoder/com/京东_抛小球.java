9ef26683b0f58244ec9dc25c63c20f02ddc861d1
zxwtry <zxwtry@qq.com>
2017-07-15 22:09:19 +0800

    添加 nowcoder com 京东_抛小球

---
+++ src/nowcoder/com/京东_抛小球.java
@@ -0,0 +1,18 @@
+package nowcoder.com;
+
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     nowcoder.com
+ * @file        京东_抛小球.java
+ * @date        2017年7月15日 下午10:03:10
+ * @details     
+ */
+public class 京东_抛小球 {
+    public class Balls {
+        public int calcDistance(int A, int B, int C, int D) {
+            return (A + B + C + D) * 3;
+        }
+    }
+}
