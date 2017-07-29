b06df92d5abf8b26be80479c84199253b2325d2f
zxwtry <zxwtry@qq.com>
2017-07-29 22:07:02 +0800

    添加 leetcode P009_PalindromeNumber Solution AC

--- src/leetcode/P009_PalindromeNumber.java
+++ src/leetcode/P009_PalindromeNumber.java
@@ -1,78 +1,25 @@
 package leetcode;
 
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P009_PalindromeNumber.java
+ * @date        2017年7月29日 下午10:06:22
+ * @details     Solution: AC 198ms 82.56%
+ */
 public class P009_PalindromeNumber {
-	/*
-	 * 	16 ms
-	 * 	20.14%
-	 */
 	static class Solution {
 	    public boolean isPalindrome(int x) {
-	    	char[] c = String.valueOf(x).toCharArray();
-	    	int sta = 0, end = c.length - 1;
-	        while (sta < end) {
-	        	if (c[sta] == c[end]) {
-	        		sta ++;
-	        		end --;
-	        	} else {
-	        		return false;
-	        	}
+	        if (x < 0) return false;
+	        int p = x, q = 0;
+	        while (p != 0) {
+	            q = q * 10 + (p % 10);
+	            p = p / 10;
 	        }
-	        return true;
+	        return q == x;
 	    }
 	}
-	/*
-	 * 	14 ms
-	 * 	26.79%
-	 */
-	static class Solution2 {
-		public boolean isPalindrome(int x) {
-			if (x < 0)
-				return false;
-			int[] c = new int[x == 0 ? 1 : (int)Math.log10(x) + 1];
-			for (int i = c.length - 1; i > -1; i --) {
-				c[i] = x % 10;
-				x = x / 10;
-			}
-	    	int sta = 0, end = c.length - 1;
-	        while (sta < end) {
-	        	if (c[sta] == c[end]) {
-	        		sta ++;
-	        		end --;
-	        	} else {
-	        		return false;
-	        	}
-	        }
-			return true;
-		}
-	}
-	/*
-	 * 	75.99%
-	 * 	11 ms
-	 */
-	static class Solution3 {
-		public boolean isPalindrome(int x) {
-			if (x < 0)
-				return false;
-			int[] c = new int[10];
-			int i = 9;
-			for (; i > -1; i --) {
-				c[i] = x % 10;
-				x = x / 10;
-				if (x == 0)
-					break;
-			}
-	    	int sta = i, end = 9;
-	        while (sta < end) {
-	        	if (c[sta] == c[end]) {
-	        		sta ++;
-	        		end --;
-	        	} else {
-	        		return false;
-	        	}
-	        }
-			return true;
-		}
-	}
-	
 }
