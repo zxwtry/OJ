d99832cd92031d8b1896edda6fa1554bebf08977
zxwtry <zxwtry@qq.com>
2017-07-29 21:52:03 +0800

    添加 leetcode P006_ZigZagConversion Solution1 AC

--- src/leetcode/P006_ZigZagConversion.java
+++ src/leetcode/P006_ZigZagConversion.java
@@ -20,10 +20,36 @@ convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
  */
 
 
+/**
+ * @author      zxwtry
+ * @email       zxwtry@qq.com
+ * @project     OJ
+ * @package     leetcode
+ * @file        P006_ZigZagConversion.java
+ * @date        2017年7月29日 下午9:49:57
+ * @details     Solution1: AC 46ms 95.49%
+ * @details     Solution2: AC 77ms 32.95%
+ */
 public class P006_ZigZagConversion {
-	public static void main(String[] args) {
-//		System.out.println(new Solution2().convert("PAYPALISHIRING", 3));
-		System.out.println(new Solution2().convert("AB", 1));
+	static class Solution1 {
+	    public String convert(String s, int n) {
+	        if (n == 1) return s; 
+            int sn = s == null ? 0 : s.length();
+            int r = 2 * n - 2;
+            int c = sn / r + 1;
+            char[] ans = new char[sn];
+            int ai = 0;
+            for (int i = 0; i < n; i ++) {
+                int tn = i == 0 || (i == n - 1) ? 1 : 2;
+                for (int ci = 0; ci < c; ci ++) {
+                    for (int ti = 0; ti < tn; ti ++) {
+                        int si = r * ci + (ti == 0 ? i : 2*n - 2 - i);
+                        if (si < sn) ans[ai ++] = s.charAt(si);
+                    }
+                }
+            }
+            return new String(ans);
+	    }
 	}
 	static class Solution2 {
 	    boolean isDown = true;
