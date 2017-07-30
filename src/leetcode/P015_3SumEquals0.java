1adbdb1bd8c4c0956ba8981b3a0249adbbebf8c1
zxwtry <zxwtry@qq.com>
2017-07-30 22:39:08 +0800

    添加 leetcode P015_3SumEquals0 Solution AC

--- src/leetcode/P015_3SumEquals0.java
+++ src/leetcode/P015_3SumEquals0.java
@@ -5,14 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class P015_3SumEquals0 {
-	public static void main(String[] args) {
-		System.out.println(new Solution().threeSum(new int[] {-1, 0, 1, 2, -1, -4}));
-		System.out.println(new Solution().threeSum(new int[] {-2, 0, 0, 2, 2}));
-	}
-	/*
-	 * 	9ms
-	 * 	33.46%
-	 */
 	static class Solution {
 	    public List<List<Integer>> threeSum(int[] nums) {
 	        List<List<Integer>> ans = new LinkedList<List<Integer>>();
@@ -25,11 +17,7 @@ public class P015_3SumEquals0 {
 	        	while (j < k) {
 	        		int sum = nums[i] + nums[j] + nums[k];
         			if (sum == 0) {
-        				LinkedList<Integer> one = new LinkedList<Integer>();
-        				one.add(nums[i]);
-        				one.add(nums[j]);
-        				one.add(nums[k]);
-        				ans.add(one);
+        				ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
         				do {
         					j ++;
         				} while (j != nums.length && j != 0 && nums[j] == nums[j-1]);
