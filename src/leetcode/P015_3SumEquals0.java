8cec83f79ca1fb9e79448e8c5aaedaec59478d38
zxwtry <zxwtry@qq.com>
2017-07-31 21:40:08 +0800

    添加 leetcode P015_3SumEquals0 AC

--- src/leetcode/P015_3SumEquals0.java
+++ src/leetcode/P015_3SumEquals0.java
@@ -6,32 +6,34 @@ import java.util.List;
 
 public class P015_3SumEquals0 {
 	static class Solution {
-	    public List<List<Integer>> threeSum(int[] nums) {
+	    public List<List<Integer>> threeSum(int[] ns) {
+	        int nn = ns == null ? 0 : ns.length;
 	        List<List<Integer>> ans = new LinkedList<List<Integer>>();
-	        Arrays.sort(nums);
+	        if (nn < 3) return ans; 
+	        Arrays.sort(ns);
 	        int i = 0, j = 0, k = 0;
-	        for (i = 0; i != nums.length; i ++) {
-	        	if (i != 0 && nums[i] == nums[i - 1])
+	        for (i = 0; i != nn; i ++) {
+	        	if (i != 0 && ns[i] == ns[i - 1])
 	        		continue;
-	        	j = i + 1; k = nums.length - 1;
+	        	j = i + 1; k = nn - 1;
 	        	while (j < k) {
-	        		int sum = nums[i] + nums[j] + nums[k];
+	        		int sum = ns[i] + ns[j] + ns[k];
         			if (sum == 0) {
-        				ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+        				ans.add(Arrays.asList(ns[i], ns[j], ns[k]));
         				do {
         					j ++;
-        				} while (j != nums.length && j != 0 && nums[j] == nums[j-1]);
+        				} while (j != nn && ns[j] == ns[j-1]);
         				do {
         					k --;
-        				} while (k != -1 && k != nums.length - 1 && nums[k] == nums[k + 1]);
+        				} while (k != -1 && ns[k] == ns[k + 1]);
         			} else if (sum < 0) {
         				do {
         					j ++;
-        				} while (j != nums.length && j != 0 && nums[j] == nums[j-1]);
+        				} while (j != nn && ns[j] == ns[j-1]);
         			} else {
         				do {
         					k --;
-        				} while (k != -1 && k != nums.length - 1 && nums[k] == nums[k + 1]);
+        				} while (k != -1 && ns[k] == ns[k + 1]);
         			}
 	        	}
 	        }
