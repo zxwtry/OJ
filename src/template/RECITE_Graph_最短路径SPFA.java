299b2e8d000eede01678bb7b55b06e64491d573f
zxwtry <zxwtry@qq.com>
2017-07-31 16:12:56 +0800

    添加 template RECITE_Graph_最短路径SPFA AC

--- src/template/RECITE_Graph_最短路径SPFA.java
+++ src/template/RECITE_Graph_最短路径SPFA.java
@@ -21,71 +21,71 @@ public class RECITE_Graph_最短路径SPFA {
         while (sc.hasNext()) {
             solve(sc);
         }
-        
         sc.close();
     }
-    
+
     static void solve(Scanner sc) {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int s = sc.nextInt();
         int t = sc.nextInt();
         SPFA spfa = new SPFA(n, m);
-        for (int i = 0; i < m; i ++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            spfa.addEdge(a, b, c);
-            spfa.addEdge(b, a, c);
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            spfa.addEdge(u, v, w);
+            spfa.addEdge(v, u, w);
         }
         System.out.println(spfa.spfa(s, t));
     }
 
-    static class SPFA{
-        public int[] v,next,w,head,dis;
+    static class SPFA {
+        public int[] vs, next, ws, head, dis;
         public int e;
         public boolean[] vis;
-        public final int inf = (int)(1e9);
-        
-        public SPFA(int n,int m){
-            head = new int[n+5];
-            v = new int[(m+5)<<1];
-            next = new int[(m+5)<<1];
-            w = new int[(m+5)<<1];
-            dis = new int[n+5];
-            vis = new boolean[n+5];
+        public final int inf = Integer.MAX_VALUE / 4;
+
+        public SPFA(int n, int m) {
+            vs      =   new int[(m + 5) * 2];
+            ws      =   new int[(m + 5) * 2];
+            next    =   new int[(m + 5) * 2];
+            head    =   new int[n + 5];
+            dis     =   new int[n + 5];
+            vis     =   new boolean[n + 5];
             rewind();
         }
-        
+
         public void rewind() {
-            e=0;
+            e = 0;
             Arrays.fill(vis, false);
             Arrays.fill(head, -1);
             Arrays.fill(dis, inf);
         }
-        
-        public void addEdge(int U,int V,int W){
-            v[e] = V;
-            next[e] = head[U];
-            w[e] = W;
-            head[U] = e++;
+
+        public void addEdge(int u, int v, int w) {
+            vs[e] = v;
+            ws[e] = w;
+            next[e] = head[u];
+            head[u] = e++;
         }
-        
-        public int spfa(int s,int t){
+
+        public int spfa(int s, int t) {
             Queue<Integer> q = new LinkedList<Integer>();
-            dis[s] = 0; vis[s]=true;
+            dis[s] = 0;
+            vis[s] = true;
             q.add(s);
-            while(q.size() > 0){
-                int x = q.poll(); 
+            while (q.size() > 0) {
+                int x = q.poll();
                 vis[x] = false;
-                for(int i = head[x]; i != -1; i = next[i]){
-                    int nxt = v[i];
-                    int d = dis[x] + w[i];
-                    if(d < dis[nxt]){
-                        dis[nxt] = d;
-                        if(vis[nxt] == false){
-                            vis[nxt] = true;
-                            q.add(nxt);
+                for (int i = head[x]; i != -1; i = next[i]) {
+                    int v = vs[i];
+                    int d = dis[x] + ws[i];
+                    if (d < dis[v]) {
+                        dis[v] = d;
+                        if (vis[v] == false) {
+                            vis[v] = true;
+                            q.add(v);
                         }
                     }
                 }
