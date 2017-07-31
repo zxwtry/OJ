9bee76441bed99c6e3b23abbf53c5b669467369d
zxwtry <zxwtry@qq.com>
2017-07-31 21:44:52 +0800

    添加 leetcode P016_3SumClosest Solution AC

--- src/leetcode/P016_3SumClosest.java
+++ src/leetcode/P016_3SumClosest.java
@@ -3,27 +3,17 @@ package leetcode;
 import java.util.Arrays;
 
 public class P016_3SumClosest {
-	public static void main(String[] args) {
-//		System.out.println(new Solution().threeSumClosest(new int[]{-1,2,1,-4}, 1));
-//		System.out.println(new Solution().threeSumClosest(new int[]{0, 2, 1, -3}, 1));
-		System.out.println(new Solution().threeSumClosest(new int[]{1,1,-1,-1,3}, -1));
-	}
-	/*
-	 * 	12 ms
-	 * 	67.05%
-	 */
 	static class Solution {
-	    public int threeSumClosest(int[] nums, int target) {
-	    	if (nums == null || nums.length < 3)
-	    		return 0;
-	    	int ans = nums[0] + nums[1] + nums[2];
+	    public int threeSumClosest(int[] ns, int target) {
+	        int nn = ns == null ? 0 : ns.length;
+	        if (nn < 3) return 0;
+	    	int ans = ns[0] + ns[1] + ns[2];
 	    	int cut = Integer.MAX_VALUE;
-	    	Arrays.sort(nums);
-	    	int i = 0, j = 0, k = 0;
-	    	for (i = 0; i != nums.length; i ++) {
-	    		j = i + 1; k = nums.length - 1;
+	    	Arrays.sort(ns);
+	    	for (int i = 0; i != nn; i ++) {
+	    		int j = i + 1, k = nn - 1;
 	    		while (j < k) {
-	    			int sum = nums[i] + nums[j] + nums[k];
+	    			int sum = ns[i] + ns[j] + ns[k];
 	    			int real = sum - target;
 	    			int tmp = Math.abs(real);
 	    			if (real == 0) {
