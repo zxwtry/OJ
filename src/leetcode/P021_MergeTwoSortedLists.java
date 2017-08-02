028647406c4078cc2c14c2512fb16a89454bcf55
zxwtry <zxwtry@qq.com>
2017-08-02 22:08:52 +0800

    添加 leetcode P021_MergeTwoSortedLists Solution AC

--- src/leetcode/P021_MergeTwoSortedLists.java
+++ src/leetcode/P021_MergeTwoSortedLists.java
@@ -4,29 +4,26 @@ import tools.ListNode辅助.ListNode;
 
 public class P021_MergeTwoSortedLists {
 	static class Solution {
-	    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-	    	if (l1 == null || l2 == null)
-	    		return l1 == null ? l2 : l1;
-	    	if (l1.val > l2.val)
-	    		return mergeTwoLists(l2, l1);
-	    	ListNode pre = l1, c1 = l1.next, c2 = l2;
-	    	while (true) {
-	    		while (c1 != null && c1.val <= c2.val) {
-	    			pre = c1;
-	    			c1 = c1.next;
-	    		}
-	    		pre.next = c2;
-	    		if (c1 == null)
-	    			break;
-	    		while (c2 != null && c2.val <= c1.val) {
-	    			pre = c2;
-	    			c2 = c2.next;
-	    		}
-	    		pre.next = c1;
-	    		if (c2 == null)
-	    			break;
-	    	}
-	    	return l1;
+	    public ListNode mergeTwoLists(ListNode l, ListNode r) {
+	        if (l == null || r == null) return l == null ? r : l;
+	        if (l.val > r.val) return mergeTwoLists(r, l);
+	        ListNode p = l;
+	        ListNode lc = l.next;
+	        ListNode rc = r;
+	        while (lc != null && rc != null) {
+	            if (lc.val < rc.val) {
+	                p.next = lc;
+	                p = lc;
+	                lc = lc.next;
+	            } else {
+	                p.next = rc;
+	                p = rc;
+	                rc = rc.next;
+	            }
+	        }
+	        if (lc != null) p.next = lc;
+	        if (rc != null) p.next = rc;
+	        return l;
 	    }
 	}
 }
