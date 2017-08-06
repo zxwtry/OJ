77a5241faa4580a869c9fcfe3ddc512718df8096
zxwtry <zxwtry@qq.com>
2017-08-06 09:03:19 +0800

    添加   leetcode P025_ReverseNodesInKGroup

--- src/leetcode/P025_ReverseNodesInKGroup.java
+++ src/leetcode/P025_ReverseNodesInKGroup.java
@@ -26,55 +26,37 @@ package leetcode;
 import tools.ListNode辅助.ListNode;;
 
 public class P025_ReverseNodesInKGroup {
-	/*
-	 * 	花了很长时间
-	 * 	代码写得非常乱，今后需要整理一下
-	 * 	可能还有先全反转，对尾部二次反转的做法，可能更快
-	 * 	1 ms
-	 * 	4.84%
-	 */
+    public static void main(String[] args) {
+        ListNode h = tools.ListNode辅助.A_一维生成器(new int[] {
+                1, 2, 3
+        });
+        int k = 4;
+        ListNode ans = new Solution1().reverseKGroup(h, k);
+        tools.ListNode辅助.B_打印链表(ans, 100, 4);
+    }
 	static class Solution1 {
-	    public ListNode reverseKGroup(ListNode head, int k) {
-	    	if (head == null || head.next == null)
-	    		return head;
-	    	int count = 1;
-	    	ListNode ans = head, pre = null, cur = null, next_pre = null, next_cur = null, rev_cur = null, rev_pre = null, temp = null;
-	    	while (ans != null && count != k) {
-	    		ans = ans.next;
-	    		count ++;
-	    	}
-	    	cur = ans;
-	    	if (ans == null)
-	    		return head;
-	    	while (cur != null) {
-	    		if (count != k) {
-	    			count ++;
-	    			cur = cur.next;
-	    		} else {
-	    			if (pre == null) {
-	    				next_pre = head;
-	    				rev_cur = next_pre;
-	    			} else {
-	    				next_pre = pre.next;
-	    				rev_cur = next_pre;
-	    				pre.next = cur;
-	    			}
-	    			next_cur = cur.next;
-	    			rev_pre = next_cur;
-	    			while (rev_cur != next_cur) {
-	    				temp = rev_cur.next;
-	    				rev_cur.next = rev_pre;
-	    				rev_pre = rev_cur;
-	    				if (rev_cur == cur)
-	    					break;
-	    				rev_cur = temp;
-	    			}
-	    			cur = next_cur;
-	    			pre = next_pre;
-	    			count = 1;
-	    		}
-	    	}
-	    	return ans;
+	    public ListNode reverseKGroup(ListNode h, int k) {
+	        ListNode ans = null;
+	        ListNode a = null, b = null, t = h;
+	        ListNode p = null;
+	        while (true) {
+	            int i = 0; 
+	            for (; i < k && t != null; i ++) {
+	                b = t.next;
+	                
+	                t.next = a;
+	                if (a != null) a.next = b;
+	                if (p != null) p.next = t;
+	                
+	                t = b;
+	            }
+	            
+	            if (p != null && ans == null) ans = a;
+	            p = b;
+	            if (t == null) break;
+	        }
+//	        if (a != null) a.next = null;
+	        return ans;
 	    }
 	}
 }
