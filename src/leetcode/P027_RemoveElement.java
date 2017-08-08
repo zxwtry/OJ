636c7b59ef1c5225270972b8ec4477e62b79ecf2
zxwtry <zxwtry@qq.com>
2017-08-08 21:55:38 +0800

    添加 leetcode P027_RemoveElement AC

--- src/leetcode/P027_RemoveElement.java
+++ src/leetcode/P027_RemoveElement.java
@@ -18,29 +18,19 @@ package leetcode;
 
 
 public class P027_RemoveElement {
-	public static void main(String[] args) {
-		int[] nums = {1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3};
-		int ans = new Solution1().removeElement(nums, 3);
-		for (int i = 0; i != ans; i ++)
-			System.out.println(nums[i]);
-	}
-	/*
-	 * 	1 ms
-	 * 	3.96%
-	 */
 	static class Solution1 {
 		public int removeElement(int[] nums, int val) {
-	        if (nums == null || nums.length == 0)
-	        	return 0;
-	        int step = 0;
-	        for (int i = 0; i != nums.length; i ++) {
-	        	if (nums[i] == val) {
-	        		step ++;
-	        	} else {
-	        		nums[i - step] = nums[i];
+		    int len = nums == null ? 0 : nums.length;
+	        int cnt = 0;
+	        for (int i = 0; i != len; i ++) {
+	        	if (nums[i] != val) {
+	        	    if (i != cnt) {
+	        	        nums[cnt] = nums[i];
+	        	    }
+	        	    cnt ++;
 	        	}
 	        }
-	        return nums.length - step;
+	        return cnt;
 	    }
 	}
 }
