94ce32dcf497c8b38c747f543a2bfcb4a94ba516
zxwtry <zxwtry@qq.com>
2017-08-12 23:48:00 +0800

    添加 leetcode P032_LongestValidParentheses AC

--- src/leetcode/P032_LongestValidParentheses.java
+++ src/leetcode/P032_LongestValidParentheses.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.Stack;
-
 /*
  * 	Given a string containing just the characters '(' and ')', 
  * 	find the length of the longest valid (well-formed) parentheses substring.
@@ -14,41 +12,25 @@ import java.util.Stack;
  */
 
 public class P032_LongestValidParentheses {
-	static class Solution1 {
-	    public int longestValidParentheses(String s) {
-	    	if (s == null)
-	    		return 0;
-	    	boolean[] isMatch = new boolean[s.length()];
-	    	Stack<Integer> stack = new Stack<Integer>();
-	    	int ans = 0, i = 0, cou = 0, left = 0, right = 0;
-	    	for (i = 0; i != isMatch.length; i ++) {
-	    		if (s.charAt(i) == '(') {
-	    			left ++;
-	    		} else {
-	    			right ++;
-	    		}
-	    	}
-	    	if (right == 0 || left == 0)
-	    		return 0;
-	    	if ((left - 1) * (right - 1) == 0 && s.charAt(0) == '(')
-	    		return 2;
-	    	for (i = 0; i != isMatch.length; i ++) {
-	    		if (s.charAt(i) == '(') {
-	    			stack.add(i);
-	    		} else {
-	    			if (stack.isEmpty())
-	    				continue;
-	    			isMatch[i] = true;
-	    			isMatch[stack.peek()] = true;
-	    			stack.pop();
-	    		}
-	    	}
-	    	for (i = 0; i!= isMatch.length; i ++) {
-	    		if(isMatch[i])	++cou;
-	    		else	cou = 0;
-	    		ans = Math.max(ans, cou);
-	    	}
-	    	return ans;
-	    }
-	}
+    static class Solution {
+        public int longestValidParentheses(String s) {
+            int sn = s == null ? 0 : s.length();
+            int[] arr = new int[sn + 1];
+            int ans = 0;
+            char pc = '\0';
+            for (int i = 1; i < sn; i ++) {
+                char c = s.charAt(i);
+                if (c == ')' && pc == '(') {
+                    arr[i + 1] = Math.max(arr[i - 1] + 2, arr[i + 1]);
+                }
+                int j = i - arr[i] - 1;
+                if (j > -1 && c == ')' && s.charAt(j) == '(') {
+                    arr[i + 1] = Math.max(arr[i] + 2 + arr[j], arr[i + 1]);
+                }
+                ans = Math.max(ans, arr[i + 1]);
+                pc = c;
+            }
+            return ans;
+        }
+    }
 }
