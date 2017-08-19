d5cbe3c74198781aef2497f59636030767c23497
zxwtry <zxwtry@qq.com>
2017-08-19 23:46:34 +0800

    添加 leetcode P039_Combination_Sum AC

--- src/leetcode/P039_Combination_Sum.java
+++ src/leetcode/P039_Combination_Sum.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -23,7 +24,69 @@ import java.util.List;
 
 public class P039_Combination_Sum {
 	public static void main(String[] args) {
-		System.out.println(new Solution1().combinationSum(new int[] {10, 1, 2, 7, 6, 1, 5, 1, 1, 1, 1}, 8));
+	    int v = 19;
+		System.out.println(new Solution1().combinationSum(new int[] {10, 1, 2, 7, 6, 1, 5, 1, 1, 1, 1}, v));
+		System.out.println(new Solution2().combinationSum(new int[] {10, 1, 2, 7, 6, 1, 5, 1, 1, 1, 1}, v));
+	}
+	
+	static class Solution2 {
+	    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+	        int cn = candidates == null ? 0 : candidates.length;
+	        if (cn == 0) {
+	            return new ArrayList<>(0);
+	        }
+	        Arrays.sort(candidates);
+	        
+	        int selectedLength = getSelectedLength(candidates, 0, cn - 1, target);
+	        int[] selected = new int[selectedLength];
+	        //去除重复方法：如果candidates[i- 1] == candidates[i]
+	        //             那么，i就不用选了
+	        
+	        List<List<Integer>> ans = new LinkedList<>();
+	        search(ans, candidates, 0, selected, 0, target);
+	        return ans;
+	    }
+
+        private void search(List<List<Integer>> ans, int[] candidates, 
+                int candidateIndex, int[] selected, int selectedIndex, int target) {
+            if (target == 0) {
+                ArrayList<Integer> oneAns = new ArrayList<>(selectedIndex);
+                for (int i = 0; i < selectedIndex; i ++) {
+                    oneAns.add(selected[i]);
+                }
+                ans.add(oneAns);
+                return;
+            }
+            if (candidateIndex == candidates.length) {
+                return;
+            }
+            if (target < candidates[candidateIndex]) {
+                return;
+            }
+            if (candidateIndex != 0 && candidates[candidateIndex - 1] == candidates[candidateIndex]) {
+                search(ans, candidates, candidateIndex + 1, selected, selectedIndex, target);
+                return;
+            }
+            selected[selectedIndex] = candidates[candidateIndex];
+            //已经选择当前   通过target退出
+            search(ans, candidates, candidateIndex, selected, selectedIndex + 1, target - candidates[candidateIndex]);
+            //不选择当前       通过candidateIndex退出
+            search(ans, candidates, candidateIndex + 1, selected, selectedIndex, target);
+        }
+
+        int getSelectedLength(int[] arr, int i, int j, int target) {
+	        int min = getMin(arr, i, j);
+	        return target / min + 1;
+	    }
+	    
+	    int getMin(int[] arr, int i, int j) {
+	        int min = arr[i];
+	        for (int k = i + 1; k <= j; k ++) {
+	            min = Math.min(min, arr[k]);
+	        }
+	        return min;
+	    }
+	    
 	}
 	/*
 	 * 	6 ms
