29b028cfc41121c223745f245622ecac8e7354af
zxwtry <zxwtry@qq.com>
2017-08-21 23:58:52 +0800

    添加 leetcode P041_FirstMissingPositive AC

--- src/leetcode/P041_FirstMissingPositive.java
+++ src/leetcode/P041_FirstMissingPositive.java
@@ -16,6 +16,47 @@ public class P041_FirstMissingPositive {
 //		System.out.println(new Solution1().firstMissingPositive(new int[]{3,4,-1,1}));
 		System.out.println(new Solution1().firstMissingPositive(new int[]{8, 5, 4, 3, 2, 1}));
 	}
+	static class Solution2 {
+	    public int firstMissingPositive(int[] ns) {
+	        int nn = ns == null ? 0 : ns.length;
+	        if (nn < 1) {
+	            return 1;
+	        }
+	        //两个指针，维持最大可能和最小可能
+	        int min = 0;
+	        int max = nn - 1;
+	        for (int i = 0; i < nn; i ++) {
+	            int v = ns[i] - 1;
+	            if (v < min || v > max) {
+	                max --;
+	                //意味着[min, max]之前肯定有空格
+	                //max --肯定没错
+	                continue;
+	            }
+	            if (v == min) {
+	                min ++;
+	                continue;
+	            }
+	            if (v == max) {
+	                max --;
+	                continue;
+	            }
+	            if (v == i) {
+	                continue;
+	            }
+	            
+	        }
+	    }
+	    //[i, j]
+	    //k是候选下标
+	    int place(int[] ns, int i, int j, int k) {
+	        int index = ns[k] - 1;
+	        if (ns[index] == index) {
+	            
+	        }
+	    }
+	}
+	
 	/*
 	 * 	测试用例肯定特别多，HashMap完成不了
 	 * 	时间O(N) 空间O(1)　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
