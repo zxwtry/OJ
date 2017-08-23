28944230310d6e22066df4407746de13820cb24d
zxwtry <zxwtry@qq.com>
2017-08-23 09:21:42 +0800

    添加 leetcode P042_TrappingRainWater AC

--- src/leetcode/P042_TrappingRainWater.java
+++ src/leetcode/P042_TrappingRainWater.java
@@ -5,62 +5,34 @@ package leetcode;
  */
 
 public class P042_TrappingRainWater {
-	public static void main(String[] args) {
-		int[] input = null;
-		input = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
-		input = new int[]{2,0,2};
-		System.out.println(new Solution2().trap(input));
-	}
-	/*
-	 * 	AC
-	 * 	2 ms
-	 * 	21.39%
-	 */
 	static class Solution {
+	    /**
+	     * 积水问题，最重要的是找到最高的那块
+	     * 从两边往最高的那块：
+	     *     之前的最大值，就是能到的最大高度
+	     */
 	    public int trap(int[] height) {
-	    	if (height == null || height.length < 3)
-	    		return 0;
-	    	int maxI = 0;
-	    	for (int i = 1; i != height.length; i ++)
-	    		if (height[i] > height[maxI])
-	    			maxI = i;
-	    	int[] hori = new int[height.length];
-	    	hori[0] = height[0];
-	    	hori[maxI] = height[maxI];
-	    	hori[height.length - 1] = height[height.length - 1];
-	    	int ans = 0;
-	    	for (int i = 1; i < maxI; i ++) {
-	    		hori[i] = height[i] > hori[i - 1] ? height[i] : hori[i - 1];
-	    		ans += hori[i] - height[i];
-	    	}
-	    	for (int i = height.length - 2; i > maxI; i --) {
-	    		hori[i] = height[i] > hori[i + 1] ? height[i] : hori[i + 1];
-	    		ans += hori[i] - height[i];
-	    	}
-	        return ans;
-	    }
-	}
-	/*
-	 * 	AC
-	 */
-	static class Solution2 {
-	    public int trap(int[] height) {
-	    	if (height == null || height.length < 3)
-	    		return 0;
-	    	int maxI = 0;
-	    	for (int i = 1; i != height.length; i ++)
-	    		if (height[i] > height[maxI])
-	    			maxI = i;
-	    	int ans = 0, pre = height[0];
-	    	for (int i = 1; i < maxI; i ++) {
-	    		pre = height[i] > pre ? height[i] : pre;
-	    		ans += pre - height[i];
-	    	}
-	    	pre = height[height.length - 1];
-	    	for (int i = height.length - 2; i > maxI; i --) {
-	    		pre = height[i] > pre ? height[i] : pre;
-	    		ans += pre - height[i];
-	    	}
+	        int heightLength = height == null ? 0 : height.length;
+	        if (heightLength < 3) {
+	            return 0;
+	        }
+	        int maxIndex = 0;
+	        int ans = 0;
+	        for (int index = 1; index < heightLength; index ++) {
+	            if (height[index] > height[maxIndex]) {
+	                maxIndex = index;
+	            }
+	        }
+	        int leftMax = height[0];
+	        for (int index = 1; index < maxIndex; index ++) {
+	            leftMax = Math.max(leftMax, height[index]);
+	            ans += leftMax - height[index];
+	        }
+	        int rightMax = height[heightLength - 1];
+	        for (int index = heightLength - 2; index > maxIndex; index --) {
+	            rightMax = Math.max(rightMax, height[index]);
+	            ans += rightMax - height[index];
+	        }
 	        return ans;
 	    }
 	}
