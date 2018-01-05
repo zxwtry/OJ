3e6e81a35fe14c248b326ebae67631f7e21dd073
zxwtry <zxwtry@qq.com>
2018-01-05 22:41:10 +0800

    add cpp/poj10xx/P1000.cpp

---
+++ cpp/poj10xx/P1000.cpp
@@ -0,0 +1,20 @@
+/*
+    author:  zxwtry
+    email:   zxwtry@qq.com
+    project: ojcpp
+    gcc:     4.8.4
+    date:    2018-01-05 20:44:00
+    status:  728K   0MS
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    long long a,b;
+    cin>>a;
+    cin>>b;
+    cout<<(a+b)<<endl;     
+    return 0;
+}
