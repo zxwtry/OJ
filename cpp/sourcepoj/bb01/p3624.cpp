201e4d8c809546afb8d934ef887371eb5af63066
zxwtry <zxwtry@qq.com>
2020-01-31 18:57:30 +0800

    time add commit push

---
+++ cpp/sourcepoj/bb01/p3624.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+
+const int COSTMAX = 13880;
+const int COSTNUM = 3502;
+int cost[COSTNUM] = {0};
+int value[COSTNUM] = {0};
+int maxValue[COSTMAX] = {0};
+
+int main() {
+    if (getenv("ZXWPC")) {
+        freopen("p3624.in", "r", stdin);
+        freopen("p3624.out", "w", stdout);
+    }
+    int costNum = 0;
+    int costMax = 0;
+    scanf("%d %d\n", &costNum, &costMax);
+    for (int numIndex = 0; numIndex < costNum; numIndex++) {
+        scanf("%d %d\n", &cost[numIndex], &value[numIndex]);
+    }
+    for (int costIndex = 1; costIndex <= costMax; costIndex++) {
+        for (int numIndex = 0; numIndex < costNum; numIndex++) {
+            if (costIndex >= cost[numIndex]) {
+                maxValue[costIndex] = std::max(
+                    maxValue[costIndex],
+                    maxValue[costIndex - cost[numIndex]] + value[numIndex]);
+            }
+        }
+        maxValue[costIndex] =
+            std::max(maxValue[costIndex], maxValue[costIndex - 1]);
+    }
+    printf("%d\n", maxValue[costMax]);
+    return 0;
+}
