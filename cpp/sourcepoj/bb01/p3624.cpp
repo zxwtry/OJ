56b8e060c4bca742fb22ed14116f25d4b72c3ab1
zxwtry <zxwtry@qq.com>
2020-02-04 17:56:34 +0800

    time add commit push

--- cpp/sourcepoj/bb01/p3624.cpp
+++ cpp/sourcepoj/bb01/p3624.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <stdio.h>
 
+using namespace std;
+
 const int COSTMAX = 13880;
 const int COSTNUM = 3502;
 int cost[COSTNUM] = {0};
 int value[COSTNUM] = {0};
-int maxValue[COSTNUM][COSTMAX] = {0};
+int maxValue[COSTMAX] = {0};
 
 int main() {
     if (getenv("ZXWPC")) {
@@ -15,15 +17,29 @@ int main() {
     int costNum = 0;
     int costMax = 0;
     scanf("%d %d\n", &costNum, &costMax);
-    for (int numIndex = 0; numIndex < costNum; numIndex++) {
+    for (int numIndex = 1; numIndex <= costNum; numIndex++) {
         scanf("%d %d\n", &cost[numIndex], &value[numIndex]);
     }
-    for (int costIndex = 1; costIndex <= costMax; costIndex++) {
-        for (int numIndex = 0; numIndex < costNum; numIndex++) {
-            if (costIndex < cost[numIndex]) {
+    // for (int costIndex = 1; costIndex <= costMax; costIndex++) {
+    //     for (int numIndex = 1; numIndex <= costNum; numIndex++) {
+    //         if (costIndex >= cost[numIndex]) {
+    //             maxValue[numIndex][costIndex] = std::max(
+    //                 maxValue[numIndex - 1][costIndex - cost[numIndex]] +
+    //                     value[numIndex],
+    //                 maxValue[numIndex - 1][costIndex]);
+    //         } else {
+    //             maxValue[numIndex][costIndex] =
+    //                 maxValue[numIndex - 1][costIndex];
+    //         }
+    //     }
+    // }
 
-            } else {
-            }
+    for (int numIndex = 1; numIndex <= costNum; numIndex++) {
+        for (int costIndex = costMax; costIndex >= cost[numIndex];
+             costIndex--) {
+            maxValue[costIndex] =
+                std::max(maxValue[costIndex - cost[numIndex]] + value[numIndex],
+                         maxValue[costIndex]);
         }
     }
     printf("%d\n", maxValue[costMax]);
