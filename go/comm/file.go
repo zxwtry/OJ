39de4bc9ec7dcb1054ce363627f4cd8b63929b53
zxwtry <zxwtry@qq.com>
2020-02-23 21:56:48 +0800

    time add commit push

--- go/comm/file.go
+++ go/comm/file.go
@@ -1,19 +1,27 @@
 package comm
 
-import "io/ioutil"
-
-import "strings"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
 
 func RenameFile() {
-	fileDir := "D:/file/ffout/"
-	files, _ := ioutil.ReadDir(fileDir)
-	for _, fileHandler := range files {
-		fileName := fileHandler.Name()
-		nameIndex := strings.Index(fileName, "后宫甄环传")
-		if nameIndex == -1 {
-
-		} else {
-
+	fileDirFormat := "D:/file/video/13017548/%d/lua.flv.bili2api.80/"
+	for i := 10; i <= 28; i++ {
+		fileDir := fmt.Sprintf(fileDirFormat, i)
+		fileList, _ := ioutil.ReadDir(fileDir)
+		for _, filePath := range fileList {
+			fileName := filePath.Name()
+			if len(fileName) > 4 && fileName[len(fileName)-4:] == ".blv" {
+				blvIndexStr := fileName[:len(fileName)-4]
+				blvIndex, _ := strconv.Atoi(blvIndexStr)
+				fileNewName := fmt.Sprintf("D:/file/video/连城诀%d-%02d.blv", i, blvIndex)
+				fmt.Println(fileDir + fileName)
+				fmt.Println(fileNewName)
+				os.Rename(fileDir+fileName, fileNewName)
+			}
 		}
 	}
 }
