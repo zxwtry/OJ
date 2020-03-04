f4e295c2a58230b7cbd21aa65261f77656ee0dde
zxwtry <zxwtry@qq.com>
2020-03-05 00:56:33 +0800

    time add commit push

---
+++ go/comm/jin.go
@@ -0,0 +1,45 @@
+package comm
+
+import "fmt"
+
+func JinGenerateDoFile() {
+	minValue := 1
+	maxValue := 2
+
+	formatStr := "xtabond2  dtfp l1.dtfp  er  er1   fee age  re2  ca1  cp1 de1  ab2    ,gmm(dtfp ,l(%d %d) collapse ) gmm( re2,l(%d %d) collapse )  gmm(ca1  ,l(%d %d) collapse )  gmm(cp1  ,l(%d %d) collapse )  gmm(de1   ,l(%d %d) collapse )  gmm(  ab2 ,l(%d %d) collapse )      iv(l(%d %d)age  )  iv(l(%d %d) fee   )  iv(l(%d %d) er1  ) iv(l(%d %d) er  )robust two"
+
+	for i0 := minValue; i0 <= maxValue; i0++ {
+
+		for i0 := minValue; i0 <= maxValue; i0++ {
+
+			for i0 := minValue; i0 <= maxValue; i0++ {
+
+				for i0 := minValue; i0 <= maxValue; i0++ {
+
+					for i0 := minValue; i0 <= maxValue; i0++ {
+
+						for i0 := minValue; i0 <= maxValue; i0++ {
+
+							for i0 := minValue; i0 <= maxValue; i0++ {
+
+								for i0 := minValue; i0 <= maxValue; i0++ {
+
+									for i0 := minValue; i0 <= maxValue; i0++ {
+
+										for i0 := minValue; i0 <= maxValue; i0++ {
+
+											for i0 := minValue; i0 <= maxValue; i0++ {
+
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	doStr := fmt.Sprintf(formatStr)
+}
