6d4f8285f7e5ea9b0d9cd5d9c49d2a697d923d0c
zxwtry <zxwtry@qq.com>
2020-04-04 12:57:04 +0800

    time add commit push

---
+++ python/proj/chi/shang_quan.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: xiaosu
+"""
+import pandas as pd
+import numpy as np
+import math
+from numpy import array
+
+# 1读取数据
+df = pd.read_excel('表格名.xlsx', encoding='gb2312')
+# 2数据预处理 ,去除空值的记录
+df.dropna()
+# 定义熵值法函数
+
+
+def cal_weight(x):
+    '''熵值法计算变量的权重'''
+    # 标准化
+    x = x.apply(lambda x: ((x - np.min(x)) / (np.max(x) - np.min(x))))
+    '''
+    如果数据实际不为零，则赋予最小值
+    if x==0:
+        x=0.00001
+    else:
+        pass
+    '''
+    # 求k
+    rows = x.index.size  # 行
+    cols = x.columns.size  # 列
+    k = 1.0 / math.log(rows)
+
+    lnf = [[None] * cols for i in range(rows)]
+
+    # 矩阵计算--
+    # 信息熵
+
+    x = array(x)
+    lnf = [[None] * cols for i in range(rows)]
+    lnf = array(lnf)
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if x[i][j] == 0:
+                lnfij = 0.0
+            else:
+                p = x[i][j] / x.sum(axis=0)[j]
+                lnfij = math.log(p) * p * (-k)
+            lnf[i][j] = lnfij
+    lnf = pd.DataFrame(lnf)
+    E = lnf
+
+    # 计算冗余度
+    d = 1 - E.sum(axis=0)
+    # 计算各指标的权重
+    w = [[None] * 1 for i in range(cols)]
+    for j in range(0, cols):
+        wj = d[j] / sum(d)
+        w[j] = wj
+        # 计算各样本的综合得分,用最原始的数据
+
+    w = pd.DataFrame(w)
+    return w
+
+
+if __name__ == '__main__':
+    # 计算df各字段的权重
+    w = cal_weight(df)  # 调用cal_weight
+    w.index = df.columns
+    w.columns = ['weight']
+    print(w)  # 输出权重
+    print('熵权法计算权重运行完成!')
